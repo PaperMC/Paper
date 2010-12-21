51ec4222544a25b1764a5ae1e0a868b9eb1a6ee0
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2010-12-21 15:32:27 +0000

    Let's get this project started
    By: Dinnerbone <dinnerbone@dinnerbone.com>

---
+++ paper-api/src/org/bukkit/Server.java
@@ -0,0 +1,21 @@
+
+package org.bukkit;
+
+/**
+ * Represents a server implementation
+ */
+public interface Server {
+    /**
+     * Gets the name of this server implementation
+     *
+     * @return name of this server implementation
+     */
+    public String getName();
+
+    /**
+     * Gets the version string of this server implementation.
+     *
+     * @return version of this server implementation
+     */
+    public String getVersion();
+}
