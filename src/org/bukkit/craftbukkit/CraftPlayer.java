b04fa07b450b20621978e504f9aae458eb1c96bb
Dinnerbone <dinnerbone@dinnerbone.com>
2010-12-27 02:13:03 +0000

    Added chunk/block/location/world

--- src/org/bukkit/craftbukkit/CraftPlayer.java
+++ src/org/bukkit/craftbukkit/CraftPlayer.java
@@ -2,7 +2,9 @@
 package org.bukkit.craftbukkit;
 
 import net.minecraft.server.EntityPlayerMP;
+import org.bukkit.Location;
 import org.bukkit.Player;
+import org.bukkit.World;
 
 public class CraftPlayer implements Player {
     private EntityPlayerMP player;
@@ -21,5 +23,13 @@ public class CraftPlayer implements Player {
 
     public boolean isOnline() {
         return server.server.g(name);
+    }
+
+    public Location getLocation() {
+        return new Location(getWorld(), player.p, player.q, player.r);
+    }
+
+    public World getWorld() {
+        return server.getWorld(player.b.e);
     }
 }
