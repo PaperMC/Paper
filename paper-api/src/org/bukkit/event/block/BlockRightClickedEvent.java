66676f3800eb076e2d8dda0ec15a002c5aa3fb01
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2010-12-31 09:09:02 -0500

    Implemented BlockRightClickedEvent
    By: durron597 <martin.jared@gmail.com>

--- paper-api/src/org/bukkit/event/block/BlockRightClickedEvent.java
+++ paper-api/src/org/bukkit/event/block/BlockRightClickedEvent.java
@@ -4,19 +4,50 @@
 package org.bukkit.event.block;
 
 import org.bukkit.Block;
+import org.bukkit.BlockFace;
+import org.bukkit.ItemStack;
+import org.bukkit.Player;
 
 /**
- * Not implemented yet
+ * @author durron597
  */
 public class BlockRightClickedEvent extends BlockEvent {
-
+	protected Player clicker;
+	protected BlockFace direction;
+	protected ItemStack clickedWith;
+	
 	/**
-	 * @param type
-	 * @param theBlock
+	 * @param type The type of event this is
+	 * @param theBlock The clicked block
+	 * @param direction The face we clicked from
+	 * @param clicker The player who clicked a block
+	 * @param clickedWith Item in player's hand
 	 */
-	public BlockRightClickedEvent(Type type, Block theBlock) {
+	public BlockRightClickedEvent(Type type, Block theBlock, BlockFace direction, Player clicker, ItemStack clickedWith) {
 		super(type, theBlock);
-		// TODO Auto-generated constructor stub
+		this.direction = direction;
+		this.clicker = clicker;
+		this.clickedWith = clickedWith;
 	}
 
+	/**
+	 * @return the clicker
+	 */
+	public Player getClicker() {
+		return clicker;
+	}
+
+	/**
+	 * @return the direction
+	 */
+	public BlockFace getDirection() {
+		return direction;
+	}
+
+	/**
+	 * @return the clickedWith
+	 */
+	public ItemStack getClickedWith() {
+		return clickedWith;
+	}
 }
