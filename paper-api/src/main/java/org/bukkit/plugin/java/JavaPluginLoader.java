63595772bd81141ce83a68ecfa7771d464989879
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-03 02:41:18 +0000

    Fixed logic issue toggling plugins
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/plugin/java/JavaPluginLoader.java
+++ paper-api/src/main/java/org/bukkit/plugin/java/JavaPluginLoader.java
@@ -144,7 +144,7 @@ public final class JavaPluginLoader implements PluginLoader {
             throw new IllegalArgumentException("Plugin is not associated with this PluginLoader");
         }
 
-        if (plugin.isEnabled()) {
+        if (!plugin.isEnabled()) {
             JavaPlugin jPlugin = (JavaPlugin)plugin;
 
             server.getPluginManager().callEvent(new PluginEvent(Event.Type.PLUGIN_ENABLE, plugin));
@@ -158,7 +158,7 @@ public final class JavaPluginLoader implements PluginLoader {
             throw new IllegalArgumentException("Plugin is not associated with this PluginLoader");
         }
 
-        if (!plugin.isEnabled()) {
+        if (plugin.isEnabled()) {
             JavaPlugin jPlugin = (JavaPlugin)plugin;
 
             server.getPluginManager().callEvent(new PluginEvent(Event.Type.PLUGIN_DISABLE, plugin));
