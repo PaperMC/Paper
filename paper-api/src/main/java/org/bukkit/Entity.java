f7ed510964d9064e80274b8ffb97e7f939ea6c61
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-03 16:34:29 +0000

    ... Entity has no health.
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/Entity.java
+++ paper-api/src/main/java/org/bukkit/Entity.java
@@ -32,17 +32,4 @@ public interface Entity {
      * @return Entity ID
      */
     public int getEntityID();
-    /**
-     * Gets the entitys health from 0-20, where 0 is dead and 20 is full
-     *
-     * @return Health represented from 0-20
-     */
-    public int getHealth();
-
-    /**
-     * Sets the entitys health from 0-20, where 0 is dead and 20 is full
-     *
-     * @param health New health represented from 0-20
-     */
-    public void setHealth(int health);
 }
