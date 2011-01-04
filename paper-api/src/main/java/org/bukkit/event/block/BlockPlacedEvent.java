740077970e6bab257d9f1374ee3a8fda9325be39
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-04 22:05:53 +0000

    Javadoc
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/event/block/BlockPlacedEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockPlacedEvent.java
@@ -11,24 +11,36 @@ public class BlockPlacedEvent extends BlockEvent implements Cancellable {
     private boolean cancel;
     private Player player;
 
-    /**
-     * @param type
-     * @param theBlock
-     */
     public BlockPlacedEvent(Type type, Block theBlock) {
         super(type, theBlock);
         cancel = false;
     }
-    
+
+    /**
+     * Gets the player who placed this block
+     *
+     * @return Player who placed the block
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Gets the cancellation state of this event. A cancelled event will not
+     * be executed in the server, but will still pass to other plugins
+     *
+     * @return true if this event is cancelled
+     */
     public boolean isCancelled() {
-        // TODO Auto-generated method stub
         return cancel;
     }
 
+    /**
+     * Sets the cancellation state of this event. A cancelled event will not
+     * be executed in the server, but will still pass to other plugins
+     *
+     * @param cancel true if you wish to cancel this event
+     */
     public void setCancelled(boolean cancel) {
         this.cancel = cancel;
     }
--- paper-api/src/main/java/org/bukkit/event/world/ChunkUnloadedEvent.java
+++ paper-api/src/main/java/org/bukkit/event/world/ChunkUnloadedEvent.java
@@ -14,12 +14,23 @@ public class ChunkUnloadedEvent extends ChunkLoadedEvent implements Cancellable 
         super(type, chunk);
     }
 
+    /**
+     * Gets the cancellation state of this event. A cancelled event will not
+     * be executed in the server, but will still pass to other plugins
+     *
+     * @return true if this event is cancelled
+     */
     public boolean isCancelled() {
         return cancel;
     }
 
+    /**
+     * Sets the cancellation state of this event. A cancelled event will not
+     * be executed in the server, but will still pass to other plugins
+     *
+     * @param cancel true if you wish to cancel this event
+     */
     public void setCancelled(boolean cancel) {
         this.cancel = cancel;
     }
-
 }
