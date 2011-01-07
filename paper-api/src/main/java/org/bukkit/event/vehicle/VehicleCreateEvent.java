ca9cd7aa45bb7da7eeab5b9930a7ddaf7f37c25a
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-07 12:26:10 -0800

    Moving some minecart flags that don't belong in the event out of the event.
    By: sk89q <the.sk89q@gmail.com>

--- paper-api/src/main/java/org/bukkit/event/vehicle/VehicleCreateEvent.java
+++ paper-api/src/main/java/org/bukkit/event/vehicle/VehicleCreateEvent.java
@@ -1,6 +1,5 @@
 package org.bukkit.event.vehicle;
 
-import org.bukkit.Vector;
 import org.bukkit.Vehicle;
 
 /**
@@ -9,41 +8,8 @@ import org.bukkit.Vehicle;
  * @author sk89q
  */
 public class VehicleCreateEvent extends VehicleEvent {
-    private boolean slowWhenEmpty;
-    private Vector derailedVelocityFactor;
-    private Vector flyingVelocityFactor;
-    
-    public VehicleCreateEvent(Type type, Vehicle vehicle,
-            boolean slowWhenEmpty, Vector derailedVelocityFactor,
-            Vector flyingVelocityFactor) {
+    public VehicleCreateEvent(Type type, Vehicle vehicle) {
         
         super(type, vehicle);
-        this.slowWhenEmpty = slowWhenEmpty;
-        this.derailedVelocityFactor = derailedVelocityFactor;
-        this.flyingVelocityFactor = flyingVelocityFactor;
-    }
-    
-    public void setSlowWhenEmpty(boolean setting) {
-        slowWhenEmpty = setting;
-    }
-    
-    public boolean shouldSlowWhenEmpty() {
-        return slowWhenEmpty;
-    }
-    
-    public void setDerailedVelocityFactor(Vector setting) {
-        derailedVelocityFactor = setting.clone();
-    }
-    
-    public Vector getDerailedVelocityFactor() {
-        return derailedVelocityFactor.clone();
-    }
-    
-    public void setFlyingVelocityFactor(Vector setting) {
-        flyingVelocityFactor = setting.clone();
-    }
-    
-    public Vector getFlyingVelocityFactor() {
-        return flyingVelocityFactor.clone();
     }
 }
