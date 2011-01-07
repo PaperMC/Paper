88076b8a294775c58198de04ceb1ab812bb17eaf
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-07 10:00:00 +0000

    Documented + added server.matchPlayer
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/Server.java
+++ paper-api/src/main/java/org/bukkit/Server.java
@@ -1,6 +1,7 @@
 
 package org.bukkit;
 
+import java.util.List;
 import org.bukkit.plugin.PluginManager;
 
 /**
@@ -37,6 +38,18 @@ public interface Server {
      * @return Player if it was found, otherwise null
      */
     public Player getPlayer(String name);
+
+    /**
+     * Attempts to match any players with the given name, and returns a list
+     * of all possibly matches
+     *
+     * This list is not sorted in any particular order. If an exact match is found,
+     * the returned list will only contain a single result.
+     *
+     * @param name Name to match
+     * @return List of all possible players
+     */
+    public List<Player> matchPlayer(String name);
 
     /**
      * Gets the PluginManager for interfacing with plugins
