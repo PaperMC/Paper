b0bf52751f2bb2e62735884aed44d5b8bd987be5
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2011-01-07 16:26:08 -0800

    Added passenger methods to Vehicle/LivingEntity.
    By: sk89q <the.sk89q@gmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftVehicle.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftVehicle.java
@@ -17,8 +17,22 @@ public abstract class CraftVehicle extends CraftEntity implements Vehicle {
     public Entity getPassenger() {
         return ((CraftWorld)getWorld()).toCraftEntity(getHandle().j);
     }
+    
+    public boolean setPassenger(Entity passenger) {
+        ((CraftEntity)passenger).getHandle().setPassengerOf(getHandle());
+        return true;
+    }
 
     public boolean isEmpty() {
         return getHandle().j == null;
+    }
+    
+    public boolean eject() {
+        if (getHandle().j == null) {
+            return false;
+        }
+        
+        getHandle().j.setPassengerOf(null);
+        return true;
     }
 }
