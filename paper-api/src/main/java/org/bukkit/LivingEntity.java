880f2c11a512f34afdc50442dda16b918f17b65b
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-07 16:25:52 -0800

    Added passenger methods to Vehicle/LivingEntity.
    By: sk89q <the.sk89q@gmail.com>

--- paper-api/src/main/java/org/bukkit/LivingEntity.java
+++ paper-api/src/main/java/org/bukkit/LivingEntity.java
@@ -35,4 +35,28 @@ public interface LivingEntity extends Entity {
      * @return
      */
     public Arrow shootArrow();
+    
+    /**
+     * Returns whether this entity is inside a vehicle.
+     * 
+     * @return
+     */
+    public boolean isInsideVehicle();
+    
+    /**
+     * Leave the current vehicle. If the entity is currently in a vehicle
+     * (and is removed from it), true will be returned, otherwise false will
+     * be returned.
+     * 
+     * @return
+     */
+    public boolean leaveVehicle();
+    
+    /**
+     * Get the vehicle that this player is inside. If there is no vehicle,
+     * null will be returned.
+     * 
+     * @return
+     */
+    public Vehicle getVehicle();
 }
--- paper-api/src/main/java/org/bukkit/Vehicle.java
+++ paper-api/src/main/java/org/bukkit/Vehicle.java
@@ -29,9 +29,24 @@ public interface Vehicle extends Entity {
     public Entity getPassenger();
     
     /**
+     * Set the passenger of a vehicle.
+     * 
+     * @param passenger
+     * @return false if it could not be done for whatever reason
+     */
+    public boolean setPassenger(Entity passenger);
+    
+    /**
      * Returns true if the vehicle has no passengers.
      * 
      * @return
      */
     public boolean isEmpty();
+    
+    /**
+     * Eject any passenger. True if there was a passenger.
+     * 
+     * @return
+     */
+    public boolean eject();
 }
