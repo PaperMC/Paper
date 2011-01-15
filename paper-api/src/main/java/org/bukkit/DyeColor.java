548842223742c045f7944f483606139b0973e1b8
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-15 00:08:34 +0000

    Fixed dye colours
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/DyeColor.java
+++ paper-api/src/main/java/org/bukkit/DyeColor.java
@@ -8,22 +8,22 @@ import java.util.Map;
  * All supported color values for dyes and cloth
  */
 public enum DyeColor {
-    BLACK((byte) 0x0),
-    RED((byte) 0x1),
-    GREEN((byte) 0x2),
-    BROWN((byte) 0x3),
-    BLUE((byte) 0x4),
-    PURPLE((byte) 0x5),
-    CYAN((byte) 0x6),
-    SILVER((byte) 0x7),
-    GRAY((byte) 0x8),
-    PINK((byte) 0x9),
-    LIME((byte) 0xA),
-    YELLOW((byte) 0xB),
-    LIGHT_BLUE((byte) 0xC),
-    MAGENTA((byte) 0xD),
-    ORANGE((byte) 0xE),
-    WHITE((byte) 0xF);
+    WHITE((byte) 0x0),
+    ORANGE((byte) 0x1),
+    MAGENTA((byte) 0x2),
+    LIGHT_BLUE((byte) 0x3),
+    YELLOW((byte) 0x4),
+    LIME((byte) 0x5),
+    PINK((byte) 0x6),
+    GRAY((byte) 0x7),
+    SILVER((byte) 0x8),
+    CYAN((byte) 0x9),
+    PURPLE((byte) 0xA),
+    BLUE((byte) 0xB),
+    BROWN((byte) 0xC),
+    GREEN((byte) 0xD),
+    RED((byte) 0xE),
+    BLACK((byte) 0xF);
 
     private final byte data;
     private final static Map<Byte, DyeColor> colors = new HashMap<Byte, DyeColor>();
