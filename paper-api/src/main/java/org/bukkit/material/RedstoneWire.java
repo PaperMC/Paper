f8decbe750304f1dc08dd7fae01a7b527f33f50a
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-15 20:35:22 +0000

    RedstoneWire MaterialData
    By: Dinnerbone <dinnerbone@dinnerbone.com>

---
+++ paper-api/src/main/java/org/bukkit/material/RedstoneWire.java
@@ -0,0 +1,35 @@
+
+package org.bukkit.material;
+
+import org.bukkit.Material;
+
+/**
+ * Represents redstone wire
+ */
+public class RedstoneWire extends MaterialData implements Redstone {
+    public RedstoneWire(final int type) {
+        super(type);
+    }
+
+    public RedstoneWire(final Material type) {
+        super(type);
+    }
+
+    public RedstoneWire(final int type, final byte data) {
+        super(type, data);
+    }
+
+    public RedstoneWire(final Material type, final byte data) {
+        super(type, data);
+    }
+
+    /**
+     * Gets the current state of this Material, indicating if it's powered or
+     * unpowered
+     *
+     * @return true if powered, otherwise false
+     */
+    public boolean isPowered() {
+        return getData() > 0;
+    }
+}
