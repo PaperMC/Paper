7314f0ade22f31a65cd3656470b9b5fca813ffb8
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-15 20:30:27 +0000

    RedstoneTorch MaterialData + Redstone interface
    By: Dinnerbone <dinnerbone@dinnerbone.com>

---
+++ paper-api/src/main/java/org/bukkit/material/Redstone.java
@@ -0,0 +1,15 @@
+
+package org.bukkit.material;
+
+/**
+ * Indicated a Material that may carry or create a Redstone current
+ */
+public interface Redstone {
+    /**
+     * Gets the current state of this Material, indicating if it's powered or
+     * unpowered
+     *
+     * @return true if powered, otherwise false
+     */
+    public boolean isPowered();
+}
---
+++ paper-api/src/main/java/org/bukkit/material/RedstoneTorch.java
@@ -0,0 +1,35 @@
+
+package org.bukkit.material;
+
+import org.bukkit.Material;
+
+/**
+ * Represents a redstone torch
+ */
+public class RedstoneTorch extends Torch implements Redstone {
+    public RedstoneTorch(final int type) {
+        super(type);
+    }
+
+    public RedstoneTorch(final Material type) {
+        super(type);
+    }
+
+    public RedstoneTorch(final int type, final byte data) {
+        super(type, data);
+    }
+
+    public RedstoneTorch(final Material type, final byte data) {
+        super(type, data);
+    }
+
+    /**
+     * Gets the current state of this Material, indicating if it's powered or
+     * unpowered
+     *
+     * @return true if powered, otherwise false
+     */
+    public boolean isPowered() {
+        return getItemType() == Material.REDSTONE_TORCH_ON;
+    }
+}
