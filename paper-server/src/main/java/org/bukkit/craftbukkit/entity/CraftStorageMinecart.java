a33d5b32d054ccf55cc3ba990e8d79918745210c
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2011-01-15 21:29:17 +0000

    All inventory stuff in org.bukkit.craftbukkit moved to org.bukkit.craftbukkit.inventory
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftStorageMinecart.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftStorageMinecart.java
@@ -3,7 +3,7 @@ package org.bukkit.craftbukkit.entity;
 import net.minecraft.server.EntityMinecart;
 
 import org.bukkit.inventory.Inventory;
-import org.bukkit.craftbukkit.CraftInventory;
+import org.bukkit.craftbukkit.inventory.CraftInventory;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.StorageMinecart;
 
