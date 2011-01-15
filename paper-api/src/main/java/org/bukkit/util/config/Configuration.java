c59341049fb056743a49d0c885bedf12b7f91571
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-14 23:38:40 -0800

    Added Configuration.getBoolean().
    By: sk89q <the.sk89q@gmail.com>

--- paper-api/src/main/java/org/bukkit/util/config/Configuration.java
+++ paper-api/src/main/java/org/bukkit/util/config/Configuration.java
@@ -196,4 +196,24 @@ public class Configuration {
             return def;
         }
     }
+
+    /**
+     * Gets a boolean at a location. This will either return an boolean
+     * or the default value. If the object at the particular location is not
+     * actually a boolean, the default value will be returned.
+     * 
+     * @param path SK's dot notation supported
+     * @param def default value
+     * @return boolean or default
+     */
+    public boolean getBoolean(String path, boolean def) {
+        Object o = getProperty(path);
+        if (o == null) {
+            return def;
+        } else if (o instanceof Boolean) {
+            return (Boolean)o;
+        } else {
+            return def;
+        }
+    }
 }
