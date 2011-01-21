77283a97beb2d0877a3f5858df282d10141b8a2f
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-01-21 19:41:43 +0800

    EntityExplodeEvent keeps track of its Location.
    By: Adam Tanner <adam@adamtanner.org>

--- paper-api/src/main/java/org/bukkit/event/entity/EntityExplodeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityExplodeEvent.java
@@ -4,6 +4,7 @@ package org.bukkit.event.entity;
 import java.util.List;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.Location;
 import org.bukkit.event.Cancellable;
 
 /**
@@ -12,10 +13,12 @@ import org.bukkit.event.Cancellable;
  */
 public class EntityExplodeEvent extends EntityEvent implements Cancellable {
     private boolean cancel;
+    private Location location;
     private List blocks;
-    
-    public EntityExplodeEvent (Type type, Entity what, List<Block> blocks) {
+
+    public EntityExplodeEvent (Type type, Entity what, Location location, List<Block> blocks) {
         super(type.ENTITY_EXPLODE, what);
+        this.location = location;
         this.cancel = false;
         this.blocks = blocks;
     }
@@ -27,13 +30,22 @@ public class EntityExplodeEvent extends EntityEvent implements Cancellable {
     public void setCancelled(boolean cancel) {
         this.cancel = cancel;
     }
-    
+
     /**
      * Returns the list of blocks that would have been removed or were
      * removed from the explosion event.
      */
     public List<Block> blockList() {
         return blocks;
+    }
+
+    /**
+     * Returns the location where the explosion happened.
+     * It is not possible to get this value from the Entity as
+     * the Entity no longer exists in the world.
+     */
+    public Location getLocation() {
+        return location;
     }
 
 }
