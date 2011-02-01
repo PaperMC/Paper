4de2b0d48eefd7675188a19c5a4a93abbd476a03
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-02-01 23:21:53 +0000

    Added NoteBlock.play, Dispenser.dispense
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/block/Dispenser.java
+++ paper-api/src/main/java/org/bukkit/block/Dispenser.java
@@ -6,4 +6,12 @@ package org.bukkit.block;
  * @author sk89q
  */
 public interface Dispenser extends BlockState, ContainerBlock {
+    /**
+     * Attempts to dispense the contents of this block<br />
+     * <br />
+     * If the block is no longer a dispenser, this will return false
+     * 
+     * @return true if successful, otherwise false
+     */
+    public boolean dispense();
 }
--- paper-api/src/main/java/org/bukkit/block/NoteBlock.java
+++ paper-api/src/main/java/org/bukkit/block/NoteBlock.java
@@ -19,4 +19,12 @@ public interface NoteBlock extends BlockState {
      * @param note
      */
     public void setNote(byte note);
+    /**
+     * Attempts to play the note at block<br />
+     * <br />
+     * If the block is no longer a note block, this will return false
+     *
+     * @return true if successful, otherwise false
+     */
+    public boolean play();
 }
