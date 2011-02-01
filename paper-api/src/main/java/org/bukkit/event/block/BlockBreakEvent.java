d7d0c66b753bb982e7693235c77296cf1e03dd62
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-02-01 18:07:16 +0100

    Interface for BlockBreakEvent
    By: Meaglin <meaglin.wasabi@gmail.com>

---
+++ paper-api/src/main/java/org/bukkit/event/block/BlockBreakEvent.java
@@ -0,0 +1,39 @@
+package org.bukkit.event.block;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+
+/**
+ *
+ * @author Meaglin
+ */
+public class BlockBreakEvent extends BlockEvent implements Cancellable {
+
+    private Player player;
+    private boolean cancel;
+
+    public BlockBreakEvent(final Block theBlock, Player player) {
+        super(Event.Type.BLOCK_BREAK, theBlock);
+        this.player = player;
+        this.cancel = false;
+    }
+
+    /**
+     * Returns the player doing the damage
+     *
+     * @return
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+}
