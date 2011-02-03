913e6d484a3d95ea5d08fc0269c025a18ce0336d
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-02-03 18:18:20 -0500

    Adding Creature.setTarget() per jlogsdon
    By: James Logsdon <dwarf@girsbrain.org>

--- paper-api/src/main/java/org/bukkit/entity/Creature.java
+++ paper-api/src/main/java/org/bukkit/entity/Creature.java
@@ -6,5 +6,12 @@ package org.bukkit.entity;
  *
  */
 public interface Creature extends LivingEntity{
-
+    /**
+     * Instructs this Creature to set the specified LivingEntity as its target.
+     * Hostile creatures may attack their target, and friendly creatures may
+     * follow their target.
+     *
+     * @param target New LivingEntity to target
+     */
+    public void setTarget(LivingEntity target);
 }
