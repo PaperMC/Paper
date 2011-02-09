f0f27689370ec0ff777e66e0875d1296b4cbb66a
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-02-09 13:44:57 +0000

    Added Creature.getTarget
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/entity/Creature.java
+++ paper-api/src/main/java/org/bukkit/entity/Creature.java
@@ -1,17 +1,23 @@
 package org.bukkit.entity;
 
 /**
- * Represents a Creature. Any LivingEntity that is not human is a Creature.
- * @author Cogito
- *
+ * Represents a Creature. Creatures are non-intelligent monsters or animals which
+ * have very simple abilities.
  */
-public interface Creature extends LivingEntity{
+public interface Creature extends LivingEntity {
     /**
      * Instructs this Creature to set the specified LivingEntity as its target.
      * Hostile creatures may attack their target, and friendly creatures may
      * follow their target.
      *
-     * @param target New LivingEntity to target
+     * @param target New LivingEntity to target, or null to clear the target
      */
     public void setTarget(LivingEntity target);
+
+    /**
+     * Gets the current target of this Creature
+     *
+     * @return Current target of this creature, or null if none exists
+     */
+    public LivingEntity getTarget();
 }
