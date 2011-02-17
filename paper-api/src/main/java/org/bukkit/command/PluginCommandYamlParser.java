d3b62214a64c695b1b8cc617ec2480e09ed4b336
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-02-16 21:45:15 -0800

    Fixed alias parsing for commands. object.toString().split(",") is NOT how you parse a YAML list!
    By: sk89q <the.sk89q@gmail.com>

--- paper-api/src/main/java/org/bukkit/command/PluginCommandYamlParser.java
+++ paper-api/src/main/java/org/bukkit/command/PluginCommandYamlParser.java
@@ -9,13 +9,13 @@ import org.bukkit.plugin.Plugin;
 
 public class PluginCommandYamlParser {
 
+    @SuppressWarnings("unchecked")
     public static List<Command> parse(Plugin plugin) {
         List<Command> pluginCmds = new ArrayList<Command>();
         Object object = plugin.getDescription().getCommands();
         if (object == null)
             return pluginCmds;
 
-        @SuppressWarnings("unchecked")
         Map<String, Map<String, Object>> map = (Map<String, Map<String, Object>>)object;
 
         if (map != null) {
@@ -34,9 +34,13 @@ public class PluginCommandYamlParser {
 
                 if (aliases != null) {
                     List<String> aliasList = new ArrayList<String>();
-
-                    for(String a : aliases.toString().split(",")) {
-                            aliasList.add(a);
+                    
+                    if (aliases instanceof List) {
+                        for (Object o : (List<Object>)aliases) {
+                            aliasList.add(o.toString());
+                        }
+                    } else {
+                        aliasList.add(aliases.toString());
                     }
 
                     newCmd.setAliases(aliasList);
