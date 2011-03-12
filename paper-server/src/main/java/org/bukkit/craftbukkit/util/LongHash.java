46286db7eb0bc2c6aac9fbbf66171583c685fa24
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2011-03-12 14:59:50 +0100

    Clean code a bit
    By: Erik Broes <erikbroes@grum.nl>

--- paper-server/src/main/java/org/bukkit/craftbukkit/util/LongHash.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/util/LongHash.java
@@ -11,13 +11,13 @@ package org.bukkit.craftbukkit.util;
  */
 public abstract class LongHash {
     static long toLong(int msw, int lsw) {
-        return ((long)msw << 32) + lsw - Integer.MIN_VALUE;
+        return ((long) msw << 32) + lsw - Integer.MIN_VALUE;
     }
-    
+
     static int msw(long l) {
         return (int) (l >> 32);
     }
-    
+
     static int lsw(long l) {
         return (int) (l & 0xFFFFFFFF) + Integer.MIN_VALUE;
     }
@@ -29,8 +29,8 @@ public abstract class LongHash {
     public void remove(int msw, int lsw) {
         remove(toLong(msw, lsw));
     }
-    
+
     public abstract boolean containsKey(long key);
-    
+
     public abstract void remove(long key);
 }
