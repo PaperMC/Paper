a977e93d7f2dcc2d945ed8a9fa1fb0d260c09cb3
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-04-02 15:40:25 +0100

    Added the ability to set the quit message
    By: Raphfrk <raphfrk@gmail.com>

--- paper-api/src/main/java/org/bukkit/event/player/PlayerQuitEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerQuitEvent.java
@@ -3,7 +3,30 @@ package org.bukkit.event.player;
 import org.bukkit.entity.Player;
 
 public class PlayerQuitEvent extends PlayerEvent {
-    public PlayerQuitEvent(Player who) {
+
+    private String quitMessage;
+
+    public PlayerQuitEvent(Player who, String quitMessage) {
         super(Type.PLAYER_QUIT, who);
+        this.quitMessage = quitMessage;
     }
+
+    /**
+     * Gets the quit message to send to all online players
+     *
+     * @return string quit message
+     */
+    public String getQuitMessage() {
+        return quitMessage;
+    }
+
+    /**
+     * Sets the quit message to send to all online players
+     *
+     * @param quitMessage quit message
+     */
+    public void setQuitMessage(String quitMessage) {
+        this.quitMessage = quitMessage;
+    }
+
 }
