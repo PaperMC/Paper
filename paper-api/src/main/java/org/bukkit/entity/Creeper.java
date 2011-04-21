101cdea636220869b593eae8c1a1e7755fd3bd84
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-04-21 16:15:59 +0100

    Added Creeper.set/isPowered
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/entity/Creeper.java
+++ paper-api/src/main/java/org/bukkit/entity/Creeper.java
@@ -1,14 +1,21 @@
-/**
- * 
- */
+
 package org.bukkit.entity;
 
 /**
- * Represents a Creeper.
- * 
- * @author Cogito
- *
+ * Represents a Creeper
  */
 public interface Creeper extends Monster {
+    /**
+     * Checks if this Creeper is powered (Electrocuted)
+     *
+     * @return true if this creeper is powered
+     */
+    public boolean isPowered();
 
+    /**
+     * Sets the Powered status of this Creeper
+     *
+     * @param value New Powered status
+     */
+    public void setPowered(boolean value);
 }
