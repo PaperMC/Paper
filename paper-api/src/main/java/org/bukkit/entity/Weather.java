a6d7f003fda93a506ec2bf23a251c559de9dfd67
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-04-21 16:51:43 +0100

    Added Weather and WeatherStorm
    By: Dinnerbone <dinnerbone@dinnerbone.com>

---
+++ paper-api/src/main/java/org/bukkit/entity/Weather.java
@@ -0,0 +1,9 @@
+
+package org.bukkit.entity;
+
+/**
+ * Represents a Weather related entity, such as a storm
+ */
+public interface Weather extends Entity {
+    
+}
---
+++ paper-api/src/main/java/org/bukkit/entity/WeatherStorm.java
@@ -0,0 +1,9 @@
+
+package org.bukkit.entity;
+
+/**
+ * Represents an instance of a weather storm entity
+ */
+public interface WeatherStorm extends Weather {
+
+}
