3ce3db7f3a72f3cd6c5ee1d2afc485e3950a6b58
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-05-11 17:44:31 -0700

    Added ability to saddle a pig to the API. Thanks xPaw!
    By: Andrew Ardill <andrew.ardill@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Pig.java
+++ paper-api/src/main/java/org/bukkit/entity/Pig.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.bukkit.entity;
 
 /**
@@ -10,5 +7,15 @@ package org.bukkit.entity;
  *
  */
 public interface Pig extends Animals {
-
+    /**
+     * @author xPaw
+     * @return if the pig has been saddled.
+     */
+    public boolean hasSaddle();
+    
+    /**
+     * @author xPaw
+     * @param saddled set if the pig has a saddle or not.
+     */
+    public void setSaddle(boolean saddled);
 }
