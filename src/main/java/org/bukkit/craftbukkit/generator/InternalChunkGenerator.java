d40c690c8f5fe2fa351f9d6dde3c4a9851366222
Dinnerbone <dinnerbone@dinnerbone.com>
2011-06-06 21:12:03 +0100

    Fix for generator stuff now being class

--- src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java
+++ src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java
@@ -4,6 +4,6 @@ package org.bukkit.craftbukkit.generator;
 import net.minecraft.server.IChunkProvider;
 import org.bukkit.generator.ChunkGenerator;
 
-public interface InternalChunkGenerator extends ChunkGenerator, IChunkProvider {
+public abstract class InternalChunkGenerator extends ChunkGenerator implements IChunkProvider {
 
 }
--- src/main/java/org/bukkit/craftbukkit/generator/NormalChunkGenerator.java
+++ src/main/java/org/bukkit/craftbukkit/generator/NormalChunkGenerator.java
@@ -3,14 +3,19 @@ package org.bukkit.craftbukkit.generator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import net.minecraft.server.Chunk;
 import net.minecraft.server.ChunkProviderGenerate;
+import net.minecraft.server.IChunkProvider;
+import net.minecraft.server.IProgressUpdate;
 import net.minecraft.server.World;
 import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.generator.BlockPopulator;
 
-public class NormalChunkGenerator extends ChunkProviderGenerate implements InternalChunkGenerator {
+public class NormalChunkGenerator extends InternalChunkGenerator {
+    private final ChunkProviderGenerate provider;
+
     public NormalChunkGenerator(World world, long seed) {
-        super(world, seed);
+        provider = new ChunkProviderGenerate(world, seed);
     }
 
     public byte[] generate(org.bukkit.World world, Random random, int x, int z) {
@@ -23,5 +28,33 @@ public class NormalChunkGenerator extends ChunkProviderGenerate implements Inter
 
     public List<BlockPopulator> getDefaultPopulators(org.bukkit.World world) {
         return new ArrayList<BlockPopulator>();
+    }
+
+    public boolean isChunkLoaded(int i, int i1) {
+        return provider.isChunkLoaded(i, i1);
+    }
+
+    public Chunk getOrCreateChunk(int i, int i1) {
+        return provider.getOrCreateChunk(i, i1);
+    }
+
+    public Chunk getChunkAt(int i, int i1) {
+        return provider.getChunkAt(i, i1);
+    }
+
+    public void getChunkAt(IChunkProvider icp, int i, int i1) {
+        provider.getChunkAt(icp, i, i1);
+    }
+
+    public boolean saveChunks(boolean bln, IProgressUpdate ipu) {
+        return provider.saveChunks(bln, ipu);
+    }
+
+    public boolean unloadChunks() {
+        return provider.unloadChunks();
+    }
+
+    public boolean b() {
+        return provider.b();
     }
 }
