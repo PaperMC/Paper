876c4074ba92c7530bcd13dec25fa2bd4684b9d1
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-06-16 20:34:12 +0100

    Javadoc fix for BlockPopulator.java explaining which chunks exist and which don't
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/generator/BlockPopulator.java
+++ paper-api/src/main/java/org/bukkit/generator/BlockPopulator.java
@@ -12,7 +12,11 @@ public abstract class BlockPopulator {
     /**
      * Populates an area of blocks at or around the given chunk.
      *
-     * The chunks directly surrounding the specified chunk must already exist.
+     * The chunks on each side of the specified chunk must already exist; that is,
+     * there must be one north, east, south and west of the specified chunk.
+     * The "corner" chunks may not exist, in which scenario the populator should
+     * record any changes required for those chunks and perform the changes when
+     * they are ready.
      *
      * @param world The world to generate in
      * @param random The random generator to use
