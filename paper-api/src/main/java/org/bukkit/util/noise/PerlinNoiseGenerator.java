06644875636c4ac1bce5e855f10c6eea96cea23c
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-07-08 14:39:20 +0100

    Fixed static methods in PerlinNoiseGenerator
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/util/noise/PerlinNoiseGenerator.java
+++ paper-api/src/main/java/org/bukkit/util/noise/PerlinNoiseGenerator.java
@@ -189,7 +189,7 @@ public class PerlinNoiseGenerator extends NoiseGenerator {
      * @param amplitude How much to alter the amplitude by each octave
      * @return Resulting noise
      */
-    public double getNoise(double x, double y, int octaves, double frequency, double amplitude) {
+    public static double getNoise(double x, double y, int octaves, double frequency, double amplitude) {
         return instance.noise(x, y, octaves, frequency, amplitude);
     }
 
@@ -204,7 +204,7 @@ public class PerlinNoiseGenerator extends NoiseGenerator {
      * @param amplitude How much to alter the amplitude by each octave
      * @return Resulting noise
      */
-    public double getNoise(double x, double y, double z, int octaves, double frequency, double amplitude) {
+    public static double getNoise(double x, double y, double z, int octaves, double frequency, double amplitude) {
         return instance.noise(x, y, z, octaves, frequency, amplitude);
     }
 }
