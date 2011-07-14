dc424c29435864ecca2d10effeaf43246ef0d620
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-07-14 11:30:45 -0400

    Added interface for setting and getting a pig zombie's anger level.
    By: Celtic Minstrel <celtic.minstrel.ca@some.place>

--- paper-api/src/main/java/org/bukkit/entity/PigZombie.java
+++ paper-api/src/main/java/org/bukkit/entity/PigZombie.java
@@ -1,12 +1,34 @@
-/**
- *
- */
 package org.bukkit.entity;
 
 /**
  * Represents a Pig Zombie.
- *
- * @author Cogito
- *
  */
-public interface PigZombie extends Zombie {}
+public interface PigZombie extends Zombie {
+    /**
+     * Get the pig zombie's current anger level.
+     *
+     * @return The anger level.
+     */
+    int getAnger();
+
+    /**
+     * Set the pig zombie's current anger level.
+     *
+     * @param level The anger level. Higher levels of anger take longer to wear off.
+     */
+    void setAnger(int level);
+
+    /**
+     * Shorthand; sets to either 0 or the default level.
+     *
+     * @param angry Whether the zombie should be angry.
+     */
+    void setAngry(boolean angry);
+
+    /**
+     * Shorthand; gets whether the zombie is angry.
+     *
+     * @return True if the zombie is angry, otherwise false.
+     */
+    boolean isAngry();
+}
