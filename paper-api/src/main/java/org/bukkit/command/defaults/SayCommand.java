42235114caa5cc92335d7a066d16065a9f5bd6a3
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-09-04 16:28:40 +0100

    Fixed /say command not allowing more than one argument
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/command/defaults/SayCommand.java
+++ paper-api/src/main/java/org/bukkit/command/defaults/SayCommand.java
@@ -16,7 +16,7 @@ public class SayCommand extends VanillaCommand {
     @Override
     public boolean execute(CommandSender sender, String currentAlias, String[] args) {
         if (!testPermission(sender)) return true;
-        if (args.length != 1)  {
+        if (args.length == 0)  {
             sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
             return false;
         }
