e6b33d8a08908443bfc3ed08f3a9ad4dc7a31726
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-09-15 05:48:52 +0100

    Added methods for getting/setting how much exp is contained within an orb
    By: Dinnerbone <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/entity/ExperienceOrb.java
+++ paper-api/src/main/java/org/bukkit/entity/ExperienceOrb.java
@@ -3,4 +3,18 @@ package org.bukkit.entity;
 /**
  * Represents an Experience Orb.
  */
-public interface ExperienceOrb extends Entity {}
+public interface ExperienceOrb extends Entity {
+    /**
+     * Gets how much experience is contained within this orb
+     *
+     * @return Amount of experience
+     */
+    public int getExperience();
+    
+    /**
+     * Sets how much experience is contained within this orb
+     * 
+     * @param value Amount of experience
+     */
+    public void setExperience(int value);
+}
