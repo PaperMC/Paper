34f857ad15deabec6091592b9fe95e40be945334
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-11-24 20:16:16 +0000

    Added new set/get age methods to Animals
    By: Nathan Adams <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/entity/Animals.java
+++ paper-api/src/main/java/org/bukkit/entity/Animals.java
@@ -4,4 +4,18 @@ package org.bukkit.entity;
  * Represents an Animal.
  *
  */
-public interface Animals extends Creature {}
+public interface Animals extends Creature {
+    /**
+     * Gets the age of this animal.
+     * 
+     * @return Age
+     */
+    public int getAge();
+
+    /**
+     * Sets the age of this animal.
+     *
+     * @param age New age
+     */
+    public void setAge(int age);
+}
