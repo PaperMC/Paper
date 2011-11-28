5be8bd5ba50ce27c9e717ff8ac3d1aacbcc29c18
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-11-28 18:40:04 +0100

    Added the missing effects to the Effect enum.
    By: TomyLobo <tomylobo@nurfuerspam.de>

--- paper-api/src/main/java/org/bukkit/Effect.java
+++ paper-api/src/main/java/org/bukkit/Effect.java
@@ -10,8 +10,14 @@ public enum Effect {
     DOOR_TOGGLE(1003),
     EXTINGUISH(1004),
     RECORD_PLAY(1005),
+    GHAST_SHRIEK(1007),
+    GHAST_SHOOT(1008),
+    BLAZE_SHOOT(1009),
     SMOKE(2000),
-    STEP_SOUND(2001);
+    STEP_SOUND(2001),
+    POTION_BREAK(2002),
+    ENDER_SIGNAL(2003),
+    MOBSPAWNER_FLAMES(2004);
 
     private final int id;
 
