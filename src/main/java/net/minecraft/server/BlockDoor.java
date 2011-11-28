7db72dfd2c3d836a26375aa34d8aa37cf179c726
Nathan Adams <dinnerbone@dinnerbone.com>
2011-11-28 07:00:04 +0000

    Doors and trapdoors should function correctly once again

--- src/main/java/net/minecraft/server/BlockDoor.java
+++ src/main/java/net/minecraft/server/BlockDoor.java
@@ -166,8 +166,8 @@ public class BlockDoor extends Block {
                 if (!world.isStatic) {
                     this.b(world, i, j, k, i1, 0);
                 }
-            } else if (l > 0) {
                 // CraftBukkit start
+            } else if (l > 0 && Block.byId[l].isPowerSource()) {
                 org.bukkit.World bworld = world.getWorld();
                 org.bukkit.block.Block block = bworld.getBlockAt(i, j, k);
                 org.bukkit.block.Block blockTop = bworld.getBlockAt(i, j + 1, k);
