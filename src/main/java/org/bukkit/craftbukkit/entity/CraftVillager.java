0c88f05c0da5e5f4a4dbd9be3ccbe2482e7b2f79
Andrew Ardill <andrew.ardill@gmail.com>
2011-11-29 09:46:20 +1100

    Fix CraftVillager constructor to only take an EntityVillager

--- src/main/java/org/bukkit/craftbukkit/entity/CraftVillager.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftVillager.java
@@ -6,7 +6,7 @@ import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Villager;
 
 public class CraftVillager extends CraftCreature implements Villager {
-    public CraftVillager(CraftServer server, EntityCreature entity) {
+    public CraftVillager(CraftServer server, EntityVillager entity) {
         super(server, entity);
     }
 
