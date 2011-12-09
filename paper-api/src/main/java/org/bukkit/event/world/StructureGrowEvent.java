a41e663ef30c644fdc64383bef71cafe8f1572fd
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2011-12-09 16:24:23 +0000

    Override annotations do not belong here!
    By: Nathan Adams <dinnerbone@dinnerbone.com>

--- paper-api/src/main/java/org/bukkit/event/world/StructureGrowEvent.java
+++ paper-api/src/main/java/org/bukkit/event/world/StructureGrowEvent.java
@@ -73,12 +73,10 @@ public class StructureGrowEvent extends WorldEvent implements Cancellable {
         return blocks;
     }
 
-    @Override
     public boolean isCancelled() {
         return cancelled;
     }
 
-    @Override
     public void setCancelled(boolean cancel) {
         cancelled = cancel;
     }
