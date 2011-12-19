0b7a5891c5344f6ea34acafb9ebd5a84bf274999
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2011-12-20 00:25:15 +1100

    Test cloning enchanted CraftItemStacks
    By: Andrew Ardill <andrew.ardill@gmail.com>

---
+++ paper-server/src/test/java/org/bukkit/craftbukkit/inventory/CraftItemStackTest.java
@@ -0,0 +1,28 @@
+package org.bukkit.craftbukkit.inventory;
+
+import net.minecraft.server.Enchantment;
+import net.minecraft.server.StatisticList;
+import org.bukkit.inventory.ItemStack;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CraftItemStackTest {
+    @Before
+    public void setUp() throws Exception {
+        StatisticList.a();
+    }
+
+    @Test
+    public void testCloneEnchantedItem() throws Exception {
+        net.minecraft.server.ItemStack nmsItemStack = new net.minecraft.server.ItemStack(net.minecraft.server.Item.POTION);
+        nmsItemStack.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+        ItemStack itemStack = new CraftItemStack(nmsItemStack);
+        ItemStack clone = itemStack.clone();
+        assert (clone.getType().equals(itemStack.getType()));
+        assert (clone.getAmount() == itemStack.getAmount());
+        assert (clone.getDurability() == itemStack.getDurability());
+        assert (clone.getEnchantments().equals(itemStack.getEnchantments()));
+        assert (clone.getTypeId() == itemStack.getTypeId());
+        assert (clone.getData().equals(itemStack.getData()));
+    }
+}
