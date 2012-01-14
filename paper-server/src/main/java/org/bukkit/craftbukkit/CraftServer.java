b74ebb14802342e9592ddf9095fc0922e7ec43ec
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-01-14 11:14:43 +1100

    Undo commit cc60920 to allow for proper integration with the new Bukkit enum values
    By: md-5 <md_5@bigpond.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -516,7 +516,7 @@ public final class CraftServer implements Server {
         ChunkGenerator generator = creator.generator();
         File folder = new File(name);
         World world = getWorld(name);
-        WorldType type = WorldType.a(creator.type().name());
+        WorldType type = WorldType.a(creator.type().getName());
 
         if (world != null) {
             return world;
