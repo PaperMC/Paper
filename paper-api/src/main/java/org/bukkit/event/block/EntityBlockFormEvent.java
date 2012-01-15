72ce6fab3e019d6f80ad6f97442f4a10dca08605
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-01-15 09:13:00 +0000

    Added EntityBlockFormEvent for block forming by entity actions (snowmen etc). Thanks to fieldmaster for the PR.
    By: Nathan Adams <dinnerbone@dinnerbone.com>

---
+++ paper-api/src/main/java/org/bukkit/event/block/EntityBlockFormEvent.java
@@ -0,0 +1,32 @@
+package org.bukkit.event.block;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.Entity;
+
+/**
+ * Called when a block is formed by entities.
+ *<p />
+ * Examples:
+ *<ul>
+ *     <li>Snow formed by a {@link org.bukkit.entity.Snowman}.</li>
+ * </ul>
+ */
+public class EntityBlockFormEvent extends BlockFormEvent {
+    private Entity entity;
+
+    public EntityBlockFormEvent(Entity entity, Block block, BlockState blockstate) {
+        super(block, blockstate);
+
+        this.entity = entity;
+    }
+
+    /**
+     * Get the entity that formed the block.
+     *
+     * @return Entity involved in event
+     */
+    public Entity getEntity() {
+        return entity;
+    }
+}
