28fcbec3b1662f42a6a457869bdb43e9b246e615
Nathan Adams <dinnerbone@dinnerbone.com>
2012-01-19 16:09:24 +0000

    Prevent re-use of internal world dimension IDs. This fixes BUKKIT-448. Thanks to snowleo for the PR.

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -540,6 +540,16 @@ public final class CraftServer implements Server {
         }
 
         int dimension = 10 + console.worlds.size();
+        boolean used = false;
+        do {
+            for (WorldServer server : console.worlds) {
+                used = server.dimension == dimension;
+               if (used) {
+                    dimension++;
+                    break;
+                }
+            }
+        } while(used);
         boolean hardcore = false;
         WorldServer internal = new WorldServer(console, new ServerNBTManager(getWorldContainer(), name, true), name, dimension, new WorldSettings(creator.seed(), getDefaultGameMode().getValue(), true, hardcore, type), creator.environment(), generator);
 
