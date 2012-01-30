fe4de0bb015395675b7bb6a8e5adcc9f19f6afad
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-01-30 21:52:41 +0100

    Added RemoteServerCommandEvent.
    By: Erik Broes <erikbroes@grum.nl>

---
+++ paper-api/src/main/java/org/bukkit/event/server/RemoteServerCommandEvent.java
@@ -0,0 +1,25 @@
+package org.bukkit.event.server;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Remote Server Command events
+ */
+@SuppressWarnings("serial")
+public class RemoteServerCommandEvent extends ServerCommandEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    public RemoteServerCommandEvent(final CommandSender sender, final String command) {
+        super(sender, command);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
