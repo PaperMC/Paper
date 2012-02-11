e204abd0f0fd9a83c0a68985cc79b86eaa4fd295
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-02-10 17:53:22 -0800

    Allow cloning ItemStacks with unsafe enchantments. Fixes BUKKIT-621
    Prevents "IllegalArgumentException: Specified enchantment cannot be applied to this itemstack").
    
    By: mushroomhostage <mushroomhostage@yahoo.com>

--- paper-api/src/main/java/org/bukkit/inventory/ItemStack.java
+++ paper-api/src/main/java/org/bukkit/inventory/ItemStack.java
@@ -211,7 +211,7 @@ public class ItemStack implements ConfigurationSerializable {
     @Override
     public ItemStack clone() {
         ItemStack result = new ItemStack(type, amount, durability);
-        result.addEnchantments(getEnchantments());
+        result.addUnsafeEnchantments(getEnchantments());
 
         return result;
     }
