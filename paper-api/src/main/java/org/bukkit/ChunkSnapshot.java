d500e975981b27541df60cd5fc05edbf89754baf
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-03-01 13:55:29 -0600

    Implemented snapshot support for 1.2.
    By: Mike Primm <mike@primmhome.com>

--- paper-api/src/main/java/org/bukkit/ChunkSnapshot.java
+++ paper-api/src/main/java/org/bukkit/ChunkSnapshot.java
@@ -111,4 +111,11 @@ public interface ChunkSnapshot {
      * @return time in ticks
      */
     long getCaptureFullTime();
+
+    /**
+     * Test if section is empty
+     * @param sy - section Y coordinate (block Y / 16)
+     * @return true if empty, false if not
+     */
+    boolean isSectionEmpty(int sy);
 }
