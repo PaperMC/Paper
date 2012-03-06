99569c1767cf18695ca2cc1e206bc74cf47d15cb
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-03-05 22:33:22 -0500

    [Bleeding] Added missing NPE protection to removeMetadata(). Fixes BUKKIT-933
    By: rmichela <deltahat@gmail.com>

--- paper-api/src/main/java/org/bukkit/metadata/MetadataStoreBase.java
+++ paper-api/src/main/java/org/bukkit/metadata/MetadataStoreBase.java
@@ -82,6 +82,7 @@ public abstract class MetadataStoreBase<T> {
     public synchronized void removeMetadata(T subject, String metadataKey, Plugin owningPlugin) {
         String key = cachedDisambiguate(subject, metadataKey);
         List<MetadataValue> metadataList = metadataMap.get(key);
+        if (metadataList == null) return;
         for (int i = 0; i < metadataList.size(); i++) {
             if (metadataList.get(i).getOwningPlugin().equals(owningPlugin)) {
                 metadataList.remove(i);
