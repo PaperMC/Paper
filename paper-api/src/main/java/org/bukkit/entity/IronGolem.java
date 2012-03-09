1d06f749fb6512e8901e436ae05492a4c745a1bb
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-03-08 20:22:11 -0600

    [Bleeding] Added way to get/set if an Iron Golem was player created. Addresses BUKKIT-1229
    By: aPunch <alexpuncochar@yahoo.com>

--- paper-api/src/main/java/org/bukkit/entity/IronGolem.java
+++ paper-api/src/main/java/org/bukkit/entity/IronGolem.java
@@ -5,4 +5,19 @@ package org.bukkit.entity;
  */
 public interface IronGolem extends Golem {
 
+    /**
+     * Gets whether this iron golem was built by a player.
+     *
+     * @return Whether this iron golem was built by a player
+     */
+    public boolean isPlayerCreated();
+
+    /**
+     * Sets whether this iron golem was built by a player or not.
+     *
+     * @param playerCreated
+     *              true if you want to set the iron golem as being player created,
+     *              false if you want it to be a natural village golem.
+     */
+    public void setPlayerCreated(boolean playerCreated);
 }
