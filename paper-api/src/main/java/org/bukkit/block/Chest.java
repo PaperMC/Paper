82f7680bd0ffc70bc23bd6822a3365dce8f0c270
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-03-19 15:02:10 -0400

    [Bleeding] Add Chest.getBlockInventory() for convenience; this always returns a single chest inventory. Addresses BUKKIT-1247
    By: Celtic Minstrel <celtic.minstrel.ca@some.place>

--- paper-api/src/main/java/org/bukkit/block/Chest.java
+++ paper-api/src/main/java/org/bukkit/block/Chest.java
@@ -1,6 +1,16 @@
 package org.bukkit.block;
 
+import org.bukkit.inventory.Inventory;
+
 /**
  * Represents a chest.
  */
-public interface Chest extends BlockState, ContainerBlock {}
+public interface Chest extends BlockState, ContainerBlock {
+    /**
+     * Returns the chest's inventory. If this is a double chest, it returns just
+     * the portion of the inventory linked to this half of the chest.
+     *
+     * @return The inventory.
+     */
+    Inventory getBlockInventory();
+}
