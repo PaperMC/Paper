9055a20d71698922c891ac2ff802a8d7e63f78be
Wesley Wolfe <weswolf@aol.com>
2012-03-20 01:44:35 -0500

    Added EntityThrownExpBottle for diff visibility.

---
+++ src/main/java/net/minecraft/server/EntityThrownExpBottle.java
@@ -0,0 +1,44 @@
+package net.minecraft.server;
+
+public class EntityThrownExpBottle extends EntityProjectile {
+
+    public EntityThrownExpBottle(World world) {
+        super(world);
+    }
+
+    public EntityThrownExpBottle(World world, EntityLiving entityliving) {
+        super(world, entityliving);
+    }
+
+    public EntityThrownExpBottle(World world, double d0, double d1, double d2) {
+        super(world, d0, d1, d2);
+    }
+
+    protected float e() {
+        return 0.07F;
+    }
+
+    protected float c() {
+        return 0.7F;
+    }
+
+    protected float d() {
+        return -20.0F;
+    }
+
+    protected void a(MovingObjectPosition movingobjectposition) {
+        if (!this.world.isStatic) {
+            this.world.triggerEffect(2002, (int) Math.round(this.locX), (int) Math.round(this.locY), (int) Math.round(this.locZ), 0);
+            int i = 3 + this.world.random.nextInt(5) + this.world.random.nextInt(5);
+
+            while (i > 0) {
+                int j = EntityExperienceOrb.getOrbValue(i);
+
+                i -= j;
+                this.world.addEntity(new EntityExperienceOrb(this.world, this.locX, this.locY, this.locZ, j));
+            }
+
+            this.die();
+        }
+    }
+}
