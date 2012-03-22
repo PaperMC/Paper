0a925ff067c6230554c4336627f621decfcd1e79
rmichela <deltahat@gmail.com>
2012-03-22 00:21:18 -0400

    [Bleeding] Added support for custom index help topics in help.yml. Addresses BUKKIT-1263

---
+++ src/main/java/org/bukkit/craftbukkit/help/CustomIndexHelpTopic.java
@@ -0,0 +1,40 @@
+package org.bukkit.craftbukkit.help;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.help.HelpMap;
+import org.bukkit.help.HelpTopic;
+import org.bukkit.help.IndexHelpTopic;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ */
+public class CustomIndexHelpTopic extends IndexHelpTopic {
+    private List<String> futureTopics;
+    private HelpMap helpMap;
+    
+    public CustomIndexHelpTopic(HelpMap helpMap, String name, String shortText, String permission, List<String> futureTopics, String preamble) {
+        super(name, shortText, permission, new HashSet<HelpTopic>(), preamble);
+        this.helpMap = helpMap;
+        this.futureTopics = futureTopics;
+    }
+
+    @Override
+    public String getFullText(CommandSender sender) {
+        if (futureTopics != null) {
+            List<HelpTopic> topics = new LinkedList<HelpTopic>();
+            for (String futureTopic : futureTopics) {
+                HelpTopic topic = helpMap.getHelpTopic(futureTopic);
+                if (topic != null) {
+                    topics.add(topic);
+                }
+            }
+            setTopicsCollection(topics);
+            futureTopics = null;
+        }
+        
+        return super.getFullText(sender);
+    }
+}
