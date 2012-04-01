058bd748b65b0d787e675ede74d948c787df532c
feildmaster <admin@feildmaster.com>
2012-04-01 04:58:11 -0500

    Fixed interacting with Air returning a false block.

--- src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
@@ -142,7 +142,7 @@ public class CraftEventFactory {
         if (action != Action.LEFT_CLICK_AIR && action != Action.RIGHT_CLICK_AIR) {
             throw new IllegalArgumentException();
         }
-        return callPlayerInteractEvent(who, action, 0, 255, 0, 0, itemstack);
+        return callPlayerInteractEvent(who, action, 0, 256, 0, 0, itemstack);
     }
 
     public static PlayerInteractEvent callPlayerInteractEvent(EntityHuman who, Action action, int clickedX, int clickedY, int clickedZ, int clickedFace, ItemStack itemstack) {
