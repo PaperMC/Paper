a963880bd8f3a690b91dbe4e314191ac31e22e69
feildmaster <admin@feildmaster.com>
2012-04-02 09:01:48 -0500

    Add BlockRedstoneEvent to Redstone Lamps, Addresses BUKKIT-1431

--- src/main/java/net/minecraft/server/BlockRedstoneLamp.java
+++ src/main/java/net/minecraft/server/BlockRedstoneLamp.java
@@ -2,6 +2,11 @@ package net.minecraft.server;
 
 import java.util.Random;
 
+// CraftBukkit start
+import org.bukkit.craftbukkit.event.CraftEventFactory;
+import org.bukkit.event.block.BlockRedstoneEvent;
+// CraftBukkit end
+
 public class BlockRedstoneLamp extends Block {
 
     private final boolean a;
@@ -20,6 +25,11 @@ public class BlockRedstoneLamp extends Block {
             if (this.a && !world.isBlockIndirectlyPowered(i, j, k)) {
                 world.c(i, j, k, this.id, 4);
             } else if (!this.a && world.isBlockIndirectlyPowered(i, j, k)) {
+                // CraftBukkit start
+                if (CraftEventFactory.callRedstoneChange(world, i, j, k, 0, 15).getNewCurrent() != 15) {
+                    return;
+                }
+                // CraftBukkit end
                 world.setTypeId(i, j, k, Block.REDSTONE_LAMP_ON.id);
             }
         }
@@ -30,6 +40,11 @@ public class BlockRedstoneLamp extends Block {
             if (this.a && !world.isBlockIndirectlyPowered(i, j, k)) {
                 world.c(i, j, k, this.id, 4);
             } else if (!this.a && world.isBlockIndirectlyPowered(i, j, k)) {
+                // CraftBukkit start
+                if (CraftEventFactory.callRedstoneChange(world, i, j, k, 0, 15).getNewCurrent() != 15) {
+                    return;
+                }
+                // CraftBukkit end
                 world.setTypeId(i, j, k, Block.REDSTONE_LAMP_ON.id);
             }
         }
@@ -37,6 +52,11 @@ public class BlockRedstoneLamp extends Block {
 
     public void a(World world, int i, int j, int k, Random random) {
         if (!world.isStatic && this.a && !world.isBlockIndirectlyPowered(i, j, k)) {
+            // CraftBukkit start
+            if (CraftEventFactory.callRedstoneChange(world, i, j, k, 15, 0).getNewCurrent() != 0) {
+                return;
+            }
+            // CraftBukkit end
             world.setTypeId(i, j, k, Block.REDSTONE_LAMP_OFF.id);
         }
     }
