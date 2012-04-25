f6c072f78f2ae17b1bb4232e34b3c523bb00c9d3
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-04-25 16:27:11 -0500

    Add IgniteCause.FIREBALL for BUKKIT-1566
    By: Travis Watkins <amaranth@ubuntu.com>

--- paper-api/src/main/java/org/bukkit/event/block/BlockIgniteEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockIgniteEvent.java
@@ -70,6 +70,10 @@ public class BlockIgniteEvent extends BlockEvent implements Cancellable {
          * Block ignition caused by lightning.
          */
         LIGHTNING,
+        /**
+         * Block ignition caused by a player using a fireball.
+         */
+        FIREBALL,
     }
 
     @Override
