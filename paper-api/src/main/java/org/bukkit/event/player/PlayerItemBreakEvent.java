803c3013d5a6197e8e5dd9b30ee9daa2973b8e78
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-05-06 21:36:07 -0600

    Add PlayerItemBreakEvent. Addresses BUKKIT-1600
    By: Travis Ralston <travpc@gmail.com>

---
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerItemBreakEvent.java
@@ -0,0 +1,38 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Fired when a player's item breaks (such as a shovel or flint and steel).
+ * The item that's breaking will exist in the inventory with a stack size of 0.
+ * After the event, the item's durability will be reset to 0.
+ */
+public class PlayerItemBreakEvent extends PlayerEvent {
+    private static final HandlerList handlers = new HandlerList();
+    private final ItemStack brokenItem;
+
+    public PlayerItemBreakEvent(final Player player, final ItemStack brokenItem) {
+        super(player);
+        this.brokenItem = brokenItem;
+    }
+
+    /**
+     * Gets the item that broke
+     *
+     * @return The broken item
+     */
+    public ItemStack getBrokenItem() {
+        return brokenItem;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
