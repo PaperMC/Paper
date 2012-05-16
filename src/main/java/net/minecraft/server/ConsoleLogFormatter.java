e91169ebadf259898f76f6141d741b327f503d67
Travis Watkins <amaranth@ubuntu.com>
2012-05-15 19:58:46 -0500

    Add ConsoleLogFormatter for diff visibility

---
+++ src/main/java/net/minecraft/server/ConsoleLogFormatter.java
@@ -0,0 +1,51 @@
+package net.minecraft.server;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+final class ConsoleLogFormatter extends Formatter {
+
+    private SimpleDateFormat a = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    ConsoleLogFormatter() {}
+
+    public String format(LogRecord logrecord) {
+        StringBuilder stringbuilder = new StringBuilder();
+
+        stringbuilder.append(this.a.format(Long.valueOf(logrecord.getMillis())));
+        Level level = logrecord.getLevel();
+
+        if (level == Level.FINEST) {
+            stringbuilder.append(" [FINEST] ");
+        } else if (level == Level.FINER) {
+            stringbuilder.append(" [FINER] ");
+        } else if (level == Level.FINE) {
+            stringbuilder.append(" [FINE] ");
+        } else if (level == Level.INFO) {
+            stringbuilder.append(" [INFO] ");
+        } else if (level == Level.WARNING) {
+            stringbuilder.append(" [WARNING] ");
+        } else if (level == Level.SEVERE) {
+            stringbuilder.append(" [SEVERE] ");
+        } else if (level == Level.SEVERE) {
+            stringbuilder.append(" [" + level.getLocalizedName() + "] ");
+        }
+
+        stringbuilder.append(logrecord.getMessage());
+        stringbuilder.append('\n');
+        Throwable throwable = logrecord.getThrown();
+
+        if (throwable != null) {
+            StringWriter stringwriter = new StringWriter();
+
+            throwable.printStackTrace(new PrintWriter(stringwriter));
+            stringbuilder.append(stringwriter.toString());
+        }
+
+        return stringbuilder.toString();
+    }
+}
