fdbd4efbd6270ba20eaa44460ad5b11c645bfa9b
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-07-01 05:02:44 -0500

    Treat ComplexParts as proxies for setLastDamageCause. Fixes BUKKIT-1821
    By: feildmaster <admin@feildmaster.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftComplexPart.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftComplexPart.java
@@ -5,6 +5,7 @@ import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.ComplexEntityPart;
 import org.bukkit.entity.ComplexLivingEntity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class CraftComplexPart extends CraftEntity implements ComplexEntityPart {
     public CraftComplexPart(CraftServer server, EntityComplexPart entity) {
@@ -13,6 +14,16 @@ public class CraftComplexPart extends CraftEntity implements ComplexEntityPart {
 
     public ComplexLivingEntity getParent() {
         return (ComplexLivingEntity) getHandle().owner.getBukkitEntity();
+    }
+
+    @Override
+    public void setLastDamageCause(EntityDamageEvent cause) {
+        getParent().setLastDamageCause(cause);
+    }
+
+    @Override
+    public EntityDamageEvent getLastDamageCause() {
+        return getParent().getLastDamageCause();
     }
 
     @Override
