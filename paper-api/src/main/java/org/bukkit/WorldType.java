4b4cf7e5fb07f9851350290130a9a797223f75bb
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-08-02 08:55:11 -0500

    Add LargeBiomes WorldType.
    By: feildmaster <admin@feildmaster.com>

--- paper-api/src/main/java/org/bukkit/WorldType.java
+++ paper-api/src/main/java/org/bukkit/WorldType.java
@@ -9,7 +9,8 @@ import java.util.Map;
 public enum WorldType {
     NORMAL("DEFAULT"),
     FLAT("FLAT"),
-    VERSION_1_1("DEFAULT_1_1");
+    VERSION_1_1("DEFAULT_1_1"),
+    LARGE_BIOMES("LARGEBIOMES");
 
     private final static Map<String, WorldType> BY_NAME = Maps.newHashMap();
     private final String name;
