7383d52df77284ed85c98a230b87db950b389603
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-08-03 06:24:55 -0500

    Implement new AsyncPlayerChatEvent. Addresses BUKKIT-2064
    Added two utility collections for use with PlayerChatEvents allowing lazier
    initialization of events and less need to synchronize against the player
    list.
    
    Provided a hidden queue system for similar logic to pre-1.3 chat. When a
    plugin is listening for the deprecated PlayerChatEvent, all chat will be
    delayed to be mirror executed from the main thread. All developers are
    encouraged to immediately update to the developmental Bukkit chat API as a
    minimum transition for server stability.
    
    Additionally, changes were required to bring thread-safety to the flow
    logic. CopyOnWriteArrayList is the only viable means to produce thread
    safety with minimal diff; using a sane pre-implemented collection would
    require reworking of sections of NMS logic.
    
    As a minor change, implemented expected functionality for
    PlayerCommandPreProcessEvent. Setting the player should now change the
    player executing the command.
    
    By: Wesley Wolfe <weswolf@aol.com>

---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/util/LazyPlayerSet.java
@@ -0,0 +1,25 @@
+package org.bukkit.craftbukkit.util;
+
+import java.util.HashSet;
+import java.util.List;
+import net.minecraft.server.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+
+import org.bukkit.entity.Player;
+
+public class LazyPlayerSet extends LazyHashSet<Player> {
+
+    @Override
+    HashSet<Player> makeReference() {
+        if (reference != null) {
+            throw new IllegalStateException("Reference already created!");
+        }
+        List<EntityPlayer> players = MinecraftServer.getServer().getServerConfigurationManager().players;
+        HashSet<Player> reference = new HashSet<Player>(players.size());
+        for (EntityPlayer player : players) {
+            reference.add(player.getBukkitEntity());
+        }
+        return reference;
+    }
+
+}
