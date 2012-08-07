3e3b6ad8261f06006ab40c13a59f3f6bca25ec45
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-08-07 01:55:48 -0500

    Purge outdated biomes. Fixes BUKKIT-1087
    By: Wesley Wolfe <weswolf@aol.com>

--- paper-api/src/main/java/org/bukkit/block/Biome.java
+++ paper-api/src/main/java/org/bukkit/block/Biome.java
@@ -4,17 +4,11 @@ package org.bukkit.block;
  * Holds all accepted Biomes in the default server
  */
 public enum Biome {
-    RAINFOREST,
     SWAMPLAND,
-    SEASONAL_FOREST,
     FOREST,
-    SAVANNA,
-    SHRUBLAND,
     TAIGA,
     DESERT,
     PLAINS,
-    ICE_DESERT,
-    TUNDRA,
     HELL,
     SKY,
     OCEAN,
