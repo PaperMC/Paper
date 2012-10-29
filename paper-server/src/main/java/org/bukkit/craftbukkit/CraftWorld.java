28e9f1ac43b5c1218a3f6c742391fde131981a6b
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-10-29 12:38:34 -0500

    Get skull data before destroying block. Fixes BUKKIT-2723
    Skull blocks store their type in a tile entity and use their block data
    as rotation. When breaking a block the block data is used for determining
    what item to drop. Simply changing this to use the skull method for getting
    their drop data is not enough because their tile entity is already gone.
    Therefore we have to special case skulls to get the correct data _and_ get
    that data before breaking the block.
    
    By: Travis Watkins <amaranth@ubuntu.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
@@ -1083,7 +1083,11 @@ public class CraftWorld implements World {
         int blockY = block.getY();
         int blockZ = block.getZ();
         // following code is lifted from Explosion.a(boolean), and modified
-        net.minecraft.server.Block.byId[blockId].dropNaturally(this.world, blockX, blockY, blockZ, block.getData(), yield, 0);
+        int data = block.getData();
+        if (blockId == net.minecraft.server.Block.SKULL.id) {
+            data = net.minecraft.server.Block.SKULL.getDropData(this.world, blockX, blockY, blockZ);
+        }
+        net.minecraft.server.Block.byId[blockId].dropNaturally(this.world, blockX, blockY, blockZ, data, yield, 0);
         block.setType(org.bukkit.Material.AIR);
         // not sure what this does, seems to have something to do with the 'base' material of a block.
         // For example, WOODEN_STAIRS does something with WOOD in this method
