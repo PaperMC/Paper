38f6b24c1caae78d0eef297e2db134aec04644b7
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-10-31 06:35:57 -0500

    Add BlockCommandSender for Command block
    By: Travis Watkins <amaranth@ubuntu.com>

---
+++ paper-api/src/main/java/org/bukkit/command/BlockCommandSender.java
@@ -0,0 +1,12 @@
+package org.bukkit.command;
+
+import org.bukkit.block.Block;
+
+public interface BlockCommandSender extends CommandSender {
+    /**
+     * Returns the block this command sender belongs to
+     *
+     * @return Block for the command sender
+     */
+    public Block getBlock();
+}
