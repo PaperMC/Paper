7f06ccfa0e20f09bbaaba8f967f31f6b2144d1d6
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-11-30 02:49:19 -0600

    Load chunks asynchronously for players.
    When a player triggers a chunk load via walking around or teleporting there
    is no need to stop everything and get this chunk on the main thread. The
    client is used to having to wait some time for this chunk and the server
    doesn't immediately do anything with it except send it to the player. At
    the same time chunk loading is the last major source of file IO that still
    runs on the main thread.
    
    These two facts make it possible to offload chunks loaded for this reason
    to another thread. However, not all parts of chunk loading can happen off
    the main thread. For this we use the new AsynchronousExecutor system to
    split chunk loading in to three pieces. The first is loading data from
    disk, decompressing it, and parsing it in to an NBT structure.  The second
    piece is creating entities and tile entities in the chunk and adding them
    to the world, this is still done on the main thread. The third piece is
    informing everyone who requested a chunk load that the load is finished.
    For this we register callbacks and then run them on the main thread once
    the previous two stages are finished.
    
    There are still cases where a chunk is needed immediately and these will
    still trigger chunk loading entirely on the main thread. The most obvious
    case is plugins using the API to request a chunk load. We also must load
    the chunk immediately when something in the world tries to access it. In
    these cases we ignore any possibly pending or in progress chunk loading
    that is happening asynchronously as we will have the chunk loaded by the
    time they are finished.
    
    The hope is that overall this system will result in less CPU time and
    pauses due to blocking file IO on the main thread thus giving more
    consistent performance. Testing so far has shown that this also speeds up
    chunk loading client side although some of this is likely to be because
    we are sending less chunks at once for the client to process.
    
    Thanks for @ammaraskar for help with the implementation of this feature.
    
    By: Travis Watkins <amaranth@ubuntu.com>

---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/chunkio/QueuedChunk.java
@@ -0,0 +1,36 @@
+package org.bukkit.craftbukkit.chunkio;
+
+import net.minecraft.server.ChunkProviderServer;
+import net.minecraft.server.ChunkRegionLoader;
+import net.minecraft.server.NBTTagCompound;
+import net.minecraft.server.World;
+
+class QueuedChunk {
+    long coords;
+    ChunkRegionLoader loader;
+    World world;
+    ChunkProviderServer provider;
+    NBTTagCompound compound;
+
+    public QueuedChunk(long coords, ChunkRegionLoader loader, World world, ChunkProviderServer provider) {
+        this.coords = coords;
+        this.loader = loader;
+        this.world = world;
+        this.provider = provider;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) coords ^ world.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof QueuedChunk) {
+            QueuedChunk other = (QueuedChunk) object;
+            return coords == other.coords && world == other.world;
+        }
+
+        return false;
+    }
+}
