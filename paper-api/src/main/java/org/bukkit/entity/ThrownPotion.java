d347286b224196e0e66af4ddf62b6280e154270f
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-12-17 09:45:56 +0100

    Add ability to modify ThrownPotion properties. Adds BUKKIT-3197
    By: Olof Larsson <olof@sylt.nu>

--- paper-api/src/main/java/org/bukkit/entity/ThrownPotion.java
+++ paper-api/src/main/java/org/bukkit/entity/ThrownPotion.java
@@ -2,6 +2,7 @@ package org.bukkit.entity;
 
 import java.util.Collection;
 
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
 /**
@@ -13,4 +14,24 @@ public interface ThrownPotion extends Projectile {
      * @return The potion effects
      */
     public Collection<PotionEffect> getEffects();
+
+    /**
+     * Returns a copy of the ItemStack for this thrown potion.
+     * <p>
+     * Altering this copy will not alter the thrown potion directly.
+     * If you want to alter the thrown potion, you must use the
+     * {@link #setItemStack(ItemStack) setItemStack} method.
+     *
+     * @return A copy of the ItemStack for this thrown potion.
+     */
+    public ItemStack getItem();
+
+    /**
+     * Set the ItemStack for this thrown potion.
+     * <p>
+     * The ItemStack must be a potion, otherwise an exception is thrown.
+     *
+     * @param item New ItemStack
+     */
+    public void setItem(ItemStack item);
 }
