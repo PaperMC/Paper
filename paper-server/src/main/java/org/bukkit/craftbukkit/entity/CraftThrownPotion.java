d9c6b907168702048e7ac819d1dc5d57c850bc13
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-12-17 09:45:30 +0100

    Add ability to modify ThrownPotion properties. Adds BUKKIT-3197
    By: Olof Larsson <olof@sylt.nu>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftThrownPotion.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftThrownPotion.java
@@ -4,26 +4,42 @@ import java.util.Collection;
 
 import net.minecraft.server.EntityPotion;
 
+import org.apache.commons.lang.Validate;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ThrownPotion;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.Potion;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionEffect;
 
 public class CraftThrownPotion extends CraftProjectile implements ThrownPotion {
-    private Collection<PotionEffect> effects = null;
-
     public CraftThrownPotion(CraftServer server, EntityPotion entity) {
         super(server, entity);
     }
 
+    // TODO: This one does not handle custom NBT potion effects does it?
+    // In that case this method could be said to be misleading or incorrect
     public Collection<PotionEffect> getEffects() {
-        if (effects == null) {
-            effects = Potion.getBrewer().getEffectsFromDamage(getHandle().getPotionValue());
-        }
+        return Potion.getBrewer().getEffectsFromDamage(getHandle().getPotionValue());
+    }
 
-        return effects;
+    public ItemStack getItem() {
+        // We run this method once since it will set the item stack if there is none.
+        getHandle().getPotionValue();
+
+        return CraftItemStack.asBukkitCopy(getHandle().c);
+    }
+
+    public void setItem(ItemStack item) {
+        // The ItemStack must not be null.
+        Validate.notNull(item, "ItemStack cannot be null.");
+
+        // The ItemStack must be a potion.
+        Validate.isTrue(item.getType() == Material.POTION, "ItemStack must be a potion. This item stack was " + item.getType() + ".");
+
+        getHandle().c = CraftItemStack.asNMSCopy(item);
     }
 
     @Override
