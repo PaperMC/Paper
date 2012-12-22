d21d3199b9b0a78bb3dc1cdc949df5fe694d3a04
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-12-21 23:56:54 -0600

    Implement API to get and set FireworkMeta on Firework entities
    By: feildmaster <admin@feildmaster.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftFirework.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftFirework.java
@@ -1,15 +1,37 @@
 package org.bukkit.craftbukkit.entity;
 
 import net.minecraft.server.EntityFireworks;
+import net.minecraft.server.Item;
+import net.minecraft.server.ItemStack;
 
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public class CraftFirework extends CraftEntity implements Firework {
+    private static final int FIREWORK_ITEM_INDEX = 8;
+
+    private final CraftItemStack item;
 
     public CraftFirework(CraftServer server, EntityFireworks entity) {
         super(server, entity);
+
+        ItemStack item = getHandle().getDataWatcher().f(FIREWORK_ITEM_INDEX);
+
+        if (item == null) {
+            item = new ItemStack(Item.FIREWORKS);
+            getHandle().getDataWatcher().watch(FIREWORK_ITEM_INDEX, item);
+        }
+
+        this.item = CraftItemStack.asCraftMirror(item);
+
+        // Ensure the item is a firework...
+        if (this.item.getType() != Material.FIREWORK) {
+            this.item.setType(Material.FIREWORK);
+        }
     }
 
     @Override
@@ -24,5 +46,13 @@ public class CraftFirework extends CraftEntity implements Firework {
 
     public EntityType getType() {
         return EntityType.FIREWORK;
+    }
+
+    public FireworkMeta getFireworkMeta() {
+        return (FireworkMeta) item.getItemMeta();
+    }
+
+    public void setFireworkMeta(FireworkMeta meta) {
+        item.setItemMeta(meta);
     }
 }
