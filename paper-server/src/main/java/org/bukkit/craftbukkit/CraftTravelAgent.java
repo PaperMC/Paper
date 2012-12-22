a7602dc99357743352787748e1f63eeffc082db1
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2012-12-22 11:46:24 -0700

    [Bleeding] Add experimental support for entity portal traveling
    By: EdGruberman <ed@rjump.com>

---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftTravelAgent.java
@@ -0,0 +1,74 @@
+package org.bukkit.craftbukkit;
+
+import net.minecraft.server.ChunkCoordinates;
+import net.minecraft.server.PortalTravelAgent;
+import net.minecraft.server.WorldServer;
+
+import org.bukkit.Location;
+import org.bukkit.TravelAgent;
+
+public class CraftTravelAgent extends PortalTravelAgent implements TravelAgent {
+
+    private int searchRadius = 128;
+    private int creationRadius = 16;
+    private boolean canCreatePortal = true;
+
+    public CraftTravelAgent(WorldServer worldserver) {
+        super(worldserver);
+    }
+
+    public Location findOrCreate(Location target) {
+        WorldServer worldServer = ((CraftWorld) target.getWorld()).getHandle();
+        boolean before = worldServer.chunkProviderServer.forceChunkLoad;
+        worldServer.chunkProviderServer.forceChunkLoad = true;
+
+        Location found = this.findPortal(target);
+        if (found == null) {
+            if (this.getCanCreatePortal() && this.createPortal(target)) {
+                found = this.findPortal(target);
+            } else {
+                found = target; // fallback to original if unable to find or create
+            }
+        }
+
+        worldServer.chunkProviderServer.forceChunkLoad = before;
+        return found;
+    }
+
+    public Location findPortal(Location location) {
+        PortalTravelAgent pta = ((CraftWorld) location.getWorld()).getHandle().s();
+        ChunkCoordinates found = pta.findPortal(location.getX(), location.getY(), location.getZ(), this.getSearchRadius());
+        return found != null ? new Location(location.getWorld(), found.x, found.y, found.z, location.getYaw(), location.getPitch()) : null;
+    }
+
+    public boolean createPortal(Location location) {
+        PortalTravelAgent pta = ((CraftWorld) location.getWorld()).getHandle().s();
+        return pta.createPortal(location.getX(), location.getY(), location.getZ(), this.getCreationRadius());
+    }
+
+    public TravelAgent setSearchRadius(int radius) {
+        this.searchRadius = radius;
+        return this;
+    }
+
+    public int getSearchRadius() {
+        return this.searchRadius;
+    }
+
+    public TravelAgent setCreationRadius(int radius) {
+        this.creationRadius = radius < 2 ? 0 : radius;
+        return this;
+    }
+
+    public int getCreationRadius() {
+        return this.creationRadius;
+    }
+
+    public boolean getCanCreatePortal() {
+        return this.canCreatePortal;
+    }
+
+    public void setCanCreatePortal(boolean create) {
+        this.canCreatePortal = create;
+    }
+}
