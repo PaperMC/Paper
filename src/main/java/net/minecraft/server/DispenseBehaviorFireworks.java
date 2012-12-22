f72a08c22e4c499ab22a42d5c2d6a0e512823bdb
Travis Watkins <amaranth@ubuntu.com>
2012-12-22 12:59:53 -0600

    Fire BlockDispenseEvent for fireworks. Adds BUKKIT-3246

--- src/main/java/net/minecraft/server/DispenseBehaviorFireworks.java
+++ src/main/java/net/minecraft/server/DispenseBehaviorFireworks.java
@@ -1,5 +1,10 @@
 package net.minecraft.server;
 
+// CraftBukkit start
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.event.block.BlockDispenseEvent;
+// CraftBukkit end
+
 public class DispenseBehaviorFireworks extends DispenseBehaviorItem {
 
     final MinecraftServer b;
@@ -13,10 +18,41 @@ public class DispenseBehaviorFireworks extends DispenseBehaviorItem {
         double d0 = isourceblock.getX() + (double) enumfacing.c();
         double d1 = (double) ((float) isourceblock.getBlockY() + 0.2F);
         double d2 = isourceblock.getZ() + (double) enumfacing.e();
-        EntityFireworks entityfireworks = new EntityFireworks(isourceblock.k(), d0, d1, d2, itemstack);
+
+        // CraftBukkit start
+        World world = isourceblock.k();
+        ItemStack itemstack1 = itemstack.a(1);
+        org.bukkit.block.Block block = world.getWorld().getBlockAt(isourceblock.getBlockX(), isourceblock.getBlockY(), isourceblock.getBlockZ());
+        CraftItemStack craftItem = CraftItemStack.asCraftMirror(itemstack1);
+
+        BlockDispenseEvent event = new BlockDispenseEvent(block, craftItem.clone(), new org.bukkit.util.Vector(d0, d1, d2));
+        if (!BlockDispenser.eventFired) {
+            world.getServer().getPluginManager().callEvent(event);
+        }
+
+        if (event.isCancelled()) {
+            itemstack.count++;
+            return itemstack;
+        }
+
+        if (!event.getItem().equals(craftItem)) {
+            itemstack.count++;
+            // Chain to handler for new item
+            ItemStack eventStack = CraftItemStack.asNMSCopy(event.getItem());
+            IDispenseBehavior idispensebehavior = (IDispenseBehavior) BlockDispenser.a.a(eventStack.getItem());
+            if (idispensebehavior != IDispenseBehavior.a && idispensebehavior != this) {
+                idispensebehavior.a(isourceblock, eventStack);
+                return itemstack;
+            }
+        }
+
+        itemstack1 = CraftItemStack.asNMSCopy(event.getItem());
+        EntityFireworks entityfireworks = new EntityFireworks(isourceblock.k(), event.getVelocity().getX(), event.getVelocity().getY(), event.getVelocity().getZ(), itemstack1);
 
         isourceblock.k().addEntity(entityfireworks);
-        itemstack.a(1);
+        // itemstack.a(1); // Handled during event processing
+        // CraftBukkit end
+
         return itemstack;
     }
 
