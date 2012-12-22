673aa7144b4b2a49e36e9e0cbbc5881a232ab9e6
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2012-12-21 23:55:02 -0600

    Add firework api to get and set Firework ItemMeta
    By: feildmaster <admin@feildmaster.com>

--- paper-api/src/main/java/org/bukkit/entity/Firework.java
+++ paper-api/src/main/java/org/bukkit/entity/Firework.java
@@ -1,5 +1,19 @@
 package org.bukkit.entity;
 
-public interface Firework extends Entity {
+import org.bukkit.inventory.meta.FireworkMeta;
 
+public interface Firework extends Entity {
+    /**
+     * Get a copy of the fireworks meta
+     *
+     * @return A copy of the current Firework meta
+     */
+    FireworkMeta getFireworkMeta();
+
+    /**
+     * Apply the provided meta to the fireworks
+     *
+     * @param meta The FireworkMeta to apply
+     */
+    void setFireworkMeta(FireworkMeta meta);
 }
