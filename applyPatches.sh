efa13af2836a23eac5bcce64f6ae3a058a9259d3
Aikar <aikar@aikar.co>
2013-01-19 14:04:56 -0500

    Make apply patches target HEAD of upstream and squelch some fluff on applying.

--- applyPatches.sh
+++ applyPatches.sh
@@ -6,17 +6,20 @@ echo "Rebuilding Forked projects.... "
 function applyPatch {
     what=$1
     target=$2
+    cd $basedir/$what
+    git branch -f upstream >/dev/null
+
     cd $basedir
     if [ ! -d  "$basedir/$target" ]; then
-        git clone $1 $target
+        git clone $1 $target -b upstream
     fi
     cd "$basedir/$target"
     echo "Resetting $target to $what..."
-    git remote rm upstream 2>/dev/null
-    git remote add upstream ../$what
-    git checkout master
-    git fetch upstream
-    git reset --hard upstream/master
+    git remote rm upstream 2>/dev/null 2>&1
+    git remote add upstream ../$what >/dev/null 2>&1
+    git checkout master >/dev/null 2>&1
+    git fetch upstream >/dev/null 2>&1
+    git reset --hard upstream/upstream
     echo "  Applying patches to $target..."
     git am --3way $basedir/${what}-Patches/*.patch
     if [ "$?" != "0" ]; then
