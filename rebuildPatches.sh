41b7c5f46532943172c8949d1f9988a03b4a167f
Aikar <aikar@aikar.co>
2013-01-21 21:46:26 -0500

    Update rebuildPatches to ignore git version string.

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -5,8 +5,16 @@ echo "Rebuilding patch files from current fork state..."
 function cleanupPatches {
     cd $1
     for patch in *.patch; do
-        lines=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)" | wc -l)
-        if [ "$lines" == "0" ] ; then
+        gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
+        diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
+
+        testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
+        if [ "x$testver" != "x" ]; then
+            diffs=$(echo "$diffs" | head -n -2)
+        fi
+        
+
+        if [ "x$diffs" == "x" ] ; then
             git reset HEAD $patch >/dev/null
             git checkout -- $patch >/dev/null
         fi
