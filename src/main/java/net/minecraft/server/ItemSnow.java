d47849df8c4bee23ac85897a41537142ab81ab7b
Travis Watkins <amaranth@ubuntu.com>
2013-03-16 07:38:54 -0500

    Remove duplicate place logic for snow. Fixes BUKKIT-3756

--- src/main/java/net/minecraft/server/ItemSnow.java
+++ src/main/java/net/minecraft/server/ItemSnow.java
@@ -20,14 +20,19 @@ public class ItemSnow extends ItemBlockWithAuxData {
                 int j1 = world.getData(i, j, k);
                 int k1 = j1 & 7;
 
-                // CraftBukkit start
-                // if (k1 <= 6 && world.b(block.b(world, i, j, k)) && world.setData(i, j, k, k1 + 1 | j1 & -8, 2)) {
-                if (k1 <= 6 && world.b(block.b(world, i, j, k)) && processBlockPlace(world, entityhuman, itemstack, i, j, k, Block.SNOW.id, k1 + 1 | j1 & -8, clickedX, clickedY, clickedZ)) {
+                // CraftBukkit start - redirect to common handler
+                if (k1 <= 6 && world.b(block.b(world, i, j, k)) && ItemBlock.processBlockPlace(world, entityhuman, itemstack, i, j, k, Block.SNOW.id, k1 + 1 | j1 & -8, clickedX, clickedY, clickedZ)) {
+                    return true;
+                }
+                /*
+                if (k1 <= 6 && world.b(block.b(world, i, j, k)) && world.setData(i, j, k, k1 + 1 | j1 & -8, 2)) {
                     // CraftBukkit end
                     world.makeSound((double) ((float) i + 0.5F), (double) ((float) j + 0.5F), (double) ((float) k + 0.5F), block.stepSound.getPlaceSound(), (block.stepSound.getVolume1() + 1.0F) / 2.0F, block.stepSound.getVolume2() * 0.8F);
                     --itemstack.count;
                     return true;
                 }
+                */
+                // CraftBukkit end
             }
 
             return super.interactWith(itemstack, entityhuman, world, i, j, k, l, f, f1, f2);
