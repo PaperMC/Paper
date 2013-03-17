5cf4c1bb6c02dde3ede17bd19594a9ba555b9a47
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2013-03-17 15:52:23 -0400

    Make HopperMinecart implement InventoryHolder. Adds BUKKIT-3796
    By: Michael Limiero <mike5713@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/minecart/HopperMinecart.java
+++ paper-api/src/main/java/org/bukkit/entity/minecart/HopperMinecart.java
@@ -1,9 +1,10 @@
 package org.bukkit.entity.minecart;
 
 import org.bukkit.entity.Minecart;
+import org.bukkit.inventory.InventoryHolder;
 
 /**
  * Represents a Minecart with a Hopper inside it
  */
-public interface HopperMinecart extends Minecart {
+public interface HopperMinecart extends Minecart, InventoryHolder {
 }
