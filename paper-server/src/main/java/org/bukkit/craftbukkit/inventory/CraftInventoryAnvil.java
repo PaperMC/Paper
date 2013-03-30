cd79fecc90c3872010e827ce3f3985d211420908
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2013-03-30 02:51:26 -0500

    Include anvil result in inventory size. Fixes BUKKIT-3741
    By: Travis Watkins <amaranth@ubuntu.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryAnvil.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryAnvil.java
@@ -4,7 +4,23 @@ import net.minecraft.server.IInventory;
 import org.bukkit.inventory.AnvilInventory;
 
 public class CraftInventoryAnvil extends CraftInventory implements AnvilInventory {
-    public CraftInventoryAnvil(IInventory anvil) {
-        super(anvil);
+    private final IInventory resultInventory;
+
+    public CraftInventoryAnvil(IInventory inventory, IInventory resultInventory) {
+        super(inventory);
+        this.resultInventory = resultInventory;
+    }
+
+    public IInventory getResultInventory() {
+        return resultInventory;
+    }
+
+    public IInventory getIngredientsInventory() {
+        return inventory;
+    }
+
+    @Override
+    public int getSize() {
+        return getResultInventory().getSize() + getIngredientsInventory().getSize();
     }
 }
