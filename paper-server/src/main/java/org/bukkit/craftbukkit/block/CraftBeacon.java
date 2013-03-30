504a6af6e824e81858c5265893538f93403a558f
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2013-03-29 22:26:27 -0500

    Add Beacon block state for hopper events. Fixes BUKKIT-3932
    By: Travis Watkins <amaranth@ubuntu.com>

---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftBeacon.java
@@ -0,0 +1,37 @@
+package org.bukkit.craftbukkit.block;
+
+import net.minecraft.server.TileEntityBeacon;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.Beacon;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.inventory.CraftInventory;
+import org.bukkit.inventory.Inventory;
+
+public class CraftBeacon extends CraftBlockState implements Beacon {
+    private final CraftWorld world;
+    private final TileEntityBeacon beacon;
+
+    public CraftBeacon(final Block block) {
+        super(block);
+
+        world = (CraftWorld) block.getWorld();
+        beacon = (TileEntityBeacon) world.getTileEntityAt(getX(), getY(), getZ());
+    }
+
+    public Inventory getInventory() {
+        return new CraftInventory(beacon);
+    }
+
+    @Override
+    public boolean update(boolean force) {
+        boolean result = super.update(force);
+
+        if (result) {
+            beacon.update();
+        }
+
+        return result;
+    }
+}
+
