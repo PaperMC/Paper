5c9c366ee5775c8714a3a384dc0f3d87c4f133e8
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2013-04-06 12:00:31 -0400

    Make AnvilInventory.getItem() use both containers. Fixes BUKKIT-2788
    The AnvilInventory reports its size as the sum of the ingredient and
    result inventories, but when trying to access the slots, only the
    ingredient inventory is used, leading to an ArrayIndexOutOfBounds exception.
    
    This change overrides getItem(I) and setItem(I) to use both inventories,
    with the slot number adjusted based on their size.
    
    By: Andre LeBlanc <andre@norcode.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryAnvil.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryAnvil.java
@@ -1,7 +1,9 @@
 package org.bukkit.craftbukkit.inventory;
 
 import net.minecraft.server.IInventory;
+
 import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.ItemStack;
 
 public class CraftInventoryAnvil extends CraftInventory implements AnvilInventory {
     private final IInventory resultInventory;
@@ -17,6 +19,26 @@ public class CraftInventoryAnvil extends CraftInventory implements AnvilInventor
 
     public IInventory getIngredientsInventory() {
         return inventory;
+    }
+
+    @Override
+    public ItemStack getItem(int slot) {
+        if (slot < getIngredientsInventory().getSize()) {
+            net.minecraft.server.ItemStack item = getIngredientsInventory().getItem(slot);
+            return item == null ? null : CraftItemStack.asCraftMirror(item);
+        } else {
+            net.minecraft.server.ItemStack item = getResultInventory().getItem(slot - getIngredientsInventory().getSize());
+            return item == null ? null : CraftItemStack.asCraftMirror(item);
+        }
+    }
+
+    @Override
+    public void setItem(int index, ItemStack item) {
+        if (index < getIngredientsInventory().getSize()) {
+            getIngredientsInventory().setItem(index, (item == null ? null : CraftItemStack.asNMSCopy(item)));
+        } else {
+            getResultInventory().setItem((index - getIngredientsInventory().getSize()), (item == null ? null : CraftItemStack.asNMSCopy(item)));
+        }
     }
 
     @Override
