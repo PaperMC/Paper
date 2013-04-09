606b5ba706ee078543dd7111c38d6b1895fa21f3
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2013-04-09 23:55:57 +0200

    Fix off-by-one error in DyeColor. Fixes BUKKIT-3938
    By: Acrobot <andrzejp@dysleksja.pl>

--- paper-api/src/main/java/org/bukkit/DyeColor.java
+++ paper-api/src/main/java/org/bukkit/DyeColor.java
@@ -164,7 +164,7 @@ public enum DyeColor {
      */
     public static DyeColor getByWoolData(final byte data) {
         int i = 0xff & data;
-        if (i > BY_WOOL_DATA.length) {
+        if (i >= BY_WOOL_DATA.length) {
             return null;
         }
         return BY_WOOL_DATA[i];
@@ -179,7 +179,7 @@ public enum DyeColor {
      */
     public static DyeColor getByDyeData(final byte data) {
         int i = 0xff & data;
-        if (i > BY_DYE_DATA.length) {
+        if (i >= BY_DYE_DATA.length) {
             return null;
         }
         return BY_DYE_DATA[i];
