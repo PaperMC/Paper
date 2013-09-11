6e0bd25f527b929f80d8212fe5d0b7287bdcb42b
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2013-09-10 21:19:59 -0400

    Add SpawnReason for Entity Reinforcements. Fixes BUKKIT-4744
    By: EvilSeph <evilseph@gmail.com>

--- paper-api/src/main/java/org/bukkit/event/entity/CreatureSpawnEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/CreatureSpawnEvent.java
@@ -148,6 +148,10 @@ public class CreatureSpawnEvent extends EntityEvent implements Cancellable {
          */
         SLIME_SPLIT,
         /**
+         * When an entity calls for reinforcements
+         */
+        REINFORCEMENTS,
+        /**
          * When a creature is spawned by plugins
          */
         CUSTOM,
