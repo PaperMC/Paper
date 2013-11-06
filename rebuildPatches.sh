5b808a6d450625abecfd718e1e4a6ac7007a24cd
mrkirby153 <mr.austinwhyte@gmail.com>
2013-11-05 16:26:20 -0800

    Batch files now work in directories with spaces in it
    Before the batch files could not run if the path to the directory had one
    or more spaces in it. EG: "C:/Desktop/Spigot Repo". Now works with the
    patch

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -3,7 +3,7 @@
 basedir=`pwd`
 echo "Rebuilding patch files from current fork state..."
 function cleanupPatches {
-    cd $1
+    cd "$1"
     for patch in *.patch; do
         gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
         diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
@@ -23,11 +23,11 @@ function cleanupPatches {
 function savePatches {
     what=$1
     target=$2
-    cd $basedir/$target/
-    git format-patch --no-stat -N -o $basedir/${what}-Patches/ upstream/upstream
-    cd $basedir
-    git add $basedir/${what}-Patches
-    cleanupPatches $basedir/${what}-Patches
+    cd "$basedir/$target"
+    git format-patch --no-stat -N -o "$basedir/${what}-Patches/" upstream/upstream
+    cd "$basedir"
+    git add "$basedir/${what}-Patches"
+    cleanupPatches "$basedir/${what}-Patches"
     echo "  Patches saved for $what to $what-Patches/"
 }
 
