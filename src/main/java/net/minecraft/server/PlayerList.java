9969a86e839560f6fe4cfbfefa1c5d4f47df0a54
Aikar <aikar@aikar.co>
2013-12-02 20:09:22 -0500

    Use strict lookup for op permission refresh. Fixes BUKKIT-4698
    Adding or removing operators was mistakenly using a loose player lookup
    method, which would cause a permission refreshes on an online player whos
    name starts with the name of the (offline) opped player.
    
    Add/Remove op operations are exact name match only and the permission
    refresh will behave the same way.

--- src/main/java/net/minecraft/server/PlayerList.java
+++ src/main/java/net/minecraft/server/PlayerList.java
@@ -819,7 +819,7 @@ public abstract class PlayerList {
         this.operators.add(s.toLowerCase());
 
         // CraftBukkit start
-        Player player = server.server.getPlayer(s);
+        Player player = server.server.getPlayerExact(s);
         if (player != null) {
             player.recalculatePermissions();
         }
@@ -830,7 +830,7 @@ public abstract class PlayerList {
         this.operators.remove(s.toLowerCase());
 
         // CraftBukkit start
-        Player player = server.server.getPlayer(s);
+        Player player = server.server.getPlayerExact(s);
         if (player != null) {
             player.recalculatePermissions();
         }
