dd12820c58871c6e05b6a8cbe9197ac64c6a00a3
t00thpick1 <t00thpick1dirko@gmail.com>
2014-01-11 15:48:46 -0500

    Add StatisticManager.java for diff visibility.

---
+++ src/main/java/net/minecraft/server/StatisticManager.java
@@ -0,0 +1,61 @@
+package net.minecraft.server;
+ 
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class StatisticManager {
+
+    protected final Map a = new HashMap();
+
+    public StatisticManager() {
+    }
+
+    public boolean a(Achievement achievement) {
+        return this.a((Statistic) achievement) > 0;
+    }
+
+    public boolean b(Achievement achievement) {
+        return achievement.c == null || this.a(achievement.c);
+    }
+
+    public void b(EntityHuman entityhuman, Statistic statistic, int i) {
+        if (!statistic.d() || this.b((Achievement) statistic)) {
+            this.a(entityhuman, statistic, this.a(statistic) + i);
+        }
+    }
+
+    public void a(EntityHuman entityhuman, Statistic statistic, int i) {
+        StatisticWrapper statisticwrapper = (StatisticWrapper) this.a.get(statistic);
+
+        if (statisticwrapper == null) {
+            statisticwrapper = new StatisticWrapper();
+            this.a.put(statistic, statisticwrapper);
+        }
+
+        statisticwrapper.a(i);
+    }
+
+    public int a(Statistic statistic) {
+        StatisticWrapper statisticwrapper = (StatisticWrapper) this.a.get(statistic);
+
+        return statisticwrapper == null ? 0 : statisticwrapper.a();
+    }
+
+    public IJsonStatistic b(Statistic statistic) {
+        StatisticWrapper statisticwrapper = (StatisticWrapper) this.a.get(statistic);
+
+        return statisticwrapper != null ? statisticwrapper.b() : null;
+    }
+
+    public IJsonStatistic a(Statistic statistic, IJsonStatistic ijsonstatistic) {
+        StatisticWrapper statisticwrapper = (StatisticWrapper) this.a.get(statistic);
+
+        if (statisticwrapper == null) {
+            statisticwrapper = new StatisticWrapper();
+            this.a.put(statistic, statisticwrapper);
+        }
+
+        statisticwrapper.a(ijsonstatistic);
+        return ijsonstatistic;
+    }
+}
