31fccf6d8edeeb3e5f3ad87cd4a82cd51fdcfcd7
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2014-01-14 22:38:03 -0600

    Implement UnsafeValues for give command. Addresses BUKKIT-5286
    By: Wesley Wolfe <weswolf@aol.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/util/CraftMagicNumbers.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/util/CraftMagicNumbers.java
@@ -1,11 +1,23 @@
 package org.bukkit.craftbukkit.util;
 
+import java.util.List;
+
 import net.minecraft.server.Block;
 import net.minecraft.server.Blocks;
 import net.minecraft.server.Item;
-import org.bukkit.Material;
+import net.minecraft.server.MojangsonParser;
+import net.minecraft.server.NBTTagCompound;
 
-public final class CraftMagicNumbers {
+import org.bukkit.Material;
+import org.bukkit.UnsafeValues;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.StringUtil;
+
+@SuppressWarnings("deprecation")
+public final class CraftMagicNumbers implements UnsafeValues {
+    public static final UnsafeValues INSTANCE = new CraftMagicNumbers();
+
     private CraftMagicNumbers() {}
 
     public static Block getBlock(org.bukkit.block.Block block) {
@@ -66,5 +78,26 @@ public final class CraftMagicNumbers {
         }
 
         return block;
+    }
+
+    @Override
+    public Material getMaterialFromInternalName(String name) {
+        return getMaterial((Item) Item.REGISTRY.a(name));
+    }
+
+    @Override
+    public List<String> tabCompleteInternalMaterialName(String token, List<String> completions) {
+        return StringUtil.copyPartialMatches(token, Item.REGISTRY.b(), completions);
+    }
+
+    @Override
+    public ItemStack modifyItemStack(ItemStack stack, String arguments) {
+        net.minecraft.server.ItemStack nmsStack = CraftItemStack.asNMSCopy(stack);
+
+        nmsStack.setTag((NBTTagCompound) MojangsonParser.a(arguments));
+
+        stack.setItemMeta(CraftItemStack.getItemMeta(nmsStack));
+
+        return stack;
     }
 }
