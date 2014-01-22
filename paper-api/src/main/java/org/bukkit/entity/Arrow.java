b3c27cf60d7aa73aaf044f88db14647137e8c771
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2014-01-21 22:33:49 -0500

    Add setCritical and isCritical methods to Arrow.java. Adds BUKKIT-5113
    By: MorphanOne <morphanone@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Arrow.java
+++ paper-api/src/main/java/org/bukkit/entity/Arrow.java
@@ -20,4 +20,23 @@ public interface Arrow extends Projectile {
      * @param knockbackStrength the knockback strength value
      */
     public void setKnockbackStrength(int knockbackStrength);
+
+    /**
+     * Gets whether this arrow is critical.
+     * <p>
+     * Critical arrows have increased damage and cause particle effects.
+     * <p>
+     * Critical arrows generally occur when a player fully draws a bow before
+     * firing.
+     *
+     * @return true if it is critical
+     */
+    public boolean isCritical();
+
+    /**
+     * Sets whether or not this arrow should be critical.
+     *
+     * @param critical whether or not it should be critical
+     */
+    public void setCritical(boolean critical);
 }
