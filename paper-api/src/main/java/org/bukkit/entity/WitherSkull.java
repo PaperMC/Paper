05119d68ce9dde16d4636a035c5fca65f13ced2f
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2014-01-26 20:08:28 -0500

    [Bleeding] Add setCharged and getCharged to WitherSkull. Adds BUKKIT-3060
    By: t00thpick1 <t00thpick1dirko@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/WitherSkull.java
+++ paper-api/src/main/java/org/bukkit/entity/WitherSkull.java
@@ -1,8 +1,21 @@
 package org.bukkit.entity;
 
 /**
- * Represents a wither skull {@link Fireball}
+ * Represents a wither skull {@link Fireball}.
  */
 public interface WitherSkull extends Fireball {
 
+    /**
+     * Sets the charged status of the wither skull.
+     *
+     * @param charged whether it should be charged
+     */
+    public void setCharged(boolean charged);
+
+    /**
+     * Gets whether or not the wither skull is charged.
+     *
+     * @return whether the wither skull is charged
+     */
+    public boolean isCharged();
 }
