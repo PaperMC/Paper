b6831792ec0ee830ac88bbb51dc4d9d3f0bae3a3
GJ <gjmcferrin@gmail.com>
2014-02-07 13:27:47 -0500

    Add files from mc-dev for diff visibility.

---
+++ src/main/java/net/minecraft/server/BlockMonsterEggs.java
@@ -0,0 +1,119 @@
+package net.minecraft.server;
+
+import java.util.Random;
+
+import net.minecraft.util.org.apache.commons.lang3.tuple.ImmutablePair;
+
+public class BlockMonsterEggs extends Block {
+
+    public static final String[] a = new String[] { "stone", "cobble", "brick", "mossybrick", "crackedbrick", "chiseledbrick"};
+
+    public BlockMonsterEggs() {
+        super(Material.CLAY);
+        this.c(0.0F);
+        this.a(CreativeModeTab.c);
+    }
+
+    public void postBreak(World world, int i, int j, int k, int l) {
+        if (!world.isStatic) {
+            EntitySilverfish entitysilverfish = new EntitySilverfish(world);
+
+            entitysilverfish.setPositionRotation((double) i + 0.5D, (double) j, (double) k + 0.5D, 0.0F, 0.0F);
+            world.addEntity(entitysilverfish);
+            entitysilverfish.s();
+        }
+
+        super.postBreak(world, i, j, k, l);
+    }
+
+    public int a(Random random) {
+        return 0;
+    }
+
+    public static boolean a(Block block) {
+        return block == Blocks.STONE || block == Blocks.COBBLESTONE || block == Blocks.SMOOTH_BRICK;
+    }
+
+    public static int a(Block block, int i) {
+        if (i == 0) {
+            if (block == Blocks.COBBLESTONE) {
+                return 1;
+            }
+
+            if (block == Blocks.SMOOTH_BRICK) {
+                return 2;
+            }
+        } else if (block == Blocks.SMOOTH_BRICK) {
+            switch (i) {
+                case 1:
+                    return 3;
+
+                case 2:
+                    return 4;
+
+                case 3:
+                    return 5;
+            }
+        }
+
+        return 0;
+    }
+
+    public static ImmutablePair b(int i) {
+        switch (i) {
+            case 1:
+                return new ImmutablePair(Blocks.COBBLESTONE, Integer.valueOf(0));
+
+            case 2:
+                return new ImmutablePair(Blocks.SMOOTH_BRICK, Integer.valueOf(0));
+
+            case 3:
+                return new ImmutablePair(Blocks.SMOOTH_BRICK, Integer.valueOf(1));
+
+            case 4:
+                return new ImmutablePair(Blocks.SMOOTH_BRICK, Integer.valueOf(2));
+
+            case 5:
+                return new ImmutablePair(Blocks.SMOOTH_BRICK, Integer.valueOf(3));
+
+            default:
+                return new ImmutablePair(Blocks.STONE, Integer.valueOf(0));
+        }
+    }
+
+    protected ItemStack j(int i) {
+        switch (i) {
+            case 1:
+                return new ItemStack(Blocks.COBBLESTONE);
+
+            case 2:
+                return new ItemStack(Blocks.SMOOTH_BRICK);
+
+            case 3:
+                return new ItemStack(Blocks.SMOOTH_BRICK, 1, 1);
+
+            case 4:
+                return new ItemStack(Blocks.SMOOTH_BRICK, 1, 2);
+
+            case 5:
+                return new ItemStack(Blocks.SMOOTH_BRICK, 1, 3);
+
+            default:
+                return new ItemStack(Blocks.STONE);
+        }
+    }
+
+    public void dropNaturally(World world, int i, int j, int k, int l, float f, int i1) {
+        if (!world.isStatic) {
+            EntitySilverfish entitysilverfish = new EntitySilverfish(world);
+
+            entitysilverfish.setPositionRotation((double) i + 0.5D, (double) j, (double) k + 0.5D, 0.0F, 0.0F);
+            world.addEntity(entitysilverfish);
+            entitysilverfish.s();
+        }
+    }
+
+    public int getDropData(World world, int i, int j, int k) {
+        return world.getData(i, j, k);
+    }
+}
---
+++ src/main/java/net/minecraft/server/WorldGenWitchHut.java
@@ -0,0 +1,87 @@
+package net.minecraft.server;
+
+import java.util.Random;
+
+public class WorldGenWitchHut extends WorldGenScatteredPiece {
+
+    private boolean e;
+
+    public WorldGenWitchHut() {}
+
+    public WorldGenWitchHut(Random random, int i, int j) {
+        super(random, i, 64, j, 7, 5, 9);
+    }
+
+    protected void a(NBTTagCompound nbttagcompound) {
+        super.a(nbttagcompound);
+        nbttagcompound.setBoolean("Witch", this.e);
+    }
+
+    protected void b(NBTTagCompound nbttagcompound) {
+        super.b(nbttagcompound);
+        this.e = nbttagcompound.getBoolean("Witch");
+    }
+
+    public boolean a(World world, Random random, StructureBoundingBox structureboundingbox) {
+        if (!this.a(world, structureboundingbox, 0)) {
+            return false;
+        } else {
+            this.a(world, structureboundingbox, 1, 1, 1, 5, 1, 7, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 1, 4, 2, 5, 4, 7, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 2, 1, 0, 4, 1, 0, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 2, 2, 2, 3, 3, 2, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 1, 2, 3, 1, 3, 6, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 5, 2, 3, 5, 3, 6, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 2, 2, 7, 4, 3, 7, Blocks.WOOD, 1, Blocks.WOOD, 1, false);
+            this.a(world, structureboundingbox, 1, 0, 2, 1, 3, 2, Blocks.LOG, Blocks.LOG, false);
+            this.a(world, structureboundingbox, 5, 0, 2, 5, 3, 2, Blocks.LOG, Blocks.LOG, false);
+            this.a(world, structureboundingbox, 1, 0, 7, 1, 3, 7, Blocks.LOG, Blocks.LOG, false);
+            this.a(world, structureboundingbox, 5, 0, 7, 5, 3, 7, Blocks.LOG, Blocks.LOG, false);
+            this.a(world, Blocks.FENCE, 0, 2, 3, 2, structureboundingbox);
+            this.a(world, Blocks.FENCE, 0, 3, 3, 7, structureboundingbox);
+            this.a(world, Blocks.AIR, 0, 1, 3, 4, structureboundingbox);
+            this.a(world, Blocks.AIR, 0, 5, 3, 4, structureboundingbox);
+            this.a(world, Blocks.AIR, 0, 5, 3, 5, structureboundingbox);
+            this.a(world, Blocks.FLOWER_POT, 7, 1, 3, 5, structureboundingbox);
+            this.a(world, Blocks.WORKBENCH, 0, 3, 2, 6, structureboundingbox);
+            this.a(world, Blocks.CAULDRON, 0, 4, 2, 6, structureboundingbox);
+            this.a(world, Blocks.FENCE, 0, 1, 2, 1, structureboundingbox);
+            this.a(world, Blocks.FENCE, 0, 5, 2, 1, structureboundingbox);
+            int i = this.a(Blocks.WOOD_STAIRS, 3);
+            int j = this.a(Blocks.WOOD_STAIRS, 1);
+            int k = this.a(Blocks.WOOD_STAIRS, 0);
+            int l = this.a(Blocks.WOOD_STAIRS, 2);
+
+            this.a(world, structureboundingbox, 0, 4, 1, 6, 4, 1, Blocks.SPRUCE_WOOD_STAIRS, i, Blocks.SPRUCE_WOOD_STAIRS, i, false);
+            this.a(world, structureboundingbox, 0, 4, 2, 0, 4, 7, Blocks.SPRUCE_WOOD_STAIRS, k, Blocks.SPRUCE_WOOD_STAIRS, k, false);
+            this.a(world, structureboundingbox, 6, 4, 2, 6, 4, 7, Blocks.SPRUCE_WOOD_STAIRS, j, Blocks.SPRUCE_WOOD_STAIRS, j, false);
+            this.a(world, structureboundingbox, 0, 4, 8, 6, 4, 8, Blocks.SPRUCE_WOOD_STAIRS, l, Blocks.SPRUCE_WOOD_STAIRS, l, false);
+
+            int i1;
+            int j1;
+
+            for (i1 = 2; i1 <= 7; i1 += 5) {
+                for (j1 = 1; j1 <= 5; j1 += 4) {
+                    this.b(world, Blocks.LOG, 0, j1, -1, i1, structureboundingbox);
+                }
+            }
+
+            if (!this.e) {
+                i1 = this.a(2, 5);
+                j1 = this.a(2);
+                int k1 = this.b(2, 5);
+
+                if (structureboundingbox.b(i1, j1, k1)) {
+                    this.e = true;
+                    EntityWitch entitywitch = new EntityWitch(world);
+
+                    entitywitch.setPositionRotation((double) i1 + 0.5D, (double) j1, (double) k1 + 0.5D, 0.0F, 0.0F);
+                    entitywitch.a((GroupDataEntity) null);
+                    world.addEntity(entitywitch);
+                }
+            }
+
+            return true;
+        }
+    }
+}
