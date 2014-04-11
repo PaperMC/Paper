6ec832fffe1566949ed05285d869a109998f1fcf
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2014-04-11 16:22:37 -0600

    Add missing entity effects. Adds BUKKIT-3311
    There are many effects that were not present in the API prior to
    this commit. These effects are being used by the implementation,
    but cannot be accessed via plugins.
    
    This commit exposes these effects using the EntityEffects enum,
    allowing for plugin authors to make use of these effects. However,
    many of the effects require certain conditions to be met before
    they will be visible to the client, much like some of the existing
    effects.
    
    By: LordRalex <lordralex@gmail.com>

--- paper-api/src/main/java/org/bukkit/EntityEffect.java
+++ paper-api/src/main/java/org/bukkit/EntityEffect.java
@@ -45,7 +45,56 @@ public enum EntityEffect {
     /**
      * When a sheep eats a LONG_GRASS block.
      */
-    SHEEP_EAT(10);
+    SHEEP_EAT(10),
+
+    /**
+     * When an Iron Golem gives a rose.
+     * <p>
+     * This will not play an effect if the entity is not an iron golem.
+     */
+    IRON_GOLEM_ROSE(11),
+
+    /**
+     * Hearts from a villager.
+     * <p>
+     * This will not play an effect if the entity is not a villager.
+     */
+    VILLAGER_HEART(12),
+
+    /**
+     * When a villager is angry.
+     * <p>
+     * This will not play an effect if the entity is not a villager.
+     */
+    VILLAGER_ANGRY(13),
+
+    /**
+     * Happy particles from a villager.
+     * <p>
+     * This will not play an effect if the entity is not a villager.
+     */
+    VILLAGER_HAPPY(14),
+
+    /**
+     * Magic particles from a witch.
+     * <p>
+     * This will not play an effect if the entity is not a witch.
+     */
+    WITCH_MAGIC(15),
+
+    /**
+     * When a zombie transforms into a villager by shaking violently.
+     * <p>
+     * This will not play an effect if the entity is not a zombie.
+     */
+    ZOMBIE_TRANSFORM(16),
+
+    /**
+     * When a firework explodes.
+     * <p>
+     * This will not play an effect if the entity is not a firework.
+     */
+    FIREWORK_EXPLODE(17);
 
     private final byte data;
     private final static Map<Byte, EntityEffect> BY_DATA = Maps.newHashMap();
