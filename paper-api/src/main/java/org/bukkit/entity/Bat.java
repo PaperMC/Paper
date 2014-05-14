14d4cd125c4868868fd77c853759fe88f34c1d11
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2014-05-13 20:39:02 -0500

    Add awake flag for bats. Adds BUKKIT-5606
    By: Wesley Wolfe <wesley.d.wolfe+git@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Bat.java
+++ paper-api/src/main/java/org/bukkit/entity/Bat.java
@@ -3,4 +3,25 @@ package org.bukkit.entity;
 /**
  * Represents a Bat
  */
-public interface Bat extends Ambient {}
+public interface Bat extends Ambient {
+
+    /**
+     * Checks the current waking state of this bat.
+     * <p>
+     * This does not imply any persistence of state past the method call.
+     *
+     * @return true if the bat is awake or false if it is currently hanging
+     *     from a block
+     */
+    boolean isAwake();
+
+    /**
+     * This method modifies the current waking state of this bat.
+     * <p>
+     * This does not prevent a bat from spontaneously awaking itself, or from
+     * reattaching itself to a block.
+     *
+     * @param state the new state
+     */
+    void setAwake(boolean state);
+}
