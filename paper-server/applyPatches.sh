7fb737c0e7a10ca6612849194c2fae50af291727
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2014-12-03 14:19:09 -0500

    Improve patch scripts, add applyPatches.sh
    When working on CraftBukkit, one may need to update to the latest.
    Before adding new work, you would need to rebuild your patched files to latest.
    Currently BuildTools does this for you, but BuildTools does not support segment based actions.
    
    So this new script replicates the behavior of rebuilding the patched files using nms-patches.
    
    BuildTools can then be updated to simply call this script.
    
    Also, improved makePatches by removing dos2unix. This command does not exists on all systems,
    and can very simply be replaced by sed.
    
    Finally, hard coded path to /ls as a user may of aliased ls to default change the format, so
    call it directly to ensure we get the desired behavior.
    
    By: Aikar <aikar@aikar.co>

---
+++ paper-server/applyPatches.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Please run this script again with the clean decompile sources as an argument. In most cases this will be ../work/decompile-XXXX"
+    exit
+fi
+
+nms=$1/net/minecraft/server
+cb=src/main/java/net/minecraft/server
+#clean up and rebuild
+rm -rf $cb
+mkdir -p $cb
+for file in $(/bin/ls nms-patches)
+do
+    patchFile="nms-patches/$file"
+    file="$(echo $file | cut -d. -f1).java"
+
+    echo "Patching $file < $patchFile"
+    sed -i 's/\r//' "$nms/$file"
+
+    cp "$nms/$file" "$cb/$file"
+    patch -d src/main/java/ "net/minecraft/server/$file" < "$patchFile"
+done
