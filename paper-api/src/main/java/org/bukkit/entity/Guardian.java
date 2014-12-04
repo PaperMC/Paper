55bf7af69818a4ec558206ae6df54a04ce0a26a7
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2014-12-04 20:03:59 +0100

    API for getting/setting a elder Guardian
    By: Fabian Faßbender <fabian.fassbender42@googlemail.com>

--- paper-api/src/main/java/org/bukkit/entity/Guardian.java
+++ paper-api/src/main/java/org/bukkit/entity/Guardian.java
@@ -1,4 +1,16 @@
 package org.bukkit.entity;
 
 public interface Guardian extends Monster {
+
+    /**
+     * Check if the Guardian is an elder Guardian
+     * @return true if the Guardian is an Elder Guardian, false if not
+     */
+    public boolean isElder();
+
+    /**
+     * Set the Guardian to an elder Guardian or not
+     * @param shouldBeElder True if this Guardian should be a elder Guardian, false if not
+     */
+    public void setElder(boolean shouldBeElder);
 }
