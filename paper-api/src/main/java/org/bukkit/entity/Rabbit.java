48b97d3e0508f50966333412f824a41c0e32ad3c
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2014-12-04 12:55:49 +0100

    Add RabbitType API.
    By: FearThe1337 <admin@fearthe1337.com>

--- paper-api/src/main/java/org/bukkit/entity/Rabbit.java
+++ paper-api/src/main/java/org/bukkit/entity/Rabbit.java
@@ -1,4 +1,49 @@
 package org.bukkit.entity;
 
 public interface Rabbit extends Animals {
+
+    /**
+     * @return The type of rabbit.
+     */
+    public Type getRabbitType();
+
+    /**
+     * @param type Sets the type of rabbit for this entity.
+     */
+    public void setRabbitType(Type type);
+
+    /**
+     * Represents the various types a Rabbit might be.
+     */
+    public enum Type {
+
+        /**
+         * Chocolate colored rabbit.
+         */
+        BROWN,
+        /**
+         * Pure white rabbit.
+         */
+        WHITE,
+        /**
+         * Black rabbit.
+         */
+        BLACK,
+        /**
+         * Black with white patches, or white with black patches?
+         */
+        BLACK_AND_WHITE,
+        /**
+         * Golden bunny.
+         */
+        GOLD,
+        /**
+         * Salt and pepper colored, whatever that means.
+         */
+        SALT_AND_PEPPER,
+        /**
+         * Rabbit with pure white fur, blood red horizontal eyes, and is hostile to players.
+         */
+        THE_KILLER_BUNNY
+    }
 }
