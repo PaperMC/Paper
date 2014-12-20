ce7dd34b5c96a417a2ad870d66c79180eacda57d
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2014-12-20 16:58:45 +0100

    aW() does re register Attributes which causes a IllegalArgumentException. This fixes it and sets the Guardian back to a normal Guardian without any problem.

--- src/main/java/org/bukkit/craftbukkit/entity/CraftGuardian.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftGuardian.java
@@ -38,7 +38,13 @@ public class CraftGuardian extends CraftMonster implements Guardian {
 
             // Since minecraft does not reset the elder Guardian to a guardian we have to do that
             entity.a(0.85F, 0.85F);
-            entityGuardian.aW();
+
+            // Since aW() calls its supers it will try to re register attributes which is invalid
+            // PAIL: rename and check these on update
+            entityGuardian.getAttributeInstance(GenericAttributes.e).setValue(6.0D);
+            entityGuardian.getAttributeInstance(GenericAttributes.d).setValue(0.5D);
+            entityGuardian.getAttributeInstance(GenericAttributes.b).setValue(16.0D);
+            entityGuardian.getAttributeInstance(GenericAttributes.maxHealth).setValue(30.0D);
 
             // Update pathfinding (random stroll back to 80)
             entityGuardian.bq.b(80);
