ec7bb38953f307aa88259e56ab93f4d160886fa0
Zach Brown <zach.brown@destroystokyo.com>
2016-01-26 00:04:43 -0600

    Add upstreamMerge.sh and update CB submodule

---
+++ upstreamMerge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PS1="$"
+basedir=`pwd`
+
+function update {
+    cd "$basedir/$1"
+    git fetch && git reset --hard origin/master
+    cd ../
+    git add $1
+}
+
+update Bukkit
+update CraftBukkit
+
