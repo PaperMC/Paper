ea61fac2eddf6afdd6986de4d6c42bc643f1dae0
DemonWav <demonwav@gmail.com>
2016-01-26 00:36:28 -0600

    Add Paperclip support

---
+++ paperclip.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cp ./PaperSpigot-Server/target/paperspigot*.jar ./Paperclip/paperspigot-1.8.8.jar
+cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
+cd ./Paperclip
+mvn clean package
+cd ..
+cp ./Paperclip/target/paperclip*.jar ./Paperclip.jar
+
+echo ""
+echo ""
+echo ""
+echo "Build success!"
+echo "Copied final jar to $(pwd)/Paperclip.jar"
