d9169df994e7bd86decc020bc8a2f6d80c7931e9
Zach Brown <zach.brown@destroystokyo.com>
2016-02-05 03:58:20 -0600

    Build Scripts: Be a little more explicit in paperclip.sh

--- paperclip.sh
+++ paperclip.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cp ./PaperSpigot-Server/target/paperspigot*.jar ./Paperclip/paperspigot-1.8.8.jar
+cp ./PaperSpigot-Server/target/paperspigot*-SNAPSHOT.jar ./Paperclip/paperspigot-1.8.8.jar
 cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
 mvn clean package
 cd ..
-cp ./Paperclip/target/paperclip*.jar ./Paperclip.jar
+cp ./Paperclip/target/paperclip*-SNAPSHOT.jar ./Paperclip.jar
 
 echo ""
 echo ""
