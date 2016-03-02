f941b8b7adb947eab621d696cdabd336e179935b
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-02 16:42:20 +0000

    SPIGOT-1644: Add END_GATEWAY as a teleport reason
    By: Thinkofdeath <thinkofdeath@spigotmc.org>

--- paper-api/src/main/java/org/bukkit/event/player/PlayerTeleportEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerTeleportEvent.java
@@ -61,6 +61,11 @@ public class PlayerTeleportEvent extends PlayerMoveEvent {
          */
         SPECTATE,
         /**
+         * Indicates the teleportation was caused by a player entering an End
+         * gateway
+         */
+        END_GATEWAY,
+        /**
          * Indicates the teleportation was caused by an event not covered by
          * this enum
          */
