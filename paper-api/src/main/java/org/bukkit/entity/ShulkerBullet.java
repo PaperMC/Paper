3589bbb79e0b38cdaefa3e902238360158a3017e
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-03 17:29:52 +1100

    Treat ShulkerBullet as Projectile
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/ShulkerBullet.java
+++ paper-api/src/main/java/org/bukkit/entity/ShulkerBullet.java
@@ -1,22 +1,6 @@
 package org.bukkit.entity;
 
-import org.bukkit.projectiles.ProjectileSource;
-
-public interface ShulkerBullet extends Entity {
-
-    /**
-     * Retrieve the shooter of this bullet.
-     *
-     * @return the {@link ProjectileSource} that shot this bullet
-     */
-    ProjectileSource getShooter();
-
-    /**
-     * Set the shooter of this bullet.
-     *
-     * @param source the {@link ProjectileSource} that shot this bullet
-     */
-    void setShooter(ProjectileSource source);
+public interface ShulkerBullet extends Projectile {
 
     /**
      * Retrieve the target of this bullet.
