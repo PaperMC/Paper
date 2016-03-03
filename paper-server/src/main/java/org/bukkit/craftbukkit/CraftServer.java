73251dc4518733dc354d73149955e8a3a02e3767
Riley Park <rileysebastianpark@gmail.com>
2016-03-03 02:15:57 -0600

    Expose server CommandMap

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -2182,6 +2182,7 @@ public final class CraftServer implements Server {
         return this.helpMap;
     }
 
+    @Override // Paper - add override
     public SimpleCommandMap getCommandMap() {
         return this.commandMap;
     }
