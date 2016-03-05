382597b99e8f4ab21bacc70518f93e228640f475
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-06 09:46:35 +1100

    SPIGOT-1773: Chorus fruit teleport reason
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/player/PlayerTeleportEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerTeleportEvent.java
@@ -66,6 +66,11 @@ public class PlayerTeleportEvent extends PlayerMoveEvent {
          */
         END_GATEWAY,
         /**
+         * Indicates the teleportation was caused by a player consuming chorus
+         * fruit
+         */
+        CHORUS_FRUIT,
+        /**
          * Indicates the teleportation was caused by an event not covered by
          * this enum
          */
