db47bb9f6b5d062d90bc391ca8530ed7b7356eba
kashike <kashike@vq.lc>
2016-03-08 18:28:43 -0800

    Don't nest if we don't need to when cerealising text components

--- src/main/java/net/minecraft/server/PacketPlayOutChat.java
+++ src/main/java/net/minecraft/server/PacketPlayOutChat.java
@@ -29,7 +29,14 @@ public class PacketPlayOutChat implements Packet<PacketListenerPlayOut> {
     public void b(PacketDataSerializer packetdataserializer) throws IOException {
         // Spigot start
         if (components != null) {
-            packetdataserializer.a(net.md_5.bungee.chat.ComponentSerializer.toString(components));
+            //packetdataserializer.a(net.md_5.bungee.chat.ComponentSerializer.toString(components)); // Paper - comment, replaced with below
+            // Paper start - don't nest if we don't need to so that we can preserve formatting
+            if (this.components.length == 1) {
+                packetdataserializer.a(net.md_5.bungee.chat.ComponentSerializer.toString(this.components[0]));
+            } else {
+                packetdataserializer.a(net.md_5.bungee.chat.ComponentSerializer.toString(this.components));
+            }
+            // Paper end
         } else {
             packetdataserializer.a(this.a);
         }
