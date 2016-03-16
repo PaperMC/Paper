9ec13752b339e14aff0b6e5b74dd4f0f52380673
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2016-03-16 19:08:24 -0400

    Implement Boat#getWoodType and Boat#setWoodType(TreeSpecies)
    By: Matthew <stteg@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftBoat.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftBoat.java
@@ -1,6 +1,7 @@
 package org.bukkit.craftbukkit.entity;
 
 import net.minecraft.server.EntityBoat;
+import org.bukkit.TreeSpecies;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Boat;
 import org.bukkit.entity.EntityType;
@@ -9,6 +10,16 @@ public class CraftBoat extends CraftVehicle implements Boat {
 
     public CraftBoat(CraftServer server, EntityBoat entity) {
         super(server, entity);
+    }
+
+    @Override
+    public TreeSpecies getWoodType() {
+        return getTreeSpecies(getHandle().r()); // PAIL: Rename getType
+    }
+
+    @Override
+    public void setWoodType(TreeSpecies species) {
+        getHandle().a(getBoatType(species)); // PAIL: Rename setType
     }
 
     public double getMaxSpeed() {
@@ -59,5 +70,41 @@ public class CraftBoat extends CraftVehicle implements Boat {
 
     public EntityType getType() {
         return EntityType.BOAT;
+    }
+
+    public static TreeSpecies getTreeSpecies(EntityBoat.EnumBoatType boatType) {
+        switch (boatType) {
+            case SPRUCE:
+                return TreeSpecies.REDWOOD;
+            case BIRCH:
+                return TreeSpecies.BIRCH;
+            case JUNGLE:
+                return TreeSpecies.JUNGLE;
+            case ACACIA:
+                return TreeSpecies.ACACIA;
+            case DARK_OAK:
+                return TreeSpecies.DARK_OAK;
+            case OAK:
+            default:
+                return TreeSpecies.GENERIC;
+        }
+    }
+
+    public static EntityBoat.EnumBoatType getBoatType(TreeSpecies species) {
+        switch (species) {
+            case REDWOOD:
+                return EntityBoat.EnumBoatType.SPRUCE;
+            case BIRCH:
+                return EntityBoat.EnumBoatType.BIRCH;
+            case JUNGLE:
+                return EntityBoat.EnumBoatType.JUNGLE;
+            case ACACIA:
+                return EntityBoat.EnumBoatType.ACACIA;
+            case DARK_OAK:
+                return EntityBoat.EnumBoatType.DARK_OAK;
+            case GENERIC:
+            default:
+                return EntityBoat.EnumBoatType.OAK;
+        }
     }
 }
