441c95cf26525bc0eb47ed1ce69dc5f9d1e67d7c
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-16 19:53:51 -0400

    Add EnderCrystal#getBeamTarget and EnderCrystal#setBeamTarget(Location)
    By: Matthew <stteg@hotmail.com>

--- paper-api/src/main/java/org/bukkit/entity/EnderCrystal.java
+++ paper-api/src/main/java/org/bukkit/entity/EnderCrystal.java
@@ -1,5 +1,7 @@
 package org.bukkit.entity;
 
+import org.bukkit.Location;
+
 /**
  * A crystal that heals nearby EnderDragons
  */
@@ -20,4 +22,20 @@ public interface EnderCrystal extends Entity {
      * @param showing whether the bedrock slate should be shown
      */
     void setShowingBottom(boolean showing);
+
+    /**
+     * Gets the location that this end crystal is pointing its beam to.
+     *
+     * @return the location that the beam is pointed to, or null if the beam is not shown
+     */
+    Location getBeamTarget();
+
+    /**
+     * Sets the location that this end crystal is pointing to. Passing a null
+     * value will remove the current beam.
+     *
+     * @param location the location to point the beam to
+     * @throws IllegalArgumentException for differing worlds
+     */
+    void setBeamTarget(Location location);
 }
