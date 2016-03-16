5fd25db78d4aeafe4d15b3f87e1da1eefe96dac6
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-16 19:50:51 -0400

    Add EnderCrystal#isShowingBottom and EnderCrystal#setShowingBottom(boolean)
    By: Matthew <stteg@hotmail.com>

--- paper-api/src/main/java/org/bukkit/entity/EnderCrystal.java
+++ paper-api/src/main/java/org/bukkit/entity/EnderCrystal.java
@@ -4,4 +4,20 @@ package org.bukkit.entity;
  * A crystal that heals nearby EnderDragons
  */
 public interface EnderCrystal extends Entity {
+
+    /**
+     * Return whether or not this end crystal is showing the
+     * bedrock slate underneath it.
+     *
+     * @return true if the bottom is being shown
+     */
+    boolean isShowingBottom();
+
+    /**
+     * Sets whether or not this end crystal is showing the
+     * bedrock slate underneath it.
+     *
+     * @param showing whether the bedrock slate should be shown
+     */
+    void setShowingBottom(boolean showing);
 }
