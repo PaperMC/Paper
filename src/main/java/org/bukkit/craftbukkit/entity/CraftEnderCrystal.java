a12b1a47706a53b06ca18ae4ae9f696ea5699f3a
Matthew <stteg@hotmail.com>
2016-03-16 19:56:48 -0400

    Implement EndGateway#getExitLocation and EndGateway#setExitLocation(Location)

--- src/main/java/org/bukkit/craftbukkit/entity/CraftEnderCrystal.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftEnderCrystal.java
@@ -1,6 +1,8 @@
 package org.bukkit.craftbukkit.entity;
 
+import net.minecraft.server.BlockPosition;
 import net.minecraft.server.EntityEnderCrystal;
+import org.bukkit.Location;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.EntityType;
@@ -18,6 +20,23 @@ public class CraftEnderCrystal extends CraftEntity implements EnderCrystal {
     @Override
     public void setShowingBottom(boolean showing) {
         getHandle().a(showing); // PAIL: Rename setShowingBottom
+    }
+
+    @Override
+    public Location getBeamTarget() {
+        BlockPosition pos = getHandle().j(); // PAIL: Rename getBeamTarget
+        return pos == null ? null : new Location(getWorld(), pos.getX(), pos.getY(), pos.getZ());
+    }
+
+    @Override
+    public void setBeamTarget(Location location) {
+        if (location == null) {
+            getHandle().a((BlockPosition) null); // PAIL: Rename setBeamTarget
+        } else if (location.getWorld() != getWorld()) {
+            throw new IllegalArgumentException("Cannot set beam target location to different world");
+        } else {
+            getHandle().a(new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ()));
+        }
     }
 
     @Override
