622a61ce4572a27a31ad62ccc173713a13c3d0b4
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-16 21:30:03 -0400

    Add EndGateway#getExitLocation and EndGateway#setExitLocation(Location)
    By: Matthew <stteg@hotmail.com>

--- paper-api/src/main/java/org/bukkit/block/EndGateway.java
+++ paper-api/src/main/java/org/bukkit/block/EndGateway.java
@@ -1,3 +1,26 @@
 package org.bukkit.block;
 
-public interface EndGateway extends BlockState {}
+import org.bukkit.Location;
+
+/**
+ * Represents an end gateway.
+ */
+public interface EndGateway extends BlockState {
+
+    /**
+     * Gets the location that entities are teleported to when 
+     * entering the gateway portal.
+     * 
+     * @return the gateway exit location
+     */
+    Location getExitLocation();
+
+    /**
+     * Sets the exit location that entities are teleported to when
+     * they enter the gateway portal.
+     * 
+     * @param location the new exit location
+     * @throws IllegalArgumentException for differing worlds
+     */
+    void setExitLocation(Location location);
+}
