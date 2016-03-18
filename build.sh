d6b621b533b4339c6b8255afbb40e75292b14d66
Aikar <aikar@aikar.co>
2016-03-18 01:50:06 -0400

    remove that comment...

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh
+git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh
 if [ "$1" == "--jar" ]; then
 	mvn clean install && ./paperclip.sh
 fi
