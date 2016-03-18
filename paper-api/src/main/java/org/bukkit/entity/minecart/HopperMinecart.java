032db7ea4f43420cdf30903f5761c69960822f08
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-03-18 11:35:59 -0400

    Add isEnabled / setEnabled for HopperMinecart
    By: Matthew <stteg@hotmail.com>

--- paper-api/src/main/java/org/bukkit/entity/minecart/HopperMinecart.java
+++ paper-api/src/main/java/org/bukkit/entity/minecart/HopperMinecart.java
@@ -7,4 +7,19 @@ import org.bukkit.inventory.InventoryHolder;
  * Represents a Minecart with a Hopper inside it
  */
 public interface HopperMinecart extends Minecart, InventoryHolder {
+
+    /**
+     * Checks whether or not this Minecart will pick up 
+     * items into its inventory.
+     * 
+     * @return true if the Minecart will pick up items
+     */
+    boolean isEnabled();
+
+    /**
+     * Sets whether this Minecart will pick up items.
+     * 
+     * @param enabled new enabled state
+     */
+    void setEnabled(boolean enabled);
 }
