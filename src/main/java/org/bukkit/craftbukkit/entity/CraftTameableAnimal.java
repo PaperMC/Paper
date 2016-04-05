3dc138d98059b6841694d75615a9efadb4a65732
md_5 <git@md-5.net>
2016-04-05 20:32:05 +1000

    SPIGOT-2135: Also clear sit pathfinder on toggle status

--- src/main/java/org/bukkit/craftbukkit/entity/CraftTameableAnimal.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftTameableAnimal.java
@@ -71,6 +71,7 @@ public class CraftTameableAnimal extends CraftAnimals implements Tameable, Creat
 
     public void setSitting(boolean sitting) {
         getHandle().setSitting(sitting);
+        getHandle().getGoalSit().setSitting(sitting);
     }
 
     @Override
