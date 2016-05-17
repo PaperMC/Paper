62b75235ddeff92e976fcd1de3aba47c12131ec2
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-05-18 00:44:18 +0200

    Add interface to modify the pumpkin head "derp mode" of a snowman
    By: minoneer <minoneer@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Snowman.java
+++ paper-api/src/main/java/org/bukkit/entity/Snowman.java
@@ -5,4 +5,20 @@ package org.bukkit.entity;
  */
 public interface Snowman extends Golem {
 
+    /**
+     * Gets whether this snowman is in "derp mode", meaning it is not wearing a
+     * pumpkin.
+     *
+     * @return True if the snowman is bald, false if it is wearing a pumpkin
+     */
+    boolean isDerp();
+
+    /**
+     * Sets whether this snowman is in "derp mode", meaning it is not wearing a
+     * pumpkin. NOTE: This value is not persisted to disk and will therefore
+     * reset when the chunk is reloaded.
+     *
+     * @param derpMode True to remove the pumpkin, false to add a pumpkin
+     */
+    void setDerp(boolean derpMode);
 }
