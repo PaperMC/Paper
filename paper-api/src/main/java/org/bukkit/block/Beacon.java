bab44fa0f5b1b924a317410c0881293e99403b30
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-07-23 10:29:30 +1000

    Implement basic Beacon Block API
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/Beacon.java
+++ paper-api/src/main/java/org/bukkit/block/Beacon.java
@@ -1,9 +1,42 @@
 package org.bukkit.block;
 
+import java.util.Collection;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.potion.PotionEffect;
 
 /**
  * Represents a beacon.
  */
 public interface Beacon extends BlockState, InventoryHolder {
+
+    /**
+     * Returns the list of players within the beacon's range of effect.
+     *
+     * @return the players in range
+     */
+    Collection<LivingEntity> getEntitiesInRange();
+
+    /**
+     * Returns the tier of the beacon pyramid (0-4). The tier refers to the
+     * beacon's power level, based on how many layers of blocks are in the
+     * pyramid. Tier 1 refers to a beacon with one layer of 9 blocks under it.
+     *
+     * @return the beacon tier
+     */
+    int getTier();
+
+    /**
+     * Returns the primary effect set on the beacon
+     *
+     * @return the primary effect
+     */
+    PotionEffect getPrimaryEffect();
+
+    /**
+     * Returns the secondary effect set on the beacon.
+     *
+     * @return the secondary effect or null if no secondary effect
+     */
+    PotionEffect getSecondaryEffect();
 }
