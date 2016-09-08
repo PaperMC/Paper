df6649ed5d4ed154f32ca59ed7473164032646c7
AlphaBlend <whizkid3000@hotmail.com>
2016-09-08 08:47:08 -0700

    Add source to PlayerExpChangeEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerExpChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerExpChangeEvent.java
@@ -1,20 +1,42 @@
 package org.bukkit.event.player;
 
+import org.bukkit.entity.Entity; // Paper
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+
+import org.jetbrains.annotations.Nullable; // Paper
 
 /**
  * Called when a players experience changes naturally
  */
 public class PlayerExpChangeEvent extends PlayerEvent {
     private static final HandlerList handlers = new HandlerList();
+    // Paper start
+    @Nullable
+    private final Entity source;
     private int exp;
 
     public PlayerExpChangeEvent(@NotNull final Player player, final int expAmount) {
+          this(player, null, expAmount);
+    }
+
+    public PlayerExpChangeEvent(@NotNull final Player player, @Nullable final Entity sourceEntity, final int expAmount) {
          super(player);
+         source = sourceEntity;
          exp = expAmount;
     }
+
+    /**
+     * Get the source that provided the experience.
+     *
+     * @return The source of the experience
+     */
+    @Nullable
+    public Entity getSource() {
+        return source;
+    }
+    // Paper end
 
     /**
      * Get the amount of experience the player will receive
