500492767285a88ab23989ce51335e4ffc21f323
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-11-17 17:24:01 +1100

    SPIGOT-2768: Fully implement map color
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/inventory/meta/MapMeta.java
+++ paper-api/src/main/java/org/bukkit/inventory/meta/MapMeta.java
@@ -1,5 +1,7 @@
 package org.bukkit.inventory.meta;
 
+import org.bukkit.Color;
+
 /**
  * Represents a map that can be scalable.
  */
@@ -37,11 +39,38 @@ public interface MapMeta extends ItemMeta {
     String getLocationName();
 
     /**
-     * Sets the location name.
+     * Sets the location name. A custom map color will alter the display of the
+     * map in an inventory slot.
      *
      * @param name the name to set
      */
     void setLocationName(String name);
+
+    /**
+     * Checks for existence of a map color.
+     *
+     * @return true if this has a custom map color
+     */
+    boolean hasColor();
+
+    /**
+     * Gets the map color that is set. A custom map color will alter the display
+     * of the map in an inventory slot.
+     * <p>
+     * Plugins should check that hasColor() returns <code>true</code> before
+     * calling this method.
+     *
+     * @return the map color that is set
+     */
+    Color getColor();
+
+    /**
+     * Sets the map color. A custom map color will alter the display of the map
+     * in an inventory slot.
+     *
+     * @param color the color to set
+     */
+    void setColor(Color color);
 
     MapMeta clone();
 }
