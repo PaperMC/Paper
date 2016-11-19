b57d05eeea4647f58d39dd701b57efb0e74b2783
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-11-19 18:53:34 +1100

    Add additional APIs for Evoker / EvokerFangs
    By: momothereal <momothereal.mc@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Evoker.java
+++ paper-api/src/main/java/org/bukkit/entity/Evoker.java
@@ -3,4 +3,42 @@ package org.bukkit.entity;
 /**
  * Represents an Evoker.
  */
-public interface Evoker extends Monster { }
+public interface Evoker extends Monster {
+
+    /**
+     * Represents the current spell the Evoker is using.
+     */
+    public enum Spell {
+
+        /**
+         * No spell is being evoked.
+         */
+        NONE,
+        /**
+         * The spell that summons Vexes.
+         */
+        SUMMON,
+        /**
+         * The spell that summons Fangs.
+         */
+        FANGS,
+        /**
+         * The "wololo" spell.
+         */
+        WOLOLO;
+    }
+
+    /**
+     * Gets the {@link Spell} the Evoker is currently using.
+     *
+     * @return the current spell
+     */
+    Spell getCurrentSpell();
+
+    /**
+     * Sets the {@link Spell} the Evoker is currently using.
+     *
+     * @param spell the spell the evoker should be using
+     */
+    void setCurrentSpell(Spell spell);
+}
--- paper-api/src/main/java/org/bukkit/entity/EvokerFangs.java
+++ paper-api/src/main/java/org/bukkit/entity/EvokerFangs.java
@@ -3,4 +3,19 @@ package org.bukkit.entity;
 /**
  * Represents Evoker Fangs.
  */
-public interface EvokerFangs extends Entity { }
+public interface EvokerFangs extends Entity {
+
+    /**
+     * Gets the {@link LivingEntity} which summoned the fangs.
+     *
+     * @return the {@link LivingEntity} which summoned the fangs
+     */
+    LivingEntity getOwner();
+
+    /**
+     * Sets the {@link LivingEntity} which summoned the fangs.
+     *
+     * @param owner the {@link LivingEntity} which summoned the fangs
+     */
+    void setOwner(LivingEntity owner);
+}
