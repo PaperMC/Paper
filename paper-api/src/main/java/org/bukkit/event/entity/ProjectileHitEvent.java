ff46831b2dac61f78c97765896f3c8146146f075
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-11-28 12:46:55 +1100

    Add hitEntity to ProjectileHitEvent
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/entity/ProjectileHitEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/ProjectileHitEvent.java
@@ -1,5 +1,6 @@
 package org.bukkit.event.entity;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.HandlerList;
 
@@ -8,14 +9,29 @@ import org.bukkit.event.HandlerList;
  */
 public class ProjectileHitEvent extends EntityEvent {
     private static final HandlerList handlers = new HandlerList();
+    private final Entity hitEntity;
 
     public ProjectileHitEvent(final Projectile projectile) {
+        this(projectile, null);
+    }
+
+    public ProjectileHitEvent(final Projectile projectile, Entity hitEntity) {
         super(projectile);
+        this.hitEntity = hitEntity;
     }
 
     @Override
     public Projectile getEntity() {
         return (Projectile) entity;
+    }
+
+    /**
+     * Gets the entity that was hit, if it was an entity that was hit.
+     *
+     * @return hit entity or else null
+     */
+    public Entity getHitEntity() {
+        return hitEntity;
     }
 
     @Override
