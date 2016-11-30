8b0439979a23c77f4d85f51c82b0fa708101f4d1
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-11-30 16:41:08 +1100

    Add some expansions to Anvil Inventory API
    By: Xor Boole <mcyoung@mit.edu>

--- paper-api/src/main/java/org/bukkit/inventory/AnvilInventory.java
+++ paper-api/src/main/java/org/bukkit/inventory/AnvilInventory.java
@@ -4,4 +4,26 @@ package org.bukkit.inventory;
  * Interface to the inventory of an Anvil.
  */
 public interface AnvilInventory extends Inventory {
+
+    /**
+     * Get the name to be applied to the repaired item. An empty string denotes
+     * the default item name.
+     *
+     * @return the rename text
+     */
+    String getRenameText();
+
+    /**
+     * Get the experience cost (in levels) to complete the current repair.
+     *
+     * @return the experience cost
+     */
+    int getRepairCost();
+
+    /**
+     * Set the experience cost (in levels) to complete the current repair.
+     *
+     * @param levels the experience cost
+     */
+    void setRepairCost(int levels);
 }
