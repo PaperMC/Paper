8c5665d841ba2a3f0aa782b66d47b15092b852e7
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2016-12-02 09:38:47 +1100

    SPIGOT-911: Add hitBlock to PorjectileHitEvent
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/entity/ProjectileHitEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/ProjectileHitEvent.java
@@ -1,5 +1,6 @@
 package org.bukkit.event.entity;
 
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.HandlerList;
@@ -10,19 +11,38 @@ import org.bukkit.event.HandlerList;
 public class ProjectileHitEvent extends EntityEvent {
     private static final HandlerList handlers = new HandlerList();
     private final Entity hitEntity;
+    private final Block hitBlock;
 
     public ProjectileHitEvent(final Projectile projectile) {
-        this(projectile, null);
+        this(projectile, null, null);
     }
 
     public ProjectileHitEvent(final Projectile projectile, Entity hitEntity) {
+        this(projectile, hitEntity, null);
+    }
+
+    public ProjectileHitEvent(final Projectile projectile, Block hitBlock) {
+        this(projectile, null, hitBlock);
+    }
+
+    public ProjectileHitEvent(final Projectile projectile, Entity hitEntity, Block hitBlock) {
         super(projectile);
         this.hitEntity = hitEntity;
+        this.hitBlock = hitBlock;
     }
 
     @Override
     public Projectile getEntity() {
         return (Projectile) entity;
+    }
+
+    /**
+     * Gets the block that was hit, if it was a block that was hit.
+     *
+     * @return hit block or else null
+     */
+    public Block getHitBlock() {
+        return hitBlock;
     }
 
     /**
