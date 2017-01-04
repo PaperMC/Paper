72f65b248822e9d083c84db301754b945d3fe9da
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2017-01-05 09:39:23 +1100

    SPIGOT-2988: Update JavaDoc in Plugin#saveDefaultConfig
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/plugin/Plugin.java
+++ paper-api/src/main/java/org/bukkit/plugin/Plugin.java
@@ -58,9 +58,9 @@ public interface Plugin extends TabExecutor {
 
     /**
      * Saves the raw contents of the default config.yml file to the location
-     * retrievable by {@link #getConfig()}. If there is no default config.yml
-     * embedded in the plugin, an empty config.yml file is saved. This should
-     * fail silently if the config.yml already exists.
+     * retrievable by {@link #getConfig()}.
+     * <p>
+     * This should fail silently if the config.yml already exists.
      */
     public void saveDefaultConfig();
 
