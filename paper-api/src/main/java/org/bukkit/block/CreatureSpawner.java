a082c22d479d59c109bb2820c8c937627fe0ea47
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2017-02-22 09:59:23 +1100

    Deprecate magic values in CreatureSpawner
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/CreatureSpawner.java
+++ paper-api/src/main/java/org/bukkit/block/CreatureSpawner.java
@@ -25,14 +25,19 @@ public interface CreatureSpawner extends BlockState {
      * Set the spawner mob type.
      *
      * @param creatureType The creature type's name.
+     * @deprecated magic value, use
+     * {@link #setSpawnedType(org.bukkit.entity.EntityType)}.
      */
+    @Deprecated
     public void setCreatureTypeByName(String creatureType);
 
     /**
      * Get the spawner's creature type.
      *
      * @return The creature type's name.
+     * @deprecated magic value, use {@link #getSpawnedType()}.
      */
+    @Deprecated
     public String getCreatureTypeName();
 
     /**
