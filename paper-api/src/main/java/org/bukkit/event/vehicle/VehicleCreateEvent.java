9d860f7ef68654e5bcfe41238e5780846e646f48
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2017-03-15 20:46:13 +1100

    Make VehicleCreateEvent Cancellable.
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/vehicle/VehicleCreateEvent.java
+++ paper-api/src/main/java/org/bukkit/event/vehicle/VehicleCreateEvent.java
@@ -1,16 +1,28 @@
 package org.bukkit.event.vehicle;
 
 import org.bukkit.entity.Vehicle;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
  * Raised when a vehicle is created.
  */
-public class VehicleCreateEvent extends VehicleEvent {
+public class VehicleCreateEvent extends VehicleEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
 
     public VehicleCreateEvent(final Vehicle vehicle) {
         super(vehicle);
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 
     @Override
