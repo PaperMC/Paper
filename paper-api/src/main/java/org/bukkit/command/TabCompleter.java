6882eee6d032ee1cd99db3071e41ae0d3a283067
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2017-04-13 17:45:15 -0700

    Clarify behavior of tab completing in command blocks
    By: Pokechu22 <Pokechu022@gmail.com>

--- paper-api/src/main/java/org/bukkit/command/TabCompleter.java
+++ paper-api/src/main/java/org/bukkit/command/TabCompleter.java
@@ -10,7 +10,9 @@ public interface TabCompleter {
     /**
      * Requests a list of possible completions for a command argument.
      *
-     * @param sender Source of the command
+     * @param sender Source of the command.  For players tab-completing a
+     *     command inside of a command block, this will be the player, not
+     *     the command block. 
      * @param command Command which was executed
      * @param alias The alias used
      * @param args The arguments passed to the command, including final
