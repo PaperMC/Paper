f965ee5c7f66c5eefec8d26941810171057b5ffd
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2017-04-29 17:11:43 -0400

    Implement getColor / setColor for Shulker entity
    By: Matthew <stteg@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftShulker.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftShulker.java
@@ -1,7 +1,8 @@
 package org.bukkit.craftbukkit.entity;
 
-import net.minecraft.server.EntityGolem;
+import com.google.common.base.Preconditions;
 import net.minecraft.server.EntityShulker;
+import org.bukkit.DyeColor;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Shulker;
@@ -20,5 +21,17 @@ public class CraftShulker extends CraftGolem implements Shulker {
     @Override
     public EntityShulker getHandle() {
         return (EntityShulker) entity;
+    }
+
+    @Override
+    public DyeColor getColor() {
+        return DyeColor.getByWoolData(getHandle().getDataWatcher().get(EntityShulker.bw));
+    }
+
+    @Override
+    public void setColor(DyeColor color) {
+        Preconditions.checkArgument(color != null, "color");
+
+        getHandle().getDataWatcher().set(EntityShulker.bw, color.getWoolData());
     }
 }
