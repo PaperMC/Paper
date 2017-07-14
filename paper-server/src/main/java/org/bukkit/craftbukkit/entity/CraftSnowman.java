7145fd4941701f435a501c0e05be444da4e78d50
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2017-07-15 09:53:22 +1000

    SPIGOT-3417: Event for shearing snowman
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftSnowman.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftSnowman.java
@@ -12,12 +12,12 @@ public class CraftSnowman extends CraftGolem implements Snowman {
 
     @Override
     public boolean isDerp() {
-        return getHandle().hasPumpkin();
+        return !getHandle().hasPumpkin();
     }
 
     @Override
     public void setDerp(boolean derpMode) {
-        getHandle().setHasPumpkin(derpMode);
+        getHandle().setHasPumpkin(!derpMode);
     }
 
     @Override
