f3b8afd56a94f9bfd3da7c046a24e7d4923ffbd8
DemonWav <demonwav@gmail.com>
2017-09-23 13:17:20 -0500

    Move eclipse compiler patch to POM changes patch Also update to Paperclip 1.1-SNAPSHOT

--- scripts/paperclip.sh
+++ scripts/paperclip.sh
@@ -12,7 +12,7 @@ vanillajar="../Minecraft/$mcver/$mcver.jar"
     cd "$workdir/Paperclip"
     mvn clean package "-Dmcver=$mcver" "-Dpaperjar=$paperjar" "-Dvanillajar=$vanillajar"
 )
-cp "$workdir/Paperclip/target/paperclip-${mcver}.jar" "$basedir/paperclip.jar"
+cp "$workdir/Paperclip/assembly/target/paperclip-${mcver}.jar" "$basedir/paperclip.jar"
 
 echo ""
 echo ""
