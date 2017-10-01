59fa98bfb7ca40b65af91af573708fab4e1b4f6a
md_5 <git@md-5.net>
2017-10-01 11:01:49 +1100

    SPIGOT-3587: Set head rotation when spawning entity

--- src/main/java/org/bukkit/craftbukkit/CraftWorld.java
+++ src/main/java/org/bukkit/craftbukkit/CraftWorld.java
@@ -1108,6 +1108,7 @@ public class CraftWorld implements World {
 
             if (entity != null) {
                 entity.setLocation(x, y, z, yaw, pitch);
+                entity.setHeadRotation(yaw); // SPIGOT-3587
             }
         } else if (Hanging.class.isAssignableFrom(clazz)) {
             Block block = getBlockAt(location);
