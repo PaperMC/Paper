48b1b913731b3c88f2eb6023df4f875189293b69
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2017-11-07 20:06:16 -0500

    Add max fuse tick & explosion radius methods to Creeper
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Creeper.java
+++ paper-api/src/main/java/org/bukkit/entity/Creeper.java
@@ -18,4 +18,36 @@ public interface Creeper extends Monster {
      * @param value New Powered status
      */
     public void setPowered(boolean value);
+
+    /**
+     * Set the maximum fuse ticks for this Creeper, where the maximum ticks 
+     * is the amount of time in which a creeper is allowed to be in the 
+     * primed state before exploding.
+     *
+     * @param ticks the new maximum fuse ticks
+     */
+    public void setMaxFuseTicks(int ticks);
+
+    /**
+     * Get the maximum fuse ticks for this Creeper, where the maximum ticks 
+     * is the amount of time in which a creeper is allowed to be in the 
+     * primed state before exploding.
+     *
+     * @return the maximum fuse ticks
+     */
+    public int getMaxFuseTicks();
+
+    /**
+     * Set the explosion radius in which this Creeper's explosion will affect.
+     *
+     * @param radius the new explosion radius
+     */
+    public void setExplosionRadius(int radius);
+
+    /**
+     * Get the explosion radius in which this Creeper's explosion will affect.
+     *
+     * @return the explosion radius
+     */
+    public int getExplosionRadius();
 }
