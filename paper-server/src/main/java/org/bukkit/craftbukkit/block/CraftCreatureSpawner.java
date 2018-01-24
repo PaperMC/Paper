6f9c0bf3cb456ab08a6aba477175f89cd7f4cfc5
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-01-24 17:18:25 +1100

    Expand CreatureSpawner API
    By: Senmori <thesenmori@gmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftCreatureSpawner.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftCreatureSpawner.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.block;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.server.MinecraftKey;
 import net.minecraft.server.TileEntityMobSpawner;
 import org.bukkit.Material;
@@ -56,5 +57,68 @@ public class CraftCreatureSpawner extends CraftBlockEntityState<TileEntityMobSpa
     @Override
     public void setDelay(int delay) {
         this.getSnapshot().getSpawner().spawnDelay = delay;
+    }
+
+    @Override
+    public int getMinSpawnDelay() {
+        return this.getSnapshot().getSpawner().minSpawnDelay;
+    }
+
+    @Override
+    public void setMinSpawnDelay(int spawnDelay) {
+        Preconditions.checkArgument(spawnDelay <= getMaxSpawnDelay(), "Minimum Spawn Delay must be less than or equal to Maximum Spawn Delay");
+        this.getSnapshot().getSpawner().minSpawnDelay = spawnDelay;
+    }
+
+    @Override
+    public int getMaxSpawnDelay() {
+        return this.getSnapshot().getSpawner().maxSpawnDelay;
+    }
+
+    @Override
+    public void setMaxSpawnDelay(int spawnDelay) {
+        Preconditions.checkArgument(spawnDelay > 0, "Maximum Spawn Delay must be greater than 0.");
+        Preconditions.checkArgument(spawnDelay >= getMinSpawnDelay(), "Maximum Spawn Delay must be greater than or equal to Minimum Spawn Delay");
+        this.getSnapshot().getSpawner().maxSpawnDelay = spawnDelay;
+    }
+
+    @Override
+    public int getMaxNearbyEntities() {
+        return this.getSnapshot().getSpawner().maxNearbyEntities;
+    }
+
+    @Override
+    public void setMaxNearbyEntities(int maxNearbyEntities) {
+        this.getSnapshot().getSpawner().maxNearbyEntities = maxNearbyEntities;
+    }
+
+    @Override
+    public int getSpawnCount() {
+        return this.getSnapshot().getSpawner().spawnCount;
+    }
+
+    @Override
+    public void setSpawnCount(int count) {
+        this.getSnapshot().getSpawner().spawnCount = count;
+    }
+
+    @Override
+    public int getRequiredPlayerRange() {
+        return this.getSnapshot().getSpawner().requiredPlayerRange;
+    }
+
+    @Override
+    public void setRequiredPlayerRange(int requiredPlayerRange) {
+        this.getSnapshot().getSpawner().requiredPlayerRange = requiredPlayerRange;
+    }
+
+    @Override
+    public int getSpawnRange() {
+        return this.getSnapshot().getSpawner().spawnRange;
+    }
+
+    @Override
+    public void setSpawnRange(int spawnRange) {
+        this.getSnapshot().getSpawner().spawnRange = spawnRange;
     }
 }
