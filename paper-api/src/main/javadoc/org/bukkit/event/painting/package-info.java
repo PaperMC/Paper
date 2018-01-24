92ef4319223a22a435815a40cf2704191baec380
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-01-24 17:12:02 +1100

    Remove outdated package-info
    By: md_5 <git@md-5.net>

--- paper-api/src/main/javadoc/org/bukkit/event/painting/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * {@link org.bukkit.event.Event Events} relating to {@link
- * org.bukkit.entity.Painting paintings}, but deprecated for more general
- * {@link org.bukkit.event.hanging hanging} events.
- */
-package org.bukkit.event.painting;
-
