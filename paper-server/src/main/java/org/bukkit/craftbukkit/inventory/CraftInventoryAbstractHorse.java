a626609da1bcb83a3447c719e52c4864e9b83309
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-01-28 09:55:16 +1100

    SPIGOT-2911: Implement AbstractHorseInventory
    By: md_5 <git@md-5.net>

---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryAbstractHorse.java
@@ -0,0 +1,22 @@
+package org.bukkit.craftbukkit.inventory;
+
+import net.minecraft.server.IInventory;
+import org.bukkit.inventory.AbstractHorseInventory;
+import org.bukkit.inventory.ItemStack;
+
+public class CraftInventoryAbstractHorse extends CraftInventory implements AbstractHorseInventory {
+
+    public CraftInventoryAbstractHorse(IInventory inventory) {
+        super(inventory);
+    }
+
+    @Override
+    public ItemStack getSaddle() {
+        return getItem(0);
+    }
+
+    @Override
+    public void setSaddle(ItemStack stack) {
+        setItem(0, stack);
+    }
+}
--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryHorse.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryHorse.java
@@ -4,22 +4,14 @@ import net.minecraft.server.IInventory;
 import org.bukkit.inventory.HorseInventory;
 import org.bukkit.inventory.ItemStack;
 
-public class CraftInventoryHorse extends CraftInventory implements HorseInventory {
+public class CraftInventoryHorse extends CraftInventoryAbstractHorse implements HorseInventory {
 
     public CraftInventoryHorse(IInventory inventory) {
         super(inventory);
     }
 
-    public ItemStack getSaddle() {
-        return getItem(0);
-    }
-
     public ItemStack getArmor() {
        return getItem(1);
-    }
-
-    public void setSaddle(ItemStack stack) {
-        setItem(0, stack);
     }
 
     public void setArmor(ItemStack stack) {
