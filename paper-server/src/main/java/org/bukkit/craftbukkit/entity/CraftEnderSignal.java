7453a5a9ebcc514cb06f22d7f656fadb5dbe87d6
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-02-03 17:48:41 +1100

    Expand EnderSignal API
    By: Senmori <thesenmori@gmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftEnderSignal.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftEnderSignal.java
@@ -1,6 +1,9 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
+import net.minecraft.server.BlockPosition;
 import net.minecraft.server.EntityEnderSignal;
+import org.bukkit.Location;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EnderSignal;
 import org.bukkit.entity.EntityType;
@@ -20,7 +23,39 @@ public class CraftEnderSignal extends CraftEntity implements EnderSignal {
         return "CraftEnderSignal";
     }
 
+    @Override
     public EntityType getType() {
         return EntityType.ENDER_SIGNAL;
+    }
+
+    @Override
+    public Location getTargetLocation() {
+        return new Location(getWorld(), getHandle().a, getHandle().b, getHandle().c, getHandle().yaw, getHandle().pitch); // PAIL rename targetX, targetY, targetZ
+    }
+
+    @Override
+    public void setTargetLocation(Location location) {
+        Preconditions.checkArgument(getWorld().equals(location.getWorld()), "Cannot target EnderSignal across worlds");
+        getHandle().a(new BlockPosition(location.getX(), location.getY(), location.getZ()));
+    }
+
+    @Override
+    public boolean getDropItem() {
+        return getHandle().e; // PAIL rename getDropItem
+    }
+
+    @Override
+    public void setDropItem(boolean shouldDropItem) {
+        getHandle().e = shouldDropItem; // PAIL rename getDropItem
+    }
+
+    @Override
+    public int getDespawnTimer() {
+        return getHandle().d; // PAIL rename despawnTimer
+    }
+
+    @Override
+    public void setDespawnTimer(int time) {
+        getHandle().d = time; // PAIL rename despawnTimer
     }
 }
