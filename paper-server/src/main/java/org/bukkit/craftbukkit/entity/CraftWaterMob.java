4ce8158350594ae2d173bf50f09583c86229d7ba
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-07-16 08:51:34 +1000

    Fish / Dolphin should be WaterMob, and WaterMob Creature
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftWaterMob.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftWaterMob.java
@@ -3,10 +3,9 @@ package org.bukkit.craftbukkit.entity;
 import net.minecraft.server.EntityWaterAnimal;
 
 import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.WaterMob;
 
-public class CraftWaterMob extends CraftLivingEntity implements WaterMob {
+public class CraftWaterMob extends CraftCreature implements WaterMob {
 
     public CraftWaterMob(CraftServer server, EntityWaterAnimal entity) {
         super(server, entity);
