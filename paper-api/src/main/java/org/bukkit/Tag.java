93510b0edfbdc1a38121bc5df904c275be05063d
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-07-15 16:47:59 +1000

    Add clarifying documentation to Tag
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/Tag.java
+++ paper-api/src/main/java/org/bukkit/Tag.java
@@ -6,6 +6,9 @@ import java.util.Set;
  * Represents a tag that may be defined by the server or a resource pack to
  * group like things together.
  *
+ * Note that whilst all tags defined within this interface must be present in
+ * implementations, their existence is not guaranteed across future versions.
+ *
  * @param <T> the type of things grouped by this tag
  */
 public interface Tag<T extends Keyed> {
