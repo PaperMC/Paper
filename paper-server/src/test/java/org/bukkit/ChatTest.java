844204b34528a355756a5499f19b9e8e98469148
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-07-17 16:42:30 -0600

    SPIGOT-4035: Enforce case-sensitivity for chat format code matching.
    This maintains case-insensitivity for URL matching, and adds a unit test to verify correct conversion via CraftChatMessage.
    
    By: Hex <hex@hex.lc>

--- paper-server/src/test/java/org/bukkit/ChatTest.java
+++ paper-server/src/test/java/org/bukkit/ChatTest.java
@@ -1,9 +1,12 @@
 package org.bukkit;
 
 import net.minecraft.server.EnumChatFormat;
+import net.minecraft.server.IChatBaseComponent;
 import org.bukkit.craftbukkit.util.CraftChatMessage;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ChatTest {
 
@@ -18,5 +21,29 @@ public class ChatTest {
             Assert.assertNotNull(CraftChatMessage.getColor(format));
             Assert.assertEquals(format, CraftChatMessage.getColor(CraftChatMessage.getColor(format)));
         }
+    }
+
+    @Test
+    public void testColorConversion() {
+        String test = String.format("%1$sA%1$sa%1$sB%1$sb%1$sC%1$sc%1$sD%1$sd%1$sE%1$se%1$sZ%1$sz%1$s", ChatColor.COLOR_CHAR);
+        IChatBaseComponent name = CraftChatMessage.fromStringOrNull(test);
+        assertEquals(test.replace(String.valueOf(ChatColor.COLOR_CHAR), ""),
+                CraftChatMessage.fromComponent(name).replace(String.valueOf(ChatColor.COLOR_CHAR), ""));
+    }
+
+    @Test
+    public void testURLJsonConversion() {
+        IChatBaseComponent[] components;
+        components = CraftChatMessage.fromString("https://spigotmc.org/test Test Message");
+        assertEquals("TextComponent{text='', siblings=[TextComponent{text='https://spigotmc.org/test', siblings=[], style=Style{hasParent=true, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=ClickEvent{action=OPEN_URL, value='https://spigotmc.org/test'}, hoverEvent=null, insertion=null}}, TextComponent{text=' Test Message', siblings=[], style=Style{hasParent=true, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}], style=Style{hasParent=false, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}",
+                components[0].toString());
+
+        components = CraftChatMessage.fromString("123 " + ChatColor.GOLD + "https://spigotmc.org " + ChatColor.BOLD + "test");
+        assertEquals("TextComponent{text='', siblings=[TextComponent{text='123 ', siblings=[], style=Style{hasParent=true, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}, TextComponent{text='https://spigotmc.org', siblings=[], style=Style{hasParent=true, color=§6, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=ClickEvent{action=OPEN_URL, value='https://spigotmc.org'}, hoverEvent=null, insertion=null}}, TextComponent{text=' ', siblings=[], style=Style{hasParent=true, color=§6, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}, TextComponent{text='test', siblings=[], style=Style{hasParent=true, color=§6, bold=true, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}], style=Style{hasParent=false, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}",
+                components[0].toString());
+
+        components = CraftChatMessage.fromString("multiCase http://SpigotMC.ORg/SpOngeBobMeEMeGoESHeRE");
+        assertEquals("TextComponent{text='', siblings=[TextComponent{text='multiCase ', siblings=[], style=Style{hasParent=true, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}, TextComponent{text='http://SpigotMC.ORg/SpOngeBobMeEMeGoESHeRE', siblings=[], style=Style{hasParent=true, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=ClickEvent{action=OPEN_URL, value='http://SpigotMC.ORg/SpOngeBobMeEMeGoESHeRE'}, hoverEvent=null, insertion=null}}], style=Style{hasParent=false, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}",
+                components[0].toString());
     }
 }
