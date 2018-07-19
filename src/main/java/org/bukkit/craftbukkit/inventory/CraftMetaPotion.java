80fefcf5ef80c54e27324bb6c8e61f0bdc056f44
md_5 <git@md-5.net>
2018-07-19 11:34:19 +1000

    SPIGOT-4047: Ignore non-existent potions

--- src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaPotion.java
+++ src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaPotion.java
@@ -71,6 +71,11 @@ class CraftMetaPotion extends CraftMetaItem implements PotionMeta {
             for (int i = 0; i < length; i++) {
                 NBTTagCompound effect = list.getCompound(i);
                 PotionEffectType type = PotionEffectType.getById(effect.getByte(ID.NBT));
+                // SPIGOT-4047: Vanilla just disregards these
+                if (type == null) {
+                    continue;
+                }
+
                 int amp = effect.getByte(AMPLIFIER.NBT);
                 int duration = effect.getInt(DURATION.NBT);
                 boolean ambient = effect.getBoolean(AMBIENT.NBT);
