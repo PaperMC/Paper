3323421ffe18c0b14ee4142ba9ea626abe670092
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-07-20 09:58:45 +1000

    SPIGOT-4075: Add documentation to Levelled regarding fluids
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/data/Levelled.java
+++ paper-api/src/main/java/org/bukkit/block/data/Levelled.java
@@ -4,6 +4,12 @@ package org.bukkit.block.data;
  * 'level' represents the amount of fluid contained within this block, either by
  * itself or inside a cauldron.
  * <br>
+ * In the case of water and lava blocks the levels have special meanings: a
+ * level of 0 corresponds to a source block, 1-7 regular fluid heights, and 8-15
+ * to "falling" fluids. All falling fluids have the same behaviour, but the
+ * level corresponds to that of the block above them, equal to
+ * <code>this.level - 8</code>
+ * <br>
  * May not be higher than {@link #getMaximumLevel()}.
  */
 public interface Levelled extends BlockData {
