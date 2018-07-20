66b62a63b5d1e594b95bc365f61bdcae43b80c0d
md_5 <git@md-5.net>
2018-07-20 10:32:17 +1000

    Fix bad old API call in CraftBlockState

--- src/main/java/org/bukkit/craftbukkit/block/CraftBlockState.java
+++ src/main/java/org/bukkit/craftbukkit/block/CraftBlockState.java
@@ -178,7 +178,7 @@ public class CraftBlockState implements BlockState {
         );
 
         // Update levers etc
-        if (applyPhysics && getData() instanceof Attachable) {
+        if (false && applyPhysics && getData() instanceof Attachable) { // Call does not map to new API
             world.getHandle().applyPhysics(pos.shift(CraftBlock.blockFaceToNotch(((Attachable) getData()).getAttachedFace())), newBlock.getBlock());
         }
 
