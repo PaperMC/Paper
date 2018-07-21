3270034d95032b3a6690d762653d4a2fb3cac7af
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-07-20 23:37:03 -0500

    AnvilDamageEvent

--- src/main/java/net/minecraft/server/ContainerAnvil.java
+++ src/main/java/net/minecraft/server/ContainerAnvil.java
@@ -67,7 +67,16 @@ public class ContainerAnvil extends ContainerAnvilAbstract {
 
             if (!entityhuman.abilities.canInstantlyBuild && iblockdata.a((Tag) TagsBlock.ANVIL) && entityhuman.getRandom().nextFloat() < 0.12F) {
                 IBlockData iblockdata1 = BlockAnvil.c(iblockdata);
-
+                // Paper start
+                com.destroystokyo.paper.event.block.AnvilDamagedEvent event = new com.destroystokyo.paper.event.block.AnvilDamagedEvent(getBukkitView(), iblockdata1 != null ? org.bukkit.craftbukkit.block.data.CraftBlockData.fromData(iblockdata1) : null);
+                if (!event.callEvent()) {
+                    return;
+                } else if (event.getDamageState() == com.destroystokyo.paper.event.block.AnvilDamagedEvent.DamageState.BROKEN) {
+                    iblockdata1 = null;
+                } else {
+                    iblockdata1 = ((org.bukkit.craftbukkit.block.data.CraftBlockData) event.getDamageState().getMaterial().createBlockData()).getState().set(BlockAnvil.FACING, iblockdata.get(BlockAnvil.FACING));
+                }
+                // Paper end
                 if (iblockdata1 == null) {
                     world.a(blockposition, false);
                     world.triggerEffect(1029, blockposition, 0);
