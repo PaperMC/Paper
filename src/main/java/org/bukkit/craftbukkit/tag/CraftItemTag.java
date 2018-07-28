212fd0f230cb8eebe43e977f171e147c9d85ac71
md_5 <git@md-5.net>
2018-07-28 14:39:35 +1000

    SPIGOT-4166: Fix Tags being corrupted by early access (and not reflecting reloads)

--- src/main/java/org/bukkit/craftbukkit/tag/CraftItemTag.java
+++ src/main/java/org/bukkit/craftbukkit/tag/CraftItemTag.java
@@ -4,25 +4,24 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 import net.minecraft.server.Item;
+import net.minecraft.server.MinecraftKey;
+import net.minecraft.server.TagsServer;
 import org.bukkit.Material;
-import org.bukkit.Tag;
 import org.bukkit.craftbukkit.util.CraftMagicNumbers;
 
-public class CraftItemTag implements Tag<Material> {
+public class CraftItemTag extends CraftTag<Item, Material> {
 
-    private final net.minecraft.server.Tag<Item> handle;
-
-    public CraftItemTag(net.minecraft.server.Tag<Item> handle) {
-        this.handle = handle;
+    public CraftItemTag(TagsServer<Item> registry, MinecraftKey tag) {
+        super(registry, tag);
     }
 
     @Override
     public boolean isTagged(Material item) {
-        return handle.isTagged(CraftMagicNumbers.getItem(item));
+        return getHandle().isTagged(CraftMagicNumbers.getItem(item));
     }
 
     @Override
     public Set<Material> getValues() {
-        return Collections.unmodifiableSet(handle.a().stream().map((item) -> CraftMagicNumbers.getMaterial(item)).collect(Collectors.toSet()));
+        return Collections.unmodifiableSet(getHandle().a().stream().map((item) -> CraftMagicNumbers.getMaterial(item)).collect(Collectors.toSet()));
     }
 }
