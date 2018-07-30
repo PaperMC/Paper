cb8ec6543b80e5f15fb2949a036458af4fe78d6d
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-07-30 11:38:05 +0200

    SPIGOT-3917: Add EntityDropItemEvent
    By: Articdive <articdive@gmail.com>

---
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityDropItemEvent.java
@@ -0,0 +1,49 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Thrown when an entity creates an item drop.
+ */
+public class EntityDropItemEvent extends EntityEvent implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private final Item drop;
+    private boolean cancel = false;
+
+    public EntityDropItemEvent(final Entity entity, final Item drop) {
+        super(entity);
+        this.drop = drop;
+    }
+
+    /**
+     * Gets the Item created by the entity
+     *
+     * @return Item created by the entity
+     */
+    public Item getItemDrop() {
+        return drop;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
