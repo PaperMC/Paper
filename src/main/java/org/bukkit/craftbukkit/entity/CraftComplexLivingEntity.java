eab0f58cd80832f1e6c8e6ea8549e6a682d46ef9
Aikar <aikar@aikar.co>
2018-08-10 22:11:49 -0400

    Make EnderDragon implement Mob

--- src/main/java/org/bukkit/craftbukkit/entity/CraftComplexLivingEntity.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftComplexLivingEntity.java
@@ -1,17 +1,18 @@
 package org.bukkit.craftbukkit.entity;
 
+import net.minecraft.server.EntityInsentient;
 import net.minecraft.server.EntityLiving;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.ComplexLivingEntity;
 
-public abstract class CraftComplexLivingEntity extends CraftLivingEntity implements ComplexLivingEntity {
-    public CraftComplexLivingEntity(CraftServer server, EntityLiving entity) {
+public abstract class CraftComplexLivingEntity extends CraftMob implements ComplexLivingEntity { // Paper
+    public CraftComplexLivingEntity(CraftServer server, EntityInsentient entity) { // Paper
         super(server, entity);
     }
 
     @Override
-    public EntityLiving getHandle() {
-        return (EntityLiving) entity;
+    public EntityInsentient getHandle() { // Paper
+        return (EntityInsentient) entity; // Paper
     }
 
     @Override
