1141cf85fe41658a3d7162ebb4e9966c2d98e827
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-08-14 23:07:05 +0800

    Add ServerLoadEvent
    By: Pante <matthiasngeozhongsim@yahoo.com>

---
+++ paper-api/src/main/java/org/bukkit/event/server/ServerLoadEvent.java
@@ -0,0 +1,51 @@
+package org.bukkit.event.server;
+
+import org.bukkit.Warning;
+import org.bukkit.event.HandlerList;
+
+/**
+ * This event is called when either the server startup or reload has completed.
+ *
+ * @deprecated draft API
+ */
+@Deprecated
+@Warning(false)
+public class ServerLoadEvent extends ServerEvent {
+
+    /**
+     * Represents the context in which the enclosing event has been completed.
+     */
+    public enum LoadType {
+        STARTUP, RELOAD;
+    }
+
+    private static final HandlerList handlers = new HandlerList();
+    private final LoadType type;
+
+    /**
+     * Creates a {@code ServerLoadEvent} with a given loading type.
+     *
+     * @param type the context in which the server was loaded
+     */
+    public ServerLoadEvent(LoadType type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets the context in which the server was loaded.
+     *
+     * @return the context in which the server was loaded
+     */
+    public LoadType getType() {
+        return type;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
