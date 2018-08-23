68e76b43b8e4f215222add61ab70b35448f05191
miclebrick <miclebrick@outlook.com>
2018-08-23 11:45:32 -0400

    Optimize CraftBlockData Creation
    Avoids a hashmap lookup by cacheing a reference to the CraftBlockData
    and cloning it when one is needed.

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/data/CraftBlockData.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/data/CraftBlockData.java
@@ -579,7 +579,17 @@ public class CraftBlockData implements BlockData {
         return craft;
     }
 
+    // Paper start - optimize creating BlockData to not need a map lookup
+    static {
+        // Initialize cached data for all IBlockData instances after registration
+        Block.BLOCK_STATE_REGISTRY.iterator().forEachRemaining(net.minecraft.world.level.block.state.BlockState::createCraftBlockData);
+    }
     public static CraftBlockData fromData(net.minecraft.world.level.block.state.BlockState data) {
+        return data.createCraftBlockData();
+    }
+
+    public static CraftBlockData createData(net.minecraft.world.level.block.state.BlockState data) {
+        // Paper end
         return CraftBlockData.MAP.getOrDefault(data.getBlock().getClass(), CraftBlockData::new).apply(data);
     }
 
