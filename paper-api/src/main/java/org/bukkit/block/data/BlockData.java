f8d657dc453c74911c67f2318e4b824e6b98db44
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-08-25 14:52:01 +1000

    Remove draft status from BlockData merge / matches
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/data/BlockData.java
+++ paper-api/src/main/java/org/bukkit/block/data/BlockData.java
@@ -32,9 +32,7 @@ public interface BlockData extends Cloneable {
      *
      * @param data the data to merge from
      * @return a new instance of this blockdata with the merged data
-     * @deprecated draft API
      */
-    @Deprecated
     BlockData merge(BlockData data);
 
     /**
@@ -51,9 +49,7 @@ public interface BlockData extends Cloneable {
      *
      * @param data the data to match against (normally a parsed constant)
      * @return if there is a match
-     * @deprecated draft API
      */
-    @Deprecated
     boolean matches(BlockData data);
 
     /**
