3edc1cf4e077c5b777d0be6786e25cf6a854d9cf
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-08-26 18:48:32 +1000

    SPIGOT-4301: Fix more invalid enchants
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/util/CraftNamespacedKey.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/util/CraftNamespacedKey.java
@@ -9,6 +9,9 @@ public final class CraftNamespacedKey {
     }
 
     public static NamespacedKey fromStringOrNull(String string) {
+        if (string == null || string.isEmpty()) {
+            return null;
+        }
         MinecraftKey minecraft = MinecraftKey.a(string);
         return (minecraft == null) ? null : fromMinecraft(minecraft);
     }
