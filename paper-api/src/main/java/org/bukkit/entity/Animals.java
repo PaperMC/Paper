0eec8c8041363b7553f0dcaa7d8ca5956f7fd235
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-09-01 19:27:49 +1000

    SPIGOT-4338: breedCause API
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/Animals.java
+++ paper-api/src/main/java/org/bukkit/entity/Animals.java
@@ -1,6 +1,25 @@
 package org.bukkit.entity;
 
+import java.util.UUID;
+
 /**
  * Represents an Animal.
  */
-public interface Animals extends Ageable {}
+public interface Animals extends Ageable {
+
+    /**
+     * Get the UUID of the entity that caused this entity to enter the
+     * {@link #canBreed()} state.
+     *
+     * @return uuid if set, or null
+     */
+    UUID getBreedCause();
+
+    /**
+     * Set the UUID of the entity that caused this entity to enter the
+     * {@link #canBreed()} state.
+     *
+     * @param uuid new uuid, or null
+     */
+    void setBreedCause(UUID uuid);
+}
