eb271172cece3250d0cba99da3c28fbfb6a5c368
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-09-01 14:12:20 +1000

    Fix unbound shaped recipe ingredients
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftRecipe.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftRecipe.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.inventory;
 
+import net.minecraft.server.RecipeItemStack;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.RecipeChoice;
@@ -9,8 +10,10 @@ public interface CraftRecipe extends Recipe {
     void addToCraftingManager();
 
     default net.minecraft.server.RecipeItemStack toNMS(RecipeChoice bukkit) {
-        if (bukkit instanceof RecipeChoice.MaterialChoice) {
-            return new net.minecraft.server.RecipeItemStack(((RecipeChoice.MaterialChoice) bukkit).getChoices().stream().map((mat) -> new net.minecraft.server.RecipeItemStack.StackProvider(CraftItemStack.asNMSCopy(new ItemStack(mat)))));
+        if (bukkit == null) {
+            return RecipeItemStack.a;
+        } else if (bukkit instanceof RecipeChoice.MaterialChoice) {
+            return new RecipeItemStack(((RecipeChoice.MaterialChoice) bukkit).getChoices().stream().map((mat) -> new net.minecraft.server.RecipeItemStack.StackProvider(CraftItemStack.asNMSCopy(new ItemStack(mat)))));
         } else {
             throw new IllegalArgumentException("Unknown recipe stack instance " + bukkit);
         }
