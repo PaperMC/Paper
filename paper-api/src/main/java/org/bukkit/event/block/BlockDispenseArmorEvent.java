0c68176c25587b986b2fcdb0306e82b85d9623f4
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-09-08 00:14:49 -0400

    Add BlockDispenseArmorEvent
    By: Parker Hawke <hawkeboyz2@hotmail.com>

---
+++ paper-api/src/main/java/org/bukkit/event/block/BlockDispenseArmorEvent.java
@@ -0,0 +1,37 @@
+package org.bukkit.event.block;
+
+import org.bukkit.Warning;
+import org.bukkit.block.Block;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+/**
+ * Called when an equippable item is dispensed from a block and equipped on a
+ * nearby entity.
+ * <p>
+ * If a Block Dispense Armor event is cancelled, the equipment will not be
+ * equipped on the target entity.
+ *
+ * @deprecated draft API
+ */
+@Deprecated
+@Warning(false)
+public class BlockDispenseArmorEvent extends BlockDispenseEvent {
+
+    private final LivingEntity target;
+
+    public BlockDispenseArmorEvent(Block block, ItemStack dispensed, LivingEntity target) {
+        super(block, dispensed, new Vector(0, 0, 0));
+        this.target = target;
+    }
+
+    /**
+     * Get the living entity on which the armor was dispensed.
+     *
+     * @return the target entity
+     */
+    public LivingEntity getTargetEntity() {
+        return target;
+    }
+}
