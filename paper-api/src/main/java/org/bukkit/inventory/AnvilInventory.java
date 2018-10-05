37945d9ca3a165c4b7401d4cfcfe492f350b4dbb
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-10-05 15:04:22 -0400

    Add maximum repair cost API to AnvilInventory
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-api/src/main/java/org/bukkit/inventory/AnvilInventory.java
+++ paper-api/src/main/java/org/bukkit/inventory/AnvilInventory.java
@@ -26,4 +26,24 @@ public interface AnvilInventory extends Inventory {
      * @param levels the experience cost
      */
     void setRepairCost(int levels);
+
+    /**
+     * Get the maximum experience cost (in levels) to be allowed by the current
+     * repair. If the result of {@link #getRepairCost()} exceeds the returned
+     * value, the repair result will be air to due being "too expensive".
+     * <p>
+     * By default, this level is set to 40. Players in creative mode ignore the
+     * maximum repair cost.
+     *
+     * @return the maximum experience cost
+     */
+    int getMaximumRepairCost();
+
+    /**
+     * Set the maximum experience cost (in levels) to be allowed by the current
+     * repair. The default value set by vanilla Minecraft is 40.
+     *
+     * @param levels the maximum experience cost
+     */
+    void setMaximumRepairCost(int levels);
 }
