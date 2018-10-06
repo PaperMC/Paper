6fb32733f27b8a38cd9fed38419ae3ff9343a3d1
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-10-06 10:55:06 +1000

    Clarify Colorable docs further
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/material/Colorable.java
+++ paper-api/src/main/java/org/bukkit/material/Colorable.java
@@ -10,7 +10,8 @@ public interface Colorable {
     /**
      * Gets the color of this object.
      * <br>
-     * This may be null to represent the default color of an object.
+     * This may be null to represent the default color of an object, if the
+     * object has a special default color (e.g Shulkers).
      *
      * @return The DyeColor of this object.
      */
@@ -19,7 +20,8 @@ public interface Colorable {
     /**
      * Sets the color of this object to the specified DyeColor.
      * <br>
-     * This may be null to represent the default color of an object.
+     * This may be null to represent the default color of an object, if the
+     * object has a special default color (e.g Shulkers).
      *
      * @param color The color of the object, as a DyeColor.
      */
