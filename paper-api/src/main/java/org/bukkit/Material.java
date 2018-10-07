119dfa37f0c51142cba77b094c9bf6d623938cf6
Aikar <aikar@aikar.co>
2018-10-06 21:14:29 -0400

    Material API additions

--- paper-api/src/main/java/org/bukkit/Material.java
+++ paper-api/src/main/java/org/bukkit/Material.java
@@ -135,6 +135,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * An enum of all material IDs accepted by the official server and client
  */
+@SuppressWarnings({"DeprecatedIsStillUsed", "deprecation"}) // Paper
 public enum Material implements Keyed, Translatable {
     //<editor-fold desc="Materials" defaultstate="collapsed">
     AIR(9648, 0),
@@ -4816,6 +4817,22 @@ public enum Material implements Keyed, Translatable {
             return Registry.BLOCK.get(material.key);
         });
     }
+
+    // Paper start
+
+    /**
+     * @return If the type is either AIR, CAVE_AIR or VOID_AIR
+     */
+    public boolean isEmpty() {
+        switch (this) {
+            case AIR:
+            case CAVE_AIR:
+            case VOID_AIR:
+                return true;
+        }
+        return false;
+    }
+    // Paper end
 
     /**
      * Do not use for any reason.
