7b3013d59bad9262c4577a5fc580177667000ed1
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-10-12 03:47:26 -0500

    Add more Witch API

--- paper-api/src/main/java/org/bukkit/entity/Witch.java
+++ paper-api/src/main/java/org/bukkit/entity/Witch.java
@@ -2,6 +2,11 @@ package org.bukkit.entity;
 
 import com.destroystokyo.paper.entity.RangedEntity;
 
+// Paper start
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+// Paper end
+
 /**
  * Represents a Witch
  */
@@ -13,4 +18,38 @@ public interface Witch extends Raider, RangedEntity { // Paper
      * @return whether the witch is drinking a potion
      */
     boolean isDrinkingPotion();
+
+    // Paper start
+    /**
+     * Get time remaining (in ticks) the Witch is drinking a potion
+     *
+     * @return Time remaining (in ticks)
+     */
+    int getPotionUseTimeLeft();
+
+    /**
+     * Set time remaining (in ticks) that the Witch is drinking a potion.
+     * <p>
+     * This only has an effect while the Witch is drinking a potion.
+     *
+     * @param ticks Time in ticks remaining
+     * @see #isDrinkingPotion
+     */
+    void setPotionUseTimeLeft(int ticks);
+
+    /**
+     * Get the potion the Witch is drinking
+     *
+     * @return The potion the witch is drinking
+     */
+    @org.jetbrains.annotations.NotNull
+    ItemStack getDrinkingPotion();
+
+    /**
+     * Set the potion the Witch should drink
+     *
+     * @param potion Potion to drink
+     */
+    void setDrinkingPotion(@Nullable ItemStack potion);
+    // Paper end
 }
