b1ab9ad45242770c7c512f020614b9ff21dd43c3
Zach Brown <zach@zachbr.io>
2018-10-26 23:58:58 -0400

    [CI-SKIP] Stop trying to standardize commit hashes
    This appears to be creating more problems than its actually solving.
    Oh and put my echo back in here just because I want it there.

--- scripts/rebuildPatches.sh
+++ scripts/rebuildPatches.sh
@@ -12,10 +12,7 @@ echo "Rebuilding patch files from current fork state..."
 function cleanupPatches {
     cd "$1"
     for patch in *.patch; do
-        sed -Ei.bak 's/index [a-f0-9]+\.\.[a-f0-9]+/index 7ac07ac07ac0..7ac07ac07ac0/g' "$patch"
-        sed -Ei.bak 's/^From [a-f0-9]{32,}/From 7ac07ac07ac07ac07ac07ac07ac07ac07ac07ac0/g' "$patch"
-        rm "$patch.bak"
-        $gitcmd add -A $patch
+        echo "$patch"
         gitver=$(tail -n 2 "$patch" | grep -ve "^$" | tail -n 1)
         diffs=$($gitcmd diff --staged "$patch" | grep --color=none -E "^(\+|\-)" | grep --color=none -Ev "(From [a-f0-9]{32,}|\-\-\- a|\+\+\+ b|^.index)")
 
