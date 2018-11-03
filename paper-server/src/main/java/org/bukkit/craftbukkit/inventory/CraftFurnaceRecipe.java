ed68e3973ee39521d1bea5e9eda7aa7efbc16516
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2018-11-03 21:04:33 +1100

    SPIGOT-4438: Add choice API to furnace recipes
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftFurnaceRecipe.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftFurnaceRecipe.java
@@ -1,24 +1,22 @@
 package org.bukkit.craftbukkit.inventory;
 
-import java.util.stream.Stream;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.RecipeItemStack;
-
 import org.bukkit.NamespacedKey;
 import org.bukkit.craftbukkit.util.CraftNamespacedKey;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 
 public class CraftFurnaceRecipe extends FurnaceRecipe implements CraftRecipe {
-    public CraftFurnaceRecipe(NamespacedKey key, ItemStack result, ItemStack source, float experience, int cookingTime) {
-        super(key, result, source.getType(), source.getDurability(), experience, cookingTime);
+    public CraftFurnaceRecipe(NamespacedKey key, ItemStack result, RecipeChoice source, float experience, int cookingTime) {
+        super(key, result, source, experience, cookingTime);
     }
 
     public static CraftFurnaceRecipe fromBukkitRecipe(FurnaceRecipe recipe) {
         if (recipe instanceof CraftFurnaceRecipe) {
             return (CraftFurnaceRecipe) recipe;
         }
-        CraftFurnaceRecipe ret = new CraftFurnaceRecipe(recipe.getKey(), recipe.getResult(), recipe.getInput(), recipe.getExperience(), recipe.getCookingTime());
+        CraftFurnaceRecipe ret = new CraftFurnaceRecipe(recipe.getKey(), recipe.getResult(), recipe.getInputChoice(), recipe.getExperience(), recipe.getCookingTime());
         ret.setGroup(recipe.getGroup());
         return ret;
     }
@@ -26,8 +24,7 @@ public class CraftFurnaceRecipe extends FurnaceRecipe implements CraftRecipe {
     @Override
     public void addToCraftingManager() {
         ItemStack result = this.getResult();
-        RecipeItemStack input = new RecipeItemStack(Stream.of(new RecipeItemStack.StackProvider(CraftItemStack.asNMSCopy(this.getInput()))));
 
-        MinecraftServer.getServer().getCraftingManager().a(new net.minecraft.server.FurnaceRecipe(CraftNamespacedKey.toMinecraft(this.getKey()), this.getGroup(), input, CraftItemStack.asNMSCopy(result), getExperience(), getCookingTime()));
+        MinecraftServer.getServer().getCraftingManager().a(new net.minecraft.server.FurnaceRecipe(CraftNamespacedKey.toMinecraft(this.getKey()), this.getGroup(), toNMS(this.getInputChoice()), CraftItemStack.asNMSCopy(result), getExperience(), getCookingTime()));
     }
 }
