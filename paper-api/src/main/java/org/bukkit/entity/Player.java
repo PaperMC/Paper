0face3d06ac9dd6b48812619e498a309a71194f5
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-11-23 14:32:28 +1100

    Remove no longer applicable caveats to setPlayerListName
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/Player.java
+++ paper-api/src/main/java/org/bukkit/entity/Player.java
@@ -63,23 +63,9 @@ public interface Player extends HumanEntity, Conversable, OfflinePlayer, PluginM
     /**
      * Sets the name that is shown on the in-game player list.
      * <p>
-     * The name cannot be longer than 16 characters, but {@link ChatColor} is
-     * supported.
-     * <p>
      * If the value is null, the name will be identical to {@link #getName()}.
-     * <p>
-     * This name is case sensitive and unique, two names with different casing
-     * will appear as two different people. If a player joins afterwards with
-     * a name that conflicts with a player's custom list name, the joining
-     * player's player list name will have a random number appended to it (1-2
-     * characters long in the default implementation). If the joining player's
-     * name is 15 or 16 characters long, part of the name will be truncated at
-     * the end to allow the addition of the two digits.
      *
      * @param name new player list name
-     * @throws IllegalArgumentException if the name is already used by someone
-     *     else
-     * @throws IllegalArgumentException if the length of the name is too long
      */
     public void setPlayerListName(String name);
 
