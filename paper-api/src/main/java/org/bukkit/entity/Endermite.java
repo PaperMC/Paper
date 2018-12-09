3f4b83c99692e4590fe3176028570e65f5256e71
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-12-10 10:39:39 +1100

    Add PlayerSpawned API for Endermites
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/Endermite.java
+++ paper-api/src/main/java/org/bukkit/entity/Endermite.java
@@ -1,4 +1,22 @@
 package org.bukkit.entity;
 
 public interface Endermite extends Monster {
+
+    /**
+     * Gets whether this Endermite was spawned by a player.
+     *
+     * An Endermite spawned by a player will be attacked by nearby Enderman.
+     *
+     * @return player spawned status
+     */
+    boolean isPlayerSpawned();
+
+    /**
+     * Sets whether this Endermite was spawned by a player.
+     *
+     * An Endermite spawned by a player will be attacked by nearby Enderman.
+     *
+     * @param playerSpawned player spawned status
+     */
+    void setPlayerSpawned(boolean playerSpawned);
 }
