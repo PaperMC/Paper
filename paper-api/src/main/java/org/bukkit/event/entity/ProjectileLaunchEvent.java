8be6b43c7d40c122248cf004f8fc9c2c46e837a0
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2018-12-26 08:00:00 +1100

    Make ProjectileLaunchEvent extend EntitySpawnEvent
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/entity/ProjectileLaunchEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/ProjectileLaunchEvent.java
@@ -3,13 +3,11 @@ package org.bukkit.event.entity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.HandlerList;
 
 /**
  * Called when a projectile is launched.
  */
-public class ProjectileLaunchEvent extends EntityEvent implements Cancellable {
-    private static final HandlerList handlers = new HandlerList();
+public class ProjectileLaunchEvent extends EntitySpawnEvent implements Cancellable {
     private boolean cancelled;
 
     public ProjectileLaunchEvent(Entity what) {
@@ -27,14 +25,5 @@ public class ProjectileLaunchEvent extends EntityEvent implements Cancellable {
     @Override
     public Projectile getEntity() {
         return (Projectile) entity;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return handlers;
-    }
-
-    public static HandlerList getHandlerList() {
-        return handlers;
     }
 }
