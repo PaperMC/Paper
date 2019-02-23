258a2ce7b8e0a86d2589b62e7f3b0b066738f866
Aikar <aikar@aikar.co>
2019-02-23 11:26:21 -0500

    Paper Utils

---
+++ paper-api/src/main/java/com/destroystokyo/paper/util/SneakyThrow.java
@@ -0,0 +1,19 @@
+package com.destroystokyo.paper.util;
+
+import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NullMarked;
+
+@ApiStatus.Internal
+@NullMarked
+public final class SneakyThrow {
+
+    public static void sneaky(final Throwable exception) {
+        SneakyThrow.throwSneaky(exception);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Throwable> void throwSneaky(final Throwable exception) throws T {
+        throw (T) exception;
+    }
+
+}
