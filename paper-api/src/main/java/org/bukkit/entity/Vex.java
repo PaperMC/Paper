a9891b05f19a8e4783ce6383dccfdb69fe3e0513
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2019-03-05 16:47:25 +1100

    SPIGOT-4650: Charging API for Vex
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/Vex.java
+++ paper-api/src/main/java/org/bukkit/entity/Vex.java
@@ -3,4 +3,23 @@ package org.bukkit.entity;
 /**
  * Represents a Vex.
  */
-public interface Vex extends Monster { }
+public interface Vex extends Monster {
+
+    /**
+     * Gets the charging state of this entity.
+     *
+     * When this entity is charging it will having a glowing red texture.
+     *
+     * @return charging state
+     */
+    boolean isCharging();
+
+    /**
+     * Sets the charging state of this entity.
+     *
+     * When this entity is charging it will having a glowing red texture.
+     *
+     * @param charging new state
+     */
+    void setCharging(boolean charging);
+}
