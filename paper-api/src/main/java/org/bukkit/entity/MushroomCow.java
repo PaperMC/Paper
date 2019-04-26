3eb336e441d98f2e60f3e33335a7fa0483390826
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2019-04-26 14:48:04 +1000

    Add variant API to MushroomCow
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/MushroomCow.java
+++ paper-api/src/main/java/org/bukkit/entity/MushroomCow.java
@@ -1,8 +1,38 @@
 package org.bukkit.entity;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Represents a mushroom {@link Cow}
  */
 public interface MushroomCow extends Cow {
 
+    /**
+     * Get the variant of this cow.
+     *
+     * @return cow variant
+     */
+    @NotNull
+    public Variant getVariant();
+
+    /**
+     * Set the variant of this cow.
+     *
+     * @param variant cow variant
+     */
+    public void setVariant(@NotNull Variant variant);
+
+    /**
+     * Represents the variant of a cow - ie its color.
+     */
+    public enum Variant {
+        /**
+         * Red mushroom cow.
+         */
+        RED,
+        /**
+         * Brown mushroom cow.
+         */
+        BROWN;
+    }
 }
