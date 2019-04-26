fb39d0943e3c3d52509a6507df91bf7378c7e7fc
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2019-04-27 09:30:16 +1000

    SPIGOT-4778: Bell Attachment is missing
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/data/type/Bell.java
+++ paper-api/src/main/java/org/bukkit/block/data/type/Bell.java
@@ -1,11 +1,27 @@
 package org.bukkit.block.data.type;
 
 import org.bukkit.block.data.Directional;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * 'attachment' denotes how the bell is attached to its block.
  */
 public interface Bell extends Directional {
+
+    /**
+     * Gets the value of the 'attachment' property.
+     *
+     * @return the 'attachment' value
+     */
+    @NotNull
+    Attachment getAttachment();
+
+    /**
+     * Sets the value of the 'attachment' property.
+     *
+     * @param attachment the new 'attachment' value
+     */
+    void setAttachment(@NotNull Attachment attachment);
 
     /**
      * What the bell is attached to.
