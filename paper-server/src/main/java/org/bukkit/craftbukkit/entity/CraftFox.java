4d73820fbbe05b76a2ea1eeff323a2d769a7295d
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2019-04-28 17:59:12 +1000

    SPIGOT-4803: Fox related APIs
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftFox.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftFox.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.server.EntityFox;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
@@ -24,5 +25,42 @@ public class CraftFox extends CraftAnimals implements Fox {
     @Override
     public String toString() {
         return "CraftFox";
+    }
+
+    @Override
+    public Type getFoxType() {
+        return Type.values()[getHandle().dV().ordinal()];
+    }
+
+    @Override
+    public void setFoxType(Type type) {
+        Preconditions.checkArgument(type != null, "type");
+
+        getHandle().a(EntityFox.Type.values()[type.ordinal()]);
+    }
+
+    @Override
+    public boolean isCrouching() {
+        return getHandle().ef();
+    }
+
+    @Override
+    public void setCrouching(boolean crouching) {
+        getHandle().t(crouching);
+    }
+
+    @Override
+    public boolean isSitting() {
+        return getHandle().dW();
+    }
+
+    @Override
+    public void setSitting(boolean sitting) {
+        getHandle().r(sitting);
+    }
+
+    @Override
+    public void setSleeping(boolean sleeping) {
+        getHandle().x(sleeping);
     }
 }
