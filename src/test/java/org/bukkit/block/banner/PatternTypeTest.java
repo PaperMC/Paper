d7d28b12447015d5f6e78b90ad7fa58fddedebc6
md_5 <git@md-5.net>
2019-04-30 15:20:11 +1000

    SPIGOT-4818: Unit test banner patterns

---
+++ src/test/java/org/bukkit/block/banner/PatternTypeTest.java
@@ -0,0 +1,33 @@
+package org.bukkit.block.banner;
+
+import junit.framework.Assert;
+import net.minecraft.server.EnumBannerPatternType;
+import org.bukkit.support.AbstractTestingBase;
+import org.junit.Test;
+
+public class PatternTypeTest extends AbstractTestingBase {
+
+    @Test
+    public void testToBukkit() {
+        for (EnumBannerPatternType nms : EnumBannerPatternType.values()) {
+            PatternType bukkit = PatternType.getByIdentifier(nms.b());
+
+            Assert.assertNotNull("No Bukkit banner for " + nms + " " + nms.b(), bukkit);
+        }
+    }
+
+    @Test
+    public void testToNMS() {
+        for (PatternType bukkit : PatternType.values()) {
+            EnumBannerPatternType found = null;
+            for (EnumBannerPatternType nms : EnumBannerPatternType.values()) {
+                if (bukkit.getIdentifier().equals(nms.b())) {
+                    found = nms;
+                    break;
+                }
+            }
+
+            Assert.assertNotNull("No NMS banner for " + bukkit + " " + bukkit.getIdentifier(), found);
+        }
+    }
+}
