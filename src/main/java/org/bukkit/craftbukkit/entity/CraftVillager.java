c441dba815351c36ffbfa3f37d64ef0291a2a777
md_5 <git@md-5.net>
2019-05-05 12:03:20 +1000

    SPIGOT-4834: API for Villager levels and experience

--- src/main/java/org/bukkit/craftbukkit/entity/CraftVillager.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftVillager.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import java.util.Locale;
 import net.minecraft.server.EntityVillager;
 import net.minecraft.server.IRegistry;
@@ -50,5 +51,29 @@ public class CraftVillager extends CraftAbstractVillager implements Villager {
     public void setVillagerType(Type type) {
         Validate.notNull(type);
         getHandle().setVillagerData(getHandle().getVillagerData().withType(IRegistry.VILLAGER_TYPE.get(CraftNamespacedKey.toMinecraft(type.getKey()))));
+    }
+
+    @Override
+    public int getVillagerLevel() {
+        return getHandle().getVillagerData().getLevel();
+    }
+
+    @Override
+    public void setVillagerLevel(int level) {
+        Preconditions.checkArgument(0 <= level && level <= 5, "level must be between [0, 5]");
+
+        getHandle().setVillagerData(getHandle().getVillagerData().withLevel(level));
+    }
+
+    @Override
+    public int getVillagerExperience() {
+        return getHandle().dV();
+    }
+
+    @Override
+    public void setVillagerExperience(int experience) {
+        Preconditions.checkArgument(experience >= 0, "Experience must be positive");
+
+        getHandle().t(experience);
     }
 }
