a7f92c83889b8ac1a1214335c041fb8937a03afe
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2019-05-23 20:48:20 +1000

    SPIGOT-4972: Cannot add contextual data to loot table API
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftLootTable.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftLootTable.java
@@ -90,7 +90,7 @@ public class CraftLootTable implements org.bukkit.loot.LootTable {
             builder.set(LootContextParameters.DAMAGE_SOURCE, DamageSource.playerAttack(nmsKiller));
         }
 
-        return builder.build(LootContextParameterSets.EMPTY);
+        return builder.build(getHandle().a());
     }
 
     @Override
