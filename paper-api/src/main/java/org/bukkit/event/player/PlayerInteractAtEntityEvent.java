9fc26a2efbb08e4575e9ac8a852670eb7fde7c35
simpleauthority <jacob@algorithmjunkie.com>
2019-07-11 10:35:56 -0700

    Amend PlayerInteractAtEntityEvent javadoc for ArmorStands

--- paper-api/src/main/java/org/bukkit/event/player/PlayerInteractAtEntityEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerInteractAtEntityEvent.java
@@ -13,6 +13,9 @@ import org.jetbrains.annotations.NotNull;
  * <br>
  * Note that the client may sometimes spuriously send this packet in addition to {@link PlayerInteractEntityEvent}.
  * Users are advised to listen to this (parent) class unless specifically required.
+ * <br>
+ * Note that interacting with Armor Stands fires this event only and not its parent and as such users are expressly required
+ * to listen to this event for that scenario.
  */
 public class PlayerInteractAtEntityEvent extends PlayerInteractEntityEvent {
     private static final HandlerList handlers = new HandlerList();
