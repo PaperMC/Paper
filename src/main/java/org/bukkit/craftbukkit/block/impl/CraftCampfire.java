656701746abf477beb78cb6108298b20dc053cf1
md_5 <git@md-5.net>
2019-12-09 12:52:46 +1100

    SPIGOT-5417: Campfire should implement Directional

--- src/main/java/org/bukkit/craftbukkit/block/impl/CraftCampfire.java
+++ src/main/java/org/bukkit/craftbukkit/block/impl/CraftCampfire.java
@@ -3,7 +3,7 @@
  */
 package org.bukkit.craftbukkit.block.impl;
 
-public final class CraftCampfire extends org.bukkit.craftbukkit.block.data.CraftBlockData implements org.bukkit.block.data.type.Campfire, org.bukkit.block.data.Lightable, org.bukkit.block.data.Waterlogged {
+public final class CraftCampfire extends org.bukkit.craftbukkit.block.data.CraftBlockData implements org.bukkit.block.data.type.Campfire, org.bukkit.block.data.Directional, org.bukkit.block.data.Lightable, org.bukkit.block.data.Waterlogged {
 
     public CraftCampfire() {
         super();
@@ -25,6 +25,25 @@ public final class CraftCampfire extends org.bukkit.craftbukkit.block.data.Craft
     @Override
     public void setSignalFire(boolean signalFire) {
         set(SIGNAL_FIRE, signalFire);
+    }
+
+    // org.bukkit.craftbukkit.block.data.CraftDirectional
+
+    private static final net.minecraft.server.BlockStateEnum<?> FACING = getEnum(net.minecraft.server.BlockCampfire.class, "facing");
+
+    @Override
+    public org.bukkit.block.BlockFace getFacing() {
+        return get(FACING, org.bukkit.block.BlockFace.class);
+    }
+
+    @Override
+    public void setFacing(org.bukkit.block.BlockFace facing) {
+        set(FACING, facing);
+    }
+
+    @Override
+    public java.util.Set<org.bukkit.block.BlockFace> getFaces() {
+        return getValues(FACING, org.bukkit.block.BlockFace.class);
     }
 
     // org.bukkit.craftbukkit.block.data.CraftLightable
