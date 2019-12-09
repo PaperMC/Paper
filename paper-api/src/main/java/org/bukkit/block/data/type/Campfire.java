36f60e00a9ab6ed31378adb81c26752c9b20f0f8
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2019-12-09 12:52:34 +1100

    SPIGOT-5417: Campfire should implement Directional
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/data/type/Campfire.java
+++ paper-api/src/main/java/org/bukkit/block/data/type/Campfire.java
@@ -1,5 +1,6 @@
 package org.bukkit.block.data.type;
 
+import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.Lightable;
 import org.bukkit.block.data.Waterlogged;
 
@@ -7,7 +8,7 @@ import org.bukkit.block.data.Waterlogged;
  * 'signal_fire' denotes whether the fire is extra smokey due to having a hay
  * bale placed beneath it.
  */
-public interface Campfire extends Lightable, Waterlogged {
+public interface Campfire extends Directional, Lightable, Waterlogged {
 
     /**
      * Gets the value of the 'signal_fire' property.
