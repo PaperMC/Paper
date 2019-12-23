b99f20b1b47fc8ea30a4319bfce5b83680e07f9c
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2019-12-23 12:34:30 +1100

    SPIGOT-5413: Add TrustedPlayer API for foxes
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/Fox.java
+++ paper-api/src/main/java/org/bukkit/entity/Fox.java
@@ -1,6 +1,7 @@
 package org.bukkit.entity;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * What does the fox say?
@@ -42,6 +43,40 @@ public interface Fox extends Animals, Sittable {
      * @param sleeping true if sleeping
      */
     void setSleeping(boolean sleeping);
+
+    /**
+     * Gets the first trusted player.
+     *
+     * @return the owning AnimalTamer, or null if not owned
+     */
+    @Nullable
+    public AnimalTamer getFirstTrustedPlayer();
+
+    /**
+     * Set the first trusted player.
+     * <p>
+     * The first trusted player may only be removed after the second.
+     *
+     * @param player the AnimalTamer to be trusted
+     */
+    public void setFirstTrustedPlayer(@Nullable AnimalTamer player);
+
+    /**
+     * Gets the second trusted player.
+     *
+     * @return the owning AnimalTamer, or null if not owned
+     */
+    @Nullable
+    public AnimalTamer getSecondTrustedPlayer();
+
+    /**
+     * Set the second trusted player.
+     * <p>
+     * The second trusted player may only be added after the first.
+     *
+     * @param player the AnimalTamer to be trusted
+     */
+    public void setSecondTrustedPlayer(@Nullable AnimalTamer player);
 
     /**
      * Represents the various different fox types there are.
