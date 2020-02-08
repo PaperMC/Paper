6b39c5d0839c6200a221f6f5e271fe0440fdb8cf
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2020-02-09 10:47:11 +1100

    SPIGOT-5468: Improve Beehive TileEntity API
    By: ShaneBee <shanebolenback@me.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftBeehive.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftBeehive.java
@@ -1,12 +1,18 @@
 package org.bukkit.craftbukkit.block;
 
 import com.google.common.base.Preconditions;
+import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.server.BlockPosition;
+import net.minecraft.server.Entity;
 import net.minecraft.server.TileEntityBeehive;
+import net.minecraft.server.TileEntityBeehive.ReleaseStatus;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Beehive;
+import org.bukkit.craftbukkit.entity.CraftBee;
+import org.bukkit.entity.Bee;
 
 public class CraftBeehive extends CraftBlockEntityState<TileEntityBeehive> implements Beehive {
 
@@ -28,5 +34,53 @@ public class CraftBeehive extends CraftBlockEntityState<TileEntityBeehive> imple
     public void setFlower(Location location) {
         Preconditions.checkArgument(location == null || this.getWorld().equals(location.getWorld()), "Flower must be in same world");
         getSnapshot().flowerPos = (location == null) ? null : new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
+    @Override
+    public boolean isFull() {
+        return getSnapshot().isFull();
+    }
+
+    @Override
+    public boolean isSedated() {
+        return isPlaced() && getSnapshot().k(); // PAIL rename isSedated
+    }
+
+    @Override
+    public int getEntityCount() {
+        return getSnapshot().j(); // PAIL rename beeCount
+    }
+
+    @Override
+    public int getMaxEntities() {
+        return getSnapshot().maxBees;
+    }
+
+    @Override
+    public void setMaxEntities(int max) {
+        Preconditions.checkArgument(max > 0, "Max bees must be more than 0");
+
+        getSnapshot().maxBees = max;
+    }
+
+    @Override
+    public List<Bee> releaseEntities() {
+        List<Bee> bees = new ArrayList<>();
+
+        if (isPlaced()) {
+            TileEntityBeehive beehive = ((TileEntityBeehive) this.getTileEntityFromWorld());
+            for (Entity bee : beehive.releaseBees(this.getHandle(), ReleaseStatus.BEE_RELEASED, true)) {
+                bees.add((Bee) bee.getBukkitEntity());
+            }
+        }
+
+        return bees;
+    }
+
+    @Override
+    public void addEntity(Bee entity) {
+        Preconditions.checkArgument(entity != null, "Entity must not be null");
+
+        getSnapshot().a(((CraftBee) entity).getHandle(), false); // PAIL rename addBee
     }
 }
