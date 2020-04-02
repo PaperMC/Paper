a25f99d2546d9c3be9b9975b73491a6f5b3df320
Aikar <aikar@aikar.co>
2020-04-02 17:07:06 -0400

    Updated Upstream (Bukkit/CraftBukkit)
    Upstream has released updates that appears to apply and compile correctly.
    This update has not been tested by PaperMC and as with ANY update, please do your own testing
    
    Bukkit Changes:
    122289ff Add FaceAttachable interface to handle Grindstone facing in common with Switches
    a6db750e SPIGOT-5647: ZombieVillager entity should have getVillagerType()
    
    CraftBukkit Changes:
    bbe3d58e SPIGOT-5650: Lectern.setPage(int) causes a NullPointerException
    3075579f Add FaceAttachable interface to handle Grindstone facing in common with Switches
    95bd4238 SPIGOT-5647: ZombieVillager entity should have getVillagerType()
    4d975ac3 SPIGOT-5617: setBlockData does not work when NotPlayEvent is called by redstone current

--- scripts/upstreamMerge.sh
+++ scripts/upstreamMerge.sh
@@ -16,7 +16,7 @@ function update {
     $gitcmd fetch && $gitcmd clean -fd && $gitcmd reset --hard origin/master
     refRemote=$(git rev-parse HEAD)
     cd ../
-    $gitcmd add $1
+    $gitcmd add $1 -f
     refHEAD=$(getRef HEAD "$workdir/$1")
     echo "$1 $refHEAD - $refRemote"
     if [ "$refHEAD" != "$refRemote" ]; then
