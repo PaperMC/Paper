95cf31d8b84ccdf781ea67652058001c6d289de1
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-06-25 19:58:19 +1000

    SPIGOT-5803: Add Piglin/Zoglin Baby API
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/entity/Piglin.java
+++ paper-api/src/main/java/org/bukkit/entity/Piglin.java
@@ -3,4 +3,19 @@ package org.bukkit.entity;
 /**
  * Represents a Piglin.
  */
-public interface Piglin extends Monster { }
+public interface Piglin extends Monster {
+
+    /**
+     * Gets whether the piglin is a baby
+     *
+     * @return Whether the piglin is a baby
+     */
+    public boolean isBaby();
+
+    /**
+     * Sets whether the piglin is a baby
+     *
+     * @param flag Whether the piglin is a baby
+     */
+    public void setBaby(boolean flag);
+}
--- paper-api/src/main/java/org/bukkit/entity/Zoglin.java
+++ paper-api/src/main/java/org/bukkit/entity/Zoglin.java
@@ -3,4 +3,19 @@ package org.bukkit.entity;
 /**
  * Represents a Zoglin.
  */
-public interface Zoglin extends Monster { }
+public interface Zoglin extends Monster {
+
+    /**
+     * Gets whether the zoglin is a baby
+     *
+     * @return Whether the zoglin is a baby
+     */
+    public boolean isBaby();
+
+    /**
+     * Sets whether the zoglin is a baby
+     *
+     * @param flag Whether the zoglin is a baby
+     */
+    public void setBaby(boolean flag);
+}
