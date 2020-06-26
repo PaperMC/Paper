6c951c6abbd17b7f8f92be6cd26bdc08011903d4
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2020-06-26 10:49:28 +1000

    #671: Implement PersistentDataContainer#getKeys()
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/persistence/CraftPersistentDataContainer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/persistence/CraftPersistentDataContainer.java
@@ -1,9 +1,11 @@
 package org.bukkit.craftbukkit.persistence;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.Set;
 import net.minecraft.server.NBTBase;
 import net.minecraft.server.NBTTagCompound;
 import org.apache.commons.lang.Validate;
@@ -68,6 +70,20 @@ public final class CraftPersistentDataContainer implements PersistentDataContain
     public <T, Z> Z getOrDefault(NamespacedKey key, PersistentDataType<T, Z> type, Z defaultValue) {
         Z z = get(key, type);
         return z != null ? z : defaultValue;
+    }
+
+    @Override
+    public Set<NamespacedKey> getKeys() {
+        Set<NamespacedKey> keys = new HashSet<>();
+
+        this.customDataTags.keySet().forEach(key -> {
+            String[] keyData = key.split(":", 2);
+            if (keyData.length == 2) {
+                keys.add(new NamespacedKey(keyData[0], keyData[1]));
+            }
+        });
+
+        return keys;
     }
 
     @Override
