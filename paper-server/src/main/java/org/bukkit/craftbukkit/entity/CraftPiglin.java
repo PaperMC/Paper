c19042a233ae99b10dc2fcfc1808f924b65b2c10
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2020-06-29 09:58:04 +1000

    SPIGOT-5803: Implement the missing methods for the Piglin API.
    By: ceezuns <49824660+ceezuns@users.noreply.github.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPiglin.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPiglin.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.server.EntityPiglin;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
@@ -9,6 +10,47 @@ public class CraftPiglin extends CraftMonster implements Piglin {
 
     public CraftPiglin(CraftServer server, EntityPiglin entity) {
         super(server, entity);
+    }
+
+    @Override
+    public boolean isImmuneToZombification() {
+        return getHandle().eT();
+    }
+
+    @Override
+    public void setImmuneToZombification(boolean flag) {
+        getHandle().t(flag);
+    }
+
+    @Override
+    public boolean isAbleToHunt() {
+        return getHandle().bC;
+    }
+
+    @Override
+    public void setIsAbleToHunt(boolean flag) {
+        getHandle().bC = flag;
+    }
+
+    @Override
+    public int getConversionTime() {
+        Preconditions.checkState(isConverting(), "Entity not converting");
+        return getHandle().bA;
+    }
+
+    @Override
+    public void setConversionTime(int time) {
+        if (time < 0) {
+            getHandle().bA = -1;
+            getHandle().t(false);
+        } else {
+            getHandle().bA = time;
+        }
+    }
+
+    @Override
+    public boolean isConverting() {
+        return getHandle().eO(); // PAIL rename isConverting()
     }
 
     @Override
