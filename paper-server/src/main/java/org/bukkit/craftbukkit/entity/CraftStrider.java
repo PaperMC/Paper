8b8fc993bd71d7c496a3163f54c4ce7a9faf97d0
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2020-06-30 09:54:28 +1000

    #686: Expand Strider and Steerable entity API
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftStrider.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftStrider.java
@@ -1,6 +1,8 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.server.EntityStrider;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Strider;
@@ -9,6 +11,60 @@ public class CraftStrider extends CraftAnimals implements Strider {
 
     public CraftStrider(CraftServer server, EntityStrider entity) {
         super(server, entity);
+    }
+
+    @Override
+    public boolean isShivering() {
+        return getHandle().eL(); // PAIL rename isShivering
+    }
+
+    @Override
+    public void setShivering(boolean shivering) {
+        this.getHandle().t(shivering); // PAIL rename setShivering
+    }
+
+    @Override
+    public boolean hasSaddle() {
+        return getHandle().hasSaddle();
+    }
+
+    @Override
+    public void setSaddle(boolean saddled) {
+        getHandle().bA.setSaddle(saddled);
+    }
+
+    @Override
+    public int getBoostTicks() {
+        return getHandle().bA.a ? getHandle().bA.c : 0;
+    }
+
+    @Override
+    public void setBoostTicks(int ticks) {
+        Preconditions.checkArgument(ticks >= 0, "ticks must be >= 0");
+
+        getHandle().bA.setBoostTicks(ticks);
+    }
+
+    @Override
+    public int getCurrentBoostTicks() {
+        return getHandle().bA.a ? getHandle().bA.b : 0;
+    }
+
+    @Override
+    public void setCurrentBoostTicks(int ticks) {
+        if (!getHandle().bA.a) {
+            return;
+        }
+
+        int max = getHandle().bA.c;
+        Preconditions.checkArgument(ticks >= 0 && ticks <= max, "boost ticks must not exceed 0 or %d (inclusive)", max);
+
+        this.getHandle().bA.b = ticks;
+    }
+
+    @Override
+    public Material getSteerMaterial() {
+        return Material.WARPED_FUNGUS_ON_A_STICK;
     }
 
     @Override
