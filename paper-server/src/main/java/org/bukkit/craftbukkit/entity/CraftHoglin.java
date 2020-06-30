f5ea9fececc4bb9a2303872aa631bfbefbe1ef00
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2020-06-30 12:03:19 +1000

    SPIGOT-5899: Hoglins API similar to Piglins
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftHoglin.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftHoglin.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.server.EntityHoglin;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
@@ -9,6 +10,47 @@ public class CraftHoglin extends CraftAnimals implements Hoglin {
 
     public CraftHoglin(CraftServer server, EntityHoglin entity) {
         super(server, entity);
+    }
+
+    @Override
+    public boolean isImmuneToZombification() {
+        return getHandle().eW();
+    }
+
+    @Override
+    public void setImmuneToZombification(boolean flag) {
+        getHandle().t(flag);
+    }
+
+    @Override
+    public boolean isAbleToBeHunted() {
+        return getHandle().bA;
+    }
+
+    @Override
+    public void setIsAbleToBeHunted(boolean flag) {
+        getHandle().bA = flag;
+    }
+
+    @Override
+    public int getConversionTime() {
+        Preconditions.checkState(isConverting(), "Entity not converting");
+        return getHandle().bz;
+    }
+
+    @Override
+    public void setConversionTime(int time) {
+        if (time < 0) {
+            getHandle().bz = -1;
+            getHandle().t(false);
+        } else {
+            getHandle().bz = time;
+        }
+    }
+
+    @Override
+    public boolean isConverting() {
+        return getHandle().eO(); // PAIL rename isConverting()
     }
 
     @Override
