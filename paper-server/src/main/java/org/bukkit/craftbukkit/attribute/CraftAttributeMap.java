25ed9118c55860d5fc913707fe44606cba3ef3f1
Aikar <aikar@aikar.co>
2020-07-01 04:50:22 -0400

    Convert legacy attributes in Item Meta

--- paper-server/src/main/java/org/bukkit/craftbukkit/attribute/CraftAttributeMap.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/attribute/CraftAttributeMap.java
@@ -9,6 +9,20 @@ import org.bukkit.attribute.AttributeInstance;
 public class CraftAttributeMap implements Attributable {
 
     private final AttributeMap handle;
+    // Paper start - convert legacy attributes
+    private static final com.google.common.collect.ImmutableMap<String, String> legacyNMS = com.google.common.collect.ImmutableMap.<String, String>builder().put("generic.maxHealth", "generic.max_health").put("Max Health", "generic.max_health").put("zombie.spawnReinforcements", "zombie.spawn_reinforcements").put("Spawn Reinforcements Chance", "zombie.spawn_reinforcements").put("horse.jumpStrength", "horse.jump_strength").put("Jump Strength", "horse.jump_strength").put("generic.followRange", "generic.follow_range").put("Follow Range", "generic.follow_range").put("generic.knockbackResistance", "generic.knockback_resistance").put("Knockback Resistance", "generic.knockback_resistance").put("generic.movementSpeed", "generic.movement_speed").put("Movement Speed", "generic.movement_speed").put("generic.flyingSpeed", "generic.flying_speed").put("Flying Speed", "generic.flying_speed").put("generic.attackDamage", "generic.attack_damage").put("generic.attackKnockback", "generic.attack_knockback").put("generic.attackSpeed", "generic.attack_speed").put("generic.armorToughness", "generic.armor_toughness").build();
+
+    public static String convertIfNeeded(String nms) {
+        if (nms == null) {
+            return null;
+        }
+        nms = legacyNMS.getOrDefault(nms, nms);
+        if (!nms.toLowerCase(java.util.Locale.ROOT).equals(nms) || nms.indexOf(' ') != -1) {
+            return null;
+        }
+        return nms;
+    }
+    // Paper end
 
     public CraftAttributeMap(AttributeMap handle) {
         this.handle = handle;
--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaItem.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaItem.java
@@ -816,7 +816,7 @@ class CraftMetaItem implements ItemMeta, Damageable, Repairable, BlockDataMeta {
 
             AttributeModifier attribMod = CraftAttributeInstance.convert(nmsModifier);
 
-            String attributeName = entry.getString(CraftMetaItem.ATTRIBUTES_IDENTIFIER.NBT);
+            String attributeName = org.bukkit.craftbukkit.attribute.CraftAttributeMap.convertIfNeeded(entry.getString(CraftMetaItem.ATTRIBUTES_IDENTIFIER.NBT)); // Paper
             if (attributeName == null || attributeName.isEmpty()) {
                 continue;
             }
