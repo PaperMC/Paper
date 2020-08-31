394a95bb69c8f9b83c078e7956dbad6724f74701
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-08-31 18:39:04 +1000

    #533: Add consumed item, hand and consumeItem boolean to EntityShootBowEvent
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-api/src/main/java/org/bukkit/event/entity/EntityShootBowEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityShootBowEvent.java
@@ -2,8 +2,10 @@ package org.bukkit.event.entity;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,15 +16,21 @@ import org.jetbrains.annotations.Nullable;
 public class EntityShootBowEvent extends EntityEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private final ItemStack bow;
+    private final ItemStack consumable;
     private Entity projectile;
+    private final EquipmentSlot hand;
     private final float force;
+    private boolean consumeItem;
     private boolean cancelled;
 
-    public EntityShootBowEvent(@NotNull final LivingEntity shooter, @Nullable final ItemStack bow, @NotNull final Entity projectile, final float force) {
+    public EntityShootBowEvent(@NotNull final LivingEntity shooter, @Nullable final ItemStack bow, @Nullable final ItemStack consumable, @NotNull final Entity projectile, @NotNull final EquipmentSlot hand, final float force, final boolean consumeItem) {
         super(shooter);
         this.bow = bow;
+        this.consumable = consumable;
         this.projectile = projectile;
+        this.hand = hand;
         this.force = force;
+        this.consumeItem = consumeItem;
     }
 
     @NotNull
@@ -39,6 +47,19 @@ public class EntityShootBowEvent extends EntityEvent implements Cancellable {
     @Nullable
     public ItemStack getBow() {
         return bow;
+    }
+
+    /**
+     * Get the ItemStack to be consumed in this event (if any).
+     *
+     * For instance, bows will consume an arrow ItemStack in a player's
+     * inventory.
+     *
+     * @return the consumable item
+     */
+    @Nullable
+    public ItemStack getConsumable() {
+        return consumable;
     }
 
     /**
@@ -61,12 +82,48 @@ public class EntityShootBowEvent extends EntityEvent implements Cancellable {
     }
 
     /**
+     * Get the hand from which the bow was shot.
+     *
+     * @return the hand
+     */
+    @NotNull
+    public EquipmentSlot getHand() {
+        return hand;
+    }
+
+    /**
      * Gets the force the arrow was launched with
      *
      * @return bow shooting force, up to 1.0
      */
     public float getForce() {
         return force;
+    }
+
+    /**
+     * Set whether or not the consumable item should be consumed in this event.
+     *
+     * If set to false, it is recommended that a call to
+     * {@link Player#updateInventory()} is made as the client may disagree with
+     * the server's decision to not consume a consumable item.
+     * <p>
+     * This value is ignored for entities where items are not required
+     * (skeletons, pillagers, etc.) or with crossbows (as no item is being
+     * consumed).
+     *
+     * @param consumeItem whether or not to consume the item
+     */
+    public void setConsumeItem(boolean consumeItem) {
+        this.consumeItem = consumeItem;
+    }
+
+    /**
+     * Get whether or not the consumable item should be consumed in this event.
+     *
+     * @return true if consumed, false otherwise
+     */
+    public boolean shouldConsumeItem() {
+        return consumeItem;
     }
 
     @Override
