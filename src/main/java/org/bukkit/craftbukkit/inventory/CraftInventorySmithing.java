13fc33f73249ee2bd8edd5ac3657be65383bdf60
Phoenix616 <max@themoep.de>
2020-09-03 08:18:30 +1000

    #744: Add ability to get location from smithing inventory

--- src/main/java/org/bukkit/craftbukkit/inventory/CraftInventorySmithing.java
+++ src/main/java/org/bukkit/craftbukkit/inventory/CraftInventorySmithing.java
@@ -1,11 +1,20 @@
 package org.bukkit.craftbukkit.inventory;
 
 import net.minecraft.server.IInventory;
+import org.bukkit.Location;
 import org.bukkit.inventory.SmithingInventory;
 
 public class CraftInventorySmithing extends CraftResultInventory implements SmithingInventory {
 
-    public CraftInventorySmithing(IInventory inventory, IInventory resultInventory) {
+    private final Location location;
+
+    public CraftInventorySmithing(Location location, IInventory inventory, IInventory resultInventory) {
         super(inventory, resultInventory);
+        this.location = location;
+    }
+
+    @Override
+    public Location getLocation() {
+        return location;
     }
 }
