33b17b05328354ffe95efd1f8beaecb41f564a2e
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-09-12 08:32:51 +1000

    Update deprecation message for PotionMeta.setMainEffect
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/inventory/meta/PotionMeta.java
+++ paper-api/src/main/java/org/bukkit/inventory/meta/PotionMeta.java
@@ -80,7 +80,7 @@ public interface PotionMeta extends ItemMeta {
      *
      * @param type the potion effect type to move
      * @return true if the potion meta changed as a result of this call
-     * @deprecated use {@link org.bukkit.potion.PotionType#PotionType}
+     * @deprecated use {@link #setBasePotionData(org.bukkit.potion.PotionData)}
      */
     @Deprecated
     boolean setMainEffect(@NotNull PotionEffectType type);
