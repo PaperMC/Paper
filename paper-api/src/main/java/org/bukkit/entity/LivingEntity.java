fbccd65ba82068315c57d63ab9d5f736f72537cc
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-09-19 17:43:52 +1000

    #548: LivingEntity - add methods for getting/setting invisibility
    By: Shane Bee <shanebolenback@me.com>

--- paper-api/src/main/java/org/bukkit/entity/LivingEntity.java
+++ paper-api/src/main/java/org/bukkit/entity/LivingEntity.java
@@ -591,4 +591,18 @@ public interface LivingEntity extends Attributable, Damageable, ProjectileSource
      */
     @NotNull
     public EntityCategory getCategory();
+
+    /**
+     * Sets whether the entity is invisible or not.
+     *
+     * @param invisible If the entity is invisible
+     */
+    public void setInvisible(boolean invisible);
+
+    /**
+     * Gets whether the entity is invisible or not.
+     *
+     * @return Whether the entity is invisible
+     */
+    public boolean isInvisible();
 }
