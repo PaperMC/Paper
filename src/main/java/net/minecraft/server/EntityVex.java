Akarin-project/Akarin

Vex#getSummoner API
Get's the NPC that summoned this Vex

src/main/java/net/minecraft/server/EntityVex.java
@@ -86,6 +86,7 @@ public class EntityVex extends EntityMonster {
 
     }
 
+    public EntityInsentient getOwner() { return l(); } // Paper - OBFHELPER
     public EntityInsentient l() {
         return this.b;
     }

PaperMC/Paper

src/main/java/net/minecraft/server/EntityVex.java
@@ -89,6 +89,7 @@ public class EntityVex extends EntityMonster {
 
     }
 
+    public EntityInsentient getOwner() { return eK(); } // Paper - OBFHELPER
     public EntityInsentient eK() {
         return this.c;
     }