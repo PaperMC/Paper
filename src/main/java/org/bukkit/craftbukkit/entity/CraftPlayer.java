Machine-Maker/PaperHistoryTest

Block Entity#remove from being called on Players
This doesn't result in the same behavior as other entities and causes
several problems. Anyone ever complain about the "Cannot send chat
message" thing? That's one of the issues this causes, among others.

If a plugin developer can come up with a valid reason to call this on a
Player we will look at limiting the scope of this change. It appears to
be unintentional in the few cases we've seen so far.

server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -2557,6 +2557,15 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     public void resetCooldown() {
         getHandle().resetAttackStrengthTicker();
     }
+
+    @Override
+    public void remove() {
+        if (this.getHandle().getClass().equals(ServerPlayer.class)) { // special case for NMS plugins inheriting
+            throw new UnsupportedOperationException("Calling Entity#remove on players produces undefined (bad) behavior");
+        } else {
+            super.remove();
+        }
+    }
     // Paper end
     // Spigot start
     private final Player.Spigot spigot = new Player.Spigot()

PaperMC/Paper

src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -2015,6 +2015,15 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     public void resetCooldown() {
         getHandle().resetAttackCooldown();
     }
+
+    @Override
+    public void remove() {
+        if (this.getHandle().getClass().equals(EntityPlayer.class)) { // special case for NMS plugins inheriting
+            throw new UnsupportedOperationException("Calling Entity#remove on players produces undefined (bad) behavior");
+        } else {
+            super.remove();
+        }
+    }
     // Paper end
 
     // Spigot start