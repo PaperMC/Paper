Akarin-project/Akarin

Configurable cactus and reed natural growth heights

src/main/java/net/minecraft/server/BlockCactus.java
@@ -29,7 +29,7 @@ public class BlockCactus extends Block {
                     ;
                 }
 
-                if (i < 3) {
+            if (i < world.paperConfig.cactusMaxHeight) { // Paper - Configurable growth height
                     int j = (Integer) iblockdata.get(BlockCactus.AGE);
 
                     if (j >= (byte) range(3, ((100.0F / world.spigotConfig.cactusModifier) * 15) + 0.5F, 15)) { // Spigot

src/main/java/net/minecraft/server/BlockReed.java
@@ -25,7 +25,7 @@ public class BlockReed extends Block {
                 ;
             }
 
-            if (i < 3) {
+                if (i < world.paperConfig.reedMaxHeight) { // Paper - Configurable growth height
                 int j = (Integer) iblockdata.get(BlockReed.AGE);
 
                 if (j >= (byte) range(3, ((100.0F / world.spigotConfig.caneModifier) * 15) + 0.5F, 15)) { // Spigot

PaperMC/Paper

src/main/java/net/minecraft/server/BlockCactus.java
@@ -35,7 +35,7 @@ public class BlockCactus extends Block {
                 ;
             }
 
-            if (i < 3) {
+            if (i < worldserver.paperConfig.cactusMaxHeight) { // Paper - Configurable growth height
                 int j = (Integer) iblockdata.get(BlockCactus.AGE);
 
                 if (j >= (byte) range(3, ((100.0F / worldserver.spigotConfig.cactusModifier) * 15) + 0.5F, 15)) { // Spigot

src/main/java/net/minecraft/server/BlockReed.java
@@ -35,7 +35,7 @@ public class BlockReed extends Block {
                 ;
             }
 
-            if (i < 3) {
+                if (i < worldserver.paperConfig.reedMaxHeight) { // Paper - Configurable growth height
                 int j = (Integer) iblockdata.get(BlockReed.AGE);
 
                 if (j >= (byte) range(3, ((100.0F / worldserver.spigotConfig.caneModifier) * 15) + 0.5F, 15)) { // Spigot