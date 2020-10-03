copypastel/spigot-craftbukkit

Clear Packet Queue on Disconnect

src/main/java/net/minecraft/server/NetworkManager.java
@@ -317,6 +317,7 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
                 } else if (this.i() != null) {
                     this.i().a(new ChatComponentText("Disconnected"));
                 }
+                this.i.clear(); // Free up packet queue.
             }
 
         }

PaperMC/Paper

src/main/java/net/minecraft/server/NetworkManager.java
@@ -322,6 +322,7 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
                 } else if (this.j() != null) {
                     this.j().a(new ChatMessage("multiplayer.disconnect.generic"));
                 }
+                this.packetQueue.clear(); // Free up packet queue.
             }
 
         }