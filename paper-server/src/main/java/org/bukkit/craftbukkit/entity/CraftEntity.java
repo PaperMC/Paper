972b6a6f186b613203929ff7da71a54e43d31078
William Blake Galbreath <Blake.Galbreath@GMail.com>
2020-10-03 21:39:16 -0500

    Entity#isTicking

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftEntity.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftEntity.java
@@ -1058,4 +1058,11 @@ public abstract class CraftEntity implements org.bukkit.entity.Entity {
         return getHandle().isInLava();
     }
     // Paper end - entity liquid API
+
+    // Paper start - isTicking API
+    @Override
+    public boolean isTicking() {
+        return getHandle().isTicking();
+    }
+    // Paper end - isTicking API
 }
