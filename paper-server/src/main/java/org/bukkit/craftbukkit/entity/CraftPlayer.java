5eb09a7d931162bdaa9430a0f4e65e1ad0b4a835
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2020-10-06 18:36:43 +1100

    SPIGOT-6168: Fix error with Player.getBedSpawnLocation when world is unloaded while server running
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -720,14 +720,14 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
 
     @Override
     public Location getBedSpawnLocation() {
-        World world = getHandle().server.getWorldServer(getHandle().getSpawnDimension()).getWorld();
+        WorldServer world = getHandle().server.getWorldServer(getHandle().getSpawnDimension());
         BlockPosition bed = getHandle().getSpawn();
 
         if (world != null && bed != null) {
-            Optional<Vec3D> spawnLoc = EntityHuman.getBed(((CraftWorld) world).getHandle(), bed, getHandle().getSpawnAngle(), getHandle().isSpawnForced(), true);
+            Optional<Vec3D> spawnLoc = EntityHuman.getBed(world, bed, getHandle().getSpawnAngle(), getHandle().isSpawnForced(), true);
             if (spawnLoc.isPresent()) {
                 Vec3D vec = spawnLoc.get();
-                return new Location(world, vec.x, vec.y, vec.z);
+                return new Location(world.getWorld(), vec.x, vec.y, vec.z);
             }
         }
         return null;
