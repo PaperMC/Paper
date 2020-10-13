32e338b51321e57338974c1c645b1b208d61a53f
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-10-13 18:59:25 +1100

    #554: PoweredMinecart - add getter/setter for fuel
    By: Shane Bee <shanebolenback@me.com>

--- paper-api/src/main/java/org/bukkit/entity/minecart/PoweredMinecart.java
+++ paper-api/src/main/java/org/bukkit/entity/minecart/PoweredMinecart.java
@@ -7,4 +7,18 @@ import org.bukkit.entity.Minecart;
  * player deposits {@link org.bukkit.Material#COAL fuel}.
  */
 public interface PoweredMinecart extends Minecart {
+
+    /**
+     * Get the number of ticks until the minecart runs out of fuel.
+     *
+     * @return Number of ticks until the minecart runs out of fuel
+     */
+    public int getFuel();
+
+    /**
+     * Set the number of ticks until the minecart runs out of fuel.
+     *
+     * @param fuel Number of ticks until the minecart runs out of fuel
+     */
+    public void setFuel(int fuel);
 }
