73f42f8bd42eac08b452718e2859c8492deb778f
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-10-17 17:37:10 +1100

    #501: Add PersistentDataHolder to Chunk
    By: Nesaak <52047222+Nesaak@users.noreply.github.com>

--- paper-api/src/main/java/org/bukkit/Chunk.java
+++ paper-api/src/main/java/org/bukkit/Chunk.java
@@ -5,13 +5,14 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
+import org.bukkit.persistence.PersistentDataHolder;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a chunk of blocks
  */
-public interface Chunk {
+public interface Chunk extends PersistentDataHolder {
 
     /**
      * Gets the X-coordinate of this chunk
