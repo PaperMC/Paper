8079a5520c2e4cc93ef83cb79c59d8ebd0faedd0
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2020-11-06 18:47:40 +1100

    #559: Add wait time modification for FishHook
    By: Airtheon <Airtheonthesalion@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/FishHook.java
+++ paper-api/src/main/java/org/bukkit/entity/FishHook.java
@@ -4,6 +4,75 @@ package org.bukkit.entity;
  * Represents a fishing hook.
  */
 public interface FishHook extends Projectile {
+
+    /**
+     * Get the minimum number of ticks one has to wait for a fish biting.
+     * <p>
+     * The default is 100 ticks (5 seconds).<br>
+     * Note that this is before applying lure.
+     *
+     * @return Minimum number of ticks one has to wait for a fish biting
+     */
+    public int getMinWaitTime();
+
+    /**
+     * Set the minimum number of ticks one has to wait for a fish biting.
+     * <p>
+     * The default is 100 ticks (5 seconds).<br>
+     * Note that this is before applying lure.
+     *
+     * @param minWaitTime Minimum number of ticks one has to wait for a fish
+     * biting
+     */
+    public void setMinWaitTime(int minWaitTime);
+
+    /**
+     * Get the maximum number of ticks one has to wait for a fish biting.
+     * <p>
+     * The default is 600 ticks (30 seconds).<br>
+     * Note that this is before applying lure.
+     *
+     * @return Maximum number of ticks one has to wait for a fish biting
+     */
+    public int getMaxWaitTime();
+
+    /**
+     * Set the maximum number of ticks one has to wait for a fish biting.
+     * <p>
+     * The default is 600 ticks (30 seconds).<br>
+     * Note that this is before applying lure.
+     *
+     * @param maxWaitTime Maximum number of ticks one has to wait for a fish
+     * biting
+     */
+    public void setMaxWaitTime(int maxWaitTime);
+
+    /**
+     * Get whether the lure enchantment should be applied to reduce the wait
+     * time.
+     * <p>
+     * The default is true.<br>
+     * Lure reduces the wait time by 100 ticks (5 seconds) for each level of the
+     * enchantment.
+     *
+     * @return Whether the lure enchantment should be applied to reduce the wait
+     * time
+     */
+    public boolean getApplyLure();
+
+    /**
+     * Set whether the lure enchantment should be applied to reduce the wait
+     * time.
+     * <p>
+     * The default is true.<br>
+     * Lure reduces the wait time by 100 ticks (5 seconds) for each level of the
+     * enchantment.
+     *
+     * @param applyLure Whether the lure enchantment should be applied to reduce
+     * the wait time
+     */
+    public void setApplyLure(boolean applyLure);
+
     /**
      * Gets the chance of a fish biting.
      * <p>
