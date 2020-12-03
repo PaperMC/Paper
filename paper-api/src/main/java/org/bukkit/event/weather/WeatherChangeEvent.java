547577ccb6f565fefdbef7151265ef651e542b37
Jake Potrebic <jake.m.potrebic@gmail.com>
2020-12-02 18:25:31 -0800

    Add cause to Weather/ThunderChangeEvents

--- paper-api/src/main/java/org/bukkit/event/weather/WeatherChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/weather/WeatherChangeEvent.java
@@ -12,10 +12,20 @@ public class WeatherChangeEvent extends WeatherEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private boolean canceled;
     private final boolean to;
+    // Paper start
+    private final Cause cause;
 
+    public WeatherChangeEvent(@NotNull final World world, final boolean to, @NotNull Cause cause) {
+        super(world);
+        this.to = to;
+        this.cause = cause;
+    }
+
+    @Deprecated // Paper end
     public WeatherChangeEvent(@NotNull final World world, final boolean to) {
         super(world);
         this.to = to;
+        this.cause = Cause.UNKNOWN; // Paper
     }
 
     @Override
@@ -47,4 +57,23 @@ public class WeatherChangeEvent extends WeatherEvent implements Cancellable {
     public static HandlerList getHandlerList() {
         return handlers;
     }
+    // Paper start
+    /**
+     * Gets the cause of the weather change.
+     *
+     * @return the weather change cause
+     */
+    @NotNull
+    public Cause getCause() {
+        return cause;
+    }
+
+    public enum Cause {
+        COMMAND,
+        NATURAL,
+        SLEEP,
+        PLUGIN,
+        UNKNOWN
+    }
+    // Paper end
 }
