533940ccc1b043e8fb7689ce00277d390c69168c
Simon Gardling <titaniumtown@gmail.com>
2020-12-29 16:56:25 -0500

    update patch

--- Paper-MojangAPI/src/main/java/com/destroystokyo/paper/brigadier/BukkitBrigadierCommand.java
+++
@@ -1,9 +0,0 @@
-package com.destroystokyo.paper.brigadier;
-
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
-
-import java.util.function.Predicate;
-
-public interface BukkitBrigadierCommand <S extends BukkitBrigadierCommandSource> extends Command<S>, Predicate<S>, SuggestionProvider<S> {
-}
