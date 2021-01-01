904d0ddd37943e490637bea78b699e2d83f98d6c
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2021-01-02 08:49:22 +1100

    #778: Added get/set ItemStack methods to trident
    By: Auxilor <will@willfp.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftTrident.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftTrident.java
@@ -2,8 +2,10 @@ package org.bukkit.craftbukkit.entity;
 
 import net.minecraft.server.EntityThrownTrident;
 import org.bukkit.craftbukkit.CraftServer;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Trident;
+import org.bukkit.inventory.ItemStack;
 
 public class CraftTrident extends CraftArrow implements Trident {
 
@@ -14,6 +16,16 @@ public class CraftTrident extends CraftArrow implements Trident {
     @Override
     public EntityThrownTrident getHandle() {
         return (EntityThrownTrident) super.getHandle();
+    }
+
+    @Override
+    public ItemStack getItem() {
+        return CraftItemStack.asBukkitCopy(getHandle().trident);
+    }
+
+    @Override
+    public void setItem(ItemStack itemStack) {
+        getHandle().trident = CraftItemStack.asNMSCopy(itemStack);
     }
 
     @Override
