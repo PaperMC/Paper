d7224acea02ebb7f193f29d2ad6b5973189e5c13
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2021-02-15 19:30:12 +1100

    #581: Add methods to modify despawn delay for wandering villagers
    By: DiamondDagger590 <diamonddagger590@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/WanderingTrader.java
+++ paper-api/src/main/java/org/bukkit/entity/WanderingTrader.java
@@ -3,4 +3,29 @@ package org.bukkit.entity;
 /**
  * Represents a wandering trader NPC
  */
-public interface WanderingTrader extends AbstractVillager { }
+public interface WanderingTrader extends AbstractVillager {
+
+    /**
+     * Gets the despawn delay before this {@link WanderingTrader} is forcibly
+     * despawned.
+     *
+     * If this is less than or equal to 0, then the trader will not be
+     * despawned.
+     *
+     * @return The despawn delay before this {@link WanderingTrader} is forcibly
+     * despawned
+     */
+    public int getDespawnDelay();
+
+    /**
+     * Sets the despawn delay before this {@link WanderingTrader} is forcibly
+     * despawned.
+     *
+     * If this is less than or equal to 0, then the trader will not be
+     * despawned.
+     *
+     * @param despawnDelay The new despawn delay before this
+     * {@link WanderingTrader} is forcibly despawned
+     */
+    public void setDespawnDelay(int despawnDelay);
+}
