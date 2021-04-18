8cbaba0a8ab095b495a1492df016768117304f5d
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2021-04-18 18:03:23 +1000

    SPIGOT-6078: Add SmithItemEvent and expand SmithingInventory API
    By: Julian van den Berkmortel <julianvdberkmortel@outlook.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventorySmithing.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventorySmithing.java
@@ -1,7 +1,10 @@
 package org.bukkit.craftbukkit.inventory;
 
 import net.minecraft.world.IInventory;
+import net.minecraft.world.item.crafting.IRecipe;
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.SmithingInventory;
 
 public class CraftInventorySmithing extends CraftResultInventory implements SmithingInventory {
@@ -16,5 +19,21 @@ public class CraftInventorySmithing extends CraftResultInventory implements Smit
     @Override
     public Location getLocation() {
         return location;
+    }
+
+    @Override
+    public ItemStack getResult() {
+        return getItem(2);
+    }
+
+    @Override
+    public void setResult(ItemStack item) {
+        setItem(2, item);
+    }
+
+    @Override
+    public Recipe getRecipe() {
+        IRecipe recipe = getInventory().getCurrentRecipe();
+        return (recipe == null) ? null : recipe.toBukkitRecipe();
     }
 }
