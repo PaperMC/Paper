aa829fc936d3c53da1af48a71d0aa5e07e6f2163
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2021-04-19 18:47:42 +1000

    SPIGOT-6388: Add API to get and set the state of the "Shulker" entity
    By: Julian van den Berkmortel <julianvdberkmortel@outlook.com>

--- paper-api/src/main/java/org/bukkit/entity/Shulker.java
+++ paper-api/src/main/java/org/bukkit/entity/Shulker.java
@@ -2,4 +2,21 @@ package org.bukkit.entity;
 
 import org.bukkit.material.Colorable;
 
-public interface Shulker extends Golem, Colorable {}
+public interface Shulker extends Golem, Colorable {
+
+    /**
+     * Gets the peek state of the shulker between 0.0 and 1.0.
+     *
+     * @return the peek state of the shulker between 0.0 and 1.0
+     */
+    public float getPeek();
+
+    /**
+     * Sets the peek state of the shulker, should be in between 0.0 and 1.0.
+     *
+     * @param value peek state of the shulker, should be in between 0.0 and 1.0
+     * @throws IllegalArgumentException thrown if the value exceeds the valid
+     * range in between of 0.0 and 1.0
+     */
+    public void setPeek(float value);
+}
