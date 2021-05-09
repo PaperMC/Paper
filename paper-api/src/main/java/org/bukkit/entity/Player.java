f8fcba26b368e5c38db4a9613dc6dd15c99f1825
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2021-05-09 17:20:50 +1000

    #606: Add Player#getPing to access the player's ping
    By: retrooper <retrooperdev@gmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Player.java
+++ paper-api/src/main/java/org/bukkit/entity/Player.java
@@ -1222,6 +1222,21 @@ public interface Player extends HumanEntity, Conversable, OfflinePlayer, PluginM
     public int getClientViewDistance();
 
     /**
+     * Gets the player's estimated ping in milliseconds.
+     *
+     * In Vanilla this value represents the average of the response time to the
+     * last four application layer ping packets sent. This value does not
+     * represent the network round trip time and as such may have less
+     * granularity and be impacted by other sources. For these reasons it
+     * <b>should not</b> be used for anti-cheat purposes. Its recommended use is
+     * only as a <b>qualitative</b> indicator of connection quality (Vanilla
+     * uses it for this purpose in the tab list).
+     *
+     * @return player ping
+     */
+    public int getPing();
+
+    /**
      * Gets the player's current locale.
      *
      * The value of the locale String is not defined properly.
