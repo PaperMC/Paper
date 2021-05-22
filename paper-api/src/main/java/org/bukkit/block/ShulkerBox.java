b21ce6d13abd7a4f2d8271933fbd306d625be081
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2021-05-23 08:30:14 +1000

    SPIGOT-6464: Fix ShulkerBox#getColor for undyed shulkers
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/ShulkerBox.java
+++ paper-api/src/main/java/org/bukkit/block/ShulkerBox.java
@@ -2,7 +2,7 @@ package org.bukkit.block;
 
 import org.bukkit.DyeColor;
 import org.bukkit.loot.Lootable;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents a captured state of a ShulkerBox.
@@ -12,8 +12,8 @@ public interface ShulkerBox extends Container, Lootable, Lidded {
     /**
      * Get the {@link DyeColor} corresponding to this ShulkerBox
      *
-     * @return the {@link DyeColor} of this ShulkerBox
+     * @return the {@link DyeColor} of this ShulkerBox, or null if default
      */
-    @NotNull
+    @Nullable
     public DyeColor getColor();
 }
