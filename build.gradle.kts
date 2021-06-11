ee3cf8cc0956d3f98c53651c5a7506d8f8ada58c
Mariell Hoversholm <proximyst@proximyst.com>
2021-06-11 17:52:05 +0200

    More remapped patches
    Signed-off-by: Mariell Hoversholm <proximyst@proximyst.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.core") version "1.0.0-SNAPSHOT"
+    id("io.papermc.paperweight.core") version "1.0.0-LOCAL-SNAPSHOT"
 }
 
 group = "com.destroystokyo.paper"
@@ -36,6 +36,7 @@ subprojects {
         maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
     }
