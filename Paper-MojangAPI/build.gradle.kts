7baf64b20216da961a153d8258a644edfb02f998
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-06-13 17:57:33 -0700

    EVEN MOOOOOAAAAAAARRRRRRR patches :)

---
+++ Paper-MojangAPI/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `java-library`
+    checkstyle
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+    maven("https://libraries.minecraft.net")
+}
+
+dependencies {
+    implementation(project(":Paper-API"))
+    api("com.mojang:brigadier:1.0.18")
+
+    compileOnly("it.unimi.dsi:fastutil")
+    compileOnly("org.jetbrains:annotations:18.0.0")
+
+    testImplementation("junit:junit:4.13.1")
+    testImplementation("org.hamcrest:hamcrest-library:1.3")
+    testImplementation("org.ow2.asm:asm-tree:7.3.1")
+}
