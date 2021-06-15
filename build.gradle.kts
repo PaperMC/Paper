bbb8816211a36da2acf4d19dcf0fe0aca863e678
Kyle Wood <kyle@denwav.dev>
2021-06-15 00:56:07 -0500

    Update to paperweight 1.0.0, with automatic library imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.core") version "1.0.0-SNAPSHOT"
+    id("io.papermc.paperweight.core") version "1.0.0"
 }
 
 subprojects {
