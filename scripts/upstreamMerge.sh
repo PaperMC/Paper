c8ecb664c640639bf6b57a4e82bb5ba43518b02c
Kyle Wood <kyle@denwav.dev>
2021-06-15 01:42:57 -0500

    Remove now unnecessary scripts and update upstreamMerge.sh

--- scripts/upstreamMerge.sh
+++ scripts/upstreamMerge.sh
@@ -30,11 +30,11 @@ update Spigot
 
 if [[ "$2" = "all" || "$2" = "a" ]] ; then
 	update BuildData
-	update Paperclip
 fi
 if [ "$updated" == "1" ]; then
     echo "Rebuilding patches without filtering to improve apply ability"
     cd "$basedir"
-    scripts/rebuildPatches.sh "$basedir" nofilter 1>/dev/null|| exit 1
+    ./gradlew applyPatches -Dpaperweight.debug=true || exit 1
+    ./gradlew rebuildPatches || exit 1
 fi
 )
