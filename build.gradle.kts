9713a965ed58b30ce4c7a7b87a0058b13c92cebf
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-18 16:38:26 -0700

    Add task to print Minecraft version, add snapshots repo for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java {
         toolchain {
@@ -16,6 +17,16 @@ subprojects {
     tasks.withType<JavaCompile>().configureEach {
         options.encoding = "UTF-8"
         options.release.set(16)
+    }
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "paperSnapshots"
+                url = uri("https://papermc.io/repo/repository/maven-snapshots/")
+                credentials(PasswordCredentials::class)
+            }
+        }
     }
 
     if (name == "Paper-MojangAPI") {
@@ -72,5 +83,11 @@ paperweight {
 
         additionalSpigotMemberMappings.set(file("build-data/additional-spigot-member-mappings.csrg"))
         craftBukkitPatchPatchesDir.set(file("build-data/craftbukkit-patch-patches"))
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").forUseAtConfigurationTime().get().trim())
     }
 }
