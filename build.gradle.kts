e31ad11127e9971958ceb2d236a92b12f9892b25
Kyle Wood <demonwav@gmail.com>
2021-06-20 16:22:20 -0500

    Update paperweight to 1.1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.core") version "1.1.5"
+    id("io.papermc.paperweight.core") version "1.1.6"
 }
 
 subprojects {
