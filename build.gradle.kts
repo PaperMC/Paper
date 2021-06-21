33a04d97ab566ac6c8e8a6794e2d432234f3d2e5
Jason <11360596+jpenilla@users.noreply.github.com>
2021-06-21 08:27:46 -0700

    Set UTF-8 encoding for Javadoc and ProcessResources (#5937)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,15 @@ subprojects {
         }
     }
 
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(16)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
     }
 
     configure<PublishingExtension> {
