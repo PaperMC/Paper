3f834b6568784aa4858cc9227b7e55f3ca3690b7
Professor Bloodstone <git@bloodstone.dev>
2021-06-22 22:27:46 +0200

    Update upstreamMerge script
    No longer nukes workspace if submodules are not checked out

--- scripts/upstreamMerge.sh
+++ scripts/upstreamMerge.sh
@@ -1,41 +1,44 @@
 #!/usr/bin/env bash
 
-(
-set -e
-PS1="$"
-basedir="$(cd "$1" && pwd -P)"
-workdir="$basedir/work"
-gitcmd="git -c commit.gpgsign=false"
+set -euo pipefail
 
-updated="0"
-function getRef {
-    git ls-tree $1 $2  | cut -d' ' -f3 | cut -f1
+function git {
+  command git -c commit.gpgsign=false "${@}"
 }
+
+readonly basedir="$(cd "$(dirname "$0")/.." && pwd -P)"
+readonly workdir="${basedir}/work"
+
 function update {
-    cd "$workdir/$1"
-    $gitcmd fetch && $gitcmd clean -fd && $gitcmd reset --hard origin/master
-    refRemote=$(git rev-parse HEAD)
-    cd ../
-    $gitcmd add --force $1
-    refHEAD=$(getRef HEAD "$workdir/$1")
-    echo "$1 $refHEAD - $refRemote"
-    if [ "$refHEAD" != "$refRemote" ]; then
-        export updated="1"
-    fi
+  local -r submodule="${1?Submodule is missing}"
+  local -r submodule_dir="${workdir}/${submodule}"
+  cd "${submodule_dir}"
+  git fetch && git clean -fd && git reset --hard origin/master || return 1
+  cd "${basedir}"
+  git add --force "${submodule_dir}"
 }
 
-update Bukkit
-update CraftBukkit
-update Spigot
-
-if [[ "$2" = "all" || "$2" = "a" ]] ; then
-	update BuildData
-fi
-if [ "$updated" == "1" ]; then
+function main {
+  local -r update_all="${1:-}"
+  local -r skip_rebuild="${2:-}"
+  cd "${basedir}"
+  git submodule update --init
+  update Bukkit
+  update CraftBukkit
+  update Spigot
+  if [[ "${update_all}" == @(all|a|yes|y) ]]; then
+    update BuildData
+  fi
+  if ! git diff --cached --quiet --exit-code -- "${workdir}" && [[ "${skip_rebuild,,}" != @(yes|y) ]]; then
     echo "Rebuilding patches without filtering to improve apply ability"
-    cd "$basedir"
+    cd "${basedir}"
     ./gradlew cleanCache || exit 1 # todo: Figure out why this is necessary
     ./gradlew applyPatches -Dpaperweight.debug=true || exit 1
     ./gradlew rebuildPatches || exit 1
+  fi
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
 fi
-)
