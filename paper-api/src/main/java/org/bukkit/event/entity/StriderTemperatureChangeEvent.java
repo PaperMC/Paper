34e6b033ad4752dc4d645f6b9612759a3e50a1d1
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2021-07-03 09:36:33 +1000

    SPIGOT-6616: Cancellable StriderTemperatureChangeEvent
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/entity/StriderTemperatureChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/StriderTemperatureChangeEvent.java
@@ -1,6 +1,7 @@
 package org.bukkit.event.entity;
 
 import org.bukkit.entity.Strider;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,10 +9,11 @@ import org.jetbrains.annotations.NotNull;
  * Called when a {@link Strider}'s temperature has changed as a result of
  * entering or existing blocks it considers warm.
  */
-public class StriderTemperatureChangeEvent extends EntityEvent {
+public class StriderTemperatureChangeEvent extends EntityEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private final boolean shivering;
+    private boolean cancelled;
 
     public StriderTemperatureChangeEvent(@NotNull Strider what, boolean shivering) {
         super(what);
@@ -31,6 +33,16 @@ public class StriderTemperatureChangeEvent extends EntityEvent {
      */
     public boolean isShivering() {
         return shivering;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 
     @Override
