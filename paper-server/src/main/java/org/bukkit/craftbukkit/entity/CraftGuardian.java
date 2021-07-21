bfc74e86286f3cb2ce101c3c81e1d3331be85030
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2021-07-22 09:36:54 +1000

    SPIGOT-6545: Unable to set Guardian target via API while awareness is disabled
    By: DerFrZocker <derrieple@gmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftGuardian.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftGuardian.java
@@ -4,11 +4,17 @@ import net.minecraft.world.entity.monster.EntityGuardian;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Guardian;
+import org.bukkit.entity.LivingEntity;
 
 public class CraftGuardian extends CraftMonster implements Guardian {
 
     public CraftGuardian(CraftServer server, EntityGuardian entity) {
         super(server, entity);
+    }
+
+    @Override
+    public EntityGuardian getHandle() {
+        return (EntityGuardian) super.getHandle();
     }
 
     @Override
@@ -19,6 +25,37 @@ public class CraftGuardian extends CraftMonster implements Guardian {
     @Override
     public EntityType getType() {
         return EntityType.GUARDIAN;
+    }
+
+    @Override
+    public void setTarget(LivingEntity target) {
+        super.setTarget(target);
+
+        // clean up laser target, when target is removed
+        if (target == null) {
+            getHandle().a(0); // PAIL rename setLaserTarget
+        }
+    }
+
+    @Override
+    public boolean setLaser(boolean activated) {
+        if (activated) {
+            LivingEntity target = getTarget();
+            if (target == null) {
+                return false;
+            }
+
+            getHandle().a(target.getEntityId()); // PAIL rename setLaserTarget
+        } else {
+            getHandle().a(0); // PAIL rename setLaserTarget
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean hasLaser() {
+        return getHandle().fy(); // PAIL rename hasLaserTarget
     }
 
     @Override
