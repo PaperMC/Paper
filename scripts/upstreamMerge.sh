6139a01bdbd7b9e8bed92cc15fdcbd1cc0f1da43
Professor Bloodstone <git@bloodstone.dev>
2021-07-22 23:13:32 +0200

    Make upstream script work on bash 3.2.57
    Support the poor souls stuck using it
    
    Apparently there's a bug with 'command' immediately exiting whole script on error...

--- scripts/upstreamMerge.sh
+++ scripts/upstreamMerge.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-function git {
-  command git -c commit.gpgsign=false "${@}"
+function gitcmd {
+  git -c commit.gpgsign=false "${@}"
 }
 
 readonly basedir="$(cd "$(dirname "$0")/.." && pwd -P)"
@@ -13,23 +13,26 @@ function update {
   local -r submodule="${1?Submodule is missing}"
   local -r submodule_dir="${workdir}/${submodule}"
   cd "${submodule_dir}"
-  git fetch && git clean -fd && git reset --hard origin/master || return 1
+  gitcmd fetch && gitcmd clean -fd && gitcmd reset --hard origin/master || return 1
   cd "${basedir}"
-  git add --force "${submodule_dir}"
+  gitcmd add --force "${submodule_dir}"
 }
 
 function main {
   local -r update_all="${1:-}"
   local -r skip_rebuild="${2:-}"
   cd "${basedir}"
-  git submodule update --init
+  gitcmd submodule update --init
   update Bukkit
   update CraftBukkit
   update Spigot
-  if [[ "${update_all}" == @(all|a|yes|y) ]]; then
-    update BuildData
-  fi
-  if ! git diff --cached --quiet --exit-code -- "${workdir}" && [[ "${skip_rebuild,,}" != @(yes|y) ]]; then
+  case "${update_all:-}" in
+    all|a|yes|y) update BuildData
+  esac
+  if ! gitcmd diff --cached --quiet --exit-code -- "${workdir}" \
+      && [[ "${skip_rebuild:-}" != yes ]] \
+      && [[ "${skip_rebuild:-}" != y ]]
+    then
     echo "Rebuilding patches without filtering to improve apply ability"
     cd "${basedir}"
     ./gradlew cleanCache || exit 1 # todo: Figure out why this is necessary
