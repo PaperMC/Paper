7e9b77844316c5f00b3f61979e3ca92160d51728
Finn Künstner <finn@einfxch.de>
2021-08-01 11:42:35 +0200

    Update Gradle to 7.1.1 and Paper-MojangAPI dependencies

--- Paper-MojangAPI/build.gradle.kts
+++ Paper-MojangAPI/build.gradle.kts
@@ -19,12 +19,12 @@ dependencies {
     implementation(project(":Paper-API"))
     api("com.mojang:brigadier:1.0.18")
 
-    compileOnly("it.unimi.dsi:fastutil:8.2.2")
-    compileOnly("org.jetbrains:annotations:18.0.0")
+    compileOnly("it.unimi.dsi:fastutil:8.5.4")
+    compileOnly("org.jetbrains:annotations:21.0.1")
 
-    testImplementation("junit:junit:4.13.1")
-    testImplementation("org.hamcrest:hamcrest-library:1.3")
-    testImplementation("org.ow2.asm:asm-tree:7.3.1")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest-library:2.2")
+    testImplementation("org.ow2.asm:asm-tree:9.2")
 }
 
 configure<PublishingExtension> {
