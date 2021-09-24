1f934c6832b0791c0d776674a0a89870b7018953
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2021-09-25 08:02:58 +1000

    SPIGOT-6746: Clarify PlayerPortalEvent documentation
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/event/player/PlayerPortalEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerPortalEvent.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * Called when a player is about to teleport because it is in contact with a
- * portal.
+ * portal which will generate an exit portal.
  * <p>
  * For other entities see {@link org.bukkit.event.entity.EntityPortalEvent}
  */
