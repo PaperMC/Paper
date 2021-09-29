083a88424ae0fdf95a522f0ace84351bbb5725c3
Professor Bloodstone <git@bloodstone.dev>
2021-09-29 22:21:34 +0200

    Use future Property to skip filtering patches and rebuild before update

--- scripts/upstreamMerge.sh
+++ scripts/upstreamMerge.sh
@@ -8,6 +8,7 @@ function gitcmd {
 
 readonly basedir="$(cd "$(dirname "$0")/.." && pwd -P)"
 readonly workdir="${basedir}/work"
+readonly patchdir="${basedir}/patches"
 
 function update {
   local -r submodule="${1?Submodule is missing}"
@@ -18,10 +19,29 @@ function update {
   gitcmd add --force "${submodule_dir}"
 }
 
+function apply_rebuild {
+  ./gradlew applyPatches -Dpaperweight.debug=true || return $?
+  ./gradlew rebuildPatches -Ppaperweight.filter-patches=false || return $?
+}
+
+function is_yes {
+  local -r value="${1:-}"
+  [ "${value:-}" = yes ] || [ "${value:-}" = y ]
+}
+
+function skip_rebuild {
+  # expected to have variable called `skip_rebuild` set in context
+  is_yes "${skip_rebuild:-}"
+}
+
 function main {
   local -r update_all="${1:-}"
   local -r skip_rebuild="${2:-}"
   cd "${basedir}"
+  if ! skip_rebuild; then
+    echo "Rebuilding patches without filtering to improve mergeability"
+    apply_rebuild || return $?
+  fi
   gitcmd submodule update --init
   update Bukkit
   update CraftBukkit
@@ -29,15 +49,18 @@ function main {
   case "${update_all:-}" in
     all|a|yes|y) update BuildData
   esac
-  if ! gitcmd diff --cached --quiet --exit-code -- "${workdir}" \
-      && [[ "${skip_rebuild:-}" != yes ]] \
-      && [[ "${skip_rebuild:-}" != y ]]
-    then
-    echo "Rebuilding patches without filtering to improve apply ability"
-    cd "${basedir}"
-    ./gradlew cleanCache || exit 1 # todo: Figure out why this is necessary
-    ./gradlew applyPatches -Dpaperweight.debug=true || exit 1
-    ./gradlew rebuildPatches --filter-patches=false || exit 1
+  if ! gitcmd diff --cached --quiet --exit-code -- "${workdir}"; then
+    if ! skip_rebuild; then
+      echo "Rebuilding patches without filtering to improve apply ability"
+      cd "${basedir}"
+      ./gradlew cleanCache || return $? # todo: Figure out why this is necessary
+      apply_rebuild || return $?
+    else
+      echo "Skipping rebuild..."
+    fi
+  elif ! skip_rebuild; then
+    echo "No updates - reverting changes to patches"
+    gitcmd restore -- "${patchdir}"
   fi
 }
 
