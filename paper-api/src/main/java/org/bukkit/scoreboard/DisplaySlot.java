34f10b78da8cb242e64dcbd182f17ebe11334466
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-10-01 08:04:43 -0700

    Add missing team sidebar display slots

--- paper-api/src/main/java/org/bukkit/scoreboard/DisplaySlot.java
+++ paper-api/src/main/java/org/bukkit/scoreboard/DisplaySlot.java
@@ -1,26 +1,55 @@
 package org.bukkit.scoreboard;
 
+import net.kyori.adventure.text.format.NamedTextColor; // Paper
 /**
  * Locations for displaying objectives to the player
  */
 public enum DisplaySlot {
-    BELOW_NAME,
-    PLAYER_LIST,
-    SIDEBAR,
-    SIDEBAR_BLACK,
-    SIDEBAR_DARK_BLUE,
-    SIDEBAR_DARK_GREEN,
-    SIDEBAR_DARK_AQUA,
-    SIDEBAR_DARK_RED,
-    SIDEBAR_DARK_PURPLE,
-    SIDEBAR_GOLD,
-    SIDEBAR_GRAY,
-    SIDEBAR_DARK_GRAY,
-    SIDEBAR_BLUE,
-    SIDEBAR_GREEN,
-    SIDEBAR_AQUA,
-    SIDEBAR_RED,
-    SIDEBAR_LIGHT_PURPLE,
-    SIDEBAR_YELLOW,
-    SIDEBAR_WHITE;
+    // Paper start
+    BELOW_NAME("below_name"),
+    PLAYER_LIST("list"),
+    SIDEBAR("sidebar"),
+    SIDEBAR_TEAM_BLACK(NamedTextColor.BLACK),
+    SIDEBAR_TEAM_DARK_BLUE(NamedTextColor.DARK_BLUE),
+    SIDEBAR_TEAM_DARK_GREEN(NamedTextColor.DARK_GREEN),
+    SIDEBAR_TEAM_DARK_AQUA(NamedTextColor.DARK_AQUA),
+    SIDEBAR_TEAM_DARK_RED(NamedTextColor.DARK_RED),
+    SIDEBAR_TEAM_DARK_PURPLE(NamedTextColor.DARK_PURPLE),
+    SIDEBAR_TEAM_GOLD(NamedTextColor.GOLD),
+    SIDEBAR_TEAM_GRAY(NamedTextColor.GRAY),
+    SIDEBAR_TEAM_DARK_GRAY(NamedTextColor.DARK_GRAY),
+    SIDEBAR_TEAM_BLUE(NamedTextColor.BLUE),
+    SIDEBAR_TEAM_GREEN(NamedTextColor.GREEN),
+    SIDEBAR_TEAM_AQUA(NamedTextColor.AQUA),
+    SIDEBAR_TEAM_RED(NamedTextColor.RED),
+    SIDEBAR_TEAM_LIGHT_PURPLE(NamedTextColor.LIGHT_PURPLE),
+    SIDEBAR_TEAM_YELLOW(NamedTextColor.YELLOW),
+    SIDEBAR_TEAM_WHITE(NamedTextColor.WHITE);
+
+    public static final net.kyori.adventure.util.Index<String, DisplaySlot> NAMES = net.kyori.adventure.util.Index.create(DisplaySlot.class, DisplaySlot::getId);
+
+    private final String id;
+
+    DisplaySlot(@org.jetbrains.annotations.NotNull String id) {
+        this.id = id;
+    }
+
+    DisplaySlot(@org.jetbrains.annotations.NotNull NamedTextColor color) {
+        this.id = "sidebar.team." + color;
+    }
+
+    /**
+     * Get the string id of this display slot.
+     *
+     * @return the string id
+     */
+    public @org.jetbrains.annotations.NotNull String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return this.id;
+    }
+    // Paper end
 }
