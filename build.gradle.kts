5eb18765ee380ba6110a31ffb5b62f4357536295
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-10-11 16:31:53 -0500

    [ci skip] Misc gradle updates (#6763)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
     id("io.papermc.paperweight.core") version "1.1.12"
 }
 
