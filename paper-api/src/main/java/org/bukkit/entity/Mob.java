5c9be0772272f3e0e7d4c7a28471b51c0ecc2c2f
William Blake Galbreath <blake.galbreath@gmail.com>
2021-10-14 12:36:46 -0500

    Left handed API

--- paper-api/src/main/java/org/bukkit/entity/Mob.java
+++ paper-api/src/main/java/org/bukkit/entity/Mob.java
@@ -204,4 +204,20 @@ public interface Mob extends LivingEntity, Lootable {
      */
     void setAggressive(boolean aggressive);
     // Paper end - Missing Entity API
+
+    // Paper start - left-handed API
+    /**
+     * Check if Mob is left-handed
+     *
+     * @return True if left-handed
+     */
+    public boolean isLeftHanded();
+
+    /**
+      * Set if Mob is left-handed
+      *
+      * @param leftHanded True if left-handed
+      */
+    public void setLeftHanded(boolean leftHanded);
+    // Paper end - left-handed API
 }
