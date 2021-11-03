15a9a93b2bbbbf84282c95a343f2a5cca80cc6a4
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-11-03 13:12:06 -0700

    Update paperweight to 1.1.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.core") version "1.1.12"
+    id("io.papermc.paperweight.core") version "1.1.13"
 }
 
 allprojects {
