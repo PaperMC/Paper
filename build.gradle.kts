52205111bb36ff55b2a56a0cc08d36a060949662
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-11-03 18:22:44 -0700

    Remove redundant and unneeded repos, reorder repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,22 +34,16 @@ subprojects {
 
     repositories {
         mavenCentral()
-        maven("https://repo1.maven.org/maven2/")
-        maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
     }
 }
 
 repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations("paperclip")
-        }
+        content { onlyForConfigurations("paperclip") }
     }
 }
 
@@ -96,12 +90,12 @@ tasks.generateDevelopmentBundle {
     mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
     libraryRepositories.set(
         listOf(
+            "https://repo.maven.apache.org/maven2/",
             "https://libraries.minecraft.net/",
-            "https://maven.quiltmc.org/repository/release/",
-            "https://repo.aikar.co/content/groups/aikar",
-            "https://ci.emc.gs/nexus/content/groups/aikar/",
             "https://papermc.io/repo/repository/maven-public/",
-            "https://repo.velocitypowered.com/snapshots/"
+            "https://maven.quiltmc.org/repository/release/",
+            "https://ci.emc.gs/nexus/content/groups/aikar/",
+            "https://repo.aikar.co/content/groups/aikar",
         )
     )
 }
