7c891287edff2f7febb899b5c5f112d1b82fd1d0
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2021-12-04 13:29:36 -0500

    Add Moving Piston API

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftMovingPiston.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftMovingPiston.java
@@ -4,7 +4,7 @@ import net.minecraft.world.level.block.piston.PistonMovingBlockEntity;
 import org.bukkit.Location;
 import org.bukkit.World;
 
-public class CraftMovingPiston extends CraftBlockEntityState<PistonMovingBlockEntity> {
+public class CraftMovingPiston extends CraftBlockEntityState<PistonMovingBlockEntity> implements io.papermc.paper.block.MovingPiston { // Paper - Add Moving Piston API
 
     public CraftMovingPiston(World world, PistonMovingBlockEntity tileEntity) {
         super(world, tileEntity);
@@ -23,4 +23,26 @@ public class CraftMovingPiston extends CraftBlockEntityState<PistonMovingBlockEn
     public CraftMovingPiston copy(Location location) {
         return new CraftMovingPiston(this, location);
     }
+
+    // Paper start - Add Moving Piston API
+    @Override
+    public org.bukkit.block.data.BlockData getMovingBlock() {
+        return org.bukkit.craftbukkit.block.data.CraftBlockData.fromData(this.getTileEntity().getMovedState());
+    }
+
+    @Override
+    public org.bukkit.block.BlockFace getDirection() {
+        return org.bukkit.craftbukkit.block.CraftBlock.notchToBlockFace(this.getTileEntity().getDirection());
+    }
+
+    @Override
+    public boolean isExtending() {
+        return this.getTileEntity().isExtending();
+    }
+
+    @Override
+    public boolean isPistonHead() {
+        return this.getTileEntity().isSourcePiston();
+    }
+    // Paper end - Add Moving Piston API
 }
