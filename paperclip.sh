6f60c2243eca44b117e93e2a42ef8e58acdd5074
sulu5890 <sulu@sulu.me>
2021-12-10 16:24:00 -0600

    Update Upstream (Craftbukkit)

--- paperclip.sh
+++ paperclip.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+basedir="$(cd "$1" && pwd -P)"
+
 cp ./PaperSpigot-Server/target/paperspigot*-SNAPSHOT.jar ./Paperclip/paperspigot-1.8.8.jar
 cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
-mvn clean package
+mvn clean package -Dmcver=1.8.8 "-Dpaperjar=$basedir/Paperclip/paperspigot-1.8.8.jar" "-Dvanillajar=$basedir/Paperclip/minecraft_server.1.8.8.jar"
 cd ..
 cp ./Paperclip/target/paperclip*-SNAPSHOT.jar ./Paperclip.jar
 
