2894af04dc378d6a61e55e37a8c0b1a478c09c3d
sulu5890 <sulu@sulu.me>
2021-12-19 18:10:08 -0600

    Update Log4J (1.8.8) (#7150)
    * Update Upstream (Craftbukkit)
    
    * update log4j

--- paperclip.sh
+++ paperclip.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+basedir="$(cd "$1" && pwd -P)"
+
 cp ./PaperSpigot-Server/target/paperspigot*-SNAPSHOT.jar ./Paperclip/paperspigot-1.8.8.jar
 cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
-mvn clean package
+mvn clean package -Dmcver=1.8.8 "-Dpaperjar=$basedir/Paperclip/paperspigot-1.8.8.jar" "-Dvanillajar=$basedir/Paperclip/minecraft_server.1.8.8.jar"
 cd ..
-cp ./Paperclip/target/paperclip*-SNAPSHOT.jar ./Paperclip.jar
+cp ./Paperclip/assembly/target/paperclip*.jar ./paperclip.jar
 
 echo ""
 echo ""
 echo ""
 echo "Build success!"
-echo "Copied final jar to $(pwd)/Paperclip.jar"
+echo "Copied final jar to $(pwd)/paperclip.jar"
