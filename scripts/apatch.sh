258900b661713671b2d6f1d8e3315b445937de91
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-12-21 19:54:51 -0800

    [ci skip] Revert change to apatch script

--- scripts/apatch.sh
+++ scripts/apatch.sh
@@ -40,7 +40,7 @@ fi
 	for i in $(find . -name \*.rej); do
         	base=$(echo "$i" | sed 's/.rej//g')
 		if [ -f "$i" ]; then
-        		sed -e 's/^diff a\/\(.*\) b\/\(.*\)[[:space:]].*rejected.*$/--- \1\n+++ \2/' -i '' $i && wiggle -v -l --replace "$base" "$i"
+        		sed -e 's/^diff a\/\(.*\) b\/\(.*\)[[:space:]].*rejected.*$/--- \1\n+++ \2/' -i $i && wiggle -v -l --replace "$base" "$i"
         		rm "$base.porig" "$i"
 		else
 			echo "No such file: $base"
