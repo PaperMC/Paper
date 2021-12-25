2d43851f7928dfdcc41f6521ff9af36cc3ea0a45
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2021-12-24 23:25:04 -0500

    wip Poi API

--- test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
@@ -1,11 +1,34 @@
 package io.papermc.paper.testplugin;
 
+import io.papermc.paper.world.poi.PoiManager;
+import io.papermc.paper.world.poi.RegisteredPoi;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+        new BukkitRunnable(){
+
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    PoiManager manager = player.getWorld().getPoiManager();
+                    for (RegisteredPoi registeredPoi : manager.getPoisInRange(player.getLocation(), 16)) {
+                        player.spawnParticle(Particle.BLOCK_MARKER, registeredPoi.getLocation().toCenterLocation().add(0, 1, 0), 1, Bukkit.createBlockData(Material.REDSTONE_BLOCK));
+                    }
+                }
+            }
+        }.runTaskTimer(this, 1, 1);
     }
+
 }
