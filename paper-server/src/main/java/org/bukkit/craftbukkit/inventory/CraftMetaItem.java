751a5601b8a7f8a9e8c965f664e44bd4b25e7d86
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2021-12-30 10:14:08 +1100

    Fix map colour not being marked as a handled tag
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaItem.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaItem.java
@@ -1384,6 +1384,7 @@ class CraftMetaItem implements ItemMeta, Damageable, Repairable, BlockDataMeta {
                         ATTRIBUTES_UUID_LOW.NBT,
                         ATTRIBUTES_SLOT.NBT,
                         CraftMetaMap.MAP_SCALING.NBT,
+                        CraftMetaMap.MAP_COLOR.NBT,
                         CraftMetaMap.MAP_ID.NBT,
                         CraftMetaPotion.POTION_EFFECTS.NBT,
                         CraftMetaPotion.DEFAULT_POTION.NBT,
