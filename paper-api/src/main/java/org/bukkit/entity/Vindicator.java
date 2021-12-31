f9763773677b3e3779c4c85bb3a68df35498f4f7
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2022-01-01 09:58:00 +1100

    #275: Add isJohnny / setJohnny for Vindicator
    By: Matthew <stteg@hotmail.com>

--- paper-api/src/main/java/org/bukkit/entity/Vindicator.java
+++ paper-api/src/main/java/org/bukkit/entity/Vindicator.java
@@ -3,4 +3,21 @@ package org.bukkit.entity;
 /**
  * Represents a Vindicator.
  */
-public interface Vindicator extends Illager { }
+public interface Vindicator extends Illager {
+
+    /**
+     * Returns whether a vindicator is in "Johnny" mode.
+     *
+     * When this mode is active, vindicators will be hostile to all mobs.
+     *
+     * @return true if johnny
+     */
+    boolean isJohnny();
+
+    /**
+     * Sets the Johnny state of a vindicator.
+     *
+     * @param johnny new johnny state
+     */
+    void setJohnny(boolean johnny);
+}
