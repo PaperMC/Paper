bde37a649c797663f6704e69f74964b48bbeff94
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2022-01-01 09:58:04 +1100

    #378: Add API to check and set if horses are grazing hay
    By: Matthew <stteg@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftAbstractHorse.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftAbstractHorse.java
@@ -98,6 +98,16 @@ public abstract class CraftAbstractHorse extends CraftAnimals implements Abstrac
     }
 
     @Override
+    public boolean isEatingHaystack() {
+        return getHandle().isEating();
+    }
+
+    @Override
+    public void setEatingHaystack(boolean eatingHaystack) {
+        getHandle().setEating(eatingHaystack);
+    }
+
+    @Override
     public AbstractHorseInventory getInventory() {
         return new CraftInventoryAbstractHorse(getHandle().inventory);
     }
