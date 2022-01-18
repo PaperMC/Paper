91a7982c46cc53f5c81bd8983d35485b1690bf0b
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-01-18 11:07:54 -0700

    Warn on strange @EventHandler return types

--- paper-api/src/main/java/org/bukkit/plugin/EventExecutor.java
+++ paper-api/src/main/java/org/bukkit/plugin/EventExecutor.java
@@ -51,6 +51,12 @@ public interface EventExecutor {
         Preconditions.checkArgument(m.getParameterCount() != 0, "Incorrect number of arguments %s", m.getParameterCount());
         Preconditions.checkArgument(m.getParameterTypes()[0] == eventClass, "First parameter %s doesn't match event class %s", m.getParameterTypes()[0], eventClass);
         ClassDefiner definer = ClassDefiner.getInstance();
+        if (m.getReturnType() != Void.TYPE) {
+            final org.bukkit.plugin.java.JavaPlugin plugin = org.bukkit.plugin.java.JavaPlugin.getProvidingPlugin(m.getDeclaringClass());
+            org.bukkit.Bukkit.getLogger().warning("@EventHandler method " + m.getDeclaringClass().getName() + (Modifier.isStatic(m.getModifiers()) ? '.' : '#') + m.getName()
+                + " returns non-void type " + m.getReturnType().getName() + ". This is unsupported behavior and will no longer work in a future version of Paper."
+                + " This should be reported to the developers of " + plugin.getPluginMeta().getDisplayName() + " (" + String.join(",", plugin.getPluginMeta().getAuthors()) + ')');
+        }
         if (Modifier.isStatic(m.getModifiers())) {
             return new StaticMethodHandleEventExecutor(eventClass, m);
         } else if (definer.isBypassAccessChecks() || Modifier.isPublic(m.getDeclaringClass().getModifiers()) && Modifier.isPublic(m.getModifiers())) {
