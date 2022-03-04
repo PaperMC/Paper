6d952ef6e5102a94d4f11e3fd273aef55dfd0a46
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2022-03-03 19:12:46 -0500

    Remove Workaround for setting passengers on players

--- test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
@@ -1,11 +1,21 @@
 package io.papermc.paper.testplugin;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEntityEvent event) {
+        event.getPlayer().addPassenger(event.getRightClicked());
     }
 }
