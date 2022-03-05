84be1c082d837285c0abf4c16de40237c2fb2cc9
EpicKnarvik97 <kristian.knarvik@knett.no>
2022-03-05 20:58:47 +0100

    Expose furnace minecart push values
    Adds methods for getting and setting a furnace minecart's push values

--- paper-api/src/main/java/org/bukkit/entity/minecart/PoweredMinecart.java
+++ paper-api/src/main/java/org/bukkit/entity/minecart/PoweredMinecart.java
@@ -21,4 +21,34 @@ public interface PoweredMinecart extends Minecart {
      * @param fuel Number of ticks until the minecart runs out of fuel
      */
     public void setFuel(int fuel);
+
+    // Paper start
+    /**
+     * Get the x push of the minecart.
+     *
+     * @return The x push of the minecart
+     */
+    public double getPushX();
+
+    /**
+     * Get the z push of the minecart.
+     *
+     * @return The z push of the minecart
+     */
+    public double getPushZ();
+
+    /**
+     * Set the x push of the minecart.
+     *
+     * @param xPush The new x push of the minecart
+     */
+    public void setPushX(double xPush);
+
+    /**
+     * Set the z push of the minecart.
+     *
+     * @param zPush The new z push of the minecart
+     */
+    public void setPushZ(double zPush);
+    // Paper end
 }
