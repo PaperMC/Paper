915e452d1246a601f8b5d6999279a24a1239cda2
HexedHero <6012891+HexedHero@users.noreply.github.com>
2022-04-18 10:51:45 +0100

    Add pre-unbreaking amount to PlayerItemDamageEvent (#7724)

--- test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
@@ -1,11 +1,18 @@
 package io.papermc.paper.testplugin;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void a(PlayerItemDamageEvent event) {
+        System.out.println(event.getOriginalDamage() + " to " + event.getDamage());
     }
 }
