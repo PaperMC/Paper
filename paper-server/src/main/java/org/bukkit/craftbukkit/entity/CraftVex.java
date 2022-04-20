ed0354be301cae61dc38c3c1266db36c4a335ce5
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2022-04-21 06:45:42 +1000

    SPIGOT-6992: Add LimitedLife/LifeTicks/Bound APIs to Vex
    By: Doc <nachito94@msn.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftVex.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftVex.java
@@ -1,6 +1,9 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
+import net.minecraft.core.BlockPosition;
 import net.minecraft.world.entity.monster.EntityVex;
+import org.bukkit.Location;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Vex;
@@ -34,5 +37,39 @@ public class CraftVex extends CraftMonster implements Vex {
     @Override
     public void setCharging(boolean charging) {
         getHandle().setIsCharging(charging);
+    }
+
+    @Override
+    public Location getBound() {
+        BlockPosition blockPosition = getHandle().getBoundOrigin();
+        return (blockPosition == null) ? null : new Location(getWorld(), blockPosition.getX(), blockPosition.getY(), blockPosition.getZ());
+    }
+
+    @Override
+    public void setBound(Location location) {
+        if (location == null) {
+            getHandle().setBoundOrigin(null);
+        } else {
+            Preconditions.checkArgument(getWorld().equals(location.getWorld()), "The bound world cannot be different to the entity's world.");
+            getHandle().setBoundOrigin(new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ()));
+        }
+    }
+
+    @Override
+    public int getLifeTicks() {
+        return getHandle().limitedLifeTicks;
+    }
+
+    @Override
+    public void setLifeTicks(int lifeTicks) {
+        getHandle().setLimitedLife(lifeTicks);
+        if (lifeTicks < 0) {
+            getHandle().hasLimitedLife = false;
+        }
+    }
+
+    @Override
+    public boolean hasLimitedLife() {
+        return getHandle().hasLimitedLife;
     }
 }
