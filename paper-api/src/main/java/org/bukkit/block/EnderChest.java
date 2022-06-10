8bbd39c17045e415ee9863c10b1e8eb513ab6f19
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2022-06-10 10:57:48 +1000

    SPIGOT-7043: EnderChest does not implement Lidded
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/block/EnderChest.java
+++ paper-api/src/main/java/org/bukkit/block/EnderChest.java
@@ -3,4 +3,4 @@ package org.bukkit.block;
 /**
  * Represents a captured state of an ender chest.
  */
-public interface EnderChest extends TileState { }
+public interface EnderChest extends Lidded, TileState { }
