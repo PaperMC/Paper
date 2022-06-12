b1a97476be47c8d7c424a8f896f00730abeab236
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2022-06-12 18:12:47 +1000

    #761: Add Hangable interface to MangrovePropagule
    By: chickeneer <emcchickeneer@gmail.com>

--- paper-api/src/main/java/org/bukkit/block/data/type/MangrovePropagule.java
+++ paper-api/src/main/java/org/bukkit/block/data/type/MangrovePropagule.java
@@ -1,7 +1,8 @@
 package org.bukkit.block.data.type;
 
 import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.Hangable;
 import org.bukkit.block.data.Waterlogged;
 
-public interface MangrovePropagule extends Ageable, Sapling, Waterlogged {
+public interface MangrovePropagule extends Ageable, Hangable, Sapling, Waterlogged {
 }
