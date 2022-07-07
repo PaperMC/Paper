73e7418b840d14745efbdcf1416cc9687a012f62
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2022-07-06 23:02:17 -0400

    Paper plugin Work
    Rename to paper meta
    
    Fix provided plugins causing plugins to be loaded multiple times
    
    Combine config api
    
    rebase
    
    Cleanup, documentation, fixes
    
    Update logic
    
    Update some debugging utilities
    
    Bootstrap debugging + wtf config file delete moment
    
    Move entrypoint above bootstrap logic
    
    Move library resolution condition check outside of method
    
    Cherrypick "Also load resources from LibraryLoader"
    
    Renames, annotations, service api bytecode provider
    
    Rebase
    
    Unit tests + build fixing
    
    Permission fixes + misc plugin loading changes
    
    List patches to be squashed
    
    Fix dependency issues + don't allow dupe plugins + switch legacy loading setting + fix constructors
    
    Remove ConfigurationSerializable, remove unused classes
    
    Dependency handling for plugins + bootstrap
    
    Move papertest plugin package, fix package check
    
    Better api support, deprecate PluginLoader for removal
    
    Painfully adding support for api, this prolly doesn't work don't even bother
    
    rename
    
    Javadocs, bug fixes, platform classloaders, move a bunch of stuff to server, annotation test changes, fix plugin loading with plugins that have no dependencies, cleanup classloaders, support to create your own javaplugins
    
    Configurate improvements + misc changes
    
    Move loggers + cleanup provider loading
    
    Dependency handling + dependency fixes
    
    Plugin loader and plugin boostrapper
    
    Fail if initializer fails, remove plugin initializers
    
    New soft utilizes
    
    New classloader groups, remove utilizes, open classloader config option
    
    Add more backwards compatability for DummyBukkitPluginLoader
    
    Synchronize groups and fix resolving dependencies
    
    Rebase
    
    Boostrapper dependencies
    
    Draft Lifecycle API
    
    Fix Merge conflict in patches
    
    Introduce LifecyclePointContext
    
    Fix annotations
    
    Fix server test
    
    Better LifecyclePoint scheduling, more exception checks
    
    Fix LifecyclePointContextImpl
    
    Draft Registry API
    
    tiny fixes
    
    Make RegistryAccess, RegistryKey types, worldgen registries LifecyclePoints
    
    fix tests
    
    Enum workaround
    
    Draft Biome API
    
    rebase

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,12 +1,40 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.registry.Reference;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class TestPlugin extends JavaPlugin implements Listener {
+
+    private final String key;
+    private final Reference<Biome> biomeReference;
+
+    public TestPlugin(String key, Reference<Biome> biomeReference) {
+        this.key = key;
+        this.biomeReference = biomeReference;
+    }
+
+    @Override
+    public void onLoad() {
+        this.getLogger().log(Level.INFO, "HELLO FROM onLoad!");
+        this.getLogger().log(Level.INFO, "Secret bootstrap value: " + this.key);
+    }
 
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        // Set the custom biome to the position where a player joins the server.
+        Player player = e.getPlayer();
+        player.getWorld().setBiome(player.getLocation(), biomeReference.value());
     }
 }
--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -2,12 +2,66 @@ package io.papermc.testplugin;
 
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import io.papermc.paper.lifecycle.ServerLifecyclePoints;
+import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import io.papermc.paper.registry.Reference;
+import io.papermc.paper.registry.enums.EnumWritableRegistry;
+import io.papermc.paper.registry.ExtendedRegistry;
+import io.papermc.paper.registry.key.ResourceKey;
+import io.papermc.paper.world.biome.BiomeSpecialEffects;
+import io.papermc.paper.world.biome.ClimateSettings;
+import net.kyori.adventure.key.Key;
+import org.bukkit.block.Biome;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public class TestPluginBootstrap implements PluginBootstrap {
 
+    public String secret;
+    final ResourceKey<Biome> testBiomeKey = ResourceKey.create(ExtendedRegistry.BIOME_REGISTRY_KEY, Key.key("testplugin", "my_new_biome"));
+    Reference<Biome> biomeReference;
+
     @Override
     public void bootstrap(@NotNull PluginProviderContext context) {
+         /*final LifecyclePointFlux.ConstructedFlux<ServerLifecyclePoints.StaticRegistryInitializationContext> constructed = LifecyclePointFlux.create(ServerLifecyclePoints.STATIC_REGISTRIES_INITIALIZED)
+            .map(regContext -> "Doing something")
+            .flatMap(s -> LifecyclePointFlux.create(ServerLifecyclePoints.STATIC_REGISTRIES_INITIALIZED).map(regContext -> "Doing Something other! " + s))
+            .subscribe(System.out::println);*/
+
+        context.getLifecyclePointScheduler().schedule(ServerLifecyclePoints.WORLDGEN_REGISTRIES_INITIALIZED, registryInitializationContext -> {
+            final EnumWritableRegistry<Biome> biomeRegistry = registryInitializationContext.writableRegistryAccess()
+                .registry(ExtendedRegistry.BIOME_REGISTRY_KEY);
+
+            final ClimateSettings climateSettings = ClimateSettings.builder()
+                .precipitation(ClimateSettings.Precipitation.SNOW)
+                .build();
+
+            final BiomeSpecialEffects biomeSpecialEffects = BiomeSpecialEffects.builder()
+                .grassColor(0xE1F6FF)
+                .skyColor(0x004764)
+                .build();
+
+            final Biome newBiome = Biome.create(testBiomeKey, climateSettings, biomeSpecialEffects);
+            biomeRegistry.register(newBiome);
+            System.out.println("resourcekey: " + testBiomeKey);
+
+        }).schedule(ServerLifecyclePoints.WORLDGEN_REGISTRIES_FROZEN, worldgenRegistryFrozenContext -> {
+            final ExtendedRegistry<Biome> biomeRegistry = worldgenRegistryFrozenContext.registryAccess()
+                .registry(ExtendedRegistry.BIOME_REGISTRY_KEY).orElseThrow();
+
+            Biome testBiome = biomeRegistry.get(testBiomeKey);
+            System.out.println("test biome: " + testBiome);
+            System.out.println("temperature: " + testBiome.getClimateSettings().temperature());
+            biomeReference = biomeRegistry.getReference(testBiomeKey).orElseThrow();
+
+        })/*.schedule(constructed)*/.build(context.getConfiguration());
     }
 
+    @Override
+    public @NotNull JavaPlugin createPlugin(@NotNull PluginProviderContext context) {
+        if (context.getLifecyclePointScheduler().isBuilt())
+            context.getSLF4JLogger().info("LifecyclePointContext is built! No modifications allowed!");
+        return new TestPlugin(this.secret, this.biomeReference);
+    }
+>>>>>>> 63b805be5 (Paper plugin Work)
 }
