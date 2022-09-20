63e45760a39afde060ab404baec5550fcb1363bb
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2022-09-21 06:56:58 +1000

    Add support for Java 19
    By: md_5 <git@md-5.net>

--- paper-server/src/main/java/org/bukkit/craftbukkit/Main.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/Main.java
@@ -155,8 +155,8 @@ public class Main {
                 System.err.println("Unsupported Java detected (" + javaVersion + "). This version of Minecraft requires at least Java 17. Check your Java version with the command 'java -version'.");
                 return;
             }
-            if (javaVersion > 62.0) {
-                System.err.println("Unsupported Java detected (" + javaVersion + "). Only up to Java 18 is supported.");
+            if (javaVersion > 63.0) {
+                System.err.println("Unsupported Java detected (" + javaVersion + "). Only up to Java 19 is supported.");
                 return;
             }
 
