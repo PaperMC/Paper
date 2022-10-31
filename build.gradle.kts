4a9c9b3fb0bad179c338b086756385f06858f973
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-10-31 15:25:30 -0700

    Build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.core") version "1.3.8"
+    id("io.papermc.paperweight.core") version "1.3.9"
 }
 
 allprojects {
@@ -62,7 +62,7 @@ repositories {
 
 dependencies {
     paramMappings("net.fabricmc:yarn:1.19.2+build.1:mergedv2")
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
     decompiler("net.minecraftforge:forgeflower:1.5.605.7")
     spigotDecompiler("io.papermc:patched-spigot-fernflower:0.1+build.6")
     paperclip("io.papermc:paperclip:3.0.2")
