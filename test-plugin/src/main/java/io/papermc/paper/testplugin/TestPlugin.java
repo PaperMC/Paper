f6e947a21f25aa97fad01bcdc6e7f38c077ae630
Denery <dorofeevij@gmail.com>
2022-10-31 18:27:51 +0300

    Fix BaseFireBlock, remove plugintest diff

--- test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/paper/testplugin/TestPlugin.java
@@ -1,24 +1,11 @@
 package io.papermc.paper.testplugin;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockIgniteEvent;
-import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onEntityCombust(EntityCombustEvent e) {
-        e.setCancelled(true);
-    }
-
-    @EventHandler
-    public void onEntityCombust(BlockIgniteEvent e) {
-        e.setCancelled(true);
     }
 }
