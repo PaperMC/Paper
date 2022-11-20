4a3ae595357d8c6c48938d889d199b50f09221e5
Jason <11360596+jpenilla@users.noreply.github.com>
2022-11-20 11:26:20 -0700

    [ci skip] Add check for git repo (#8578)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,25 @@
 import java.util.Locale
 
+if (!file(".git").isDirectory) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Paper project directory is not a properly cloned Git repository.
+         
+         In order to build Paper from source you must clone
+         the Paper repository using Git, not download a code
+         zip from GitHub.
+         
+         Built Paper jars are available for download at
+         https://papermc.io/downloads
+         
+         See https://github.com/PaperMC/Paper/blob/master/CONTRIBUTING.md
+         for further information on building and modifying Paper.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
