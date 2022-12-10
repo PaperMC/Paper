d670f11766e363413fcd4926aa87ee3c9f9791cb
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2022-12-11 10:04:14 +1100

    SPIGOT-7202: Deprecate removed door effects
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/Effect.java
+++ paper-api/src/main/java/org/bukkit/Effect.java
@@ -25,43 +25,73 @@ public enum Effect {
     BOW_FIRE(1002, Type.SOUND),
     /**
      * Sound of a door opening.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_WOODEN_DOOR_OPEN
      */
+    @Deprecated
     DOOR_TOGGLE(1006, Type.SOUND),
     /**
      * Sound of a door opening.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_IRON_DOOR_OPEN
      */
+    @Deprecated
     IRON_DOOR_TOGGLE(1005, Type.SOUND),
     /**
      * Sound of a trapdoor opening.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_WOODEN_TRAPDOOR_OPEN
      */
+    @Deprecated
     TRAPDOOR_TOGGLE(1007, Type.SOUND),
     /**
      * Sound of a door opening.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_IRON_TRAPDOOR_OPEN
      */
+    @Deprecated
     IRON_TRAPDOOR_TOGGLE(1037, Type.SOUND),
     /**
      * Sound of a door opening.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_FENCE_GATE_OPEN
      */
+    @Deprecated
     FENCE_GATE_TOGGLE(1008, Type.SOUND),
     /**
      * Sound of a door closing.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_WOODEN_DOOR_CLOSE
      */
+    @Deprecated
     DOOR_CLOSE(1012, Type.SOUND),
     /**
      * Sound of a door closing.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_IRON_DOOR_CLOSE
      */
+    @Deprecated
     IRON_DOOR_CLOSE(1011, Type.SOUND),
     /**
      * Sound of a trapdoor closing.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_WOODEN_TRAPDOOR_CLOSE
      */
+    @Deprecated
     TRAPDOOR_CLOSE(1013, Type.SOUND),
     /**
      * Sound of a door closing.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_IRON_TRAPDOOR_CLOSE
      */
+    @Deprecated
     IRON_TRAPDOOR_CLOSE(1036, Type.SOUND),
     /**
      * Sound of a door closing.
+     * @deprecated no longer exists
+     * @see Sound#BLOCK_FENCE_GATE_CLOSE
      */
+    @Deprecated
     FENCE_GATE_CLOSE(1014, Type.SOUND),
     /**
      * Sound of fire being extinguished.
