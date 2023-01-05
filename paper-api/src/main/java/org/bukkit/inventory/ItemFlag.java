e03e924177a4eaab38389aacf02b47da9a219bde
Nassim Jahnke <nassim@njahnke.dev>
2023-01-05 10:45:20 +0100

    Replace ItemFlag.HIDE_POTION_EFFECTS

--- paper-api/src/main/java/org/bukkit/inventory/ItemFlag.java
+++ paper-api/src/main/java/org/bukkit/inventory/ItemFlag.java
@@ -38,4 +38,27 @@ public enum ItemFlag {
      * Setting to show/hide armor trim from armor.
      */
     HIDE_ARMOR_TRIM;
+    // Paper start
+    /**
+     * Setting to show/hide item-specific information, including, but not limited to:
+     * <ul>
+     *     <li>Potion effects on potions, tipped arrows, and suspicious stew</li>
+     *     <li>Enchanted book enchantments</li>
+     *     <li>Book author and generation</li>
+     *     <li>Record names</li>
+     *     <li>Patterns of banners and shields</li>
+     *     <li>Fish bucket variants</li>
+     *     <li>Instrument item descriptions (i.e. goat horn sounds)</li>
+     *     <li>Map data</li>
+     *     <li>Firework data</li>
+     *     <li>Crossbow projectile info</li>
+     *     <li>Bundle fullness</li>
+     *     <li>Shulker box contents</li>
+     *     <li>Spawner descriptions</li>
+     * </ul>
+     * @deprecated use {@link #HIDE_ADDITIONAL_TOOLTIP}
+     */
+    @Deprecated(since = "1.20.5")
+    public static final ItemFlag HIDE_ITEM_SPECIFICS = HIDE_ADDITIONAL_TOOLTIP;
+    // Paper end
 }
