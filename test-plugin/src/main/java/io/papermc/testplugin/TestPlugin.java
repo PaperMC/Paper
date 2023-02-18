244d87d62184bef2aa2d5aa43727fc25c631aa3b
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2023-02-18 13:07:19 -0500

    Update test paper plugin

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,38 +1,12 @@
 package io.papermc.testplugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.util.logging.Level;
-
 public final class TestPlugin extends JavaPlugin implements Listener {
-
-    private final String key;
-
-    public TestPlugin(String key) {
-        this.key = key;
-    }
-
-    @Override
-    public void onLoad() {
-        this.getLogger().log(Level.INFO, "HELLO FROM onLoad!");
-        this.getLogger().log(Level.INFO, "Secret bootstrap value: " + this.key);
-    }
 
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
-        try {
-            System.out.println(Class.forName("com.owen1212055.particlehelper.api.ParticleHelper"));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            System.out.println(Bukkit.getPluginManager().loadPlugin(new File("debuggery.jar")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
