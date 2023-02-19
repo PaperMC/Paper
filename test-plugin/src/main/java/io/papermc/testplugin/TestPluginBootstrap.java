841da90501564e5b430a8469cac6e6a77627725d
Owen <23108066+Owen1212055@users.noreply.github.com>
2023-02-19 09:57:10 -0500

    Paper Plugins (#8108)

---
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -0,0 +1,13 @@
+package io.papermc.testplugin;
+
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import org.jetbrains.annotations.NotNull;
+
+public class TestPluginBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(@NotNull PluginProviderContext context) {
+    }
+
+}
