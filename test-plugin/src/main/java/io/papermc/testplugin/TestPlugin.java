0ec9d9a4ccd0e9ca9e1e7d355e8cdfc9dfcbffff
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-02-25 13:47:13 -0800

    Registry Modification API

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,5 +1,7 @@
 package io.papermc.testplugin;
 
+import org.bukkit.GameEvent;
+import org.bukkit.Registry;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,6 +12,13 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
+
+        final GameEvent newEvent = Registry.GAME_EVENT.get(TestPluginBootstrap.NEW_EVENT);
+        if (newEvent == null) {
+            throw new RuntimeException("could not find new event");
+        } else {
+            System.out.println("New event: " + newEvent.getKey() + " " + newEvent.getRange());
+        }
     }
 
 }
