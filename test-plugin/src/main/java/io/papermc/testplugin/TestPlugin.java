5ebe54dc6d8020ca4986495982ab69745c7decf9
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2023-03-12 16:04:00 -0400

    Big updates, support item meta

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,5 +1,6 @@
 package io.papermc.testplugin;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
 import io.papermc.paper.inventory.item.properties.ItemProperties;
 import io.papermc.paper.property.StoredPropertyHolder;
 import net.kyori.adventure.text.Component;
@@ -9,7 +10,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.Consumer;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
 
@@ -20,10 +25,26 @@ public final class TestPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void swapHands(PlayerSwapHandItemsEvent event) {
-        ItemStack itemStack = Bukkit.getUnsafe().newItem(Material.DIAMOND_SWORD, 1);
-        //StoredPropertyHolder holder = itemStack.getPropertyHolder().create(ItemProperties.DISPLAY); TODO:  properly apply adapters
-        itemStack.getPropertyHolder().set(ItemProperties.DISPLAY_NAME, Component.text("hi"));
+        {
+            ItemStack itemStack = Bukkit.getUnsafe().newItem(Material.PLAYER_HEAD, 1);
 
-        event.getPlayer().getInventory().addItem(itemStack);
+            PlayerProfile profile = Bukkit.createProfile("Owen1212055");
+            profile.complete();
+            itemStack.getPropertyHolder().set(ItemProperties.SKULL_OWNER, profile);
+            itemStack.getPropertyHolder().getOrCreate(ItemProperties.DISPLAY).set(ItemProperties.DISPLAY_NAME, Component.text("GET REAL!"));
+
+            event.getPlayer().getInventory().addItem(itemStack);
+        }
+        {
+            ItemStack itemStack = Bukkit.getUnsafe().newItem(Material.PLAYER_HEAD, 1);
+            itemStack.editMeta(meta -> {
+                PlayerProfile profile = Bukkit.createProfile("Owen1212055");
+                profile.complete();
+                ((SkullMeta) meta).setPlayerProfile(profile);
+
+                meta.displayName(Component.text("GET REAL!"));
+            });
+            event.getPlayer().getInventory().addItem(itemStack);
+        }
     }
 }
