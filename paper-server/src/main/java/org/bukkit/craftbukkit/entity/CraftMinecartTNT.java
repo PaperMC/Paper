ad8f95c3df765090b8117b760eabd45d406d7bb9
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2023-04-02 13:21:31 +1000

    #1159: Add missing API for explosive minecarts
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftMinecartTNT.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftMinecartTNT.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.world.entity.vehicle.EntityMinecartTNT;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
@@ -8,6 +9,43 @@ import org.bukkit.entity.minecart.ExplosiveMinecart;
 final class CraftMinecartTNT extends CraftMinecart implements ExplosiveMinecart {
     CraftMinecartTNT(CraftServer server, EntityMinecartTNT entity) {
         super(server, entity);
+    }
+
+    @Override
+    public void setFuseTicks(int ticks) {
+        getHandle().fuse = ticks;
+    }
+
+    @Override
+    public int getFuseTicks() {
+        return getHandle().getFuse();
+    }
+
+    @Override
+    public void ignite() {
+        getHandle().primeFuse();
+    }
+
+    @Override
+    public boolean isIgnited() {
+        return getHandle().isPrimed();
+    }
+
+    @Override
+    public void explode() {
+        getHandle().explode(getHandle().getDeltaMovement().horizontalDistanceSqr());
+    }
+
+    @Override
+    public void explode(double power) {
+        Preconditions.checkArgument(0 <= power && power <= 5, "Power must be in range [0, 5] (got %s)", power);
+
+        getHandle().explode(power);
+    }
+
+    @Override
+    public EntityMinecartTNT getHandle() {
+        return (EntityMinecartTNT) super.getHandle();
     }
 
     @Override
