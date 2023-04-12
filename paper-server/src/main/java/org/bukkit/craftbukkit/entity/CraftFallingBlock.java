5d28a95165e02028f732e692518e8d098f86a236
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2023-04-13 07:08:26 +1000

    #1169: Add damage methods to FallingBlock
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftFallingBlock.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftFallingBlock.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.world.entity.item.EntityFallingBlock;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
@@ -65,5 +66,35 @@ public class CraftFallingBlock extends CraftEntity implements FallingBlock {
 
         // Second field for EntityFallingBlock
         getHandle().time = value;
+    }
+
+    @Override
+    public float getDamagePerBlock() {
+        return getHandle().fallDamagePerDistance;
+    }
+
+    @Override
+    public void setDamagePerBlock(float damage) {
+        Preconditions.checkArgument(damage >= 0.0, "damage must be >= 0.0, given %s", damage);
+
+        getHandle().fallDamagePerDistance = damage;
+        if (damage > 0.0) {
+            this.setHurtEntities(true);
+        }
+    }
+
+    @Override
+    public int getMaxDamage() {
+        return getHandle().fallDamageMax;
+    }
+
+    @Override
+    public void setMaxDamage(int damage) {
+        Preconditions.checkArgument(damage >= 0, "damage must be >= 0, given %s", damage);
+
+        getHandle().fallDamageMax = damage;
+        if (damage > 0) {
+            this.setHurtEntities(true);
+        }
     }
 }
