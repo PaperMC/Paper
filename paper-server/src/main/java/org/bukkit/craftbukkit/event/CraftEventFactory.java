ba817be945012b21331f5c150c72877991cb6ffd
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2023-05-03 20:46:43 +1000

    SPIGOT-519: Add TNTPrimeEvent
    Thanks to Pokechu22 for an earlier implementation
    
    By: Jishuna <joshl5324@gmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
@@ -150,6 +150,7 @@ import org.bukkit.event.block.EntityBlockFormEvent;
 import org.bukkit.event.block.FluidLevelChangeEvent;
 import org.bukkit.event.block.MoistureChangeEvent;
 import org.bukkit.event.block.NotePlayEvent;
+import org.bukkit.event.block.TNTPrimeEvent;
 import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
 import org.bukkit.event.entity.ArrowBodyCountChangeEvent;
 import org.bukkit.event.entity.BatToggleSleepEvent;
@@ -1733,5 +1734,15 @@ public class CraftEventFactory {
         List<org.bukkit.entity.Entity> bukkitEntities = Collections.unmodifiableList(entities.stream().map(Entity::getBukkitEntity).collect(Collectors.toList()));
         EntitiesUnloadEvent event = new EntitiesUnloadEvent(new CraftChunk((WorldServer) world, coords.x, coords.z), bukkitEntities);
         Bukkit.getPluginManager().callEvent(event);
+    }
+
+    public static boolean callTNTPrimeEvent(World world, BlockPosition pos, TNTPrimeEvent.PrimeCause cause, Entity causingEntity, BlockPosition causePosition) {
+        org.bukkit.entity.Entity bukkitEntity = (causingEntity == null) ? null : causingEntity.getBukkitEntity();
+        org.bukkit.block.Block bukkitBlock = (causePosition == null) ? null : CraftBlock.at(world, causePosition);
+
+        TNTPrimeEvent event = new TNTPrimeEvent(CraftBlock.at(world, pos), cause, bukkitEntity, bukkitBlock);
+        Bukkit.getPluginManager().callEvent(event);
+
+        return !event.isCancelled();
     }
 }
