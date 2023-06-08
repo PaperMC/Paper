ddd74452e444f4cd10e2d549dab4792f6e6c60a8
Nassim Jahnke <nassim@njahnke.dev>
2023-06-08 15:42:13 +0200

    Update test plugin

--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -1,13 +1,13 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
-import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
 import org.jetbrains.annotations.NotNull;
 
 public class TestPluginBootstrap implements PluginBootstrap {
 
     @Override
-    public void bootstrap(@NotNull PluginProviderContext context) {
+    public void bootstrap(@NotNull BootstrapContext context) {
     }
 
 }
