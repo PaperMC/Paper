b0576f313f6e9d6167a7d2d062d87d542feb547e
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2023-06-17 06:44:22 +1000

    SPIGOT-7389: Handle setting null items in ChiseledBookshelf Inventory
    By: Doc <nachito94@msn.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryChiseledBookshelf.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryChiseledBookshelf.java
@@ -3,12 +3,24 @@ package org.bukkit.craftbukkit.inventory;
 import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
 import org.bukkit.block.ChiseledBookshelf;
 import org.bukkit.inventory.ChiseledBookshelfInventory;
+import org.bukkit.inventory.ItemStack;
 
 public class CraftInventoryChiseledBookshelf extends CraftInventory implements ChiseledBookshelfInventory {
 
     public CraftInventoryChiseledBookshelf(ChiseledBookShelfBlockEntity inventory) {
         super(inventory);
 
+    }
+
+    @Override
+    public void setItem(int index, ItemStack item) {
+        net.minecraft.world.item.ItemStack nms = CraftItemStack.asNMSCopy(item);
+
+        if (nms.isEmpty()) {
+            this.getInventory().removeItemNoUpdate(index);
+        } else {
+            this.getInventory().setItem(index, nms);
+        }
     }
 
     @Override
