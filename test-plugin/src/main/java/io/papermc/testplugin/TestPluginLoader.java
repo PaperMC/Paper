b0d5e3f84abaede5de913cde152af2602bfc5565
powercas_gamer <cas@deltapvp.net>
2023-06-25 02:26:26 +0200

    remove test plugin stuff

--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginLoader.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginLoader.java
@@ -2,21 +2,10 @@ package io.papermc.testplugin;
 
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
-import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
-import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
 
 public class TestPluginLoader implements PluginLoader {
     @Override
     public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
-        final MavenLibraryResolver resolver = new MavenLibraryResolver();
-        classpathBuilder.getContext().getLogger().info("TESTING LOGGING IN LOADER");
-        resolver.addDependency(new Dependency(new DefaultArtifact("cloud.commandframework:cloud-paper:1.8.3"), null));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.spongepowered:configurate-gson:4.1.2"), null));
-        resolver.addRepository(new RemoteRepository.Builder("paper", "default", "https://repo.papermc.io/repository/maven-public/").build());
-
-        classpathBuilder.addLibrary(resolver);
     }
 }
