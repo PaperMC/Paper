913505c686ff8e32a3b13916f18d9a0762729e8a
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-07-18 18:33:58 -0700

    separate RegistryEvent and RegistrarEvent

--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -2,8 +2,9 @@ package io.papermc.testplugin;
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
-import io.papermc.paper.plugin.resource.RegisterEvent;
 import io.papermc.paper.plugin.resource.dummy.DummyResourceRegistrar;
+import io.papermc.paper.plugin.resource.dummy.NonRegistrarEvent;
+import io.papermc.paper.plugin.resource.event.RegistrarEvent;
 import io.papermc.paper.plugin.resource.hook.RegisterHooks;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,9 +15,11 @@ public class TestPluginBootstrap implements PluginBootstrap {
 
         RegisterHooks.DUMMY.add(context, event -> {
             final DummyResourceRegistrar registrar = event.registrar();
-            final RegisterEvent.Reloadable.Cause cause = event.cause();
-            System.out.println("dummy hook");
+            final RegistrarEvent.Reloadable.Cause cause = event.cause();
+            System.out.println("dummy hook: " + cause);
         });
+
+        RegisterHooks.NON_REGISTRAR_RELATED_EVENT.add(context, NonRegistrarEvent::someNonRegistrarRelatedThing);
     }
 
 }
