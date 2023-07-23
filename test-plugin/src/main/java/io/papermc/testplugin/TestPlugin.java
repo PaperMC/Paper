4b5827869e17c95034c403cfb39c3bec9b5a9aa3
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-07-23 09:11:24 -0700

    Rename to 'lifecycle'

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,7 +1,7 @@
 package io.papermc.testplugin;
 
-import io.papermc.paper.plugin.register.dummy.DummyResourceRegistrar;
-import io.papermc.paper.plugin.register.event.RegisterEvents;
+import io.papermc.paper.plugin.lifecycle.dummy.DummyResourceRegistrar;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEvents;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,7 +11,7 @@ public final class TestPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
 
-        this.handleRegisterEvent(RegisterEvents.DUMMY_STATIC, event -> {
+        this.handleLifecycleEvent(LifecycleEvents.DUMMY_STATIC, event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             System.out.println("dummy_static hook");
         });
--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -2,10 +2,11 @@ package io.papermc.testplugin;
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
-import io.papermc.paper.plugin.register.dummy.DummyResourceRegistrar;
-import io.papermc.paper.plugin.register.dummy.NonRegistrarEvent;
-import io.papermc.paper.plugin.register.event.registrar.RegistrarEvent;
-import io.papermc.paper.plugin.register.event.RegisterEvents;
+import io.papermc.paper.plugin.lifecycle.dummy.DummyResourceRegistrar;
+import io.papermc.paper.plugin.lifecycle.dummy.NonRegistrarEvent;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventHandler;
+import io.papermc.paper.plugin.lifecycle.event.registrar.RegistrarEvent;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEvents;
 import org.jetbrains.annotations.NotNull;
 
 public class TestPluginBootstrap implements PluginBootstrap {
@@ -13,16 +14,16 @@ public class TestPluginBootstrap implements PluginBootstrap {
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
 
-        context.handleRegisterEvent(RegisterEvents.DUMMY, event -> {
+        context.handleLifecycleEvent(LifecycleEvents.DUMMY, event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             final RegistrarEvent.Reloadable.Cause cause = event.cause();
             System.out.println("dummy hook: " + cause);
         });
 
-        final io.papermc.paper.plugin.register.event.RegisterEventHandler<RegistrarEvent<DummyResourceRegistrar>> handle = this::handle;
-        context.handleRegisterEvent(RegisterEvents.DUMMY, handle);
+        final LifecycleEventHandler<RegistrarEvent<DummyResourceRegistrar>> handle = this::handle;
+        context.handleLifecycleEvent(LifecycleEvents.DUMMY, handle);
 
-        context.handleRegisterEvent(RegisterEvents.NON_REGISTRAR_RELATED_EVENT, NonRegistrarEvent::someNonRegistrarRelatedThing);
+        context.handleLifecycleEvent(LifecycleEvents.NON_REGISTRAR_RELATED_EVENT, NonRegistrarEvent::someNonRegistrarRelatedThing);
     }
 
     private void handle(RegistrarEvent<DummyResourceRegistrar> event) {
