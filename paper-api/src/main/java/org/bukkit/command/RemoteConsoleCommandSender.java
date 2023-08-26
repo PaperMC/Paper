641a55cbc5d01492cfdddb4c5f8831174b9a7396
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2023-08-26 18:19:18 +1000

    SPIGOT-7467: Add getAddress to RemoteConsoleCommandSender
    By: md_5 <git@md-5.net>

--- paper-api/src/main/java/org/bukkit/command/RemoteConsoleCommandSender.java
+++ paper-api/src/main/java/org/bukkit/command/RemoteConsoleCommandSender.java
@@ -1,4 +1,17 @@
 package org.bukkit.command;
 
+import java.net.SocketAddress;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
 public interface RemoteConsoleCommandSender extends CommandSender {
+
+    /**
+     * Gets the socket address of this remote sender.
+     *
+     * @return the remote sender's address
+     */
+    @NotNull
+    @ApiStatus.Experimental
+    public SocketAddress getAddress();
 }
