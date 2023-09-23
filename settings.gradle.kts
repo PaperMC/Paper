4c17176c779c16901423886020094cbf89e34b51
Nassim Jahnke <nassim@njahnke.dev>
2023-09-23 11:46:31 +1000

    Update foojay resolver convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
 if (!file(".git").exists()) {
