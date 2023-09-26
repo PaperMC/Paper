ac251a9ed93a9f1b223d128d6864c400eccfb695
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2023-09-27 07:15:24 +1000

    #904: Update FeatureFlag for 1.20.2
    By: Doc <nachito94@msn.com>

--- paper-api/src/main/java/org/bukkit/FeatureFlag.java
+++ paper-api/src/main/java/org/bukkit/FeatureFlag.java
@@ -4,6 +4,9 @@ import org.jetbrains.annotations.ApiStatus;
 
 /**
  * This represents a Feature Flag for a World.
+ * <br>
+ * Flags which are unavailable in the current version will be null and/or
+ * removed.
  */
 @ApiStatus.Experimental
 public interface FeatureFlag extends Keyed {
@@ -15,7 +18,13 @@ public interface FeatureFlag extends Keyed {
 
     /**
      * <strong>AVAILABLE BETWEEN VERSIONS:</strong> 1.19 - 1.19.4
+     *
+     * @deprecated not available since 1.20
      */
+    @Deprecated
     @MinecraftExperimental
     public static final FeatureFlag UPDATE_1_20 = Bukkit.getUnsafe().getFeatureFlag(NamespacedKey.minecraft("update_1_20"));
+
+    @MinecraftExperimental
+    public static final FeatureFlag TRADE_REBALANCE = Bukkit.getUnsafe().getFeatureFlag(NamespacedKey.minecraft("trade_rebalance"));
 }
