215777a5f2739ac2324c0e15160f7dfaeb6128f9
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2023-09-28 06:46:54 +1000

    #1263: Remove unused implementation of AbstractProjectile#doesBounce() and #setBounce()
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/AbstractProjectile.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/AbstractProjectile.java
@@ -5,21 +5,16 @@ import org.bukkit.entity.Projectile;
 
 public abstract class AbstractProjectile extends CraftEntity implements Projectile {
 
-    private boolean doesBounce;
-
     public AbstractProjectile(CraftServer server, net.minecraft.world.entity.Entity entity) {
         super(server, entity);
-        doesBounce = false;
     }
 
     @Override
     public boolean doesBounce() {
-        return doesBounce;
+        return false;
     }
 
     @Override
-    public void setBounce(boolean doesBounce) {
-        this.doesBounce = doesBounce;
-    }
+    public void setBounce(boolean doesBounce) {}
 
 }
