851acb6496fb81dd29a9c18fb527ce6383e5a062
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2023-10-03 07:32:55 +1100

    #1267: Add more lightning API
    By: Parker Hawke <hawkeboyz2@hotmail.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftLightningStrike.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftLightningStrike.java
@@ -1,8 +1,10 @@
 package org.bukkit.craftbukkit.entity;
 
+import net.minecraft.server.level.EntityPlayer;
 import net.minecraft.world.entity.EntityLightning;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.LightningStrike;
+import org.bukkit.entity.Player;
 
 public class CraftLightningStrike extends CraftEntity implements LightningStrike {
     public CraftLightningStrike(final CraftServer server, final EntityLightning entity) {
@@ -12,6 +14,31 @@ public class CraftLightningStrike extends CraftEntity implements LightningStrike
     @Override
     public boolean isEffect() {
         return getHandle().visualOnly;
+    }
+
+    public int getFlashes() {
+        return getHandle().flashes;
+    }
+
+    public void setFlashes(int flashes) {
+        getHandle().flashes = flashes;
+    }
+
+    public int getLifeTicks() {
+        return getHandle().life;
+    }
+
+    public void setLifeTicks(int ticks) {
+        getHandle().life = ticks;
+    }
+
+    public Player getCausingPlayer() {
+        EntityPlayer player = getHandle().getCause();
+        return (player != null) ? player.getBukkitEntity() : null;
+    }
+
+    public void setCausingPlayer(Player player) {
+        getHandle().setCause((player != null) ? ((CraftPlayer) player).getHandle() : null);
     }
 
     @Override
