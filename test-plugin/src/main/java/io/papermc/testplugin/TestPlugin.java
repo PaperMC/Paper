7961717443e925dbf98a4ba12c24e638e4b621d4
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-11-21 22:35:11 -0800

    use builder pattern to create event handler configurations

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -14,25 +14,25 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         final LifecycleEventManager<Plugin> lifecycles = this.getLifecycleManager();
-        lifecycles.registerPrioritizedEventHandler(LifecycleEvents.DUMMY_STATIC, event -> {
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY_STATIC.handler(event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             System.out.println("dummy_static hook SECOND");
-        }, 1);
-        lifecycles.registerPrioritizedEventHandler(LifecycleEvents.DUMMY_STATIC, event -> {
+        }).priority(1));
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY_STATIC.handler(event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             System.out.println("dummy_static hook FIRST");
-        }, -1);
-        lifecycles.registerMonitorEventHandler(LifecycleEvents.DUMMY_STATIC, event -> {
+        }).priority(-1));
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY_STATIC.handler(event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             System.out.println("dummy_static hook FOURTH (monitor)");
-        });
-        lifecycles.registerPrioritizedEventHandler(LifecycleEvents.DUMMY_STATIC, event -> {
+        }));
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY_STATIC.handler(event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             System.out.println("dummy_static hook THIRD");
-        }, 100);
+        }).priority(100));
 
-        lifecycles.registerEventHandler(LifecycleEvents.REGISTER_ANYWHERE_EVENT, event -> {
+        lifecycles.registerEventHandler(LifecycleEvents.REGISTER_ANYWHERE_EVENT.handler(event -> {
 
-        });
+        }));
     }
 }
--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -4,9 +4,12 @@ import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.lifecycle.dummy.DummyResourceRegistrar;
 import io.papermc.paper.plugin.lifecycle.dummy.NonRegistrarEvent;
-import io.papermc.paper.plugin.lifecycle.event.LifecycleEventHandler;
+import io.papermc.paper.plugin.lifecycle.dummy.RegisterAnywhereEvent;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventOwner;
+import io.papermc.paper.plugin.lifecycle.event.handler.LifecycleEventHandler;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEvents;
+import io.papermc.paper.plugin.lifecycle.event.handler.configuration.MonitorLifecycleEventHandlerConfiguration;
 import io.papermc.paper.plugin.lifecycle.event.registrar.RegistrarEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,21 +19,25 @@ public class TestPluginBootstrap implements PluginBootstrap {
     public void bootstrap(@NotNull BootstrapContext context) {
 
         final LifecycleEventManager<BootstrapContext> lifecycles = context.getLifecycleManager();
-        lifecycles.registerEventHandler(LifecycleEvents.DUMMY, event -> {
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY.handler(event -> {
             final DummyResourceRegistrar registrar = event.registrar();
             final RegistrarEvent.Reloadable.Cause cause = event.cause();
             System.out.println("dummy hook: " + cause);
+        }));
+
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY.handler(this.handle("dummy FIRST")));
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY.handler(this.handle("dummy LAST (monitor)")).monitor());
+        lifecycles.registerEventHandler(LifecycleEvents.DUMMY.handler(this.handle("dummy SECOND")));
+
+
+        final MonitorLifecycleEventHandlerConfiguration<LifecycleEventOwner> handler = LifecycleEvents.REGISTER_ANYWHERE_EVENT.handler(event -> {
+
         });
-
-        lifecycles.registerEventHandler(LifecycleEvents.DUMMY, this.handle("dummy FIRST"));
-        lifecycles.registerMonitorEventHandler(LifecycleEvents.DUMMY, this.handle("dummy LAST (monitor)"));
-        lifecycles.registerEventHandler(LifecycleEvents.DUMMY, this.handle("dummy SECOND"));
-
-
-        lifecycles.registerEventHandler(LifecycleEvents.REGISTER_ANYWHERE_EVENT, event -> {
-
-        });
-        lifecycles.registerEventHandler(LifecycleEvents.NON_REGISTRAR_RELATED_EVENT, NonRegistrarEvent::someNonRegistrarRelatedThing);
+        // lifecycles.registerEventHandler(LifecycleEvents.DUMMY_STATIC.handler(event -> { // shouldn't compile
+        //
+        // }));
+        lifecycles.registerEventHandler(handler);
+        lifecycles.registerEventHandler(LifecycleEvents.NON_REGISTRAR_RELATED_EVENT.handler(NonRegistrarEvent::someNonRegistrarRelatedThing));
     }
 
     private LifecycleEventHandler<RegistrarEvent<DummyResourceRegistrar>> handle(final String out) {
