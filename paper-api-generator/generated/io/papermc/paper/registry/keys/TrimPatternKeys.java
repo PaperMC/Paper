b89fff049077a37420b4b83a74c90ed2308bf61c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-12-05 14:36:33 -0700

    Update generated API

--- paper-api-generator/generated/io/papermc/paper/registry/keys/TrimPatternKeys.java
+++ paper-api-generator/generated/io/papermc/paper/registry/keys/TrimPatternKeys.java
@@ -23,7 +23,7 @@ import org.jetbrains.annotations.NotNull;
         "unused",
         "SpellCheckingInspection"
 })
-@GeneratedFrom("1.20.2")
+@GeneratedFrom("1.20.3")
 @ApiStatus.Experimental
 public final class TrimPatternKeys {
     /**
