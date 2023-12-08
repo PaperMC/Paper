7c36ea0a5c7533b6f805e50530820ebb4357f697
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-12-08 11:12:59 -0700

    Update paperweight to 1.5.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.core") version "1.5.10"
+    id("io.papermc.paperweight.core") version "1.5.11"
 }
 
 allprojects {
