aa08956431e2d5c12e40916dd6080d684f783a21
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-12-19 23:40:24 -0800

    Split Position from Location type hierarchy

--- test-plugin/build.gradle.kts
+++ test-plugin/build.gradle.kts
@@ -1,7 +1,12 @@
 version = "1.0.0-SNAPSHOT"
 
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
 dependencies {
-    compileOnly(project(":paper-api"))
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    // compileOnly(project(":paper-api"))
 }
 
 tasks.processResources {
