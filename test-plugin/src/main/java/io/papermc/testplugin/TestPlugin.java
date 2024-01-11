7d1bd0b6dbb2f6213cff83181bc37c44cab2ee31
Khwaja Yousuf <kyousufmo17@gmail.com>
2024-01-10 23:00:15 -0500

    Add player join event handler in TestPlugin

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,12 +1,22 @@
 package io.papermc.testplugin;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        // Register the plugin to listen for events
         this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    // Event handler for player join event
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Your custom logic when a player joins
+        event.getPlayer().sendMessage("Welcome to the server, " + event.getPlayer().getName() + "!");
     }
 }
