20b96901d30899d5b0b77df932bee48491a486f4
Khwaja Yousuf <kyousufmo17@gmail.com>
2024-01-14 17:50:53 -0500

    Improve git patch application script

--- scripts/apatch.sh
+++ scripts/apatch.sh
@@ -3,50 +3,50 @@
 
 echo "Executing the patching script..."
 
-gitcmd="git -c commit.gpgsign=false"
+git_command="git -c commit.gpgsign=false"
 
-noapply=1
-isreject=0
+no_apply=1
+is_reject=0
 
 # Check if the "--noapplied" flag is provided
 if [[ $1 == "--noapplied" ]]; then
-    noapply=1
+    no_apply=1
     shift
 fi
 
 # Check if a file is specified
 if [ ! -z "$1" ]; then
-    file="$1"
+    patch_file="$1"
 # If no file is specified and there's a patch file in rebase-apply, use it
 elif [ -z "$1" ] && [ -f .git/rebase-apply/patch ]; then
-    file=".git/rebase-apply/patch"
-    noapply=1
-    isreject=1
+    patch_file=".git/rebase-apply/patch"
+    no_apply=1
+    is_reject=1
 else
     echo "Please specify a file"
     exit 1
 fi
 
-applied=$(echo $file | sed 's/.patch$/-applied\.patch/g')
+applied_patch=$(echo $patch_file | sed 's/.patch$/-applied\.patch/g')
 
 # Check if the "--reset" flag is provided
 if [ "$1" == "--reset" ]; then
-    $gitcmd am --abort
-    $gitcmd reset --hard
-    $gitcmd clean -f
+    $git_command am --abort
+    $git_command reset --hard
+    $git_command clean -f
     exit 0
 fi
 
 # Attempt to apply the patch using "git am"
-(test "$isreject" != "1" && $gitcmd am -3 $file) || (
+(test "$is_reject" != "1" && $git_command am -3 $patch_file) || (
     echo "Failures - Wiggling"
-    $gitcmd reset --hard
-    $gitcmd clean -f
-    errors=$($gitcmd apply --rej $file 2>&1)
+    $git_command reset --hard
+    $git_command clean -f
+    errors=$($git_command apply --rej $patch_file 2>&1)
     echo "$errors" >> ~/patch.log
-    export missingfiles=""
-    export summaryfail=""
-    export summarygood=""
+    export missing_files=""
+    export summary_fail=""
+    export summary_good=""
     for i in $(find . -name \*.rej); do
         base=$(echo "$i" | sed 's/.rej//g')
         if [ -f "$i" ]; then
@@ -54,34 +54,34 @@ fi
             rm "$base.porig" "$i"
         else
             echo "No such file: $base"
-            missingfiles="$missingfiles\n$base"
+            missing_files="$missing_files\n$base"
         fi
     done
-    for i in $($gitcmd status --porcelain | awk '{print $2}'); do
-        filedata=$(cat "$i")
-        if [ -f "$file" ] && [[ "$filedata" == *"<<<<<"* ]]; then
-            export summaryfail="$summaryfail\nFAILED TO APPLY: $i"
+    for i in $($git_command status --porcelain | awk '{print $2}'); do
+        file_data=$(cat "$i")
+        if [ -f "$patch_file" ] && [[ "$file_data" == *"<<<<<"* ]]; then
+            export summary_fail="$summary_fail\nFAILED TO APPLY: $i"
         else
-            $gitcmd add --force "$i"
-            export summarygood="$summarygood\nAPPLIED CLEAN: $i"
+            $git_command add --force "$i"
+            export summary_good="$summary_good\nAPPLIED CLEAN: $i"
         fi
     done
-    echo -e "$summarygood"
-    echo -e "$summaryfail"
+    echo -e "$summary_good"
+    echo -e "$summary_fail"
     if [[ "$errors" == *"No such file"* ]]; then
         echo "===========================";
         echo " "
         echo " MISSING FILES"
         echo $(echo "$errors" | grep "No such file")
-        echo -e "$missingfiles"
+        echo -e "$missing_files"
         echo " "
         echo "===========================";
     fi
-    $gitcmd status
-    $gitcmd diff
+    $git_command status
+    $git_command diff
 )
 
 # Move the patch file to a new name if "--noapplied" is not specified and it's not already an applied patch
-if [[ "$noapply" != "1" ]] && [[ "$file" != *-applied.patch ]]; then
-    mv "$file" "$applied"
+if [[ "$no_apply" != "1" ]] && [[ "$patch_file" != *-applied.patch ]]; then
+    mv "$patch_file" "$applied_patch"
 fi
