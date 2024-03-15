d19f69b3eb3e507583794afe88fe92285308b78d
vicisacat <victor.branchu@gmail.com>
2024-03-15 17:35:18 +0100

    Expanded Hopper API

--- paper-api/src/main/java/org/bukkit/block/Hopper.java
+++ paper-api/src/main/java/org/bukkit/block/Hopper.java
@@ -6,4 +6,20 @@ import org.bukkit.loot.Lootable;
 /**
  * Represents a captured state of a hopper.
  */
-public interface Hopper extends Container, LootableBlockInventory { } // Paper
+public interface Hopper extends Container, LootableBlockInventory { // Paper
+    // Paper start - Expanded Hopper API
+    /**
+     * Sets the cooldown before the hopper transfers or sucks in another item
+     * @param cooldown the cooldown in ticks
+     * @throws IllegalArgumentException if the passed cooldown value is negative.
+     */
+    void setTransferCooldown(@org.jetbrains.annotations.Range(from = 0, to = Integer.MAX_VALUE) int cooldown);
+
+    /**
+     * Returns the cooldown before the hopper transfers or sucks in another item
+     * @return the cooldown in ticks
+     */
+    int getTransferCooldown();
+    // Paper end - Expanded Hopper API
+}
+
