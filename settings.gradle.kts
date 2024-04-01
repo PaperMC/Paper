1d296f028eb06031bbe3fea847397586dcb62e2f
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-01 13:06:23 -0700

    Compile against & shade the filtered jar

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ import java.util.Locale
 
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
