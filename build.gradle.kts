ff77f8253918255a682eb094952b58b04b3d6117
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-26 16:19:59 -0700

    paperweight 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.core") version "1.6.0-SNAPSHOT"
+    id("io.papermc.paperweight.core") version "1.6.0"
 }
 
 allprojects {
