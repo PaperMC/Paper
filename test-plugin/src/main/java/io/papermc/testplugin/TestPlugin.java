609f685a2b3be17157c46177133f98fed3fc1211
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-05-15 20:16:11 -0700

    Properly handle 'result' type inventories

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,6 +1,14 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.event.player.ChatEvent;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.LoomInventory;
+import org.bukkit.inventory.StonecutterInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
@@ -10,6 +18,16 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
+    }
+
+    @EventHandler
+    public void onEvent(ChatEvent event) {
+        final StonecutterInventory inv = (StonecutterInventory) this.getServer().createInventory(null, InventoryType.STONECUTTER);
+        // inv.setItem(0, new ItemStack(Material.STONE));
+        this.getServer().getScheduler().runTaskLater(this, () -> {
+            inv.setItem(0, new ItemStack(Material.STONE));
+        }, 40L);
+        event.getPlayer().openInventory(inv);
     }
 
 }
