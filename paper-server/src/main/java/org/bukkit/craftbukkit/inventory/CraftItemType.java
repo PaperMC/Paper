6d2082bf7528d4d595f08cde9e11f68004fa7305
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2024-06-05 07:15:52 +1000

    #1360: Add Material#isCompostable and Material#getCompostChance
    By: mfnalex <mfnalex@jeff-media.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftItemType.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/inventory/CraftItemType.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemBlock;
 import net.minecraft.world.item.ItemRecord;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.level.block.BlockComposter;
 import net.minecraft.world.level.block.entity.TileEntityFurnace;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -174,6 +175,17 @@ public class CraftItemType<M extends ItemMeta> implements ItemType.Typed<M>, Han
     @Override
     public boolean isFuel() {
         return TileEntityFurnace.isFuel(new net.minecraft.world.item.ItemStack(item));
+    }
+
+    @Override
+    public boolean isCompostable() {
+        return BlockComposter.COMPOSTABLES.containsKey(item);
+    }
+
+    @Override
+    public float getCompostChance() {
+        Preconditions.checkArgument(isCompostable(), "The item type " + getKey() + " is not compostable");
+        return BlockComposter.COMPOSTABLES.getFloat(item);
     }
 
     @Override
