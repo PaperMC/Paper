df157b7d6c0979338644003ec18a875f2c9ffad1
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-06-14 14:48:56 -0700

    rebased on 1.21

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,8 +1,5 @@
 package io.papermc.testplugin;
 
-import io.papermc.paper.registry.keys.GameEventKeys;
-import org.bukkit.GameEvent;
-import org.bukkit.Registry;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,17 +10,6 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
-
-        final GameEvent newEvent = Registry.GAME_EVENT.get(TestPluginBootstrap.NEW_EVENT);
-        if (newEvent == null) {
-            throw new RuntimeException("could not find new event");
-        } else {
-            System.out.println("New event: " + newEvent.getKey() + " " + newEvent.getRange());
-        }
-        final GameEvent changed = Registry.GAME_EVENT.get(GameEventKeys.BLOCK_OPEN);
-        System.out.println("changed: " + changed.getRange());
-        final GameEvent same = Registry.GAME_EVENT.get(GameEventKeys.CONTAINER_OPEN);
-        System.out.println("same: " + same.getRange());
     }
 
 }
--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -2,34 +2,13 @@ package io.papermc.testplugin;
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
-import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
-import io.papermc.paper.registry.RegistryKey;
-import io.papermc.paper.registry.TypedKey;
-import io.papermc.paper.registry.event.RegistryEvents;
-import io.papermc.paper.registry.keys.GameEventKeys;
-import net.kyori.adventure.key.Key;
-import org.bukkit.GameEvent;
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.framework.qual.DefaultQualifier;
 import org.jetbrains.annotations.NotNull;
 
-@DefaultQualifier(NonNull.class)
 public class TestPluginBootstrap implements PluginBootstrap {
-
-    static final TypedKey<GameEvent> NEW_EVENT = TypedKey.create(RegistryKey.GAME_EVENT, Key.key("machine_maker", "best_event"));
 
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         // io.papermc.testplugin.brigtests.Registration.registerViaBootstrap(context);
-
-        final LifecycleEventManager<BootstrapContext> lifecycleManager = context.getLifecycleManager();
-        lifecycleManager.registerEventHandler(RegistryEvents.GAME_EVENT.entryAdd().newHandler(event -> {
-            event.builder().range(event.builder().range() * 40);
-        }).priority(10).onlyFor(GameEventKeys.BLOCK_OPEN));
-        lifecycleManager.registerEventHandler(RegistryEvents.GAME_EVENT.freeze(), event -> {
-            event.registry().register(NEW_EVENT, builder -> {
-                builder.range(2);
-            });
-        });
     }
+
 }
