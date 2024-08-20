caf3025baa0c8b4f8f9222fdc0a5c1f4753da433
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-08-19 20:35:47 -0700

    comment out varargs methods for now

--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -11,6 +11,7 @@ import io.papermc.paper.registry.event.RegistryEvents;
 import io.papermc.paper.registry.keys.tags.EnchantmentTagKeys;
 import io.papermc.paper.registry.keys.tags.ItemTypeTagKeys;
 import io.papermc.paper.registry.tag.TagKey;
+import io.papermc.paper.tag.PostFlattenTagRegistrar;
 import io.papermc.paper.tag.PreFlattenTagRegistrar;
 import io.papermc.paper.tag.TagEntry;
 import java.util.Set;
@@ -54,11 +55,11 @@ public class TestPluginBootstrap implements PluginBootstrap {
             ));
         });
 
-        manager.registerEventHandler(LifecycleEvents.TAGS.preFlatten(RegistryKey.ENCHANTMENT), event -> {
-            final PreFlattenTagRegistrar<Enchantment> registrar = event.registrar();
+        manager.registerEventHandler(LifecycleEvents.TAGS.postFlatten(RegistryKey.ENCHANTMENT), event -> {
+            final PostFlattenTagRegistrar<Enchantment> registrar = event.registrar();
             registrar.addToTag(
                 EnchantmentTagKeys.IN_ENCHANTING_TABLE,
-                Set.of(TagEntry.valueEntry(CUSTOM_ENCHANT, true))
+                Set.of(CUSTOM_ENCHANT)
             );
         });
     }
