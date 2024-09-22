8a2ec8c1fba0cc2cd0317782321ba949e1b30279
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-09-22 11:44:11 -0700

    restore vanilla teleportation logic for non-players

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,6 +1,13 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.event.player.ChatEvent;
+import java.util.Collection;
+import org.bukkit.entity.Pig;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
@@ -12,4 +19,28 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
     }
 
+    @EventHandler
+    public void onEvent(ChatEvent event) {
+        final Player player = event.getPlayer();
+        final Collection<Pig> nearby = player.getWorld().getNearbyEntitiesByType(Pig.class, player.getLocation(), 10);
+        for (final Pig pig : nearby) {
+            if (pig.getPassengers().isEmpty()) {
+                System.out.println("Teleport: " + pig.teleport(player.getLocation()));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEvent(EntityTeleportEvent event) {
+        System.out.println("EntityTeleportEvent");
+        System.out.println(event.getEntity());
+        System.out.println(event.getClass().getSimpleName());
+    }
+
+    @EventHandler
+    public void onEvent(PlayerTeleportEvent event) {
+        System.out.println("PlayerTeleportEvent");
+        System.out.println(event.getPlayer());
+        System.out.println(event.getClass().getSimpleName());
+    }
 }
