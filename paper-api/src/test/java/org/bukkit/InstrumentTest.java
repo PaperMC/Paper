5b6f5c4a3a2abec4e4d901c5966f702949020275
Bukkit/Spigot <noreply+git-bukkit@papermc.io>
2024-11-02 11:23:43 +1100

    Fix InstrumentTest
    By: md_5 <git@md-5.net>

--- paper-api/src/test/java/org/bukkit/InstrumentTest.java
+++ paper-api/src/test/java/org/bukkit/InstrumentTest.java
@@ -2,9 +2,10 @@ package org.bukkit;
 
 import static org.bukkit.support.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
+import org.bukkit.support.AbstractTestingBase;
 import org.junit.jupiter.api.Test;
 
-public class InstrumentTest {
+public class InstrumentTest extends AbstractTestingBase {
     @Test
     public void getByType() {
         for (Instrument instrument : Instrument.values()) {
