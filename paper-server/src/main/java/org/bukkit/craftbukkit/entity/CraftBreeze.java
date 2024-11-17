c7707ece4caaa2cca2593a3c03ce934711a4c658
CraftBukkit/Spigot <noreply+git-craftbukkit@papermc.io>
2024-11-17 11:22:36 +1100

    SPIGOT-7957: Fix setTarget method for Breeze
    By: Doc <nachito94@msn.com>

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftBreeze.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftBreeze.java
@@ -1,7 +1,10 @@
 package org.bukkit.craftbukkit.entity;
 
+import net.minecraft.world.entity.EntityLiving;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Breeze;
+import org.bukkit.entity.LivingEntity;
 
 public class CraftBreeze extends CraftMonster implements Breeze {
 
@@ -12,6 +15,13 @@ public class CraftBreeze extends CraftMonster implements Breeze {
     @Override
     public net.minecraft.world.entity.monster.breeze.Breeze getHandle() {
         return (net.minecraft.world.entity.monster.breeze.Breeze) entity;
+    }
+
+    @Override
+    public void setTarget(LivingEntity target) {
+        super.setTarget(target);
+        EntityLiving entityLivingTarget = (target instanceof CraftLivingEntity craftLivingEntity) ? craftLivingEntity.getHandle() : null;
+        this.getHandle().getBrain().setMemory(MemoryModuleType.ATTACK_TARGET, entityLivingTarget); // SPIGOT-7957: We need override memory for set target and trigger attack behaviours
     }
 
     @Override
