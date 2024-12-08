d23702f449f340f044e7794316925b06872e2ac7
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-12-07 20:13:17 -0700

    Add back remapper for reobf testing and dev bundle

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -12,6 +12,7 @@ val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 dependencies {
     mache("io.papermc:mache:1.21.4+build.5")
     paperclip("io.papermc:paperclip:3.0.3")
+    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
 }
 
 paperweight {
