c31d95eccb5f8fccf293333d0fecbd986bd9061d
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-12-22 11:44:49 -0800

    remove test plugin diff accidentally committed

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,9 +1,5 @@
 package io.papermc.testplugin;
 
-import io.papermc.paper.registry.RegistryAccess;
-import io.papermc.paper.registry.RegistryKey;
-import org.bukkit.Art;
-import org.bukkit.Registry;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,10 +10,6 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
-
-        final Registry<Art> registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.PAINTING_VARIANT);
-        final Art art = registry.get(TestPluginBootstrap.NEW);
-        System.out.println(art.getKey());
     }
 
 }
