51901ad4f6510397de6b397ad15ae478541c6001
Noah van der Aa <ndvdaa@gmail.com>
2024-12-22 23:36:30 +0100

    [ci skip] remove patch updating related tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,5 @@
-import io.papermc.paperweight.util.*
-import io.papermc.paperweight.util.constants.*
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import java.io.IOException
-import java.net.URI
-import java.nio.file.FileVisitResult
-import java.nio.file.Files
-import java.nio.file.SimpleFileVisitor
-import kotlin.io.path.*
-import java.nio.file.Path
-import kotlin.random.Random
 
 plugins {
     id("io.papermc.paperweight.core") version "2.0.0-beta.8" apply false
@@ -79,102 +69,6 @@ tasks.register("printPaperVersion") {
     val paperVersion = provider { project.version }
     doLast {
         println(paperVersion.get())
-    }
-}
-
-tasks.register("gibWork") {
-    val issue = providers.gradleProperty("updateTaskListIssue").get()
-    val patchesFolder = layout.projectDirectory.dir("paper-server/patches/").convertToPath()
-    val storage = layout.cache.resolve("last-updating-folder").also { it.parent.createDirectories() }
-
-    doLast {
-        val html = URI(issue).toURL().readText()
-
-        val beginMarker = "```[tasklist]"
-        val start = html.indexOf(beginMarker)
-        val end = html.indexOf("```", start + beginMarker.length)
-        val taskList = html.substring(start + beginMarker.length, end)
-
-        // Extract all incomplete tasks and select a random one
-        val incompleteTasks = taskList.split("\\n").filter { it.startsWith("- [ ]") }.map { it.replace("- [ ] ", "") }
-        if (incompleteTasks.isEmpty()) {
-            error("No incomplete tasks found in the task list.")
-        }
-
-        val next = incompleteTasks[Random.nextInt(incompleteTasks.size)]
-
-        println("checking out $next...")
-        val dir = patchesFolder.resolve("unapplied").resolve(next)
-        if (!dir.exists()) {
-            error("Unapplied patch folder $next does not exist, did someone else already check it out and forgot to mark it?")
-        }
-        dir.listDirectoryEntries("*.patch").forEach { patch ->
-            patch.copyTo(patchesFolder.resolve("sources").resolve(next).resolve(patch.fileName).also { it.createDirectories() }, overwrite = true)
-            patch.deleteIfExists()
-        }
-        if (dir.listDirectoryEntries().isEmpty()) {
-            dir.deleteIfExists()
-        }
-
-        storage.writeText(next)
-        println("please tick the box in the issue: $issue")
-        println("if you don't finish it, uncheck the task again after you commited")
-    }
-}
-
-tasks.register("showWork") {
-    val patchDir = layout.projectDirectory.dir("paper-server/patches/unapplied/").convertToPath()
-
-    doLast {
-        Files.walkFileTree(patchDir, object : SimpleFileVisitor<Path>() {
-            override fun postVisitDirectory(dir: Path?, exc: IOException?): FileVisitResult {
-                dir?.takeIf { it.listDirectoryEntries("*.patch").isNotEmpty() }?.let {
-                    println("- [ ] ${patchDir.relativize(it).pathString.replace("\\", "/")}")
-                }
-                return FileVisitResult.CONTINUE
-            }
-        })
-    }
-}
-
-tasks.register("checkWork") {
-    notCompatibleWithConfigurationCache("This task is interactive")
-    fun expandUserHome(path: String): Path {
-        return Path.of(path.replaceFirst("^~".toRegex(), System.getProperty("user.home")))
-    }
-
-    val input = providers.fileContents(layout.projectDirectory.file("$CACHE_PATH/last-updating-folder")).asText.map { it.trim() }
-    val patchFolder = layout.projectDirectory.dir("paper-server/patches/sources").dir(input)
-    val sourceFolder = layout.projectDirectory.dir("paper-server/src/minecraft/java").dir(input)
-    val targetFolder = providers.gradleProperty("cleanPaperRepo").map {
-        expandUserHome(it).resolve(input.get())
-    }
-
-    fun copy(back: Boolean = false) {
-        patchFolder.path.listDirectoryEntries().forEach {
-            val relative = patchFolder.path.relativize(it).toString().replace(".patch", "")
-            val source = sourceFolder.path.resolve(relative)
-            val target = targetFolder.get().resolve(relative)
-            if (target.isDirectory()) { return@forEach }
-            if (back) {
-                target.copyTo(source, overwrite = true)
-            } else {
-                source.copyTo(target, overwrite = true)
-            }
-        }
-    }
-
-    doLast {
-        if (!targetFolder.isPresent) {
-            error("cleanPaperRepo is required, define it in gradle.properties")
-        }
-        copy()
-        val files = patchFolder.path.listDirectoryEntries().map { it.fileName.toString().replace(".patch", "") }
-        println("Copied $files from $sourceFolder to $targetFolder")
-        println("Make the files compile, then press enter to copy them back!")
-        System.`in`.read()
-        copy(back = true)
-        println("copied back!")
     }
 }
 
