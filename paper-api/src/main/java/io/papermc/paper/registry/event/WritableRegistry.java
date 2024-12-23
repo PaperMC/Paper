8551cb23b030bb289886de957d8b00466e13d4ab
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-12-23 14:33:20 -0800

    For new registry values, allow copying from existing

--- paper-api/src/main/java/io/papermc/paper/registry/event/WritableRegistry.java
+++ paper-api/src/main/java/io/papermc/paper/registry/event/WritableRegistry.java
@@ -25,4 +25,17 @@ public interface WritableRegistry<T, B extends RegistryBuilder<T>> {
      * @param value a consumer for the entry's builder
      */
     void register(TypedKey<T> key, Consumer<? super B> value);
+
+    /**
+     * Register a new value with the specified key. This will
+     * fire a {@link RegistryEntryAddEvent} for the new entry. The
+     * builder in the consumer will be pre-filled with the values
+     * from the copyFrom key.
+     *
+     * @param key the entry's key (must be unique from others)
+     * @param copyFrom the key to copy values from (must already be registered)
+     * @param value a consumer for the entry's builder
+     * @throws IllegalArgumentException if copyFrom doesn't exist
+     */
+    void register(TypedKey<T> key, TypedKey<T> copyFrom, Consumer<? super B> value);
 }
