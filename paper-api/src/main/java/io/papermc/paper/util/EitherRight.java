0048f124020cc35cdf2b9ea706768ba691d44100
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-12-24 10:59:46 -0800

    add missing annotations on EitherRight

--- paper-api/src/main/java/io/papermc/paper/util/EitherRight.java
+++ paper-api/src/main/java/io/papermc/paper/util/EitherRight.java
@@ -1,4 +1,9 @@
 package io.papermc.paper.util;
 
+import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+@ApiStatus.Internal
 public record EitherRight<L, R>(R value) implements Either.Right<L, R> {
 }
