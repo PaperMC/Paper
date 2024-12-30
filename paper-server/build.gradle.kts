a81990e0e8d5b50af3b8921acf03de9f61996174
Nassim Jahnke <nassim@njahnke.dev>
2024-12-30 11:04:58 +0100

    Update mache, fixes dev bundle use with Java 23

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:1.21.4+build.6")
+    mache("io.papermc:mache:1.21.4+build.7")
     paperclip("io.papermc:paperclip:3.0.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
