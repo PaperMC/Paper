8900c6a26b1d07f1fb1616da122a221405bdaf67
Doc <nachito94@msn.com>
2025-01-07 15:22:09 -0300

    Add docs to InstrumentRegistryEntry

--- paper-api/src/main/java/io/papermc/paper/registry/data/InstrumentRegistryEntry.java
+++ paper-api/src/main/java/io/papermc/paper/registry/data/InstrumentRegistryEntry.java
@@ -16,14 +16,34 @@ import org.jetbrains.annotations.Contract;
 @ApiStatus.NonExtendable
 public interface InstrumentRegistryEntry {
 
+    /**
+     * Provides the duration of the instrument, which is time to use.
+     *
+     * @return the duration.
+     */
     @Contract(pure = true)
     @Positive float duration();
 
+    /**
+     * Provides the range of the instrument, which is range of the sound.
+     *
+     * @return the range.
+     */
     @Contract(pure = true)
     @Positive float range();
 
+    /**
+     * Provides the description of the instrument, which is used in the item tooltip.
+     *
+     * @return the description.
+     */
     Component description();
 
+    /**
+     * Provides the sound event of the instrument.
+     *
+     * @return the sound event.
+     */
     @Contract(pure = true)
     Either<TypedKey<Sound>, SoundEventRegistryEntry> soundEvent();
 
@@ -45,41 +65,52 @@ public interface InstrumentRegistryEntry {
     interface Builder extends InstrumentRegistryEntry, RegistryBuilder<MusicInstrument> {
 
         /**
-         * @param duration
-         * @return this builder instance.
-         * @see InstrumentRegistryEntry#duration()
-         * @see MusicInstrument#getDuration()
+         * Sets the duration of use for this instrument.
+         *
+         * @param duration the duration (positive)
+         * @return this builder
          */
         @Contract(value = "_ -> this", mutates = "this")
         Builder duration(@Positive float duration);
 
         /**
-         * @param range
-         * @return this builder instance.
-         * @see InstrumentRegistryEntry#range()
-         * @see MusicInstrument#getRange() ()
+         * Sets the range for this instrument.
+         *
+         * @param range the range (positive)
+         * @return this builder
          */
         @Contract(value = "_ -> this", mutates = "this")
         Builder range(@Positive float range);
 
         /**
-         * @param description
-         * @return this builder instance.
-         * @see InstrumentRegistryEntry#description()
-         * @see MusicInstrument#description()
+         * Sets the description for this instrument.
+         *
+         * @param description the description
+         * @return this builder
          */
         @Contract(value = "_ -> this", mutates = "this")
         Builder description(Component description);
 
         /**
-         * @param soundEvent
-         * @return this builder instance.
-         * @see InstrumentRegistryEntry#soundEvent()
-         * @see MusicInstrument#getSoundEvent()
+         * Sets the sound event for this instrument to a sound event present
+         * in the {@link io.papermc.paper.registry.RegistryKey#SOUND_EVENT} registry.
+         * <p>This will override {@link #soundEvent(Consumer)}</p>
+         *
+         * @param soundEvent the sound event
+         * @return this builder
+         * @see #soundEvent(Consumer)
          */
         @Contract(value = "_ -> this", mutates = "this")
         Builder soundEvent(TypedKey<Sound> soundEvent);
 
+        /**
+         * Sets the sound event for this instrument to a new sound event.
+         * <p>This will override {@link #soundEvent(TypedKey)}</p>
+         *
+         * @param soundEvent the sound event
+         * @return this builder
+         * @see #soundEvent(TypedKey)
+         */
         @Contract(value = "_ -> this", mutates = "this")
         Builder soundEvent(Consumer<RegistryBuilderFactory<Sound, ? extends SoundEventRegistryEntry.Builder>> soundEvent);
     }
