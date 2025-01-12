259edb30099a68edf16398a533a1aac87cd6ee25
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-01-12 14:43:31 -0800

    BlockProperty API

--- paper-server/src/generated/java/org/bukkit/craftbukkit/block/impl/CraftStair.java
+++ paper-server/src/generated/java/org/bukkit/craftbukkit/block/impl/CraftStair.java
@@ -37,7 +37,7 @@ public class CraftStair extends CraftBlockData implements Stairs {
     @Override
     public void setFacing(final BlockFace blockFace) {
         Preconditions.checkArgument(blockFace != null, "blockFace cannot be null!");
-        Preconditions.checkArgument(blockFace.isCartesian() && blockFace.getModY() == 0, "Invalid face, only cartesian horizontal face are allowed for this property!");
+        Preconditions.checkArgument(blockFace.isCardinal(), "Invalid face, only cardinal face are allowed for this property!");
         this.set(FACING, blockFace);
     }
 
