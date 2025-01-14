9f74858760f8fbb9e8a231dc8c698f1a7855599a
Bjarne Koll <lynxplay101@gmail.com>
2025-01-14 14:36:19 +0100

    [ci skip] Cleanup test plugin

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,11 +1,5 @@
 package io.papermc.testplugin;
 
-import io.papermc.paper.event.player.ChatEvent;
-import io.papermc.paper.registry.keys.PaintingVariantKeys;
-import org.bukkit.Art;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Painting;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,13 +10,5 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
-    }
-
-    @EventHandler
-    public void onEvent(ChatEvent event) {
-        final Entity targetEntity = event.getPlayer().getTargetEntity(10);
-        if (targetEntity instanceof Painting painting) {
-            painting.setArt(Art.create(f -> f.copyFrom(PaintingVariantKeys.BAROQUE).height(10)));
-        }
     }
 }
