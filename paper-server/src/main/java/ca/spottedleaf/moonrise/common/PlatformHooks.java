7188129552c33e1791daadb1fc4959b30441aa01
dkks112313 <ovcharenkoden05@gmail.com>
2025-01-14 15:38:03 +0200

    Remove redundant public modifiers in paper-server/src/main/java/ca/spottedleaf/moonrise/common/PlatformHooks.java

--- paper-server/src/main/java/ca/spottedleaf/moonrise/common/PlatformHooks.java
+++ paper-server/src/main/java/ca/spottedleaf/moonrise/common/PlatformHooks.java
@@ -25,84 +25,84 @@ import java.util.ServiceLoader;
 import java.util.function.Predicate;
 
 public interface PlatformHooks extends ChunkSystemHooks {
-    public static PlatformHooks get() {
+    static PlatformHooks get() {
         return Holder.INSTANCE;
     }
 
-    public String getBrand();
+    String getBrand();
 
-    public int getLightEmission(final BlockState blockState, final BlockGetter world, final BlockPos pos);
+    int getLightEmission(final BlockState blockState, final BlockGetter world, final BlockPos pos);
 
-    public Predicate<BlockState> maybeHasLightEmission();
+    Predicate<BlockState> maybeHasLightEmission();
 
-    public boolean hasCurrentlyLoadingChunk();
+    boolean hasCurrentlyLoadingChunk();
 
-    public LevelChunk getCurrentlyLoadingChunk(final GenerationChunkHolder holder);
+    LevelChunk getCurrentlyLoadingChunk(final GenerationChunkHolder holder);
 
-    public void setCurrentlyLoading(final GenerationChunkHolder holder, final LevelChunk levelChunk);
+    void setCurrentlyLoading(final GenerationChunkHolder holder, final LevelChunk levelChunk);
 
-    public void chunkFullStatusComplete(final LevelChunk newChunk, final ProtoChunk original);
+    void chunkFullStatusComplete(final LevelChunk newChunk, final ProtoChunk original);
 
-    public boolean allowAsyncTicketUpdates();
+    boolean allowAsyncTicketUpdates();
 
-    public void onChunkHolderTicketChange(final ServerLevel world, final ChunkHolder holder, final int oldLevel, final int newLevel);
+    void onChunkHolderTicketChange(final ServerLevel world, final ChunkHolder holder, final int oldLevel, final int newLevel);
 
-    public void chunkUnloadFromWorld(final LevelChunk chunk);
+    void chunkUnloadFromWorld(final LevelChunk chunk);
 
-    public void chunkSyncSave(final ServerLevel world, final ChunkAccess chunk, final SerializableChunkData data);
+    void chunkSyncSave(final ServerLevel world, final ChunkAccess chunk, final SerializableChunkData data);
 
-    public void onChunkWatch(final ServerLevel world, final LevelChunk chunk, final ServerPlayer player);
+    void onChunkWatch(final ServerLevel world, final LevelChunk chunk, final ServerPlayer player);
 
-    public void onChunkUnWatch(final ServerLevel world, final ChunkPos chunk, final ServerPlayer player);
+    void onChunkUnWatch(final ServerLevel world, final ChunkPos chunk, final ServerPlayer player);
 
-    public void addToGetEntities(final Level world, final Entity entity, final AABB boundingBox, final Predicate<? super Entity> predicate,
+    void addToGetEntities(final Level world, final Entity entity, final AABB boundingBox, final Predicate<? super Entity> predicate,
                                  final List<Entity> into);
 
-    public <T extends Entity> void addToGetEntities(final Level world, final EntityTypeTest<Entity, T> entityTypeTest,
+    <T extends Entity> void addToGetEntities(final Level world, final EntityTypeTest<Entity, T> entityTypeTest,
                                                     final AABB boundingBox, final Predicate<? super T> predicate,
                                                     final List<? super T> into, final int maxCount);
 
-    public void entityMove(final Entity entity, final long oldSection, final long newSection);
+    void entityMove(final Entity entity, final long oldSection, final long newSection);
 
-    public boolean configFixMC224294();
+    boolean configFixMC224294();
 
-    public boolean configAutoConfigSendDistance();
+    boolean configAutoConfigSendDistance();
 
-    public double configPlayerMaxLoadRate();
+    double configPlayerMaxLoadRate();
 
-    public double configPlayerMaxGenRate();
+    double configPlayerMaxGenRate();
 
-    public double configPlayerMaxSendRate();
+    double configPlayerMaxSendRate();
 
-    public int configPlayerMaxConcurrentLoads();
+    int configPlayerMaxConcurrentLoads();
 
-    public int configPlayerMaxConcurrentGens();
+    int configPlayerMaxConcurrentGens();
 
-    public long configAutoSaveInterval(final ServerLevel world);
+    long configAutoSaveInterval(final ServerLevel world);
 
-    public int configMaxAutoSavePerTick(final ServerLevel world);
+    int configMaxAutoSavePerTick(final ServerLevel world);
 
-    public boolean configFixMC159283();
+    boolean configFixMC159283();
 
     // support for CB chunk mustNotSave
-    public boolean forceNoSave(final ChunkAccess chunk);
+    boolean forceNoSave(final ChunkAccess chunk);
 
-    public CompoundTag convertNBT(final DSL.TypeReference type, final DataFixer dataFixer, final CompoundTag nbt,
+    CompoundTag convertNBT(final DSL.TypeReference type, final DataFixer dataFixer, final CompoundTag nbt,
                                   final int fromVersion, final int toVersion);
 
-    public boolean hasMainChunkLoadHook();
+    boolean hasMainChunkLoadHook();
 
-    public void mainChunkLoad(final ChunkAccess chunk, final SerializableChunkData chunkData);
+    void mainChunkLoad(final ChunkAccess chunk, final SerializableChunkData chunkData);
 
-    public List<Entity> modifySavedEntities(final ServerLevel world, final int chunkX, final int chunkZ, final List<Entity> entities);
+    List<Entity> modifySavedEntities(final ServerLevel world, final int chunkX, final int chunkZ, final List<Entity> entities);
 
-    public void unloadEntity(final Entity entity);
+    void unloadEntity(final Entity entity);
 
-    public void postLoadProtoChunk(final ServerLevel world, final ProtoChunk chunk);
+    void postLoadProtoChunk(final ServerLevel world, final ProtoChunk chunk);
 
-    public int modifyEntityTrackingRange(final Entity entity, final int currentRange);
+    int modifyEntityTrackingRange(final Entity entity, final int currentRange);
 
-    public static final class Holder {
+    final class Holder {
         private Holder() {
         }
 
