7f70b5e3ee0683f077d43bbe46df4b48682124f0
Y2Kwastaken <mwholder@proton.me>
2025-01-26 12:28:29 -0600

    Cleanup Annotations in MenuType and MenuType#Typed

--- paper-api/src/main/java/org/bukkit/inventory/MenuType.java
+++ paper-api/src/main/java/org/bukkit/inventory/MenuType.java
@@ -18,12 +18,14 @@ import org.bukkit.inventory.view.builder.InventoryViewBuilder;
 import org.bukkit.inventory.view.builder.LocationInventoryViewBuilder;
 import org.bukkit.inventory.view.builder.MerchantInventoryViewBuilder;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Represents different kinds of views, also known as menus, which can be
  * created and viewed by the player.
  */
+@NullMarked
 @ApiStatus.Experimental
 public interface MenuType extends Keyed, io.papermc.paper.world.flag.FeatureDependant { // Paper - make FeatureDependant
 
@@ -150,9 +152,8 @@ public interface MenuType extends Keyed, io.papermc.paper.world.flag.FeatureDepe
          * @return the created {@link InventoryView}
          * @deprecated Use {@link #create(HumanEntity, net.kyori.adventure.text.Component)} instead.
          */
-        @NotNull
         @Deprecated(since = "1.21") // Paper - adventure
-        V create(@NotNull HumanEntity player, @NotNull String title);
+        V create(HumanEntity player, @Nullable String title);
 
         // Paper start - adventure
         /**
@@ -166,11 +167,9 @@ public interface MenuType extends Keyed, io.papermc.paper.world.flag.FeatureDepe
          * @param title the title of the view
          * @return the created {@link InventoryView}
          */
-        @NotNull
-        V create(@NotNull HumanEntity player, @NotNull net.kyori.adventure.text.Component title);
+        V create(HumanEntity player, net.kyori.adventure.text.@Nullable Component title);
         // Paper end - adventure
 
-        @NotNull
         B builder();
     }
 
@@ -186,8 +185,7 @@ public interface MenuType extends Keyed, io.papermc.paper.world.flag.FeatureDepe
      * @param title the title of the view
      * @return the created {@link InventoryView}
      */
-    @NotNull
-    InventoryView create(@NotNull HumanEntity player, @NotNull net.kyori.adventure.text.Component title);
+    InventoryView create(HumanEntity player, net.kyori.adventure.text.@Nullable Component title);
     // Paper end - adventure
 
     /**
@@ -196,7 +194,6 @@ public interface MenuType extends Keyed, io.papermc.paper.world.flag.FeatureDepe
      *
      * @return the typed MenuType.
      */
-    @NotNull
     MenuType.Typed<InventoryView, InventoryViewBuilder<InventoryView>> typed();
 
     /**
@@ -213,19 +210,16 @@ public interface MenuType extends Keyed, io.papermc.paper.world.flag.FeatureDepe
      * @throws IllegalArgumentException if the provided viewClass cannot be
      * typed to this MenuType
      */
-    @NotNull
-    <V extends InventoryView, B extends InventoryViewBuilder<V>> MenuType.Typed<V, B> typed(@NotNull final Class<V> viewClass) throws IllegalArgumentException;
+    <V extends InventoryView, B extends InventoryViewBuilder<V>> MenuType.Typed<V, B> typed(final Class<V> viewClass) throws IllegalArgumentException;
 
     /**
      * Gets the {@link InventoryView} class of this MenuType.
      *
      * @return the {@link InventoryView} class of this MenuType
      */
-    @NotNull
     Class<? extends InventoryView> getInventoryViewClass();
 
-    @NotNull
-    private static <T extends MenuType> T get(@NotNull final String key) {
+    private static <T extends MenuType> T get(final String key) {
         return (T) Registry.MENU.getOrThrow(NamespacedKey.minecraft(key));
     }
 }
