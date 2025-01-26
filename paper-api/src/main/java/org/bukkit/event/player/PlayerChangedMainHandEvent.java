c828b3fb3a8a99c1a2358de2566a2d72a172a9fb
Bjarne Koll <lynxplay101@gmail.com>
2025-01-26 21:17:49 +0100

    Obsolete

--- paper-api/src/main/java/org/bukkit/event/player/PlayerChangedMainHandEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerChangedMainHandEvent.java
@@ -1,13 +1,17 @@
 package org.bukkit.event.player;
 
+import com.destroystokyo.paper.event.player.PlayerClientOptionsChangeEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.MainHand;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player changes their main hand in the client settings.
+ * @apiNote Obsolete and replaced by {@link PlayerClientOptionsChangeEvent}.
  */
+@ApiStatus.Obsolete
 public class PlayerChangedMainHandEvent extends PlayerEvent {
 
     private static final HandlerList handlers = new HandlerList();
@@ -20,11 +24,16 @@ public class PlayerChangedMainHandEvent extends PlayerEvent {
     }
 
     /**
-     * Gets the old main hand of the player.
+     * Gets the new main hand of the player. The old hand is still momentarily
+     * available via {@link Player#getMainHand()}.
      *
-     * @return the old {@link MainHand} of the player
+     * @return the new {@link MainHand} of the player
+     * @deprecated has never been functional since its implementation and simply returns the old main hand.
+     * The method is left in this broken state to not break compatibility with plugins that relied on this fact.
+     * Use {@link #getNewMainHand()} instead or migrate to {@link PlayerClientOptionsChangeEvent#getMainHand()}.
      */
     @NotNull
+    @Deprecated(since = "1.21.4", forRemoval = true)
     public MainHand getMainHand() {
         return newMainHand == MainHand.LEFT ? MainHand.RIGHT : MainHand.LEFT;
     }
