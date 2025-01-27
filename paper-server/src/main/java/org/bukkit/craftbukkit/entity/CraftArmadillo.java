b92319a3956372ef06dd0006913b5fe6d6b5e4b8
Doc <nachito94@msn.com>
2025-01-27 19:55:29 -0300

    add methods for Armadillo

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftArmadillo.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftArmadillo.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.entity;
 
+import com.google.common.base.Preconditions;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Armadillo;
 
@@ -15,7 +16,46 @@ public class CraftArmadillo extends CraftAnimals implements Armadillo {
     }
 
     @Override
+    public State getState() {
+        return this.stateToBukkit(this.getHandle().getState());
+    }
+
+    @Override
+    public void setState(final State state) {
+        Preconditions.checkArgument(state != null, "state cannot be null");
+        this.getHandle().switchToState(this.stateToNMS(state));
+    }
+
+    @Override
+    public void rollUp() {
+        this.getHandle().rollUp();
+    }
+
+    @Override
+    public void rollOut() {
+        this.getHandle().rollOut();
+    }
+
+    @Override
     public String toString() {
         return "CraftArmadillo";
+    }
+
+    private State stateToBukkit(net.minecraft.world.entity.animal.armadillo.Armadillo.ArmadilloState state) {
+        return switch (state) {
+            case IDLE -> State.IDLE;
+            case ROLLING -> State.ROLLING;
+            case SCARED -> State.SCARED;
+            case UNROLLING -> State.UNROLLING;
+        };
+    }
+
+    private net.minecraft.world.entity.animal.armadillo.Armadillo.ArmadilloState stateToNMS(State state) {
+        return switch (state) {
+            case State.IDLE -> net.minecraft.world.entity.animal.armadillo.Armadillo.ArmadilloState.IDLE;
+            case State.ROLLING -> net.minecraft.world.entity.animal.armadillo.Armadillo.ArmadilloState.ROLLING;
+            case State.SCARED -> net.minecraft.world.entity.animal.armadillo.Armadillo.ArmadilloState.SCARED;
+            case State.UNROLLING -> net.minecraft.world.entity.animal.armadillo.Armadillo.ArmadilloState.UNROLLING;
+        };
     }
 }
