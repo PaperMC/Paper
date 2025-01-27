9f48d67c493a10849e0bef382d7d6503818e1bdc
Doc <nachito94@msn.com>
2025-01-27 19:55:29 -0300

    add methods for Armadillo

--- paper-api/src/main/java/org/bukkit/entity/Armadillo.java
+++ paper-api/src/main/java/org/bukkit/entity/Armadillo.java
@@ -1,8 +1,47 @@
 package org.bukkit.entity;
 
+import org.jspecify.annotations.NullMarked;
+
 /**
  * Represents an Armadillo.
  */
+@NullMarked
 public interface Armadillo extends Animals {
+
+    /**
+     * Get the current state of the armadillo.
+     *
+     * @return the state of the armadillo
+     */
+    public State getState();
+
+    /**
+     * Set a new state for the armadillo.
+     * <br>
+     * This will also make the armadillo make the transition to the new state.
+     *
+     * @param state the new state
+     */
+    public void setState(final State state);
+
+    /**
+     * Attempt to rollUp if it in {@link State#IDLE}
+     */
+    public void rollUp();
+
+    /**
+     * Attempt to rollUp if it's not in {@link State#IDLE}
+     */
+    public void rollOut();
+
+    /**
+     * Represents the current state of the Armadillo.
+     */
+    public enum State {
+        IDLE,
+        ROLLING,
+        SCARED,
+        UNROLLING;
+    }
 
 }
