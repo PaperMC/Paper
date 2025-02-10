ea6f0eee62efd816909e2d1090916fedd86885b7
Doc <nachito94@msn.com>
2025-02-09 22:01:54 -0300

    add methods for Creaking
    - methods for activate/deactivate the creaking
    - methods for home of creaking

--- paper-api/src/main/java/org/bukkit/entity/Creaking.java
+++ paper-api/src/main/java/org/bukkit/entity/Creaking.java
@@ -1,8 +1,45 @@
 package org.bukkit.entity;
 
+import org.bukkit.Location;
+import org.jspecify.annotations.NullMarked;
+
 /**
  * Represents a Creaking.
  */
+@NullMarked
 public interface Creaking extends Monster {
+
+    /**
+     * Gets the home location for this Creaking (aka where can be a {@link org.bukkit.block.CreakingHeart}).
+     *
+     * @return the location of the home.
+     */
+    Location getHome();
+
+    /**
+     * Sets the home location for this Creaking.
+     *
+     * @param location the location of the home.
+     */
+    void setHome(final Location location);
+
+    /**
+     * Activate this Creaking to target and follow a player.
+     *
+     * @param player the target.
+     */
+    void activate(final Player player);
+
+    /**
+     * Deactivate this Creaking to the current target player.
+     */
+    void deactivate();
+
+    /**
+     * Gets if this Creaking is active.
+     *
+     * @return true if is active.
+     */
+    boolean isActive();
 
 }
