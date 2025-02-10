c1f8d36a1f8efe64c326d75ee8c3e0084a04df97
Doc <nachito94@msn.com>
2025-02-09 22:46:21 -0300

    add methods for CreakingHeart
    - allow manage creaking attached
    - helper method for spawn creaking

--- paper-api/src/main/java/org/bukkit/block/CreakingHeart.java
+++ paper-api/src/main/java/org/bukkit/block/CreakingHeart.java
@@ -1,7 +1,37 @@
 package org.bukkit.block;
 
+import org.bukkit.entity.Creaking;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
 /**
  * Represents a captured state of a creaking heart.
  */
+@NullMarked
 public interface CreakingHeart extends TileState {
+
+    /**
+     * Gets the creaking protecting this creaking heart.
+     *
+     * @return the creaking or null if this creaking heart don't have protector.
+     */
+    @Nullable
+    Creaking getCreaking();
+
+    /**
+     * Sets the creaking protecting this creaking heart.
+     *
+     * @param creaking the creaking or null for make this creaking heart don't have protector
+     * @throws IllegalArgumentException if the creaking passed it's in another world
+     */
+    void setCreaking(@Nullable Creaking creaking);
+
+    /**
+     * Attempts to spawn a creaking for protect this creaking heart.
+     *
+     * @return the {@link Creaking} for protect the creaking heart or null if fails
+     */
+    @NullMarked
+    Creaking spawnCreaking();
+
 }
