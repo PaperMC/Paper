bd0a5ac2dbd156dc2e87c429f4551154ceb8e7a5
Bjarne Koll <lynxplay101@gmail.com>
2025-02-12 11:18:31 +0100

    Align with pointy

--- paper-api/src/main/java/io/papermc/paper/tag/PostFlattenTagRegistrar.java
+++ paper-api/src/main/java/io/papermc/paper/tag/PostFlattenTagRegistrar.java
@@ -23,7 +23,7 @@ import org.jspecify.annotations.NullMarked;
  * <pre>{@code
  * class YourBootstrapClass implements PluginBootstrap {
  *
- *     public static final TypedKey<Enchantment> CUSTOM_ENCHANT = EnchantmentKeys.create(Key.key("test", "custom_enchant"));
+ *     public static final TypedKey<Enchantment> CUSTOM_POINTY_ENCHANT = EnchantmentKeys.create(Key.key("papermc:pointy"));
  *
  *     @Override
  *     public void bootstrap(BootstrapContext context) {
@@ -32,7 +32,7 @@ import org.jspecify.annotations.NullMarked;
  *             final PostFlattenTagRegistrar<Enchantment> registrar = event.registrar();
  *             registrar.addToTag(
  *                 EnchantmentTagKeys.IN_ENCHANTING_TABLE,
- *                 Set.of(CUSTOM_ENCHANT)
+ *                 Set.of(CUSTOM_POINTY_ENCHANT)
  *             );
  *         });
  *     }
