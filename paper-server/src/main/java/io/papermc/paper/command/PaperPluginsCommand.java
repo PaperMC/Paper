c2335115c7a68492dc2e22fc252d1808257adf2f
Doc <nachito94@msn.com>
2025-02-16 10:13:52 -0300

    [ci skip] remove unused import

--- paper-server/src/main/java/io/papermc/paper/command/PaperPluginsCommand.java
+++ paper-server/src/main/java/io/papermc/paper/command/PaperPluginsCommand.java
@@ -27,7 +27,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
