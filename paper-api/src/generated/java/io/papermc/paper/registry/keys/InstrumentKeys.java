f070081825058ec27d040ed32fee71b5b2da5fb3
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-02-17 15:33:26 -0800

    Remove Experimental from TypedKey (#12134)

--- paper-api/src/generated/java/io/papermc/paper/registry/keys/InstrumentKeys.java
+++ paper-api/src/generated/java/io/papermc/paper/registry/keys/InstrumentKeys.java
@@ -7,7 +7,6 @@ import io.papermc.paper.registry.RegistryKey;
 import io.papermc.paper.registry.TypedKey;
 import net.kyori.adventure.key.Key;
 import org.bukkit.MusicInstrument;
-import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 /**
@@ -25,7 +24,6 @@ import org.jspecify.annotations.NullMarked;
 })
 @GeneratedFrom("1.21.4")
 @NullMarked
-@ApiStatus.Experimental
 public final class InstrumentKeys {
     /**
      * {@code minecraft:admire_goat_horn}
@@ -92,7 +90,6 @@ public final class InstrumentKeys {
      * @param key the value's key in the registry
      * @return a new typed key
      */
-    @ApiStatus.Experimental
     public static TypedKey<MusicInstrument> create(final Key key) {
         return TypedKey.create(RegistryKey.INSTRUMENT, key);
     }
