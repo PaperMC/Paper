91c0ad0105857bd14550532ff02156701fc944f3
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-02-18 17:57:13 +0100

    Clear out test plugin

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,11 +1,7 @@
 package io.papermc.testplugin;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.inventory.CreativeCategory;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.stream.Collectors;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
 
@@ -14,10 +10,5 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
-    }
-
-    @EventHandler
-    public void on(PlayerDropItemEvent event) {
-        getLogger().info(event.getItemDrop().getItemStack().getType().getKey().asMinimalString() + ": " + event.getItemDrop().getItemStack().getCreativeCategories().stream().map(CreativeCategory::name).collect(Collectors.joining(", ")));
     }
 }
