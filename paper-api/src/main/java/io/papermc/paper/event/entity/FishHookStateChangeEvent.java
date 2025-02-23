7d6207536d84c49b4f8eb4caef1139fb7f248413
Unitarou <atelierkisame@gmail.com>
2025-02-23 15:33:27 +0900

    Implement Cancellable in FishHookStateChangeEvent.

--- paper-api/src/main/java/io/papermc/paper/event/entity/FishHookStateChangeEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/FishHookStateChangeEvent.java
@@ -1,20 +1,25 @@
 package io.papermc.paper.event.entity;
 
 import org.bukkit.entity.FishHook;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 /**
  * Called just before a {@link FishHook}'s {@link FishHook.HookState} is changed.
+ *
+ * <p>If you want to monitor a player's fishing state transitions, you can listen to them using {@link PlayerFishEvent}.</p>
  */
 @NullMarked
-public final class FishHookStateChangeEvent extends EntityEvent {
+public final class FishHookStateChangeEvent extends EntityEvent implements Cancellable {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
     private final FishHook.HookState newHookState;
+    private boolean cancelled;
 
     @ApiStatus.Internal
     public FishHookStateChangeEvent(final FishHook entity, final FishHook.HookState newHookState) {
@@ -25,7 +30,8 @@ public final class FishHookStateChangeEvent extends EntityEvent {
     /**
      * Get the <strong>new</strong> hook state of the {@link FishHook}.
      *
-     * <p>Refer to {@link FishHook#getState()} to get the current hook state.</p>
+     * <p>This is what the {@link FishHook}'s new hook state will be after this event if it isn't cancelled.<br>
+     * Refer to {@link FishHook#getState()} to get the current hook state.</p>
      *
      * @return the <strong>new</strong> hook state
      */
@@ -45,5 +51,24 @@ public final class FishHookStateChangeEvent extends EntityEvent {
 
     public static HandlerList getHandlerList() {
         return HANDLER_LIST;
+    }
+
+    /**
+     * Set whether to cancel the {@link FishHook}'s {@link FishHook.HookState} change.
+     *
+     * <p>Note: Even if you cancel this event, if the conditions for the state change are met in the next tick,
+     * the event will be called again.<br>
+     * In other words, this event may be triggered repeatedly every tick, so use it with caution.</p>
+     *
+     * @param cancel {@code true} if you wish to cancel the state change
+     */
+    @Override
+    public void setCancelled(final boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
     }
 }
