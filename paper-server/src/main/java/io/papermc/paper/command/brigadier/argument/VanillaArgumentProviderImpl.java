39df56d8a44aa835787a1eb1a65e187d0351cfb7
The-Epic <epicebic@outlook.com>
2025-03-03 07:56:24 +0000

    add stable fix

--- paper-server/src/main/java/io/papermc/paper/command/brigadier/argument/VanillaArgumentProviderImpl.java
+++ paper-server/src/main/java/io/papermc/paper/command/brigadier/argument/VanillaArgumentProviderImpl.java
@@ -68,14 +68,14 @@ import net.minecraft.commands.arguments.coordinates.Vec3Argument;
 import net.minecraft.commands.arguments.item.ItemArgument;
 import net.minecraft.commands.arguments.item.ItemPredicateArgument;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 import org.bukkit.GameMode;
@@ -174,11 +174,13 @@ public class VanillaArgumentProviderImpl implements VanillaArgumentProvider {
     @Override
     public ArgumentType<BlockState> blockState() {
         return this.wrap(BlockStateArgument.block(PaperCommands.INSTANCE.getBuildContext()), (result) -> {
-            CompoundTag tag = result.tag;
-            if (tag != null && !tag.contains("id")) {
-                tag.putString("id", BuiltInRegistries.BLOCK.getKey(result.getState().getBlock()).toString());
+            if (!result.getState().hasBlockEntity()) {
+                return CraftBlockStates.getBlockState(CraftRegistry.getMinecraftRegistry(), BlockPos.ZERO, result.getState(), null);
             }
-            return CraftBlockStates.getBlockState(CraftRegistry.getMinecraftRegistry(), BlockPos.ZERO, result.getState(), tag);
+
+            final BlockEntity blockEntity = ((EntityBlock) result.getState().getBlock()).newBlockEntity(BlockPos.ZERO, result.getState());
+            blockEntity.loadWithComponents(result.tag, CraftRegistry.getMinecraftRegistry());
+            return CraftBlockStates.getBlockState(null, BlockPos.ZERO, result.getState(), blockEntity);
         });
     }
 
