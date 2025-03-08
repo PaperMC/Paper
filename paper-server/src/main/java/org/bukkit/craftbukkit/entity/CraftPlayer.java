5b562513540b08c26f8900d8c3d9b87561d44c20
Tamion <70228790+notTamion@users.noreply.github.com>
2025-03-08 18:10:44 +0100

    just move to seperate method

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -2828,15 +2828,10 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
 
     public void updateScaledHealth(boolean sendHealth) {
         AttributeMap attributemapserver = this.getHandle().getAttributes();
-        Collection<AttributeInstance> set = new HashSet<>(1);
-
-        this.injectScaledMaxHealth(set, true);
 
         // SPIGOT-3813: Attributes before health
         if (this.getHandle().connection != null) {
-            if (!set.isEmpty()) {
-                this.getHandle().connection.send(new ClientboundUpdateAttributesPacket(this.getHandle().getId(), set));
-            }
+            this.getHandle().connection.send(new ClientboundUpdateAttributesPacket(this.getHandle().getId(), Set.of(getScaledMaxHealth())));
             if (sendHealth) {
                 this.sendHealthUpdate();
             }
@@ -2876,8 +2871,11 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
                 break;
             }
         }
+        collection.add(getScaledMaxHealth());
+    }
+
+    public AttributeInstance getScaledMaxHealth() {
         AttributeInstance dummy = new AttributeInstance(Attributes.MAX_HEALTH, (attribute) -> { });
-        // Spigot start
         double healthMod = this.scaledHealth ? this.healthScale : this.getMaxHealth();
         if ( healthMod >= Float.MAX_VALUE || healthMod <= 0 )
         {
@@ -2885,8 +2883,7 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
             this.getServer().getLogger().warning( this.getName() + " tried to crash the server with a large health attribute" );
         }
         dummy.setBaseValue(healthMod);
-        // Spigot end
-        collection.add(dummy);
+        return dummy;
     }
 
     @Override
