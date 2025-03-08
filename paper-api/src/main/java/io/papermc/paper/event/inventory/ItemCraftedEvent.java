1f6490b2e4591985b940603d235dd604f91682c1
orang3i <orangeinfotainment@gmail.com>
2025-03-08 20:48:12 +0530

    Adding ItemCraftedEvent - rebase

--- paper-api/src/main/java/io/papermc/paper/event/inventory/ItemCraftedEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/inventory/ItemCraftedEvent.java
@@ -1,4 +1,4 @@
-package org.bukkit.event.inventory;
+package io.papermc.paper.event.inventory;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -6,24 +6,37 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Called when a player picks up a crafted item from the result slot of a crafting grid.
+ */
 public class ItemCraftedEvent extends Event{
     private static final HandlerList HANDLERS = new HandlerList();
     private final Player player;
-    private final ItemStack craftedItem;
+    private final ItemStack craftedItemStack;
 
-    public ItemCraftedEvent(@NotNull Player player, @NotNull ItemStack craftedItem) {
+    public ItemCraftedEvent(@NotNull Player player, @NotNull ItemStack craftedItemStack) {
         this.player = player;
-        this.craftedItem = craftedItem;
+        this.craftedItemStack = craftedItemStack;
     }
 
+    /**
+     * Gets the Player who triggered the event by picking up the crafted item.
+     *
+     * @return Player
+     */
     @NotNull
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Gets the ItemStack that was crafted and picked up by the Player.
+     *
+     * @return ItemStack
+     */
     @NotNull
-    public ItemStack getCraftedItem() {
-        return craftedItem;
+    public ItemStack getCraftedItemStack() {
+        return craftedItemStack;
     }
 
     @NotNull
