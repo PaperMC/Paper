607cf7340373d3ec2ff1cd63b63b2d9b5f4c03c5
orang3i <orangeinfotainment@gmail.com>
2025-03-09 06:45:47 +0530

    Switch to @NullMarked

--- paper-api/src/main/java/io/papermc/paper/event/inventory/ItemCraftedEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/inventory/ItemCraftedEvent.java
@@ -4,17 +4,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NullMarked;
+
 
 /**
  * Called when a player picks up a crafted item from the result slot of a crafting grid.
  */
+@NullMarked
 public class ItemCraftedEvent extends Event{
     private static final HandlerList HANDLERS = new HandlerList();
     private final Player player;
     private final ItemStack craftedItemStack;
 
-    public ItemCraftedEvent(@NotNull Player player, @NotNull ItemStack craftedItemStack) {
+    public ItemCraftedEvent(Player player,ItemStack craftedItemStack) {
         this.player = player;
         this.craftedItemStack = craftedItemStack;
     }
@@ -24,7 +26,6 @@ public class ItemCraftedEvent extends Event{
      *
      * @return Player
      */
-    @NotNull
     public Player getPlayer() {
         return player;
     }
@@ -34,18 +35,15 @@ public class ItemCraftedEvent extends Event{
      *
      * @return ItemStack
      */
-    @NotNull
     public ItemStack getCraftedItemStack() {
         return craftedItemStack;
     }
 
-    @NotNull
     @Override
     public  HandlerList getHandlers() {
         return HANDLERS;
     }
 
-    @NotNull
     public static HandlerList getHandlerList() {
         return HANDLERS;
     }
