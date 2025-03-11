3e209cfd775047557b0c5c544ededc51ff1f63ae
david <mrminecraft00@gmail.com>
2025-03-11 19:23:34 +0100

    Add Javadocs

--- paper-api/src/main/java/org/bukkit/Bukkit.java
+++ paper-api/src/main/java/org/bukkit/Bukkit.java
@@ -1432,22 +1432,47 @@ public final class Bukkit {
         return server.getOnlineMode();
     }
 
+    /**
+     * Gets whether the Server is behind a proxy that uses online mode.
+     *
+     * @return true if the server is in proxied online mode, false otherwise
+     */
     public static boolean isProxyOnlineMode() {
         return server.isProxyOnlineMode();
     }
 
+    /**
+     * Gets whether the Server is configured for Velocity online mode
+     *
+     * @return true if the server is in Velocity online mode; false otherwise.
+     */
     public static boolean isVelocityOnlineMode() {
         return server.isVelocityOnlineMode();
     }
 
+    /**
+     * Gets whether the Server is configured for BungeeCord online mode
+     *
+     * @return true if the server is in BungeeCord online mode; false otherwise.
+     */
     public static boolean isBungeeCordOnlineMode() {
         return server.isBungeeCordOnlineMode();
     }
 
+    /**
+     * Gets whether the Server is configured for a Velocity proxy
+     *
+     * @return true if the server has Velocity enabled; false otherwise.
+     */
     public static boolean isVelocityEnabled() {
         return server.isVelocityEnabled();
     }
 
+    /**
+     * Gets whether the Server is configured for a BungeeCord proxy
+     *
+     * @return true if the server has BungeeCord enabled; false otherwise.
+     */
     public static boolean isBungeeCordEnabled() {
         return server.isBungeeCordEnabled();
     }
--- paper-api/src/main/java/org/bukkit/Server.java
+++ paper-api/src/main/java/org/bukkit/Server.java
@@ -1266,14 +1266,39 @@ public interface Server extends PluginMessageRecipient, net.kyori.adventure.audi
      */
     public boolean getOnlineMode();
 
+    /**
+     * Gets whether the Server is behind a proxy that uses online mode.
+     *
+     * @return true if the server is in proxied online mode, false otherwise
+     */
     boolean isProxyOnlineMode();
 
+    /**
+     * Gets whether the Server is configured for Velocity online mode
+     *
+     * @return true if the server is in Velocity online mode; false otherwise.
+     */
     boolean isVelocityOnlineMode();
 
+    /**
+     * Gets whether the Server is configured for BungeeCord online mode
+     *
+     * @return true if the server is in BungeeCord online mode; false otherwise.
+     */
     boolean isBungeeCordOnlineMode();
 
+    /**
+     * Gets whether the Server is configured for a Velocity proxy
+     *
+     * @return true if the server has Velocity enabled; false otherwise.
+     */
     boolean isVelocityEnabled();
 
+    /**
+     * Gets whether the Server is configured for a BungeeCord proxy
+     *
+     * @return true if the server has BungeeCord enabled; false otherwise.
+     */
     boolean isBungeeCordEnabled();
 
     /**
