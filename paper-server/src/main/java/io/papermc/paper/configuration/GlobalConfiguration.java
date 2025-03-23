cc1810fe10eb31acf08e846a42bd16903fe2dce5
ShaneBeee <shanebolenback@me.com>
2025-03-23 10:16:04 -0700

    GlobalConfiguration - change config option

--- paper-server/src/main/java/io/papermc/paper/configuration/GlobalConfiguration.java
+++ paper-server/src/main/java/io/papermc/paper/configuration/GlobalConfiguration.java
@@ -12,7 +12,6 @@ import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ServerboundPlaceRecipePacket;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Items;
 import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
@@ -203,7 +202,7 @@ public class GlobalConfiguration extends ConfigurationPart {
         public boolean suggestPlayerNamesWhenNullTabCompletions = true;
         public boolean timeCommandAffectsAllWorlds = false;
         @Comment("Allow mounting entities to a player in the vanilla '/ride' command.")
-        public boolean rideCommandSkipPlayerAsVehicleRestriction = false;
+        public boolean rideCommandAllowPlayerAsVehicle = false;
     }
 
     public Logging logging;
