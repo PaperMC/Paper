92abbc6ccc73e4da969c3aa6d71efd880197412e
Bjarne Koll <git@lynxplay.dev>
2025-03-23 22:33:00 +0100

    Deprecate Raid#getId for removal
    Deprecate as raid instances are not tied to an id until the server
    actually starts tracking them.
    
    Best attempt at keeping compat by passing the id to the raid instance
    once tracked, prior the API will return -1.

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftRaid.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftRaid.java
@@ -105,7 +105,7 @@ public final class CraftRaid implements Raid {
 
     @Override
     public int getId() {
-        return this.handle.getId();
+        return this.handle.idOrNegativeOne;
     }
 
     @Override
