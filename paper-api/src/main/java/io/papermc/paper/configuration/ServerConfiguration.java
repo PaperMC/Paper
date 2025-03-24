7f9c0c6fd1358002a58eb7457a1ee90c8b5b1520
david <github@nonswag.dev>
2025-03-24 16:31:19 +0100

    fix grammar

--- paper-api/src/main/java/io/papermc/paper/configuration/ServerConfiguration.java
+++ paper-api/src/main/java/io/papermc/paper/configuration/ServerConfiguration.java
@@ -1,38 +1,45 @@
 package io.papermc.paper.configuration;
 
+/**
+ * Represents the configuration settings for a server.
+ * <p>
+ * This interface doesn't aim to cover every possible server configuration
+ * option but focuses on selected critical settings.
+ */
 public interface ServerConfiguration {
 
     /**
-     * Gets whether the Server is behind a proxy that uses online mode.
+     * Gets whether the server is behind a proxy that uses online mode.
      *
      * @return true if the server is in proxied online mode, false otherwise
      */
     boolean isProxyOnlineMode();
 
     /**
-     * Gets whether the Server is configured for a Velocity proxy
+     * Gets whether the server is configured for a Velocity proxy.
      *
-     * @return true if the server has Velocity enabled; false otherwise.
+     * @return true if the server has Velocity enabled, false otherwise
      */
     boolean isVelocityEnabled();
+
     /**
-     * Gets whether the Server is configured for BungeeCord online mode
+     * Gets whether the server is configured for BungeeCord online mode.
      *
-     * @return true if the server is in BungeeCord online mode; false otherwise.
+     * @return true if the server is in BungeeCord online mode, false otherwise
      */
     boolean isBungeeCordEnabled();
 
     /**
-     * Gets whether the Server is configured for Velocity online mode
+     * Gets whether the server is configured for Velocity online mode.
      *
-     * @return true if the server is in Velocity online mode; false otherwise.
+     * @return true if the server is in Velocity online mode, false otherwise
      */
     boolean isVelocityOnlineMode();
 
     /**
-     * Gets whether the Server is configured for a BungeeCord proxy
+     * Gets whether the server is configured for a BungeeCord proxy.
      *
-     * @return true if the server has BungeeCord enabled; false otherwise.
+     * @return true if the server has BungeeCord enabled, false otherwise
      */
     boolean isBungeeCordOnlineMode();
 }
