672e3467f4e3bbbe0dd6aaca50049387a88679a3
Shane Freeder <theboyetronic@gmail.com>
2025-03-31 19:20:02 +0100

    Deprecate ItemMeta#get/setCustomModelData
    The API is no longer able to properly reflect the internal state here

--- paper-api/src/main/java/org/bukkit/inventory/meta/ItemMeta.java
+++ paper-api/src/main/java/org/bukkit/inventory/meta/ItemMeta.java
@@ -334,7 +334,10 @@ public interface ItemMeta extends Cloneable, ConfigurationSerializable, Persiste
      * before calling this method.
      *
      * @return the custom model data that is set
+     * @deprecated in favour of {@link io.papermc.paper.datacomponent.DataComponentTypes#CUSTOM_MODEL_DATA} and {@link io.papermc.paper.datacomponent.DataComponentTypes#ITEM_MODEL}
+     * This method is no longer able to represent the internal state of the item.
      */
+    @Deprecated(since = "1.21.4")
     int getCustomModelData();
 
     /**
