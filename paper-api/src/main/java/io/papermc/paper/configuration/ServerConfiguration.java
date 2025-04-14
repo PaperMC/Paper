10077a8ca4142484f45d232fb433d25a89a74a4d
david <github@nonswag.dev>
2025-04-14 20:19:09 +0200

    removed forwarding mode related methods

--- paper-api/src/main/java/io/papermc/paper/configuration/ServerConfiguration.java
+++ paper-api/src/main/java/io/papermc/paper/configuration/ServerConfiguration.java
@@ -14,39 +14,11 @@ public interface ServerConfiguration {
      * This method returns true if:
      * <ul>
      * <li>The server is in {@link org.bukkit.Server#getOnlineMode online mode},</li>
-     * <li>Velocity is enabled and configured to be in {@link #isVelocityOnlineMode() online mode}, or</li>
-     * <li>BungeeCord is enabled and configured to be in {@link #isBungeeCordOnlineMode() online mode}.</li>
+     * <li>Velocity is enabled and configured to be in online mode, or</li>
+     * <li>BungeeCord is enabled and configured to be in online mode.</li>
      * </ul>
      *
      * @return whether the server is in online mode or behind a proxy configured for online mode
      */
     boolean isProxyOnlineMode();
-
-    /**
-     * Gets whether the server is configured for a Velocity proxy.
-     *
-     * @return true if the server has Velocity enabled, false otherwise
-     */
-    boolean isVelocityEnabled();
-
-    /**
-     * Gets whether the server is configured for a BungeeCord proxy.
-     *
-     * @return true if the server has BungeeCord enabled, false otherwise
-     */
-    boolean isBungeeCordEnabled();
-
-    /**
-     * Gets whether the server is configured to operate in Velocity online mode.
-     *
-     * @return true if the server is in Velocity online mode and Velocity is enabled, false otherwise
-     */
-    boolean isVelocityOnlineMode();
-
-    /**
-     * Gets whether the server is configured to operate in BungeeCord online mode.
-     *
-     * @return true if the server is in BungeeCord online mode and BungeeCord is enabled, false otherwise
-     */
-    boolean isBungeeCordOnlineMode();
 }
--- paper-server/src/main/java/io/papermc/paper/configuration/PaperServerConfiguration.java
+++ paper-server/src/main/java/io/papermc/paper/configuration/PaperServerConfiguration.java
@@ -1,30 +1,8 @@
 package io.papermc.paper.configuration;
 
-import org.spigotmc.SpigotConfig;
-
 public class PaperServerConfiguration implements ServerConfiguration {
     @Override
     public boolean isProxyOnlineMode() {
         return GlobalConfiguration.get().proxies.isProxyOnlineMode();
-    }
-
-    @Override
-    public boolean isVelocityEnabled() {
-        return GlobalConfiguration.get().proxies.velocity.enabled;
-    }
-
-    @Override
-    public boolean isBungeeCordEnabled() {
-        return SpigotConfig.bungee;
-    }
-
-    @Override
-    public boolean isVelocityOnlineMode() {
-        return isVelocityEnabled() && GlobalConfiguration.get().proxies.velocity.onlineMode;
-    }
-
-    @Override
-    public boolean isBungeeCordOnlineMode() {
-        return isBungeeCordEnabled() && GlobalConfiguration.get().proxies.bungeeCord.onlineMode;
     }
 }
