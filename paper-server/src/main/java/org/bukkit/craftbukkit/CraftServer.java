219194c293676eabc48d34392249032d8fb4655e
Bjarne Koll <git@lynxplay.dev>
2025-04-28 17:25:14 +0200

    Drop useless import

--- paper-server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -32,7 +32,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
