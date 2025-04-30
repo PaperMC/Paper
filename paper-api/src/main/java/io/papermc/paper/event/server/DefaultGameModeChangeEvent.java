9e5dcf1d767bf32406507d18681b8e0f5fd61b2d
david <github@nonswag.dev>
2025-04-30 20:29:30 +0200

    [ci skip] added javadocs

--- paper-api/src/main/java/io/papermc/paper/event/server/DefaultGameModeChangeEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/server/DefaultGameModeChangeEvent.java
@@ -8,6 +8,9 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
+/**
+ * Called when the default gamemode is changed, either by command or by api.
+ */
 @NullMarked
 public class DefaultGameModeChangeEvent extends Event {
 
@@ -17,21 +20,36 @@ public class DefaultGameModeChangeEvent extends Event {
     private GameMode newGameMode;
 
     @ApiStatus.Internal
-    public DefaultGameModeChangeEvent(final @Nullable CommandSender commandSender, final GameMode newGameMode) {
+    public DefaultGameModeChangeEvent(final @Nullable CommandSender commandSender, final GameMode mode) {
         this.commandSender = commandSender;
-        this.newGameMode = newGameMode;
+        this.newGameMode = mode;
     }
 
+    /**
+     * Gets the command sender associated with this event.
+     *
+     * @return {@code null} if the default gamemode was changed via api, otherwise the {@link CommandSender}.
+     */
     public @Nullable CommandSender getCommandSender() {
         return commandSender;
     }
 
+    /**
+     * Gets the new default gamemode.
+     *
+     * @return the new default gamemode.
+     */
     public GameMode getNewGameMode() {
         return newGameMode;
     }
 
-    public DefaultGameModeChangeEvent setNewGameMode(final GameMode newGameMode) {
-        this.newGameMode = newGameMode;
+    /**
+     * Sets the new default gamemode.
+     *
+     * @param mode the new default gamemode.
+     */
+    public DefaultGameModeChangeEvent setNewGameMode(final GameMode mode) {
+        this.newGameMode = mode;
         return this;
     }
 
