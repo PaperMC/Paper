46eaf88432844c6adba9bb6b876eb3c10e8c198e
Strokkur424 <strokkur.24@gmail.com>
2025-05-10 14:14:10 +0200

    Annotate ScoreHolder as experimental

--- paper-api/src/main/java/org/bukkit/scoreboard/ScoreHolder.java
+++ paper-api/src/main/java/org/bukkit/scoreboard/ScoreHolder.java
@@ -1,8 +1,10 @@
 package org.bukkit.scoreboard;
 
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
+@ApiStatus.Experimental
 @NullMarked
 public interface ScoreHolder {
     String getScoreboardName();
