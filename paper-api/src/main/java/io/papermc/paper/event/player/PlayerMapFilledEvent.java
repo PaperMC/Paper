2ed5b8720f13c399ee5e54f2efa3c16c159a1143
Newwind <support@newwindserver.com>
2025-05-21 21:52:25 +0100

    Update PlayerMapFilledEvent.java

--- paper-api/src/main/java/io/papermc/paper/event/player/PlayerMapFilledEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/player/PlayerMapFilledEvent.java
@@ -6,35 +6,37 @@ import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when a player creates a filled map by right-clicking an empty map
  */
+@NullMarked
 public class PlayerMapFilledEvent extends PlayerEvent {
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
     private final ItemStack emptyMap;
     private final ItemStack createdMap;
 
+    @ApiStatus.Internal
+    public PlayerMapFilledEvent(final @NotNull Player player, final ItemStack emptyMap, final ItemStack createdMap) {
+        super(player);
+        this.emptyMap = emptyMap;
+        this.createdMap = createdMap;
+    }
+
     /**
      * Returns a copy of the empty map before it was consumed
      */
     public ItemStack getEmptyMap() {
-        return emptyMap;
+        return this.emptyMap;
     }
 
     /**
      * Returns the filled map which was created
      */
     public ItemStack getCreatedMap() {
-        return createdMap;
-    }
-
-    @ApiStatus.Internal
-    public PlayerMapFilledEvent(final @NotNull Player player, final ItemStack emptyMap, final ItemStack createdMap) {
-        super(player);
-        this.emptyMap = emptyMap;
-        this.createdMap = createdMap;
+        return this.createdMap;
     }
 
     @NotNull
