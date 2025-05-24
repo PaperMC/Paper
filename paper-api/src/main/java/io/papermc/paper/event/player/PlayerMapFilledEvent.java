fd06df685e6676a457be9e3848104aae8959c454
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2025-05-24 15:50:22 -0400

    Update

--- paper-api/src/main/java/io/papermc/paper/event/player/PlayerMapFilledEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/player/PlayerMapFilledEvent.java
@@ -9,14 +9,14 @@ import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.NullMarked;
 
 /**
- * Called when a player creates a filled map by right-clicking an empty map
+ * Called when a player creates a filled map by right-clicking an empty map.
  */
 @NullMarked
 public class PlayerMapFilledEvent extends PlayerEvent {
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
     private final ItemStack originalItem;
-    private final ItemStack createdMap;
+    private ItemStack createdMap;
 
     @ApiStatus.Internal
     public PlayerMapFilledEvent(final @NotNull Player player, final ItemStack originalItem, final ItemStack createdMap) {
@@ -26,17 +26,25 @@ public class PlayerMapFilledEvent extends PlayerEvent {
     }
 
     /**
-     * Returns a copy of the empty map before it was consumed
+     * Returns a copy of the empty map before it was consumed.
      */
     public ItemStack getOriginalItem() {
-        return this.originalItem;
+        return this.originalItem.clone();
     }
 
     /**
-     * Returns the filled map which was created
+     * Returns the filled map which was created.
      */
     public ItemStack getCreatedMap() {
-        return this.createdMap;
+        return this.createdMap.clone();
+    }
+
+    /**
+     * Sets the filled map that will be created.
+     * @param createdMap map item
+     */
+    public void setCreatedMap(final ItemStack createdMap) {
+        this.createdMap = createdMap.clone();
     }
 
     @NotNull
