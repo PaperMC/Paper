ead47277d2b0eade9ca671aa2cc41deef96aa9c5
ColinBashful <76665243+ColinBashful@users.noreply.github.com>
2025-06-16 17:14:46 +0200

    Allow Changing Craft Recipe Remainders
    - added a method and field to CraftItemEvent
    - made them used in ResultSlot
    - ServerGamePacketListenerImpl, the class calling CraftItemEvent sets ResultSlot's static list

--- paper-api/src/main/java/org/bukkit/event/inventory/CraftItemEvent.java
+++ paper-api/src/main/java/org/bukkit/event/inventory/CraftItemEvent.java
@@ -3,9 +3,13 @@ package org.bukkit.event.inventory;
 import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Called when the recipe of an Item is completed inside a crafting matrix.
@@ -13,17 +17,20 @@ import org.jetbrains.annotations.NotNull;
 public class CraftItemEvent extends InventoryClickEvent {
 
     private final Recipe recipe;
+    private final List<ItemStack> craftRemainders; // Paper - Add Craft Remainders
 
     @ApiStatus.Internal
     public CraftItemEvent(@NotNull Recipe recipe, @NotNull InventoryView view, @NotNull SlotType type, int slot, @NotNull ClickType click, @NotNull InventoryAction action) {
         super(view, type, slot, click, action);
         this.recipe = recipe;
+        this.craftRemainders = new ArrayList<>(Collections.nCopies(getInventory().getMatrix().length, null)); // Paper - Initialize Craft Remainders
     }
 
     @ApiStatus.Internal
     public CraftItemEvent(@NotNull Recipe recipe, @NotNull InventoryView view, @NotNull SlotType type, int slot, @NotNull ClickType click, @NotNull InventoryAction action, int key) {
         super(view, type, slot, click, action, key);
         this.recipe = recipe;
+        this.craftRemainders = new ArrayList<>(Collections.nCopies(getInventory().getMatrix().length, null)); // Paper - Initialize Craft Remainders
     }
 
     @NotNull
@@ -39,4 +46,10 @@ public class CraftItemEvent extends InventoryClickEvent {
     public Recipe getRecipe() {
         return this.recipe;
     }
+
+    // Paper start - Allows for replacing remainders
+    public List<ItemStack> getCraftRemainders() {
+        return this.craftRemainders;
+    }
+    // Paper end - Allows for replacing remainders
 }
