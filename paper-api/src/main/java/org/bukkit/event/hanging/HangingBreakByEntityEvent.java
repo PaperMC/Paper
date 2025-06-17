cb4afa01e7e601d905bedcca403c549de73a098b
Doc <nachito94@msn.com>
2025-06-17 19:49:25 -0400

    Fix Damager for HangingBreakByEntityEvent and minor improvements

--- paper-api/src/main/java/org/bukkit/event/hanging/HangingBreakByEntityEvent.java
+++ paper-api/src/main/java/org/bukkit/event/hanging/HangingBreakByEntityEvent.java
@@ -1,27 +1,30 @@
 package org.bukkit.event.hanging;
 
+import org.bukkit.damage.DamageSource;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Hanging;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NullMarked;
 
 /**
  * Triggered when a hanging entity is removed by an entity
  */
+@NullMarked
 public class HangingBreakByEntityEvent extends HangingBreakEvent {
 
     private final Entity remover;
+    private final DamageSource damageSource;
 
     @ApiStatus.Internal
-    public HangingBreakByEntityEvent(@NotNull final Hanging hanging, @NotNull final Entity remover) {
-        this(hanging, remover, HangingBreakEvent.RemoveCause.ENTITY);
+    public HangingBreakByEntityEvent(final Hanging hanging, final Entity remover, final DamageSource damageSource) {
+        this(hanging, remover, damageSource, HangingBreakEvent.RemoveCause.ENTITY);
     }
 
     @ApiStatus.Internal
-    public HangingBreakByEntityEvent(@NotNull final Hanging hanging, @NotNull final Entity remover, @NotNull final HangingBreakEvent.RemoveCause cause) {
+    public HangingBreakByEntityEvent( final Hanging hanging, final Entity remover, final DamageSource damageSource, final HangingBreakEvent.RemoveCause cause) {
         super(hanging, cause);
         this.remover = remover;
+        this.damageSource = damageSource;
     }
 
     /**
@@ -29,8 +32,16 @@ public class HangingBreakByEntityEvent extends HangingBreakEvent {
      *
      * @return the entity that removed the hanging entity
      */
-    @NotNull
     public Entity getRemover() {
         return this.remover;
+    }
+
+    /**
+     * Gets the {@link DamageSource} that caused the hanging entity to be removed.
+     *
+     * @return the damage source
+     */
+    public DamageSource getDamageSource() {
+        return damageSource;
     }
 }
