53c82b0f6341bec910d8add35c82c7c8edd50e25
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-06-20 19:18:31 -0700

    add correct keys

--- paper-generator/src/main/java/io/papermc/generator/rewriter/types/simple/StatisticRewriter.java
+++ paper-generator/src/main/java/io/papermc/generator/rewriter/types/simple/StatisticRewriter.java
@@ -5,6 +5,7 @@ import io.papermc.generator.registry.RegistryEntries;
 import io.papermc.generator.rewriter.types.registry.EnumRegistryRewriter;
 import io.papermc.generator.utils.Formatting;
 import io.papermc.typewriter.preset.model.EnumValue;
+import java.util.List;
 import java.util.Map;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -60,7 +61,7 @@ public class StatisticRewriter {
     public static class Custom extends EnumRegistryRewriter<ResourceLocation> {
 
         public Custom() {
-            super(Registries.CUSTOM_STAT, false);
+            super(Registries.CUSTOM_STAT, true);
         }
 
         @Override
@@ -98,7 +99,7 @@ public class StatisticRewriter {
         );
 
         public Type() {
-            super(Registries.STAT_TYPE, false);
+            super(Registries.STAT_TYPE, true);
         }
 
         @Override
@@ -114,9 +115,13 @@ public class StatisticRewriter {
                 throw new IllegalStateException("Unable to translate stat type generic " + genericType.getCanonicalName() + " into the api!");
             }
 
+            final List<String> arguments = List.of(
+                quoted(reference.key().location().getPath()),
+                "%s.%s".formatted(Statistic.Type.class.getSimpleName(), TYPE_MAPPING.get(genericType))
+            );
             return super.rewriteEnumValue(reference)
                 .rename(name -> FIELD_RENAMES.getOrDefault(name, name))
-                .argument("%s.%s".formatted(Statistic.Type.class.getSimpleName(), TYPE_MAPPING.get(genericType))); // find a more direct way?
+                .arguments(arguments);
 
         }
     }
