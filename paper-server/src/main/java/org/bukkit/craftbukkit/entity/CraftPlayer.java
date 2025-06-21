34f17681c51d37f6c39cb31c162d58cfa43d4fa5
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-06-21 08:34:48 -0700

    add back positive checks

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1588,7 +1588,14 @@ public class CraftPlayer extends CraftHumanEntity implements Player, PluginMessa
     }
 
     @Override
+    public void decrementStatistic(final Statistic<?> statistic, final int amount) {
+        Preconditions.checkArgument(amount > 0, "Amount must be greater than 0");
+        PaperStatistics.changeStatistic(this.getHandle().getStats(), statistic, -amount, this.getHandle());
+    }
+
+    @Override
     public void incrementStatistic(final Statistic<?> statistic, final int amount) {
+        Preconditions.checkArgument(amount > 0, "Amount must be greater than 0");
         PaperStatistics.changeStatistic(this.getHandle().getStats(), statistic, amount, this.getHandle());
     }
 
