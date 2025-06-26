7f609243907131c37d457aa367c04efbbd10f022
Owen <23108066+Owen1212055@users.noreply.github.com>
2025-06-26 15:55:03 -0400

    Configuration API (#12301)
    This implements a solution that preemptively exits the tick loop if we have already marked the connection as disconnecting. This avoids changing the result of Connection#isConnected in order to avoid other possibly unintentional changes. Fundamentally it should be investigated if closing the connection async is really still needed.
    
    This also additionally removes the login disconnecting logic for server stopping, as this was also a possible issue in the config stage but also shouldn't be an issue as connections are closed on server stop very early.
    
    Additionally, do not check for isConnecting() on VERIFYING, as that seemed to be an old diff originally trying to resolve this code, however isConnected is not updated at this point so it pretty much was useless.

--- paper-api/src/main/java/org/bukkit/plugin/messaging/PluginMessageListener.java
+++ paper-api/src/main/java/org/bukkit/plugin/messaging/PluginMessageListener.java
@@ -1,5 +1,6 @@
 package org.bukkit.plugin.messaging;
 
+import io.papermc.paper.connection.PlayerCommonConnection;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,4 +19,16 @@ public interface PluginMessageListener {
      * @param message The raw message that was sent.
      */
     public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, byte @NotNull [] message);
+
+    /**
+     * A method that will be invoked when a PluginMessageSource sends a plugin
+     * message on a registered channel.
+     *
+     * @param channel Channel that the message was sent through.
+     * @param connection Source of the message.
+     * @param message The raw message that was sent.
+     */
+    default void onPluginMessageReceived(@NotNull String channel, @NotNull PlayerCommonConnection connection, byte @NotNull [] message) {
+
+    }
 }
--- paper-server/src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
@@ -4,6 +4,7 @@ import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.mojang.authlib.GameProfile;
 import com.mojang.datafixers.util.Either;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,12 +14,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import io.papermc.paper.adventure.PaperAdventure;
+import io.papermc.paper.connection.HorriblePlayerLoginEventHack;
+import io.papermc.paper.connection.PlayerConnection;
+import io.papermc.paper.event.connection.PlayerConnectionValidateLoginEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.Connection;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ServerboundContainerClosePacket;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.util.Unit;
 import net.minecraft.world.Container;
@@ -225,6 +234,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerItemMendEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerRecipeBookClickEvent;
 import org.bukkit.event.player.PlayerRecipeBookSettingsChangeEvent;
 import org.bukkit.event.player.PlayerRecipeDiscoverEvent;
@@ -2084,5 +2094,28 @@ public class CraftEventFactory {
         }
 
         return event;
+    }
+
+    @SuppressWarnings("OptionalAssignedToNull")
+    public static Component handleLoginResult(PlayerList.LoginResult result, PlayerConnection paperConnection, Connection connection, GameProfile profile, MinecraftServer server, boolean loginPhase) {
+        PlayerConnectionValidateLoginEvent event = new PlayerConnectionValidateLoginEvent(
+            paperConnection, result.isAllowed() ? null : PaperAdventure.asAdventure(result.message())
+        );
+        event.callEvent();
+
+        Component disconnectReason = PaperAdventure.asVanilla(event.getKickMessage());
+
+        // For the login event it normally was never fired during configuration phase. In order to make this deprecation less
+        // breaky we will cache result and use it next time.
+        if (loginPhase) {
+            disconnectReason = HorriblePlayerLoginEventHack.execute(connection, server, profile,
+                disconnectReason == null ? PlayerList.LoginResult.ALLOW : new PlayerList.LoginResult(disconnectReason, disconnectReason == null ? PlayerLoginEvent.Result.KICK_OTHER : result.result())
+            );
+        } else if (connection.legacySavedLoginEventResultOverride != null) {
+            // If the override is set, use it.
+            disconnectReason = connection.legacySavedLoginEventResultOverride.orElse(null);
+        }
+
+        return disconnectReason;
     }
 }
