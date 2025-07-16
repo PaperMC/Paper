48f4af8c0215f35947cf0f8d5784ec81d58a1394
metabrixkt <admin@epserv.ru>
2025-07-16 23:06:21 +0500

    Restore world info loading behavior in `Player#loadData()` Fixes #11572

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1481,7 +1481,10 @@ public class CraftPlayer extends CraftHumanEntity implements Player, PluginMessa
 
     @Override
     public void loadData() {
-        this.server.getHandle().playerIo.load(this.getHandle(), ProblemReporter.DISCARDING);
+        this.server.getHandle().playerIo.load(this.getHandle(), ProblemReporter.DISCARDING)
+                .flatMap(this.server.getHandle()::getPlayerLevelKey)
+                .map(this.server.getServer()::getLevel)
+                .ifPresent(this.getHandle()::setServerLevel);
     }
 
     @Override
