64a3aa9214a123e1df3b7914bd1656e374783a50
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-07-26 14:59:24 -0700

    more work

--- paper-server/src/main/java/org/bukkit/craftbukkit/scoreboard/CraftScoreHolder.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/scoreboard/CraftScoreHolder.java
@@ -1,5 +1,6 @@
 package org.bukkit.craftbukkit.scoreboard;
 
+import com.google.common.base.Preconditions;
 import io.papermc.paper.adventure.PaperAdventure;
 import net.kyori.adventure.text.Component;
 import net.minecraft.world.entity.Entity;
@@ -33,6 +34,8 @@ public interface CraftScoreHolder extends ScoreHolder {
         private final String name;
 
         public CraftStringScoreHolder(final String name) {
+            Preconditions.checkArgument(name != null, "Name cannot be null");
+            Preconditions.checkArgument(name.length() <= Short.MAX_VALUE, "The name '%s' is longer than the limit of 32767 characters (%s)", name, name.length());
             this.name = name;
         }
 
