840dd9e1f8eab62679b028edeacc6a9c7b7360b2
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2025-07-26 21:35:19 +0200

    Fix dialog preconditions (#12895)

--- paper-server/src/main/java/io/papermc/paper/registry/data/dialog/action/StaticActionImpl.java
+++ paper-server/src/main/java/io/papermc/paper/registry/data/dialog/action/StaticActionImpl.java
@@ -6,6 +6,6 @@ import net.kyori.adventure.text.event.ClickEvent;
 public record StaticActionImpl(ClickEvent value) implements DialogAction.StaticAction {
 
     public StaticActionImpl {
-        Preconditions.checkArgument(value.action().readable(), "action must be readable");
+        Preconditions.checkArgument(value.action().readable() || value.action() == ClickEvent.Action.SHOW_DIALOG, "action must be readable or show_dialog");
     }
 }
