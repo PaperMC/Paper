4d44b5fd55714508649ffa3e77e16e1b8dbb7baf
Théo SZANTO <theo.mail@laposte.net>
2025-07-30 02:22:46 +0200

    Remove @Experimental annotation from the org.bukkit.damage package (#12898)

--- paper-api/src/main/java/org/bukkit/damage/package-info.java
+++ paper-api/src/main/java/org/bukkit/damage/package-info.java
@@ -1,7 +1,4 @@
 /**
  * Classes concerning damage types and sources applicable to living entities.
  */
-@ApiStatus.Experimental
 package org.bukkit.damage;
-
-import org.jetbrains.annotations.ApiStatus;
