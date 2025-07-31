98cf8220a4d876eabc91fcceb7f66de74857c9dd
Emily <emilia.lopezf.1999@gmail.com>
2025-07-31 12:04:19 -0300

    Make PlayerCommonConnection implement PluginMessageRecipient

--- paper-server/src/main/java/io/papermc/paper/connection/PaperPlayerGameConnection.java
+++ paper-server/src/main/java/io/papermc/paper/connection/PaperPlayerGameConnection.java
@@ -3,6 +3,8 @@ package io.papermc.paper.connection;
 import net.minecraft.server.level.ClientInformation;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import java.util.Set;
 
 public class PaperPlayerGameConnection extends PaperCommonConnection<ServerGamePacketListenerImpl> implements PlayerGameConnection {
 
@@ -27,5 +29,15 @@ public class PaperPlayerGameConnection extends PaperCommonConnection<ServerGameP
     @Override
     public Player getPlayer() {
         return this.handle.getCraftPlayer();
+    }
+
+    @Override
+    public void sendPluginMessage(final Plugin source, final String channel, final byte [] message) {
+        getPlayer().sendPluginMessage(source, channel, message);
+    }
+
+    @Override
+    public Set<String> getListeningPluginChannels() {
+        return getPlayer().getListeningPluginChannels();
     }
 }
