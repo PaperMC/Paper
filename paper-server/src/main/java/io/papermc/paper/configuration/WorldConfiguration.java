0e87efd86539994fa26abce20581e1a3d4dd6472
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-08-10 22:15:40 +0800

    feat: use IntOr.Disabled, and update comment

--- paper-server/src/main/java/io/papermc/paper/configuration/WorldConfiguration.java
+++ paper-server/src/main/java/io/papermc/paper/configuration/WorldConfiguration.java
@@ -31,6 +31,7 @@ import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import net.minecraft.Util;
@@ -331,8 +332,8 @@ public class WorldConfiguration extends ConfigurationPart {
 
             @Comment("Adds a cooldown to bees being released after a failed release, which can occur if the hive is blocked or it being night.")
             public boolean cooldownFailedBeehiveReleases = true;
-            @Comment("Add a delay before retrying POI acquisition when entity navigation is stuck to reduce pathfinding performance impact. (in seconds)")
-            public int stuckEntityPoiRetryDelay = 10;
+            @Comment("The delay before retrying POI acquisition when entity navigation is stuck. This will reduce pathfinding performance impact. Measured in seconds.")
+            public IntOr.Disabled stuckEntityPoiRetryDelay = new IntOr.Disabled(OptionalInt.of(10));
         }
 
         public TrackingRangeY trackingRangeY;
