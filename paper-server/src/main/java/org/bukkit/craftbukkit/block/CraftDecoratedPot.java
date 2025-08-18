46799d797c9d5ebf42b38b5f7ae9a8110f9fd431
Valgrifer <valgrifer123@gmail.com>
2025-08-18 18:37:15 +0200

    Missing annotation

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftDecoratedPot.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftDecoratedPot.java
@@ -132,7 +132,7 @@ public class CraftDecoratedPot extends CraftBlockEntityState<DecoratedPotBlockEn
     }
 
     @Override
-    public void startWobble(@NotNull final Wobble style) {
+    public void startWobble(@org.jspecify.annotations.NonNull final Wobble style) {
         Preconditions.checkArgument(style != null, "style must not be null");
 
         DecoratedPotBlockEntity.WobbleStyle originalStyle = switch (style) {
