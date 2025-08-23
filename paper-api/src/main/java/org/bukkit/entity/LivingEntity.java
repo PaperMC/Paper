675a2a5db16d82baf24f82df9576c28534d8a0df
Bjarne Koll <git@lynxplay.dev>
2025-08-23 23:34:40 +0200

    anno

--- paper-api/src/main/java/org/bukkit/entity/LivingEntity.java
+++ paper-api/src/main/java/org/bukkit/entity/LivingEntity.java
@@ -1493,6 +1493,7 @@ public interface LivingEntity extends Attributable, Damageable, ProjectileSource
      * @see #setWaypointStyle(Key)
      * @see <a href="https://minecraft.wiki/w/Waypoint_style">https://minecraft.wiki/w/Waypoint_style</a>
      */
+    @NotNull
     Key getWaypointStyle();
 
     /**
