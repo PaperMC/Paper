9a626dd7edf38cbc0239ef539038541fa86e7120
Spottedleaf <Spottedleaf@users.noreply.github.com>
2025-09-17 17:54:25 -0700

    Rewrite ticket counter system
    The old method of tracking the counts was to copy a
    Long2IntOpenHashMap each time a count was removed or added. This
    was under the assumption that tickets with counter types were not
    being added or removed frequently.
    
    The new system uses ConcurrentLong2LongChainedHashTable instead
    to support concurrently updating ticket counters for different
    positions.
    We copy the map when retrieving the forced chunks, however this
    appears to only be used in the ForceLoad command so this shouldn't
    be an issue.
    
    Additionally, the ticket counts were not updated if the ticket
    were to expire or if the ticket was UNKNOWN and added during
    ChunkHolderManager#removeTicket(). These issues are now resolved.

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -134,7 +134,7 @@ abstract class MockitoAgentProvider : CommandLineArgumentProvider {
 
 dependencies {
     implementation(project(":paper-api"))
-    implementation("ca.spottedleaf:concurrentutil:0.0.4-SNAPSHOT")
+    implementation("ca.spottedleaf:concurrentutil:0.0.4-20250917.230334-3")
     implementation("org.jline:jline-terminal-ffm:3.27.1") // use ffm on java 22+
     implementation("org.jline:jline-terminal-jni:3.27.1") // fall back to jni on java 21
     implementation("net.minecrell:terminalconsoleappender:1.3.0")
