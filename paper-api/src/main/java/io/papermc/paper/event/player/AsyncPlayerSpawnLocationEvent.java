4d7185f6ab39a95eef03a2db5341c24b21e67a40
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-09-20 12:01:34 -0700

    Add AsyncPlayerSpawnLocationEvent#isNewPlayer (#13081)

--- paper-api/src/main/java/io/papermc/paper/event/player/AsyncPlayerSpawnLocationEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/player/AsyncPlayerSpawnLocationEvent.java
@@ -4,7 +4,6 @@ import io.papermc.paper.connection.PlayerConfigurationConnection;
 import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.ApiStatus;
@@ -23,13 +22,15 @@ public class AsyncPlayerSpawnLocationEvent extends Event {
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
     private final PlayerConfigurationConnection connection;
+    private final boolean newPlayer;
     private Location spawnLocation;
 
     @ApiStatus.Internal
-    public AsyncPlayerSpawnLocationEvent(final PlayerConfigurationConnection connection, final Location spawnLocation) {
+    public AsyncPlayerSpawnLocationEvent(final PlayerConfigurationConnection connection, final Location spawnLocation, final boolean newPlayer) {
         super(true);
         this.connection = connection;
         this.spawnLocation = spawnLocation;
+        this.newPlayer = newPlayer;
     }
 
     /**
@@ -42,10 +43,11 @@ public class AsyncPlayerSpawnLocationEvent extends Event {
     }
 
     /**
-     * Gets player's spawn location.
-     * If the player has played on this server before, it's going to default to the location inside player.dat file.
+     * Gets the player's spawn location.
+     *
+     * <p>If the player has played on this server before, defaults to the location inside the player data file.
      * For new players, the default spawn location is the {@link World#getSpawnLocation() spawn location}
-     * of the {@link Server#getRespawnWorld() respawn world}.
+     * of the {@link Server#getRespawnWorld() respawn world}.</p>
      *
      * @return the spawn location
      */
@@ -60,6 +62,15 @@ public class AsyncPlayerSpawnLocationEvent extends Event {
      */
     public void setSpawnLocation(final Location location) {
         this.spawnLocation = location.clone();
+    }
+
+    /**
+     * Returns true if the player is joining the server for the first time.
+     *
+     * @return whether the player is new
+     */
+    public boolean isNewPlayer() {
+        return this.newPlayer;
     }
 
     @Override
