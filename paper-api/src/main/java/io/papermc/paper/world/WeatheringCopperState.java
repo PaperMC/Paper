bd4f0781b9f15121d4d5eebd6c9dd9ece935b8d9
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-09-22 12:03:46 -0700

    Add Javadoc

--- paper-api/src/main/java/io/papermc/paper/world/WeatheringCopperState.java
+++ paper-api/src/main/java/io/papermc/paper/world/WeatheringCopperState.java
@@ -1,5 +1,8 @@
 package io.papermc.paper.world;
 
+/**
+ * Represents the weathering state of copper blocks, items, or entities.
+ */
 public enum WeatheringCopperState {
     // Start generate - WeatheringCopperState
     UNAFFECTED,
--- paper-api/src/main/java/org/bukkit/entity/CopperGolem.java
+++ paper-api/src/main/java/org/bukkit/entity/CopperGolem.java
@@ -7,7 +7,17 @@ import org.jspecify.annotations.NullMarked;
 @NullMarked
 public interface CopperGolem extends Golem, Shearable {
 
+    /**
+     * Get the current weathering state of the copper golem.
+     *
+     * @return the weathering state
+     */
     WeatheringCopperState getWeatheringState();
 
+    /**
+     * Set the weathering state of the copper golem.
+     *
+     * @param state the new weathering state
+     */
     void setWeatheringState(WeatheringCopperState state);
 }
