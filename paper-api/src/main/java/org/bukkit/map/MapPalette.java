0bfb9187c61464d9af7707e44a303277745185bb
mintychochip <jlo2@csub.edu>
2025-09-27 22:52:03 -0700

    Expose MapPallette.colors, Add MapPallette.getNearestColor #12907

--- paper-api/src/main/java/org/bukkit/map/MapPalette.java
+++ paper-api/src/main/java/org/bukkit/map/MapPalette.java
@@ -11,8 +11,8 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Represents the palette that map items use.
  * <p>
- * These fields are hee base color ranges. Each entry corresponds to four colors of varying shades
- * with values entry to entry + 3.
+ * These fields are hee base color ranges. Each entry corresponds to four
+ * colors of varying shades with values entry to entry + 3.
  */
 public final class MapPalette {
     // Internal mechanisms
