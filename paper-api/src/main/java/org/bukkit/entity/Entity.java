5147ee636c3f42c177115fb3f16f4344d9625b3d
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-10-19 19:04:32 +0200

    Add Entity#teleportAsync(Location, TeleportFlag...)

--- paper-api/src/main/java/org/bukkit/entity/Entity.java
+++ paper-api/src/main/java/org/bukkit/entity/Entity.java
@@ -242,6 +242,16 @@ public interface Entity extends Metadatable, CommandSender, Nameable, Persistent
     /**
      * Loads/Generates(in 1.13+) the Chunk asynchronously, and then teleports the entity when the chunk is ready.
      * @param loc Location to teleport to
+     * @param teleportFlags Flags to be used in this teleportation
+     * @return A future that will be completed with the result of the teleport
+     */
+    default java.util.concurrent.@NotNull CompletableFuture<Boolean> teleportAsync(@NotNull Location loc, @NotNull io.papermc.paper.entity.TeleportFlag @NotNull... teleportFlags) {
+        return this.teleportAsync(loc, TeleportCause.PLUGIN, teleportFlags);
+    }
+
+    /**
+     * Loads/Generates(in 1.13+) the Chunk asynchronously, and then teleports the entity when the chunk is ready.
+     * @param loc Location to teleport to
      * @param cause Reason for teleport
      * @param teleportFlags Flags to be used in this teleportation
      * @return A future that will be completed with the result of the teleport
