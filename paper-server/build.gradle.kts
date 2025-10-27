9a3462f26c7d1307115533ee78f433a2ec3cb133
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-10-27 09:42:44 -0700

    Update mache

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:25w43a+build.5")
+    mache("io.papermc:mache:25w43a+build.6")
     paperclip("io.papermc:paperclip:3.0.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
