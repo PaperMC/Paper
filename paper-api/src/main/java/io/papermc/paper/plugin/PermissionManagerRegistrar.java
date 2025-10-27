400eaa17dbe85dee60fba455bbf230e415f4c499
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2025-10-27 13:43:47 -0400

    Fix tests

--- paper-api/src/main/java/io/papermc/paper/plugin/PermissionManagerRegistrar.java
+++ paper-api/src/main/java/io/papermc/paper/plugin/PermissionManagerRegistrar.java
@@ -2,10 +2,11 @@ package io.papermc.paper.plugin;
 
 import io.papermc.paper.plugin.lifecycle.event.registrar.Registrar;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 
 @ApiStatus.Experimental
 public interface PermissionManagerRegistrar extends Registrar {
 
-    void register(PermissionManager permissionManager);
+    void register(@NotNull PermissionManager permissionManager);
 
 }
