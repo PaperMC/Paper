6f3bed61e793429a5006b9734bb7ff4a876b29fa
Doc <nachito94@msn.com>
2025-11-08 19:35:24 -0300

    Change error message

--- paper-api/src/main/java/io/papermc/paper/util/MorePreconditions.java
+++ paper-api/src/main/java/io/papermc/paper/util/MorePreconditions.java
@@ -19,7 +19,7 @@ public final class MorePreconditions {
     }
 
     public static void validateFloatRangeMinInclusive(final float value, final float min, final float max, final String valueName) {
-        validateFloatRangeMinInclusive(value, min, max, _float -> String.format("%s value %.2f is out of range [%.2f, %.2f]", valueName, value, min, max));
+        validateFloatRangeMinInclusive(value, min, max, _float -> String.format("%s value %.2f is out of range (%.2f, %.2f]", valueName, value, min, max));
     }
 
     private static void validateFloatRangeMinInclusive(final float value, final float min, final float max, Function<Float, String> errorMessage) {
@@ -29,20 +29,12 @@ public final class MorePreconditions {
     }
 
     public static void validateFloatRangeMinExclusive(final float value, final float min, final float max, final String valueName) {
-        validateFloatRangeMinExclusive(value, min, max, _float -> String.format("%s value %.2f is out of range [%.2f, %.2f)", valueName, value, min, max));
+        validateFloatRangeMinExclusive(value, min, max, _float -> String.format("%s value %.2f is out of range [%.2f, %.2f]", valueName, value, min, max));
     }
 
     private static void validateFloatRangeMinExclusive(final float value, final float min, final float max, Function<Float, String> errorMessage) {
         if (!(Float.compare(value, min) > 0 && Float.compare(value, max) <= 0)) {
             throw new IllegalArgumentException(errorMessage.apply(value));
-        }
-    }
-
-    public static void validateDoubleRange(final double value, final double min, final double max, final String valueName) {
-        if (Double.compare(value, min) < 0 || Double.compare(value, max) > 0) {
-            throw new IllegalArgumentException(
-                String.format("%s value %.2f is out of range [%.2f, %.2f]", valueName, value, min, max)
-            );
         }
     }
 }
