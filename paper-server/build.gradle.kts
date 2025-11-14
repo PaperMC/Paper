0e10753e46a96a4861ff33cdf5103a31ab091557
Spottedleaf <Spottedleaf@users.noreply.github.com>
2025-11-13 17:04:23 -0800

    Update ConcurrentUtil to 0.0.6
    Fixes https://github.com/PaperMC/Paper/issues/13275
    
    See
    - https://github.com/Tuinity/ConcurrentUtil/releases/tag/v0.0.6
    - https://github.com/Tuinity/ConcurrentUtil/commit/42e72d7efdc33cbadc6f07a2e93fb276bb5b6328

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -128,7 +128,7 @@ abstract class MockitoAgentProvider : CommandLineArgumentProvider {
 
 dependencies {
     implementation(project(":paper-api"))
-    implementation("ca.spottedleaf:concurrentutil:0.0.5")
+    implementation("ca.spottedleaf:concurrentutil:0.0.6")
     implementation("org.jline:jline-terminal-ffm:3.27.1") // use ffm on java 22+
     implementation("org.jline:jline-terminal-jni:3.27.1") // fall back to jni on java 21
     implementation("net.minecrell:terminalconsoleappender:1.3.0")
