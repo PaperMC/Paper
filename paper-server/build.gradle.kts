1cb31fd3b7b095cb1feedcf3488faa1047d82bbd
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-12-04 10:25:02 -0700

    Update to 1.21.11-rc1

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:1.21.11-pre5+build.1")
+    mache("io.papermc:mache:1.21.11-rc1+build.1")
     paperclip("io.papermc:paperclip:3.0.3")
 }
 
