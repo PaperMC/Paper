e18d045c31cc83d96cb501f05914de1ab5eca504
Doc <nachito94@msn.com>
2025-12-28 13:03:33 -0300

    Add Shelf#getSlot using Vector

--- paper-api/src/main/java/org/bukkit/block/Shelf.java
+++ paper-api/src/main/java/org/bukkit/block/Shelf.java
@@ -2,6 +2,7 @@ package org.bukkit.block;
 
 import io.papermc.paper.block.TileStateInventoryHolder;
 import org.bukkit.inventory.ShelfInventory;
+import org.bukkit.util.Vector;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
@@ -12,4 +13,16 @@ public interface Shelf extends TileStateInventoryHolder {
 
     @Override
     ShelfInventory getSnapshotInventory();
+
+    /**
+     * Gets the appropriate slot based on a vector relative to this block.<br>
+     * Will return -1 if the given vector is not within the bounds of any slot.
+     * <p>
+     * The supplied vector should only contain components with values between 0.0
+     * and 1.0, inclusive.
+     *
+     * @param position a vector relative to this block
+     * @return the slot under the given vector or -1
+     */
+    int getSlot(Vector position);
 }
