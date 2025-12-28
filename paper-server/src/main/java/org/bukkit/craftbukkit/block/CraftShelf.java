bc909a17b77102acaa5a674d28e3ff3879198c0f
Doc <nachito94@msn.com>
2025-12-28 19:30:58 -0300

    Move all logic to abstract class for Selectable Slots

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftShelf.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/CraftShelf.java
@@ -1,18 +1,14 @@
 package org.bukkit.craftbukkit.block;
 
-import net.minecraft.world.level.block.SelectableSlotContainer;
 import net.minecraft.world.level.block.entity.ShelfBlockEntity;
-import net.minecraft.world.phys.Vec2;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.block.BlockFace;
 import org.bukkit.block.Shelf;
-import org.bukkit.block.data.Directional;
 import org.bukkit.craftbukkit.inventory.CraftInventoryShelf;
 import org.bukkit.inventory.ShelfInventory;
 import org.bukkit.util.Vector;
 
-public class CraftShelf extends CraftBlockEntityState<ShelfBlockEntity> implements Shelf {
+public class CraftShelf extends CraftSelectableBlockEntityState<ShelfBlockEntity> implements Shelf {
 
     public CraftShelf(World world, ShelfBlockEntity blockEntity) {
         super(world, blockEntity);
@@ -38,37 +34,7 @@ public class CraftShelf extends CraftBlockEntityState<ShelfBlockEntity> implemen
 
     @Override
     public int getSlot(Vector clickVector) {
-        BlockFace facing = ((Directional) this.getBlockData()).getFacing();
-
-        Vec2 faceVector;
-        switch (facing) {
-            case NORTH:
-                faceVector = new Vec2((float) (1.0f - clickVector.getX()), (float) clickVector.getY());
-                break;
-            case SOUTH:
-                faceVector = new Vec2((float) clickVector.getX(), (float) clickVector.getY());
-                break;
-            case WEST:
-                faceVector = new Vec2((float) clickVector.getZ(), (float) clickVector.getY());
-                break;
-            case EAST:
-                faceVector = new Vec2((float) (1f - clickVector.getZ()), (float) clickVector.getY());
-                break;
-            case DOWN:
-            case UP:
-            default:
-                return -1;
-        }
-
-        return CraftShelf.getHitSlot(faceVector);
-    }
-
-    private static int getHitSlot(Vec2 vec2) {
-        final int rows = 1;
-        final int columns = 3;
-        final int row = SelectableSlotContainer.getSection(1.0F - vec2.y, rows);
-        final int column = SelectableSlotContainer.getSection(vec2.x, columns);
-        return column + row * columns;
+        return super.getSlot(1, 3, clickVector);
     }
 
     @Override
