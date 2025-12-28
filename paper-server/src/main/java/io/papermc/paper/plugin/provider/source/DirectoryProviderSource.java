958d9dc1d9080268252608818c9e13ff0f4e0f48
theminecoder <theminecoder.dev@gmail.com>
2025-12-28 12:04:00 +1100

    Silently ignore extra directories if they dont exist

--- paper-server/src/main/java/io/papermc/paper/plugin/provider/source/DirectoryProviderSource.java
+++ paper-server/src/main/java/io/papermc/paper/plugin/provider/source/DirectoryProviderSource.java
@@ -16,15 +16,23 @@ import org.slf4j.Logger;
  */
 public class DirectoryProviderSource implements ProviderSource<Path, List<Path>> {
 
-    public static final DirectoryProviderSource INSTANCE = new DirectoryProviderSource();
+    public static final DirectoryProviderSource INSTANCE = new DirectoryProviderSource(true);
+    public static final DirectoryProviderSource INSTANCE_NO_CREATE = new DirectoryProviderSource(false);
     private static final FileProviderSource FILE_PROVIDER_SOURCE = new FileProviderSource("Directory '%s'"::formatted, false); // Paper - Remap plugins
     private static final Logger LOGGER = LogUtils.getClassLogger();
+
+    private final boolean createDirectory;
+
+    public DirectoryProviderSource(final boolean createDirectory) {
+        this.createDirectory = createDirectory;
+    }
 
     @Override
     public List<Path> prepareContext(Path context) throws IOException {
         // Symlink happy, create file if missing.
         if (!Files.isDirectory(context)) {
-            Files.createDirectories(context);
+            if(this.createDirectory) Files.createDirectories(context);
+            else return List.of();
         }
 
         final List<Path> files = new ArrayList<>();
