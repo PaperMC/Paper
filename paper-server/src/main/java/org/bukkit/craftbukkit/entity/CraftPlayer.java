8ec4917638aaf0f9fa2137ea305985fcdc990eaf
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-05 23:02:00 +0100

    missed some calls

--- paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -55,9 +55,7 @@ import net.kyori.adventure.pointer.PointersSupplier;
 import net.kyori.adventure.util.TriState;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.minecraft.advancements.AdvancementProgress;
-import net.minecraft.commands.Commands;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.GlobalPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.SectionPos;
 import net.minecraft.nbt.CompoundTag;
@@ -1392,11 +1390,9 @@ public class CraftPlayer extends CraftHumanEntity implements Player, PluginMessa
         } else {
             this.getHandle().setRespawnPosition(
                 new ServerPlayer.RespawnConfig(
-                    new LevelData.RespawnData(
-                        GlobalPos.of(
-                            ((CraftWorld) location.getWorld()).getHandle().dimension(),
-                            CraftLocation.toBlockPosition(location)
-                        ),
+                    LevelData.RespawnData.of(
+                        ((CraftWorld) location.getWorld()).getHandle().dimension(),
+                        CraftLocation.toBlockPosition(location),
                         location.getYaw(),
                         location.getPitch()
                     ),
