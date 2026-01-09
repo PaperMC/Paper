PaperMC/Paper

commit f00727c57e564f3a8cb875183a54142feb693db7

1.21.5
Co-authored-by: Bjarne Koll <git@lynxplay.dev>
Co-authored-by: Jake Potrebic <jake.m.potrebic@gmail.com>
Co-authored-by: Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
Co-authored-by: MiniDigger | Martin <admin@minidigger.dev>
Co-authored-by: Nassim Jahnke <nassim@njahnke.dev>
Co-authored-by: Noah van der Aa <ndvdaa@gmail.com>
Co-authored-by: Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
Co-authored-by: Shane Freeder <theboyetronic@gmail.com>
Co-authored-by: Spottedleaf <Spottedleaf@users.noreply.github.com>
Co-authored-by: Tamion <70228790+notTamion@users.noreply.github.com>
Co-authored-by: Warrior <50800980+Warriorrrr@users.noreply.github.com>

paper-server/src/main/java/org/bukkit/craftbukkit/legacy/CraftLegacy.java
@@ -35,10 +35,7 @@ import org.bukkit.material.MaterialData;
 /**
  * This class may seem unnecessarily slow and complicated/repetitive however it
  * is able to handle a lot more edge cases and invertible transformations (many
- * of which are not immediately obvious) than any other alternative. If you do
- * make changes to this class please make sure to contribute them back
- * https://hub.spigotmc.org/stash/projects/SPIGOT/repos/craftbukkit/browse so
- * that all may benefit.
+ * of which are not immediately obvious) than any other alternative.
  *
  * @deprecated legacy use only
  */
@@ -56,7 +53,6 @@ public final class CraftLegacy {
     private static final Map<Block, MaterialData> blockToMaterial = new HashMap<>(1024);
 
     private CraftLegacy() {
-        //
     }
 
     public static Material toLegacy(Material material) {
@@ -82,10 +78,10 @@ public final class CraftLegacy {
 
         if (mappedData == null && material.isBlock()) {
             Block block = CraftMagicNumbers.getBlock(material);
-            BlockState blockData = block.defaultBlockState();
+            BlockState state = block.defaultBlockState();
 
             // Try exact match first
-            mappedData = CraftLegacy.dataToMaterial.get(blockData);
+            mappedData = CraftLegacy.dataToMaterial.get(state);
             // Fallback to any block
             if (mappedData == null) {
                 mappedData = CraftLegacy.blockToMaterial.get(block);
@@ -153,22 +149,22 @@ public final class CraftLegacy {
         return Items.AIR;
     }
 
-    public static byte toLegacyData(BlockState blockData) {
-        return CraftLegacy.toLegacy(blockData).getData();
+    public static byte toLegacyData(BlockState state) {
+        return CraftLegacy.toLegacy(state).getData();
     }
 
-    public static Material toLegacyMaterial(BlockState blockData) {
-        return CraftLegacy.toLegacy(blockData).getItemType();
+    public static Material toLegacyMaterial(BlockState state) {
+        return CraftLegacy.toLegacy(state).getItemType();
     }
 
-    public static MaterialData toLegacy(BlockState blockData) {
+    public static MaterialData toLegacy(BlockState state) {
         MaterialData mappedData;
 
         // Try exact match first
-        mappedData = CraftLegacy.dataToMaterial.get(blockData);
+        mappedData = CraftLegacy.dataToMaterial.get(state);
         // Fallback to any block
         if (mappedData == null) {
-            mappedData = CraftLegacy.blockToMaterial.get(blockData.getBlock());
+            mappedData = CraftLegacy.blockToMaterial.get(state.getBlock());
         }
 
         return (mappedData == null) ? new MaterialData(Material.LEGACY_AIR) : mappedData;
@@ -261,7 +257,6 @@ public final class CraftLegacy {
     }
 
     public static void init() {
-        //
     }
 
     static {
@@ -353,7 +348,7 @@ public final class CraftLegacy {
                     Optional<CompoundTag> propMap = blockTag.getElement("Properties").result();
                     if (propMap.isPresent()) {
                         CompoundTag properties = propMap.get();
-                        for (String dataKey : properties.getAllKeys()) {
+                        for (String dataKey : properties.keySet()) {
                             Property state = states.getProperty(dataKey);
 
                             if (state == null) {
@@ -361,8 +356,8 @@ public final class CraftLegacy {
                                 continue;
                             }
 
-                            Preconditions.checkState(!properties.getString(dataKey).isEmpty(), "Empty data string");
-                            Optional opt = state.getValue(properties.getString(dataKey));
+                            Preconditions.checkState(properties.getString(dataKey).isPresent(), "Empty data string");
+                            Optional opt = state.getValue(properties.getStringOr(dataKey, ""));
                             Preconditions.checkArgument(opt.isPresent(), "No state value %s for %s", properties.getString(dataKey), dataKey);
 
                             blockData = blockData.setValue(state, (Comparable) opt.get());
@@ -443,9 +438,5 @@ public final class CraftLegacy {
         // From Material#isBlock before the rewrite to ItemType / BlockType
         // Git hash: 42f6cdf4c5dcdd52a27543403dcd17fb60311621
         return 0 <= material.getId() && material.getId() < 256;
-    }
-
-    public static void main(String[] args) {
-        System.err.println("");
     }
 }

paper-server/src/main/java/org/bukkit/craftbukkit/util/CraftLegacy.java
@@ -11,7 +11,6 @@ import org.bukkit.material.MaterialData;
 public final class CraftLegacy {
 
     private CraftLegacy() {
-        //
     }
 
     public static Material fromLegacy(Material material) {

commit 5c0384812313bec8e2c2e39b68dece0af7c304aa

Restore legacy conversion
Legacy conversion was broken in f00727c57e564f3a8cb875183a54142feb693db7 due to dependency loop:
legacy conversion was dependent on the max durability, but getting the max durability now also became dependent on legacy conversion.

paper-server/src/main/java/org/bukkit/craftbukkit/legacy/CraftLegacy.java
@@ -381,7 +381,37 @@ public final class CraftLegacy {
             }
 
             // Handle items (and second fallback for blocks)
-            int maxData = material.getMaxDurability() == 0 ? 16 : 1;
+            int maxData =
+                switch (material) { // We cannot rely on Material#getMaxDurability here because it relies on materialToItem which isn't filled yet
+                    case LEGACY_WOOD_AXE, LEGACY_WOOD_HOE, LEGACY_WOOD_PICKAXE, LEGACY_WOOD_SPADE,
+                         LEGACY_WOOD_SWORD,
+                         LEGACY_STONE_AXE, LEGACY_STONE_HOE, LEGACY_STONE_PICKAXE, LEGACY_STONE_SPADE,
+                         LEGACY_STONE_SWORD,
+                         LEGACY_IRON_AXE, LEGACY_IRON_HOE, LEGACY_IRON_PICKAXE, LEGACY_IRON_SPADE,
+                         LEGACY_IRON_SWORD,
+                         LEGACY_GOLD_AXE, LEGACY_GOLD_HOE, LEGACY_GOLD_PICKAXE, LEGACY_GOLD_SPADE,
+                         LEGACY_GOLD_SWORD,
+                         LEGACY_DIAMOND_AXE, LEGACY_DIAMOND_HOE, LEGACY_DIAMOND_PICKAXE, LEGACY_DIAMOND_SPADE,
+                         LEGACY_DIAMOND_SWORD,
+                         LEGACY_LEATHER_HELMET, LEGACY_LEATHER_CHESTPLATE,
+                         LEGACY_LEATHER_LEGGINGS, LEGACY_LEATHER_BOOTS,
+                         LEGACY_CHAINMAIL_HELMET, LEGACY_CHAINMAIL_CHESTPLATE,
+                         LEGACY_CHAINMAIL_LEGGINGS, LEGACY_CHAINMAIL_BOOTS,
+                         LEGACY_IRON_HELMET, LEGACY_IRON_CHESTPLATE,
+                         LEGACY_IRON_LEGGINGS, LEGACY_IRON_BOOTS,
+                         LEGACY_GOLD_HELMET, LEGACY_GOLD_CHESTPLATE,
+                         LEGACY_GOLD_LEGGINGS, LEGACY_GOLD_BOOTS,
+                         LEGACY_DIAMOND_HELMET, LEGACY_DIAMOND_CHESTPLATE,
+                         LEGACY_DIAMOND_LEGGINGS, LEGACY_DIAMOND_BOOTS,
+                         LEGACY_FISHING_ROD,
+                         LEGACY_CARROT_STICK,
+                         LEGACY_BOW,
+                         LEGACY_SHEARS,
+                         LEGACY_FLINT_AND_STEEL,
+                         LEGACY_SHIELD,
+                         LEGACY_ELYTRA -> 1;
+                    default -> 16;
+                };
             // Manually do oldold spawn eggs
             if (material == Material.LEGACY_MONSTER_EGG) {
                 maxData = 121; // Vilager + 1