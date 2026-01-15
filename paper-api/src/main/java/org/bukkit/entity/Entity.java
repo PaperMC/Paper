8def6b878643deeea29608650446c6ec7c49a2bd
Cryptite <cryptite@gmail.com>
2026-01-14 21:58:59 -0600

    Add getBlockStandingOn

--- paper-api/src/main/java/org/bukkit/entity/Entity.java
+++ paper-api/src/main/java/org/bukkit/entity/Entity.java
@@ -1322,4 +1322,27 @@ public interface Entity extends Metadatable, CommandSender, Nameable, Persistent
      */
     void broadcastHurtAnimation(@NotNull java.util.Collection<Player> players);
     // Paper end - broadcast hurt animation
+
+    // Paper start
+    /**
+     * Returns the Block that is currently supporting the player, particularly in the case of
+     * crouching over the edge of a block.
+     */
+    org.bukkit.block.Block getBlockStandingOn();
+    // Paper end
+
+    // Slice start
+    /**
+     * Returns true if the entity can be saved. If false, the entity will never be serialized or saved.
+     */
+    boolean isSaveable();
+
+    /**
+     * Sets whether the entity can be serialized and saved to disk.
+     *
+     * @param saveable the saveable status
+     * @see #isSaveable()
+     */
+    void setSaveable(boolean saveable);
+    // Slice end
 }
