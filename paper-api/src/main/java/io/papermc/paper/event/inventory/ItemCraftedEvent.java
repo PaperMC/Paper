716a49c38f292bc88575dc1fab6cbeb0eed4a9b8
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-16 19:25:33 +0100

    tweaks

--- paper-api/src/main/java/io/papermc/paper/event/inventory/ItemCraftedEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/inventory/ItemCraftedEvent.java
@@ -7,20 +7,21 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
-
 /**
  * Called when a player picks up a crafted item from the result slot of a crafting grid.
  */
 @NullMarked
 public class ItemCraftedEvent extends Event {
-    private static final HandlerList HANDLERS = new HandlerList();
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
     private final Player player;
-    private final ItemStack craftedItemStack;
+    private final ItemStack craftedItem;
 
     @ApiStatus.Internal
-    public ItemCraftedEvent(final Player player, final ItemStack craftedItemStack) {
+    public ItemCraftedEvent(final Player player, final ItemStack craftedItem) {
         this.player = player;
-        this.craftedItemStack = craftedItemStack;
+        this.craftedItem = craftedItem;
     }
 
     /**
@@ -29,24 +30,24 @@ public class ItemCraftedEvent extends Event {
      * @return player
      */
     public Player getPlayer() {
-        return player;
+        return this.player;
     }
 
     /**
-     * Gets the itemstack that was crafted and picked up by the player.
+     * Gets the item that was crafted and picked up by the player.
      *
-     * @return the itemstack
+     * @return the crafted item
      */
-    public ItemStack getCraftedItemStack() {
-        return craftedItemStack.clone();
+    public ItemStack getCraftedItem() {
+        return this.craftedItem.clone();
     }
 
     @Override
     public HandlerList getHandlers() {
-        return HANDLERS;
+        return HANDLER_LIST;
     }
 
     public static HandlerList getHandlerList() {
-        return HANDLERS;
+        return HANDLER_LIST;
     }
 }
