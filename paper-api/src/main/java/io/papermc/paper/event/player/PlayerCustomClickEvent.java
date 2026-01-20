5f38678b7c93c0e09af9aed3d4956db680957a35
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-20 19:06:51 +0100

    PlayerCustomClickEvent

--- paper-api/src/main/java/io/papermc/paper/event/player/PlayerCustomClickEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/player/PlayerCustomClickEvent.java
@@ -6,9 +6,7 @@ import io.papermc.paper.registry.data.dialog.action.DialogActionCallback;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.nbt.api.BinaryTagHolder;
 import net.kyori.adventure.text.event.ClickCallback;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
-import org.bukkit.event.EventTmp;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
@@ -23,56 +21,37 @@ import org.jspecify.annotations.Nullable;
 @ApiStatus.Experimental
 @ApiStatus.NonExtendable
 @NullMarked
-public abstract class PlayerCustomClickEvent extends EventTmp {
-
-    private final Key identifier;
-    private final PlayerCommonConnection commonConnection;
-
-    @ApiStatus.Internal
-    protected PlayerCustomClickEvent(final Key identifier, final PlayerCommonConnection commonConnection) {
-        this.identifier = identifier;
-        this.commonConnection = commonConnection;
-    }
+public interface PlayerCustomClickEvent extends Event {
 
     /**
      * The identifier of the custom click event.
      *
      * @return the identifier
      */
-    public final Key getIdentifier() {
-        return this.identifier;
-    }
+    Key getIdentifier();
 
     /**
      * The tag payload of the custom click event.
      *
      * @return the tag (if any)
      */
-    public abstract @Nullable BinaryTagHolder getTag();
+    @Nullable BinaryTagHolder getTag();
 
     /**
      * The dialog response view of the custom click event.
      *
      * @return the dialog response view
      */
-    public abstract @Nullable DialogResponseView getDialogResponseView();
+    @Nullable DialogResponseView getDialogResponseView();
 
     /**
      * The common connection of the player.
      *
      * @return the common connection
      */
-    public final PlayerCommonConnection getCommonConnection() {
-        return this.commonConnection;
-    }
+    PlayerCommonConnection getCommonConnection();
 
-    @Override
-    public HandlerList getHandlers() {
-        // this will be how handler lists will work on interfaces
-        return PlayerCustomClickEvent.getHandlerList();
-    }
-
-    public static HandlerList getHandlerList() {
+    static HandlerList getHandlerList() {
         final class Holder {
             private static final HandlerList HANDLER_LIST = new HandlerList();
         }
