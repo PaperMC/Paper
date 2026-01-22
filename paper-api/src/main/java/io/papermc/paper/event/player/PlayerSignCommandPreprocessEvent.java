0bf541322dbe9d90c231620ca824662cc5fe1b19
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-22 22:22:30 +0100

    PlayerCommandPreprocessEvent

--- paper-api/src/main/java/io/papermc/paper/event/player/PlayerSignCommandPreprocessEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/player/PlayerSignCommandPreprocessEvent.java
@@ -1,11 +1,9 @@
 package io.papermc.paper.event.player;
 
-import java.util.Set;
 import org.bukkit.block.Sign;
 import org.bukkit.block.sign.Side;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 /**
@@ -15,33 +13,19 @@ import org.jspecify.annotations.NullMarked;
  * normally be able to run.
  */
 @NullMarked
-public class PlayerSignCommandPreprocessEvent extends PlayerCommandPreprocessEvent {
-
-    private final Sign sign;
-    private final Side side;
-
-    @ApiStatus.Internal
-    public PlayerSignCommandPreprocessEvent(final Player player, final String message, final Set<Player> recipients, final Sign sign, final Side side) {
-        super(player, message, recipients);
-        this.sign = sign;
-        this.side = side;
-    }
+public interface PlayerSignCommandPreprocessEvent extends PlayerCommandPreprocessEvent {
 
     /**
      * Gets the sign that the command originated from.
      *
      * @return the sign
      */
-    public Sign getSign() {
-        return this.sign;
-    }
+    Sign getSign();
 
     /**
      * Gets the side of the sign that the command originated from.
      *
      * @return the sign side
      */
-    public Side getSide() {
-        return this.side;
-    }
+    Side getSide();
 }
