d186815458612035e65e353d587395efe4105773
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-23 17:33:09 +0100

    PlayerGameModeChangeEvent

---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/event/player/CraftPlayerGameModeChangeEvent.java
@@ -0,0 +1,59 @@
+package org.bukkit.craftbukkit.event.player;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.jspecify.annotations.Nullable;
+
+public class CraftPlayerGameModeChangeEvent extends CraftPlayerEvent implements PlayerGameModeChangeEvent {
+
+    private final GameMode newGameMode;
+    private final Cause cause;
+    private @Nullable Component cancelMessage;
+
+    private boolean cancelled;
+
+    public CraftPlayerGameModeChangeEvent(final Player player, final GameMode newGameMode, Cause cause, @Nullable Component cancelMessage) {
+        super(player);
+        this.newGameMode = newGameMode;
+        this.cause = cause;
+        this.cancelMessage = cancelMessage;
+    }
+
+    @Override
+    public GameMode getNewGameMode() {
+        return this.newGameMode;
+    }
+
+    @Override
+    public Cause getCause() {
+        return this.cause;
+    }
+
+    @Override
+    public @Nullable Component cancelMessage() {
+        return this.cancelMessage;
+    }
+
+    @Override
+    public void cancelMessage(final @Nullable Component message) {
+        this.cancelMessage = message;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(final boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return PlayerGameModeChangeEvent.getHandlerList();
+    }
+}
