c190b251ab187c256720c29d501c733206e9cfb3
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-23 17:51:55 +0100

    PlayerInputEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerInputEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerInputEvent.java
@@ -3,44 +3,25 @@ package org.bukkit.event.player;
 import org.bukkit.Input;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * This event is called when a player sends updated input to the server.
  *
  * @see Player#getCurrentInput()
  */
-public class PlayerInputEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Input input;
-
-    @ApiStatus.Internal
-    public PlayerInputEvent(@NotNull final Player player, @NotNull final Input input) {
-        super(player);
-        this.input = input;
-    }
+public interface PlayerInputEvent extends PlayerEventNew {
 
     /**
      * Gets the new input received from this player.
      *
      * @return the new input
      */
-    @NotNull
-    public Input getInput() {
-        return this.input;
-    }
+    Input getInput();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
