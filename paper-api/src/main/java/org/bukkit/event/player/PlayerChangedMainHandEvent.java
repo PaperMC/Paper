ca85657a2802761d84c01ecab3f34c12c17e72ee
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-23 17:24:10 +0100

    PlayerChangedMainHandEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerChangedMainHandEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerChangedMainHandEvent.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.MainHand;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player changes their main hand in the client settings.
@@ -13,17 +12,7 @@ import org.jetbrains.annotations.NotNull;
  * @apiNote Obsolete and replaced by {@link PlayerClientOptionsChangeEvent}.
  */
 @ApiStatus.Obsolete
-public class PlayerChangedMainHandEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final MainHand newMainHand;
-
-    @ApiStatus.Internal
-    public PlayerChangedMainHandEvent(@NotNull Player player, @NotNull MainHand newMainHand) {
-        super(player);
-        this.newMainHand = newMainHand;
-    }
+public interface PlayerChangedMainHandEvent extends PlayerEventNew {
 
     /**
      * Gets the new main hand of the player. The old hand is still momentarily
@@ -34,30 +23,20 @@ public class PlayerChangedMainHandEvent extends PlayerEvent {
      * The method is left in this broken state to not break compatibility with plugins that relied on this fact.
      * Use {@link #getNewMainHand()} instead or migrate to {@link PlayerClientOptionsChangeEvent#getMainHand()}.
      */
-    @NotNull
     @Deprecated(since = "1.21.4", forRemoval = true)
-    public MainHand getMainHand() {
-        return this.newMainHand == MainHand.LEFT ? MainHand.RIGHT : MainHand.LEFT;
-    }
+    MainHand getMainHand();
 
     /**
      * Gets the new main hand of the player.
      *
      * @return the new {@link MainHand} of the player
      */
-    @NotNull
-    public MainHand getNewMainHand() {
-        return this.newMainHand;
-    }
+    MainHand getNewMainHand();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
