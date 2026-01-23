224223b5b3fc322c9d5c45518158abf771e36a74
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-23 17:24:11 +0100

    PlayerDropItemEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerDropItemEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerDropItemEvent.java
@@ -1,56 +1,25 @@
 package org.bukkit.event.player;
 
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Thrown when a player drops an item from their inventory
  */
-public class PlayerDropItemEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Item drop;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerDropItemEvent(@NotNull final Player player, @NotNull final Item drop) {
-        super(player);
-        this.drop = drop;
-    }
+public interface PlayerDropItemEvent extends PlayerEventNew, Cancellable {
 
     /**
-     * Gets the ItemDrop created by the player
+     * Gets the item dropped by the player.
      *
-     * @return ItemDrop created by the player
+     * @return the item dropped by the player
      */
-    @NotNull
-    public Item getItemDrop() {
-        return this.drop;
-    }
+    Item getItemDrop();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
