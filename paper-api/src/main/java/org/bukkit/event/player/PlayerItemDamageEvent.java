812af4445d675cd47dfccf7df25aa3d5f33d1780
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-24 20:25:40 +0100

    PlayerItemDamageEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerItemDamageEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerItemDamageEvent.java
@@ -1,62 +1,31 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when an item used by the player takes durability damage as a result of
  * being used.
  */
-public class PlayerItemDamageEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ItemStack item;
-    private final int originalDamage;
-    private int damage;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    @Deprecated(forRemoval = true)
-    public PlayerItemDamageEvent(@NotNull Player player, @NotNull ItemStack item, int damage) {
-        this(player, item, damage, damage);
-    }
-
-    @ApiStatus.Internal
-    public PlayerItemDamageEvent(@NotNull Player player, @NotNull ItemStack item, int damage, int originalDamage) {
-        super(player);
-        this.item = item;
-        this.damage = damage;
-        this.originalDamage = originalDamage;
-    }
+public interface PlayerItemDamageEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the item being damaged.
      *
      * @return the item
      */
-    @NotNull
-    public ItemStack getItem() {
-        return this.item;
-    }
+    ItemStack getItem();
 
     /**
      * Gets the amount of durability damage this item will be taking.
      *
      * @return durability change
      */
-    public int getDamage() {
-        return this.damage;
-    }
+    int getDamage();
 
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
+    // todo javadocs?
+    void setDamage(int damage);
 
     /**
      * Gets the amount of durability damage this item would have taken before
@@ -65,28 +34,12 @@ public class PlayerItemDamageEvent extends PlayerEvent implements Cancellable {
      *
      * @return pre-reduction damage amount
      */
-    public int getOriginalDamage() {
-        return this.originalDamage;
-    }
+    int getOriginalDamage();
 
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
