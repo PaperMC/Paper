a76f414db2809431f25dcf97cdb12163f2b8c1c3
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-24 20:31:03 +0100

    PlayerItemHeldEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerItemHeldEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerItemHeldEvent.java
@@ -1,66 +1,31 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Fired when a player changes their currently held item
  */
-public class PlayerItemHeldEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final int previous;
-    private final int current;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerItemHeldEvent(@NotNull final Player player, final int previous, final int current) {
-        super(player);
-        this.previous = previous;
-        this.current = current;
-    }
+public interface PlayerItemHeldEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the previous held slot index
      *
      * @return Previous slot index
      */
-    public int getPreviousSlot() {
-        return this.previous;
-    }
+    int getPreviousSlot();
 
     /**
      * Gets the new held slot index
      *
      * @return New slot index
      */
-    public int getNewSlot() {
-        return this.current;
-    }
+    int getNewSlot();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
