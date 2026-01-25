937f99f70e8277901b33bb945f8c695d25c5bd4a
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-25 22:47:14 +0100

    PlayerPickupItemEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerPickupArrowEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerPickupArrowEvent.java
@@ -1,31 +1,16 @@
 package org.bukkit.event.player;
 
 import org.bukkit.entity.AbstractArrow;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Thrown when a player picks up an arrow from the ground.
  */
-public class PlayerPickupArrowEvent extends PlayerPickupItemEvent {
-
-    private final AbstractArrow arrow;
-
-    @ApiStatus.Internal
-    public PlayerPickupArrowEvent(@NotNull final Player player, @NotNull final Item item, @NotNull final AbstractArrow arrow) {
-        super(player, item, 0);
-        this.arrow = arrow;
-    }
+public interface PlayerPickupArrowEvent extends PlayerPickupItemEvent {
 
     /**
      * Get the arrow being picked up by the player
      *
      * @return The arrow being picked up
      */
-    @NotNull
-    public AbstractArrow getArrow() {
-        return this.arrow;
-    }
+    AbstractArrow getArrow();
 }
