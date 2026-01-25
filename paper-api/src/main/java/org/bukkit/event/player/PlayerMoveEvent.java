961652e5542f6afea237ef7257b131fb05279cf5
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-25 22:36:18 +0100

    PlayerMoveEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerMoveEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerMoveEvent.java
@@ -1,117 +1,76 @@
 package org.bukkit.event.player;
 
-import com.google.common.base.Preconditions;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Holds information for player movement events
  */
-public class PlayerMoveEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private Location from;
-    private Location to;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerMoveEvent(@NotNull final Player player, @NotNull final Location from, @Nullable final Location to) {
-        super(player);
-        this.from = from;
-        this.to = to;
-    }
+public interface PlayerMoveEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the location this player moved from
      *
      * @return Location the player moved from
      */
-    @NotNull
-    public Location getFrom() {
-        return this.from;
-    }
+    Location getFrom();
 
     /**
      * Sets the location to mark as where the player moved from
      *
      * @param from New location to mark as the players previous location
      */
-    public void setFrom(@NotNull Location from) {
-        this.validateLocation(from);
-        this.from = from.clone();
-    }
+    void setFrom(Location from);
 
     /**
      * Gets the location this player moved to
      *
      * @return Location the player moved to
      */
-    @NotNull // Paper
-    public Location getTo() {
-        return to;
-    }
+    Location getTo();
 
     /**
      * Sets the location that this player will move to
      *
      * @param to New Location this player will move to
      */
-    public void setTo(@NotNull Location to) {
-        this.validateLocation(to);
-        this.to = to.clone();
-    }
+    void setTo(Location to);
 
     /**
      * Check if the player has changed position (even within the same block) in the event
      *
      * @return whether the player has changed position or not
      */
-    public boolean hasChangedPosition() {
-        return this.hasExplicitlyChangedPosition() || !this.from.getWorld().equals(this.to.getWorld());
-    }
+    boolean hasChangedPosition();
 
     /**
      * Check if the player has changed position (even within the same block) in the event, disregarding a possible world change
      *
      * @return whether the player has changed position or not
      */
-    public boolean hasExplicitlyChangedPosition() {
-        return this.from.getX() != this.to.getX() || this.from.getY() != this.to.getY() || this.from.getZ() != this.to.getZ();
-    }
+    boolean hasExplicitlyChangedPosition();
 
     /**
      * Check if the player has moved to a new block in the event
      *
      * @return whether the player has moved to a new block or not
      */
-    public boolean hasChangedBlock() {
-        return this.hasExplicitlyChangedBlock() || !this.from.getWorld().equals(this.to.getWorld());
-    }
+    boolean hasChangedBlock();
 
     /**
      * Check if the player has moved to a new block in the event, disregarding a possible world change
      *
      * @return whether the player has moved to a new block or not
      */
-    public boolean hasExplicitlyChangedBlock() {
-        return this.from.getBlockX() != this.to.getBlockX() || this.from.getBlockY() != this.to.getBlockY() || this.from.getBlockZ() != this.to.getBlockZ();
-    }
+    boolean hasExplicitlyChangedBlock();
 
     /**
      * Check if the player has changed orientation in the event
      *
      * @return whether the player has changed orientation or not
      */
-    public boolean hasChangedOrientation() {
-        return this.from.getPitch() != this.to.getPitch() || this.from.getYaw() != this.to.getYaw();
-    }
+    boolean hasChangedOrientation();
 
     /**
      * {@inheritDoc}
@@ -121,9 +80,7 @@ public class PlayerMoveEvent extends PlayerEvent implements Cancellable {
      * fire an event
      */
     @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    boolean isCancelled();
 
     /**
      * {@inheritDoc}
@@ -133,23 +90,12 @@ public class PlayerMoveEvent extends PlayerEvent implements Cancellable {
      * fire an event
      */
     @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
+    void setCancelled(boolean cancel);
 
-    private void validateLocation(@NotNull Location loc) {
-        Preconditions.checkArgument(loc != null, "Cannot use null location!");
-        Preconditions.checkArgument(loc.getWorld() != null, "Cannot use null location with null world!");
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
