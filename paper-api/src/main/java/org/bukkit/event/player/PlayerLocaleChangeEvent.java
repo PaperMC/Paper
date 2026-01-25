9807f41355fcf877699cab231af034beb01de3ee
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-25 21:07:30 +0100

    PlayerLocaleChangeEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerLocaleChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerLocaleChangeEvent.java
@@ -1,57 +1,32 @@
 package org.bukkit.event.player;
 
-import net.kyori.adventure.translation.Translator;
+import java.util.Locale;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Called when a player changes their locale in the client settings.
  */
-public class PlayerLocaleChangeEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final String locale;
-    private final Locale adventure$locale;
-
-    @ApiStatus.Internal
-    public PlayerLocaleChangeEvent(@NotNull Player player, @NotNull String locale) {
-        super(player);
-        this.locale = locale;
-        this.adventure$locale = Objects.requireNonNullElse(Translator.parseLocale(locale), Locale.US);
-    }
+public interface PlayerLocaleChangeEvent extends PlayerEventNew {
 
     /**
      * @return the player's new locale
      * @see Player#getLocale()
      * @deprecated in favour of {@link #locale()}
      */
-    @NotNull
     @Deprecated
-    public String getLocale() {
-        return this.locale;
-    }
+    String getLocale();
 
     /**
      * @see Player#locale()
      * @return the player's new locale
      */
-    public @NotNull Locale locale() {
-        return this.adventure$locale;
-    }
+    Locale locale();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
