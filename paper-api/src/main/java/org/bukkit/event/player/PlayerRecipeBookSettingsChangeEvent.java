b8253f0c2c9eecd90008d765c5d4bdf103613e04
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-01-26 21:56:27 +0100

    PlayerRecipeBookSettingsChangeEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerRecipeBookSettingsChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerRecipeBookSettingsChangeEvent.java
@@ -1,66 +1,38 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player changes recipe book settings.
  */
-public class PlayerRecipeBookSettingsChangeEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final RecipeBookType recipeBookType;
-    private final boolean open;
-    private final boolean filtering;
-
-    @ApiStatus.Internal
-    public PlayerRecipeBookSettingsChangeEvent(@NotNull final Player player, @NotNull final RecipeBookType recipeBookType, final boolean open, final boolean filtering) {
-        super(player);
-        this.recipeBookType = recipeBookType;
-        this.open = open;
-        this.filtering = filtering;
-    }
+public interface PlayerRecipeBookSettingsChangeEvent extends PlayerEventNew {
 
     /**
      * Gets the type of recipe book the player is changing the settings for.
      *
      * @return the type of recipe book
      */
-    @NotNull
-    public RecipeBookType getRecipeBookType() {
-        return this.recipeBookType;
-    }
+    RecipeBookType getRecipeBookType();
 
     /**
      * Checks if the recipe book is being opened or closed.
      *
      * @return {@code true} if opening
      */
-    public boolean isOpen() {
-        return this.open;
-    }
+    boolean isOpen();
 
     /**
      * Checks if the recipe book filter is being enabled or disabled.
      *
      * @return {@code true} if enabling
      */
-    public boolean isFiltering() {
-        return this.filtering;
-    }
+    boolean isFiltering();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 
     /**
@@ -68,7 +40,7 @@ public class PlayerRecipeBookSettingsChangeEvent extends PlayerEvent {
      * <br>
      * Different types of recipe book are shown in different GUIs.
      */
-    public enum RecipeBookType {
+    enum RecipeBookType {
 
         /**
          * Recipe book seen in crafting table and player inventory.
@@ -85,6 +57,6 @@ public class PlayerRecipeBookSettingsChangeEvent extends PlayerEvent {
         /**
          * Recipe book seen in smoker.
          */
-        SMOKER;
+        SMOKER
     }
 }
