f3c4363e7bd53ecca846b9dc764c2e240fe4d975
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-05 19:21:46 +0100

    PlayerToggleFlightEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerToggleFlightEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerToggleFlightEvent.java
@@ -1,54 +1,24 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player toggles their flying state
  */
-public class PlayerToggleFlightEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final boolean isFlying;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerToggleFlightEvent(@NotNull final Player player, final boolean isFlying) {
-        super(player);
-        this.isFlying = isFlying;
-    }
+public interface PlayerToggleFlightEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Returns whether the player is trying to start or stop flying.
      *
      * @return flying state
      */
-    public boolean isFlying() {
-        return this.isFlying;
-    }
+    boolean isFlying();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
