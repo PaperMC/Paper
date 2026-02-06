bb1a066e5e8903df240e136864e1f6449c97d524
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-06 20:02:33 +0100

    PlayerAdvancementCriterionGrantEvent

--- paper-api/src/main/java/com/destroystokyo/paper/event/player/PlayerAdvancementCriterionGrantEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/event/player/PlayerAdvancementCriterionGrantEvent.java
@@ -2,11 +2,9 @@ package com.destroystokyo.paper.event.player;
 
 import org.bukkit.advancement.Advancement;
 import org.bukkit.advancement.AdvancementProgress;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
-import org.jetbrains.annotations.ApiStatus;
+import org.bukkit.event.player.PlayerEventNew;
 import org.jspecify.annotations.NullMarked;
 
 /**
@@ -14,67 +12,33 @@ import org.jspecify.annotations.NullMarked;
  * If cancelled the criteria will be revoked.
  */
 @NullMarked
-public class PlayerAdvancementCriterionGrantEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Advancement advancement;
-    private final String criterion;
-    private final AdvancementProgress advancementProgress;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerAdvancementCriterionGrantEvent(final Player player, final Advancement advancement, final String criterion) {
-        super(player);
-        this.advancement = advancement;
-        this.criterion = criterion;
-        this.advancementProgress = player.getAdvancementProgress(advancement);
-    }
+public interface PlayerAdvancementCriterionGrantEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Get the advancement which has been affected.
      *
      * @return affected advancement
      */
-    public Advancement getAdvancement() {
-        return this.advancement;
-    }
+    Advancement getAdvancement();
 
     /**
      * Get the criterion which has been granted.
      *
      * @return granted criterion
      */
-    public String getCriterion() {
-        return this.criterion;
-    }
+    String getCriterion();
 
     /**
-     * Gets the current AdvancementProgress.
+     * Gets the current {@link AdvancementProgress}.
      *
      * @return advancement progress
      */
-    public AdvancementProgress getAdvancementProgress() {
-        return this.advancementProgress;
-    }
+    AdvancementProgress getAdvancementProgress();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
