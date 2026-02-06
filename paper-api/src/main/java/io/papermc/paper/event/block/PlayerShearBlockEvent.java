3052b61131886fec15ed3e4180b1931d2d77b353
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-06 20:02:36 +0100

    PlayerShearBlockEvent

--- paper-api/src/main/java/io/papermc/paper/event/block/PlayerShearBlockEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/block/PlayerShearBlockEvent.java
@@ -2,13 +2,11 @@ package io.papermc.paper.event.block;
 
 import java.util.List;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerEventNew;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 /**
@@ -20,61 +18,35 @@ import org.jspecify.annotations.NullMarked;
  * Examples include shearing a pumpkin to turn it into a carved pumpkin or shearing a beehive to get honeycomb.
  */
 @NullMarked
-public class PlayerShearBlockEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Block block;
-    private final ItemStack item;
-    private final EquipmentSlot hand;
-    private final List<ItemStack> drops;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerShearBlockEvent(final Player player, final Block block, final ItemStack item, final EquipmentSlot hand, final List<ItemStack> drops) {
-        super(player);
-        this.block = block;
-        this.item = item;
-        this.hand = hand;
-        this.drops = drops;
-    }
+public interface PlayerShearBlockEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the block being sheared in this event.
      *
      * @return The {@link Block} which block is being sheared in this event.
      */
-    public Block getBlock() {
-        return this.block;
-    }
+    Block getBlock();
 
     /**
      * Gets the item used to shear the block.
      *
      * @return The {@link ItemStack} of the shears.
      */
-    public ItemStack getItem() {
-        return this.item;
-    }
+    ItemStack getItem();
 
     /**
      * Gets the hand used to shear the block.
      *
      * @return Either {@link EquipmentSlot#HAND} OR {@link EquipmentSlot#OFF_HAND}.
      */
-    public EquipmentSlot getHand() {
-        return this.hand;
-    }
+    EquipmentSlot getHand();
 
     /**
      * Gets the resulting drops of this event.
      *
      * @return A mutable {@link List list} of {@link ItemStack items} that will be dropped as result of this event.
      */
-    public List<ItemStack> getDrops() {
-        return this.drops;
-    }
+    List<ItemStack> getDrops();
 
     /**
      * Gets whether the shearing of the block should be cancelled or not.
@@ -82,9 +54,7 @@ public class PlayerShearBlockEvent extends PlayerEvent implements Cancellable {
      * @return Whether the shearing of the block should be cancelled or not.
      */
     @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    boolean isCancelled();
 
     /**
      * Sets whether the shearing of the block should be cancelled or not.
@@ -92,16 +62,12 @@ public class PlayerShearBlockEvent extends PlayerEvent implements Cancellable {
      * @param cancel whether the shearing of the block should be cancelled or not.
      */
     @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
+    void setCancelled(boolean cancel);
 
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
