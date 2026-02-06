ad45c842405e882c8114dc502981943220160f2a
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2026-02-06 14:48:11 -0500

    Bye treetype field

--- paper-server/src/main/java/io/papermc/paper/util/capture/BoneMealContext.java
+++ paper-server/src/main/java/io/papermc/paper/util/capture/BoneMealContext.java
@@ -2,6 +2,7 @@ package io.papermc.paper.util.capture;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.TreeType;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.Nullable;
 
@@ -12,7 +13,7 @@ public class BoneMealContext {
     public ServerLevel ogServerLevel;
     public ServerPlayer player;
     public boolean precancelStructureEvent = false;
-    public java.util.concurrent.atomic.AtomicReference<org.bukkit.TreeType> treeHook = new AtomicReference<>(); // just make this a field
+    public TreeType treeHook; // just make this a field
 
 
     @Nullable
