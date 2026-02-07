ec9c8e22bb166973d8e2047681c4672990a3130a
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-07 21:01:50 +0100

    CompostItemEvent

--- paper-api/src/main/java/io/papermc/paper/event/block/CompostItemEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/block/CompostItemEvent.java
@@ -1,11 +1,9 @@
 package io.papermc.paper.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.block.BlockEventNew;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 /**
@@ -13,54 +11,33 @@ import org.jspecify.annotations.NullMarked;
  * To prevent hoppers from moving items into composters, cancel the {@link InventoryMoveItemEvent}.
  */
 @NullMarked
-public class CompostItemEvent extends BlockEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ItemStack item;
-    private boolean willRaiseLevel;
-
-    @ApiStatus.Internal
-    public CompostItemEvent(final Block composter, final ItemStack item, final boolean willRaiseLevel) {
-        super(composter);
-        this.item = item;
-        this.willRaiseLevel = willRaiseLevel;
-    }
+public interface CompostItemEvent extends BlockEventNew {
 
     /**
      * Gets the item that was used on the composter.
      *
      * @return the item
      */
-    public ItemStack getItem() {
-        return this.item;
-    }
+    ItemStack getItem();
 
     /**
      * Gets whether the composter will rise a level.
      *
      * @return {@code true} if successful
      */
-    public boolean willRaiseLevel() {
-        return this.willRaiseLevel;
-    }
+    boolean willRaiseLevel();
 
     /**
      * Sets whether the composter will rise a level.
      *
      * @param willRaiseLevel {@code true} if the composter should rise a level
      */
-    public void setWillRaiseLevel(final boolean willRaiseLevel) {
-        this.willRaiseLevel = willRaiseLevel;
-    }
+    void setWillRaiseLevel(boolean willRaiseLevel);
 
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
-    }
-
 }
