50a6975045c5e70cdf20075dcace40acb324b023
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-07 21:01:48 +0100

    BellRevealRaiderEvent

--- paper-api/src/main/java/io/papermc/paper/event/block/BellRevealRaiderEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/block/BellRevealRaiderEvent.java
@@ -1,12 +1,9 @@
 package io.papermc.paper.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.entity.Raider;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.block.BlockEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.event.block.BlockEventNew;
 
 /**
  * Called when a {@link Raider} is revealed by a bell.
@@ -14,33 +11,14 @@ import org.jetbrains.annotations.NotNull;
  * @deprecated use {@link org.bukkit.event.block.BellResonateEvent}
  */
 @Deprecated(since = "1.19.4")
-public class BellRevealRaiderEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Raider raider;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public BellRevealRaiderEvent(@NotNull Block bell, @NotNull Raider raider) {
-        super(bell);
-        this.raider = raider;
-    }
+public interface BellRevealRaiderEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the raider that the bell revealed.
      *
      * @return The raider
      */
-    @NotNull
-    public Raider getEntity() {
-        return this.raider;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    Raider getEntity();
 
     /**
      * {@inheritDoc}
@@ -48,16 +26,12 @@ public class BellRevealRaiderEvent extends BlockEvent implements Cancellable {
      * This does not cancel the particle effects shown on the bell, only the entity.
      */
     @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
+    void setCancelled(boolean cancel);
 
-    @Override
-    public @NotNull HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static @NotNull HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
