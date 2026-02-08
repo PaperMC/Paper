a66b76b26d4c05d6e6aa7584fc4a6071d6fac0ec
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-02-08 16:38:11 -0700

    Update for endermux changes

--- paper-server/src/main/java/io/papermc/paper/console/endermux/PaperCommandExecutor.java
+++ paper-server/src/main/java/io/papermc/paper/console/endermux/PaperCommandExecutor.java
@@ -2,7 +2,6 @@ package io.papermc.paper.console.endermux;
 
 import net.minecraft.server.dedicated.DedicatedServer;
 import org.jspecify.annotations.NullMarked;
-import xyz.jpenilla.endermux.protocol.Payloads;
 import xyz.jpenilla.endermux.server.api.InteractiveConsoleHooks;
 
 @NullMarked
@@ -14,8 +13,7 @@ public final class PaperCommandExecutor implements InteractiveConsoleHooks.Comma
     }
 
     @Override
-    public Payloads.CommandResponse execute(final String command) {
+    public void execute(final String command) {
         this.server.handleConsoleInput(command, this.server.createCommandSourceStack());
-        return new Payloads.CommandResponse(Payloads.CommandResponse.Status.EXECUTED, command);
     }
 }
