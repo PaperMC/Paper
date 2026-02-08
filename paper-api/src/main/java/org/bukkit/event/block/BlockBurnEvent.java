97bfec5c07a11cb91879561b3878ee90bddfc29c
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-08 20:32:06 +0100

    BlockBurnEvent

--- paper-api/src/main/java/org/bukkit/event/block/BlockBurnEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockBurnEvent.java
@@ -3,9 +3,7 @@ package org.bukkit.event.block;
 import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Called when a block is destroyed as a result of being burnt by fire.
@@ -13,24 +11,7 @@ import org.jetbrains.annotations.Nullable;
  * If this event is cancelled, the block will not be destroyed as a
  * result of being burnt by fire.
  */
-public class BlockBurnEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Block ignitingBlock;
-    private boolean cancelled;
-
-    @Deprecated(since = "1.11.2", forRemoval = true)
-    @ApiStatus.Internal
-    public BlockBurnEvent(@NotNull final Block block) {
-        this(block, null);
-    }
-
-    @ApiStatus.Internal
-    public BlockBurnEvent(@NotNull final Block block, @Nullable final Block ignitingBlock) {
-        super(block);
-        this.ignitingBlock = ignitingBlock;
-    }
+public interface BlockBurnEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the block which ignited this block.
@@ -38,29 +19,12 @@ public class BlockBurnEvent extends BlockEvent implements Cancellable {
      * @return The Block that ignited and burned this block, or {@code null} if no
      * source block exists
      */
-    @Nullable
-    public Block getIgnitingBlock() {
-        return this.ignitingBlock;
-    }
+    @Nullable Block getIgnitingBlock();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
