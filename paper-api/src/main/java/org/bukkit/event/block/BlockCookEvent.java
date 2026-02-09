52e840f67ba51cc701bf8b45c635256aee752a95
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-09 20:20:01 +0100

    BlockCookEvent

--- paper-api/src/main/java/org/bukkit/event/block/BlockCookEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockCookEvent.java
@@ -1,98 +1,48 @@
 package org.bukkit.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.CookingRecipe;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
- * Called when an ItemStack is successfully cooked in a block.
+ * Called when an item is successfully cooked in a block.
  */
-public class BlockCookEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ItemStack source;
-    private ItemStack result;
-    private final CookingRecipe<?> recipe;
-
-    private boolean cancelled;
-
-    @Deprecated(forRemoval = true)
-    @ApiStatus.Internal
-    public BlockCookEvent(@NotNull final Block block, @NotNull final ItemStack source, @NotNull final ItemStack result) {
-        this(block, source, result, null);
-    }
-
-    @ApiStatus.Internal
-    public BlockCookEvent(@NotNull final Block block, @NotNull final ItemStack source, @NotNull final ItemStack result, @Nullable CookingRecipe<?> recipe) {
-        super(block);
-        this.source = source;
-        this.result = result;
-        this.recipe = recipe;
-    }
+public interface BlockCookEvent extends BlockEventNew, Cancellable {
 
     /**
-     * Gets the smelted ItemStack for this event
+     * Gets the smelted item for this event
      *
-     * @return smelting source ItemStack
+     * @return smelting source item
      */
-    @NotNull
-    public ItemStack getSource() {
-        return this.source;
-    }
+    ItemStack getSource();
 
     /**
-     * Gets the resultant ItemStack for this event
+     * Gets the resultant item for this event
      *
-     * @return smelting result ItemStack
+     * @return smelting result item
      */
-    @NotNull
-    public ItemStack getResult() {
-        return this.result;
-    }
+    ItemStack getResult();
 
     /**
-     * Sets the resultant ItemStack for this event
+     * Sets the resultant item for this event
      *
-     * @param result new result ItemStack
+     * @param result new result item
      */
-    public void setResult(@NotNull ItemStack result) {
-        this.result = result;
-    }
+    void setResult(ItemStack result);
 
     /**
      * Gets the cooking recipe associated with this event.
      *
      * @return the recipe
      */
-    @Nullable
-    public org.bukkit.inventory.CookingRecipe<?> getRecipe() {
-        return this.recipe;
-    }
+    @Nullable CookingRecipe<?> getRecipe();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
---
+++ paper-server/src/main/java/org/bukkit/craftbukkit/event/block/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package org.bukkit.craftbukkit.event.block;
+
+import org.jspecify.annotations.NullMarked;
