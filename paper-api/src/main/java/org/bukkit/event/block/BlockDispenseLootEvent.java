73af6c9f4546648ab1e7fadff5eb76af554e66e0
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-09 20:20:05 +0100

    BlockDispenseLootEvent

--- paper-api/src/main/java/org/bukkit/event/block/BlockDispenseLootEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockDispenseLootEvent.java
@@ -1,16 +1,12 @@
 package org.bukkit.event.block;
 
-import java.util.ArrayList;
 import java.util.List;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.loot.LootTable;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Called when a block dispenses loot from its designated LootTable.
@@ -21,52 +17,7 @@ import org.jetbrains.annotations.Nullable;
  * Example: A player unlocks a trial chamber vault and the vault block dispenses
  * its loot.
  */
-public class BlockDispenseLootEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Player player;
-    private List<ItemStack> dispensedLoot;
-    private final LootTable lootTable;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public BlockDispenseLootEvent(@Nullable Player player, @NotNull Block block, @NotNull List<ItemStack> dispensedLoot, @NotNull LootTable lootTable) {
-        super(block);
-        this.player = player;
-        this.dispensedLoot = dispensedLoot;
-        this.lootTable = lootTable;
-    }
-
-    /**
-     * Gets the loot that will be dispensed.
-     *
-     * @return the loot that will be dispensed
-     */
-    @NotNull
-    public List<ItemStack> getDispensedLoot() {
-        return this.dispensedLoot;
-    }
-
-    /**
-     * Sets the loot that will be dispensed.
-     *
-     * @param dispensedLoot new loot to dispense
-     */
-    public void setDispensedLoot(@Nullable List<ItemStack> dispensedLoot) {
-        this.dispensedLoot = dispensedLoot == null ? new ArrayList<>() : dispensedLoot;
-    }
-
-    /**
-     * Gets the loot table used to generate the initial loot to dispense.
-     *
-     * @return the loot table used to generate the initial loot to dispense
-     */
-    @NotNull
-    public LootTable getLootTable() {
-        return this.lootTable;
-    }
+public interface BlockDispenseLootEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the player associated with this event.
@@ -77,29 +28,33 @@ public class BlockDispenseLootEvent extends BlockEvent implements Cancellable {
      *
      * @return the player who unlocked the vault
      */
-    @Nullable
-    public Player getPlayer() {
-        return this.player;
-    }
+    @Nullable Player getPlayer();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    /**
+     * Gets the loot that will be dispensed.
+     *
+     * @return the loot that will be dispensed
+     */
+    List<ItemStack> getDispensedLoot();
 
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
+    /**
+     * Sets the loot that will be dispensed.
+     *
+     * @param dispensedLoot new loot to dispense
+     */
+    void setDispensedLoot(@Nullable List<ItemStack> dispensedLoot);
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
+    /**
+     * Gets the loot table used to generate the initial loot to dispense.
+     *
+     * @return the loot table used to generate the initial loot to dispense
+     */
+    LootTable getLootTable();
 
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
