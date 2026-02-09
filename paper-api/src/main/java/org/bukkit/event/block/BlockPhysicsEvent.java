eabb31b705cee0251b2da3fa74336308287c0e5d
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-09 20:20:07 +0100

    BlockPhysicsEvent

--- paper-api/src/main/java/org/bukkit/event/block/BlockPhysicsEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockPhysicsEvent.java
@@ -5,8 +5,6 @@ import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Thrown when a block physics check is called.
@@ -27,32 +25,7 @@ import org.jetbrains.annotations.NotNull;
  * event for each of those blocks (usually adjacent). If you are concerned about
  * monitoring these changes then you should check adjacent blocks yourself.
  */
-public class BlockPhysicsEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final BlockData changed;
-    private final Block sourceBlock;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    @Deprecated(forRemoval = true)
-    public BlockPhysicsEvent(final Block block, final BlockData changed, final int sourceX, final int sourceY, final int sourceZ) {
-        this(block, changed, block.getWorld().getBlockAt(sourceX, sourceY, sourceZ));
-    }
-
-    @ApiStatus.Internal
-    public BlockPhysicsEvent(@NotNull final Block block, @NotNull final BlockData changed) {
-        this(block, changed, block);
-    }
-
-    @ApiStatus.Internal
-    public BlockPhysicsEvent(@NotNull final Block block, @NotNull final BlockData changed, @NotNull final Block sourceBlock) {
-        super(block);
-        this.changed = changed;
-        this.sourceBlock = sourceBlock;
-    }
+public interface BlockPhysicsEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the source block that triggered this event.
@@ -60,10 +33,7 @@ public class BlockPhysicsEvent extends BlockEvent implements Cancellable {
      * @return The source block
      * @apiNote This will default to block if not set.
      */
-    @NotNull
-    public Block getSourceBlock() {
-        return this.sourceBlock;
-    }
+    Block getSourceBlock();
 
     /**
      * Gets the type of block that changed, causing this event.
@@ -71,10 +41,7 @@ public class BlockPhysicsEvent extends BlockEvent implements Cancellable {
      *
      * @return Changed block's type
      */
-    @NotNull
-    public Material getChangedType() {
-        return this.changed.getMaterial();
-    }
+    Material getChangedType();
 
     /**
      * Gets the BlockData of the block that changed, causing this event.
@@ -82,29 +49,12 @@ public class BlockPhysicsEvent extends BlockEvent implements Cancellable {
      *
      * @return Changed block's BlockData
      */
-    @NotNull
-    public BlockData getChangedBlockData() {
-        return this.changed.clone();
-    }
+    BlockData getChangedBlockData();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
