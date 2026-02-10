74b26d3473f12b17ac55de4d590978d2a0448449
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-10 20:22:49 +0100

    FluidLevelChangeEvent

--- paper-api/src/main/java/org/bukkit/event/block/FluidLevelChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/FluidLevelChangeEvent.java
@@ -1,39 +1,21 @@
 package org.bukkit.event.block;
 
-import com.google.common.base.Preconditions;
-import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when the fluid level of a block changes due to changes in adjacent
  * blocks.
  */
-public class FluidLevelChangeEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private BlockData newData;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public FluidLevelChangeEvent(@NotNull Block fluid, @NotNull BlockData newData) {
-        super(fluid);
-        this.newData = newData;
-    }
+public interface FluidLevelChangeEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the new data of the changed block.
      *
      * @return new data
      */
-    @NotNull
-    public BlockData getNewData() {
-        return this.newData;
-    }
+    BlockData getNewData();
 
     /**
      * Sets the new data of the changed block. Must be of the same Material as
@@ -41,31 +23,12 @@ public class FluidLevelChangeEvent extends BlockEvent implements Cancellable {
      *
      * @param newData the new data
      */
-    public void setNewData(@NotNull BlockData newData) {
-        Preconditions.checkArgument(newData != null, "newData null");
-        Preconditions.checkArgument(this.newData.getMaterial().equals(newData.getMaterial()), "Cannot change fluid type");
+    void setNewData(BlockData newData);
 
-        this.newData = newData.clone();
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
