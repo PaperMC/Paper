6381a69184337ee49baff79a5020aba980007e52
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-10 20:22:51 +0100

    BeaconEffectEvent

--- paper-api/src/main/java/com/destroystokyo/paper/event/block/BeaconEffectEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/event/block/BeaconEffectEvent.java
@@ -1,88 +1,48 @@
 package com.destroystokyo.paper.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.block.BlockEventNew;
 import org.bukkit.potion.PotionEffect;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when a beacon effect is being applied to a player.
  */
-@NullMarked
-public class BeaconEffectEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Player player;
-    private final boolean primary;
-    private PotionEffect effect;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public BeaconEffectEvent(final Block beacon, final PotionEffect effect, final Player player, final boolean primary) {
-        super(beacon);
-        this.effect = effect;
-        this.player = player;
-        this.primary = primary;
-    }
+public interface BeaconEffectEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the potion effect being applied.
      *
      * @return Potion effect
      */
-    public PotionEffect getEffect() {
-        return this.effect;
-    }
+    PotionEffect getEffect();
 
     /**
      * Sets the potion effect that will be applied.
      *
      * @param effect Potion effect
      */
-    public void setEffect(final PotionEffect effect) {
-        this.effect = effect;
-    }
+    void setEffect(PotionEffect effect);
 
     /**
      * Gets the player who the potion effect is being applied to.
      *
      * @return Affected player
      */
-    public Player getPlayer() {
-        return this.player;
-    }
+    Player getPlayer();
 
     /**
      * Gets whether the effect is a primary beacon effect.
      *
      * @return {@code true} if this event represents a primary effect
      */
-    public boolean isPrimary() {
-        return this.primary;
-    }
+    boolean isPrimary();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
