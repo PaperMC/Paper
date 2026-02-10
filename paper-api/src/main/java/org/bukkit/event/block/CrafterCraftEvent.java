c5de77dc5e9d1d0edcb4bcbc14c88761635e6324
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-10 20:22:49 +0100

    CrafterCraftEvent

--- paper-api/src/main/java/org/bukkit/event/block/CrafterCraftEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/CrafterCraftEvent.java
@@ -1,79 +1,40 @@
 package org.bukkit.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.CraftingRecipe;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Event called when a Crafter is about to craft an item.
  */
-public class CrafterCraftEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final CraftingRecipe recipe;
-    private ItemStack result;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public CrafterCraftEvent(@NotNull Block crafter, @NotNull CraftingRecipe recipe, @NotNull ItemStack result) {
-        super(crafter);
-        this.result = result;
-        this.recipe = recipe;
-    }
+public interface CrafterCraftEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the result for the craft.
      *
      * @return the result for the craft
      */
-    @NotNull
-    public ItemStack getResult() {
-        return this.result.clone();
-    }
+    ItemStack getResult();
 
     /**
      * Sets the result of the craft.
      *
      * @param result the result of the craft
      */
-    public void setResult(@NotNull ItemStack result) {
-        this.result = result.clone();
-    }
+    void setResult(ItemStack result);
 
     /**
      * Gets the recipe that was used to craft this item.
      *
      * @return the recipe that was used to craft this item
      */
-    @NotNull
-    public CraftingRecipe getRecipe() {
-        return this.recipe;
-    }
+    CraftingRecipe getRecipe();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
