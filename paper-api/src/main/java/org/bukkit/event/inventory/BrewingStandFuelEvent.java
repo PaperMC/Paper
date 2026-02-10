4014f7f36103c898f301f2783ebaf7deebaa2b0b
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-10 20:22:50 +0100

    BrewingStandFuelEvent

--- paper-api/src/main/java/org/bukkit/event/inventory/BrewingStandFuelEvent.java
+++ paper-api/src/main/java/org/bukkit/event/inventory/BrewingStandFuelEvent.java
@@ -1,43 +1,22 @@
 package org.bukkit.event.inventory;
 
-import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.block.BlockEventNew;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
- * Called when an ItemStack is about to increase the fuel level of a brewing
+ * Called when an item is about to increase the fuel level of a brewing
  * stand.
  */
-public class BrewingStandFuelEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ItemStack fuel;
-    private int fuelPower;
-    private boolean consuming = true;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public BrewingStandFuelEvent(@NotNull Block brewingStand, @NotNull ItemStack fuel, int fuelPower) {
-        super(brewingStand);
-        this.fuel = fuel;
-        this.fuelPower = fuelPower;
-    }
+public interface BrewingStandFuelEvent extends BlockEventNew, Cancellable {
 
     /**
-     * Gets the ItemStack of the fuel before the amount was subtracted.
+     * Gets the item of the fuel before the amount was subtracted.
      *
-     * @return the fuel ItemStack
+     * @return the fuel item
      */
-    @NotNull
-    public ItemStack getFuel() {
-        return this.fuel;
-    }
+    ItemStack getFuel();
 
     /**
      * Gets the fuel power for this fuel. Each unit of power can fuel one
@@ -45,9 +24,7 @@ public class BrewingStandFuelEvent extends BlockEvent implements Cancellable {
      *
      * @return the fuel power for this fuel
      */
-    public int getFuelPower() {
-        return this.fuelPower;
-    }
+    int getFuelPower();
 
     /**
      * Sets the fuel power for this fuel. Each unit of power can fuel one
@@ -55,46 +32,26 @@ public class BrewingStandFuelEvent extends BlockEvent implements Cancellable {
      *
      * @param fuelPower the fuel power for this fuel
      */
-    public void setFuelPower(int fuelPower) {
-        this.fuelPower = fuelPower;
-    }
+    void setFuelPower(int fuelPower);
 
     /**
      * Gets whether the brewing stand's fuel will be reduced / consumed or not.
      *
      * @return whether the fuel will be reduced or not
      */
-    public boolean isConsuming() {
-        return this.consuming;
-    }
+    boolean isConsuming();
 
     /**
      * Sets whether the brewing stand's fuel will be reduced / consumed or not.
      *
      * @param consuming whether the fuel will be reduced or not
      */
-    public void setConsuming(boolean consuming) {
-        this.consuming = consuming;
-    }
+    void setConsuming(boolean consuming);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
