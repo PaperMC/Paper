821d73982a805b82e0a7d28d9d26272c25d02b65
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-11 19:53:33 +0100

    WardenAngerChangeEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/WardenAngerChangeEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/WardenAngerChangeEvent.java
@@ -1,47 +1,25 @@
 package io.papermc.paper.event.entity;
 
-import com.google.common.base.Preconditions;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Warden;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Range;
-import org.jspecify.annotations.NullMarked;
+import org.bukkit.event.entity.EntityEventNew;
+import org.checkerframework.common.value.qual.IntRange;
 
 /**
  * Called when a Warden's anger level has changed due to another entity.
  * <p>
  * If the event is cancelled, the warden's anger level will not change.
  */
-@NullMarked
-public class WardenAngerChangeEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Entity target;
-    private final int oldAnger;
-    private int newAnger;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public WardenAngerChangeEvent(final Warden warden, final Entity target, final int oldAnger, final int newAnger) {
-        super(warden);
-        this.target = target;
-        this.oldAnger = oldAnger;
-        this.newAnger = newAnger;
-    }
+public interface WardenAngerChangeEvent extends EntityEventNew, Cancellable {
 
     /**
      * Gets the entity which triggered this anger update.
      *
      * @return triggering entity
      */
-    public Entity getTarget() {
-        return this.target;
-    }
+    Entity getTarget();
 
     /**
      * Gets the old anger level.
@@ -49,9 +27,7 @@ public class WardenAngerChangeEvent extends EntityEvent implements Cancellable {
      * @return old anger level
      * @see Warden#getAnger(Entity)
      */
-    public @Range(from = 0, to = 150) int getOldAnger() {
-        return this.oldAnger;
-    }
+    @IntRange(from = 0, to = 150) int getOldAnger();
 
     /**
      * Gets the new anger level resulting from this event.
@@ -59,9 +35,7 @@ public class WardenAngerChangeEvent extends EntityEvent implements Cancellable {
      * @return new anger level
      * @see Warden#getAnger(Entity)
      */
-    public @Range(from = 0, to = 150) int getNewAnger() {
-        return this.newAnger;
-    }
+    @IntRange(from = 0, to = 150) int getNewAnger();
 
     /**
      * Sets the new anger level resulting from this event.
@@ -72,32 +46,15 @@ public class WardenAngerChangeEvent extends EntityEvent implements Cancellable {
      * @throws IllegalArgumentException if newAnger is greater than 150
      * @see Warden#setAnger(Entity, int)
      */
-    public void setNewAnger(final @Range(from = 0, to = 150) int newAnger) {
-        Preconditions.checkArgument(newAnger <= 150, "newAnger must not be greater than 150");
-        this.newAnger = newAnger;
-    }
+    void setNewAnger(@IntRange(from = 0, to = 150) int newAnger);
 
     @Override
-    public Warden getEntity() {
-        return (Warden) super.getEntity();
-    }
+    Warden getEntity();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
