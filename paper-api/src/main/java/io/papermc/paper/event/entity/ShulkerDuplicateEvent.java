d5ad1c6758aa981e9589509546492278b64b4607
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-11 19:53:33 +0100

    ShulkerDuplicateEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/ShulkerDuplicateEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/ShulkerDuplicateEvent.java
@@ -3,28 +3,14 @@ package io.papermc.paper.event.entity;
 import org.bukkit.entity.Shulker;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
+import org.bukkit.event.entity.EntityEventNew;
 
 /**
  * Fired when a shulker duplicates itself by spawning a new shulker.
  * <p>
  * The event is fired prior to the newly created shulker, accessible via {@link #getEntity()}, being added to the world.
  */
-@NullMarked
-public class ShulkerDuplicateEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Shulker parent;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public ShulkerDuplicateEvent(final Shulker child, final Shulker parent) {
-        super(child);
-        this.parent = parent;
-    }
+public interface ShulkerDuplicateEvent extends EntityEventNew, Cancellable {
 
     /**
      * Provides the newly created shulker, which did not exist prior to the duplication.
@@ -33,9 +19,7 @@ public class ShulkerDuplicateEvent extends EntityEvent implements Cancellable {
      * @return the newly duplicated shulker.
      */
     @Override
-    public Shulker getEntity() {
-        return (Shulker) super.getEntity();
-    }
+    Shulker getEntity();
 
     /**
      * Provides the "parent" of the freshly created shulker.
@@ -43,26 +27,12 @@ public class ShulkerDuplicateEvent extends EntityEvent implements Cancellable {
      *
      * @return the previously existing shulker which duplicated.
      */
-    public Shulker getParent() {
-        return this.parent;
-    }
+    Shulker getParent();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
