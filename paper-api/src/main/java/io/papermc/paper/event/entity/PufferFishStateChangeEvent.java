bbd36ad410947ff955763054ae3eea676ed9b843
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-11 19:53:33 +0100

    PufferFishStateChangeEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/PufferFishStateChangeEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/PufferFishStateChangeEvent.java
@@ -3,31 +3,15 @@ package io.papermc.paper.event.entity;
 import org.bukkit.entity.PufferFish;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
+import org.bukkit.event.entity.EntityEventNew;
 
 /**
  * Called just before a {@link PufferFish} inflates or deflates.
  */
-@NullMarked
-public class PufferFishStateChangeEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final int newPuffState;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PufferFishStateChangeEvent(final PufferFish entity, final int newPuffState) {
-        super(entity);
-        this.newPuffState = newPuffState;
-    }
+public interface PufferFishStateChangeEvent extends EntityEventNew, Cancellable {
 
     @Override
-    public PufferFish getEntity() {
-        return (PufferFish) super.getEntity();
-    }
+    PufferFish getEntity();
 
     /**
      * Get the <strong>new</strong> puff state of the {@link PufferFish}.
@@ -37,27 +21,21 @@ public class PufferFishStateChangeEvent extends EntityEvent implements Cancellab
      *
      * @return The <strong>new</strong> puff state, 0 being not inflated, 1 being slightly inflated and 2 being fully inflated
      */
-    public int getNewPuffState() {
-        return this.newPuffState;
-    }
+    int getNewPuffState();
 
     /**
      * Get if the {@link PufferFish} is going to inflate.
      *
      * @return If it's going to inflate
      */
-    public boolean isInflating() {
-        return this.newPuffState > this.getEntity().getPuffState();
-    }
+    boolean isInflating();
 
     /**
      * Get if the {@link PufferFish} is going to deflate.
      *
      * @return If it's going to deflate
      */
-    public boolean isDeflating() {
-        return this.newPuffState < this.getEntity().getPuffState();
-    }
+    boolean isDeflating();
 
     /**
      * Set whether to cancel the {@link PufferFish} (in/de)flating.
@@ -65,21 +43,12 @@ public class PufferFishStateChangeEvent extends EntityEvent implements Cancellab
      * @param cancel {@code true} if you wish to cancel the (in/de)flation
      */
     @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
+    void setCancelled(boolean cancel);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
