db46692bf4ba7660204ec17f2ed695d3646bb353
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-02-11 19:53:30 +0100

    EntityDamageItemEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/EntityCompostItemEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/EntityCompostItemEvent.java
@@ -3,12 +3,10 @@ package io.papermc.paper.event.entity;
 import io.papermc.paper.event.block.CompostItemEvent;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
-import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when an item is about to be composted by an entity.
  */
-@NullMarked
 public interface EntityCompostItemEvent extends CompostItemEvent, Cancellable {
 
     /**
