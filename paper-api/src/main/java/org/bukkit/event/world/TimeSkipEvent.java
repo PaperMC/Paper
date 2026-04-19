86c93c55fa17cec5e42786e57123552609e57334
Nassim Jahnke <nassim@njahnke.dev>
2026-04-19 12:47:37 +0200

    Fix per-world time

--- paper-api/src/main/java/org/bukkit/event/world/TimeSkipEvent.java
+++ paper-api/src/main/java/org/bukkit/event/world/TimeSkipEvent.java
@@ -1,7 +1,6 @@
 package org.bukkit.event.world;
 
 import org.bukkit.World;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -11,59 +10,26 @@ import org.jetbrains.annotations.NotNull;
  * <p>
  * If the event is cancelled the time will not change.
  */
-// TODO - snapshot - 26.1 API needed for clock!
-public class TimeSkipEvent extends WorldEvent implements Cancellable {
+public class TimeSkipEvent extends ClockTimeSkipEvent {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
-    private final SkipReason skipReason;
-    private long skipAmount;
-
-    private boolean cancelled;
+    private final World world;
 
     @ApiStatus.Internal
     public TimeSkipEvent(@NotNull World world, @NotNull SkipReason skipReason, long skipAmount) {
-        super(world);
-        this.skipReason = skipReason;
-        this.skipAmount = skipAmount;
+        super(skipReason, skipAmount);
+        this.world = world;
     }
 
     /**
-     * Gets the reason why the time has skipped.
+     * Returns the world that time is skipped in.
      *
-     * @return a SkipReason value detailing why the time has skipped
+     * @return world that time is skipped in
      */
     @NotNull
-    public SkipReason getSkipReason() {
-        return this.skipReason;
-    }
-
-    /**
-     * Gets the amount of time that was skipped.
-     *
-     * @return Amount of time skipped
-     */
-    public long getSkipAmount() {
-        return this.skipAmount;
-    }
-
-    /**
-     * Sets the amount of time to skip.
-     *
-     * @param skipAmount Amount of time to skip
-     */
-    public void setSkipAmount(long skipAmount) {
-        this.skipAmount = skipAmount;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
+    public World getWorld() {
+        return world;
     }
 
     @NotNull
@@ -75,25 +41,5 @@ public class TimeSkipEvent extends WorldEvent implements Cancellable {
     @NotNull
     public static HandlerList getHandlerList() {
         return HANDLER_LIST;
-    }
-
-    /**
-     * An enum specifying the reason the time skipped.
-     */
-    public enum SkipReason {
-
-        /**
-         * When time is changed using the vanilla /time command.
-         */
-        COMMAND,
-        /**
-         * When time is changed by a plugin.
-         */
-        CUSTOM,
-        /**
-         * When time is changed by all players sleeping in their beds and the
-         * night skips.
-         */
-        NIGHT_SKIP
     }
 }
