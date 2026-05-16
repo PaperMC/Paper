2186e1e407ade9d303ef9def09245393c0b88ff1
Shane Freeder <theboyetronic@gmail.com>
2026-05-16 23:31:38 +0100

    Fix BlockRecieveGameEvent destination block

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,6 +1,12 @@
 package io.papermc.testplugin;
 
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
@@ -10,5 +16,23 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        if (e.getHand() == EquipmentSlot.HAND && e.getItem() != null && e.getItem().getType() == org.bukkit.Material.DIAMOND_SWORD) {
+            e.getPlayer().getLastTwoTargetBlocks(null, 5).forEach(block -> {
+                e.getPlayer().sendMessage(block.getLocation().toString());
+            });
+        }
+    }
+
+
+    @EventHandler
+    public void pistonPushEvent(BlockPistonExtendEvent e) {
+        e.getBlocks().forEach(block -> {
+            block.getRelative(e.getDirection()).getRelative(BlockFace.UP).setType(Material.BLUE_STAINED_GLASS);
+            System.out.println(block);
+        });
     }
 }
