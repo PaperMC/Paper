fd45f4b2e38a1b0ea18dee6fc8a8b420e9b069e1
Bjarne Koll <git@lynxplay.dev>
2026-05-22 11:45:39 +0200

    Upgrade to vineflower 12.0.0 (#13882)
    Co-authored-by: Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:26.1.2+build.1")
+    mache("io.papermc:mache:26.1.2+build.2")
     paperclip("io.papermc:paperclip:3.0.4")
 }
 
@@ -23,7 +23,7 @@ paperweight {
     gitFilePatches = false
 
     updatingMinecraft {
-        // oldPaperCommit = "7e80cef5198561d0db53406127e5b8bc7af51577"
+        // oldPaperCommit = "711c5de2b05df39b72c44ff54e9f9381f8d153cb"
     }
 }
 
