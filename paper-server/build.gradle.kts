d4fe85375af18bfa88f44d7c1e6a61904ae550cc
Bjarne Koll <git@lynxplay.dev>
2026-06-02 20:08:53 +0200

    [ci skip] Move paperweight and mache to non snapshots
    Paper was previously depending on snapshots of paperweight and,
    indirectly through the mache build, sculptor and codebook.
    
    As we prepare work on 26.2, a new snapshot cycle for some of these tools
    is required and 26.1.x cannot keep depending on them.

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:26.1.2+build.2")
+    mache("io.papermc:mache:26.1.2+build.3")
     paperclip("io.papermc:paperclip:3.0.4")
 }
 
