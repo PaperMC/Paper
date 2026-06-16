59797420d003e551521c1ddf071195b1c58f4624
Nassim Jahnke <nassim@njahnke.dev>
2026-06-16 17:14:35 +0200

    26.2

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:26.2-rc-2+build.2")
+    mache("io.papermc:mache:26.2+build.1")
     paperclip("io.papermc:paperclip:3.0.4")
 }
 
