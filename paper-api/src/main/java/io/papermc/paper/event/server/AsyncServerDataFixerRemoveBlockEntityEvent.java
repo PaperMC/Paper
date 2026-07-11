23b9ae577646d0fc4f1c7fbf1442c37e69ba157e
Bjarne Koll <git@lynxplay.dev>
2026-07-11 10:37:48 +0100

    Javadocs

--- paper-api/src/main/java/io/papermc/paper/event/server/AsyncServerDataFixerRemoveBlockEntityEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/server/AsyncServerDataFixerRemoveBlockEntityEvent.java
@@ -6,51 +6,77 @@ import net.kyori.adventure.key.Key;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.NullMarked;
 
-// TODO javadocs
+/**
+ * This event is emitted by the server when the data fixer is removing a block entity associated with a block
+ * due to the block entity type no longer existing in this minecraft version.
+ * <p>
+ * Notable examples include the removal of the bed block entity in 26.2.
+ * <p>
+ * <b>WARNING</b>: While this event is prefixed with 'Async' it is executed as part of the chunk loading process.
+ * It may hence either execute on the chunk loading worker threads or the servers main thread, if a chunk load
+ * is executed there. In either case, heavy or blocking work is <b>strongly</b> discouraged to ensure the
+ * server runs smoothly. Schedule large amount of work into separate thread pools.
+ */
 @NullMarked
 public class AsyncServerDataFixerRemoveBlockEntityEvent extends Event {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
     private final Key worldKey;
-    private final Key blockEntityId;
+    private final Key blockEntityType;
     private final BlockPosition blockPosition;
     private final PersistentDataContainerView persistentDataContainerView;
 
+    @ApiStatus.Internal
     public AsyncServerDataFixerRemoveBlockEntityEvent(
         final Key worldKey,
-        final Key blockEntityId,
+        final Key blockEntityType,
         final BlockPosition blockPosition,
         final PersistentDataContainerView persistentDataContainerView
     ) {
         super(!Bukkit.isPrimaryThread());
         this.worldKey = worldKey;
-        this.blockEntityId = blockEntityId;
+        this.blockEntityType = blockEntityType;
         this.blockPosition = blockPosition;
         this.persistentDataContainerView = persistentDataContainerView;
     }
 
-    public Key getBlockEntityId() {
-        return blockEntityId;
+    /**
+     * {@return the key representing the no longer existing block entity type.}
+     */
+    public Key getBlockEntityType() {
+        return blockEntityType;
     }
 
+    /**
+     * {@return the key of the world this block entity was removed from.}
+     *
+     * @see org.bukkit.Server#getWorld(Key)
+     */
     public Key getWorldKey() {
         return worldKey;
     }
 
+    /**
+     * {@return the position of the block entity that was removed.}
+     */
     public BlockPosition getBlockPosition() {
         return blockPosition;
     }
 
+    /**
+     * {@return an immutable view of the persistent data container that was attached to the removed block entity.}
+     */
     public PersistentDataContainerView getPersistentDataContainerView() {
         return persistentDataContainerView;
     }
 
     @Override
-    public @NotNull HandlerList getHandlers() {
+    public HandlerList getHandlers() {
         return HANDLER_LIST;
     }
 
