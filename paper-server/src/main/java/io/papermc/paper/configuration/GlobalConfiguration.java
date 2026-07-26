d5c888a77d3b5e520d7d87179403e70fd521021e
𝑩𝒊𝒒𝒖𝒂𝒕𝒆𝒓𝒏𝒊𝒐𝒏𝒔 <biquaternions@serlith.net>
2026-07-26 03:06:02 -0500

    Configurable TPS catchup (#14089)

--- paper-server/src/main/java/io/papermc/paper/configuration/GlobalConfiguration.java
+++ paper-server/src/main/java/io/papermc/paper/configuration/GlobalConfiguration.java
@@ -319,6 +319,8 @@ public class GlobalConfiguration extends ConfigurationPart {
             }
         }
         public int maxJoinsPerTick = 5;
+        @Constraints.Min(0)
+        public IntOr.Default catchupTicks = IntOr.Default.USE_DEFAULT;
         public boolean sendFullPosForItemEntities = false;
         public boolean loadPermissionsYmlBeforePlugins = true;
         @Constraints.Min(4)
