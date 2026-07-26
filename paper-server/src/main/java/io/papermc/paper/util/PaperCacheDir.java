9c842fb8af6ef95b3f8243514c5e565d59266ea8
Nassim Jahnke <nassim@njahnke.dev>
2026-07-26 14:00:12 +0200

    address reviews

--- paper-server/src/main/java/io/papermc/paper/util/PaperCacheDir.java
+++ paper-server/src/main/java/io/papermc/paper/util/PaperCacheDir.java
@@ -1,12 +1,15 @@
 package io.papermc.paper.util;
 
+import com.mojang.logging.LogUtils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.slf4j.Logger;
 
 public final class PaperCacheDir {
 
     private static final Path PATH = Path.of(".paper");
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public static Path get() {
         if (!Files.exists(PATH)) {
@@ -30,11 +33,23 @@ public final class PaperCacheDir {
         // Keep this for individual use until a more unified migration place is made for larger config migrations
         final Path path = Path.of(child);
         final Path target = get(newName);
-        if (Files.isRegularFile(path) && !Files.exists(target)) {
+        if (!Files.isRegularFile(path)) {
+            return target;
+        }
+
+        if (Files.exists(target)) {
+            // Delete the one in the server root
+            try {
+                Files.deleteIfExists(path);
+            } catch (final IOException e) {
+                LOGGER.error("Error deleting file: {}", child, e);
+            }
+        } else {
+            // Move into .paper
             try {
                 Files.move(path, target);
             } catch (final IOException e) {
-                throw new RuntimeException("Error moving file: " + child, e);
+                LOGGER.error("Error moving file: {}", child, e);
             }
         }
         return target;
