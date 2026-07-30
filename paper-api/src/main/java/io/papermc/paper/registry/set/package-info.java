3b28872c3abea444e1d86f9695c4e034d1ec44a4
Nassim Jahnke <nassim@njahnke.dev>
2026-07-30 08:46:05 +0200

    Add back experimental to registry set package

--- paper-api/src/main/java/io/papermc/paper/registry/set/package-info.java
+++ paper-api/src/main/java/io/papermc/paper/registry/set/package-info.java
@@ -3,7 +3,9 @@
  * <p>
  * Registry sets are collections of keys or inlined values of a type that has a registry.
  */
+@ApiStatus.Experimental
 @NullMarked
 package io.papermc.paper.registry.set;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
