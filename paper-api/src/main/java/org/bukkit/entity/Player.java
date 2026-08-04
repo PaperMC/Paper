502d56a9be7c3b756f2d68a203ef756fb1d3410a
Tau <nullvoxel@gmail.com>
2026-08-04 19:28:00 +1000

    Bump since to 26.2

--- paper-api/src/main/java/org/bukkit/entity/Player.java
+++ paper-api/src/main/java/org/bukkit/entity/Player.java
@@ -3622,7 +3622,7 @@ public interface Player extends HumanEntity, Conversable, OfflinePlayer, PluginM
      *
      * @return Percentage of attack power available
      * @see #getCooledAttackStrength(float)
-     * @since 26.1.2
+     * @since 26.2
      */
     default float getCooledAttackStrength() {
         return getCooledAttackStrength(0F);
@@ -3642,7 +3642,7 @@ public interface Player extends HumanEntity, Conversable, OfflinePlayer, PluginM
      *
      * @param strength The new percentage of attack power available
      * @see #getCooledAttackStrength(float)
-     * @since 26.1.2
+     * @since 26.2
      */
     void setCooledAttackStrength(float strength);
 
