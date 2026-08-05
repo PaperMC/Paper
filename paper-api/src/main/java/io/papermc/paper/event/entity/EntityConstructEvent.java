0dcf62a16cf892af70a8519ac1ab231d1f3babe4
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-05 17:22:47 +0200

    [ci skip] remove trailing space

--- paper-api/src/main/java/io/papermc/paper/event/entity/EntityConstructEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/EntityConstructEvent.java
@@ -35,7 +35,7 @@ public class EntityConstructEvent extends EntityEvent implements Cancellable {
     /**
      * Get an immutable list of the blocks required for this construction, including
      * any required air blocks.
-     * 
+     *
      * @return the blocks
      */
     public @Unmodifiable List<Block> getBlocks() {
