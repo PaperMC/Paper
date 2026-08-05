75357980219a6a52ddfd9d72a1ff8a6f40d80382
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-05 16:28:49 +0200

    [ci skip] update test

--- paper-server/src/test/java/org/bukkit/entity/SpawnCategoryTest.java
+++ paper-server/src/test/java/org/bukkit/entity/SpawnCategoryTest.java
@@ -1,6 +1,5 @@
 package org.bukkit.entity;
 
-import net.minecraft.world.entity.MobCategory;
 import org.bukkit.craftbukkit.util.CraftSpawnCategory;
 import org.bukkit.support.environment.Normal;
 import org.junit.jupiter.api.Test;
@@ -10,20 +9,12 @@ public class SpawnCategoryTest {
 
     @Test
     public void testMatch() {
-        for (MobCategory category : MobCategory.values()) {
-            // If it is missing a convert to Bukkit then throw a UnsupportedOperationException
-            SpawnCategory spawnCategory = CraftSpawnCategory.toBukkit(category);
-
-            if (CraftSpawnCategory.isValidForLimits(spawnCategory)) {
-                long defaultTicks = CraftSpawnCategory.getDefaultTicksPerSpawn(spawnCategory);
-                String nameConfigSpawnLimit = CraftSpawnCategory.getConfigNameSpawnLimit(spawnCategory);
-                String nameConfigTicksPerSpawn = CraftSpawnCategory.getConfigNameTicksPerSpawn(spawnCategory);
+        for (SpawnCategory category : SpawnCategory.values()) {
+            if (CraftSpawnCategory.isValidForLimits(category)) {
+                long defaultTicks = CraftSpawnCategory.getDefaultTicksPerSpawn(category);
+                String nameConfigSpawnLimit = CraftSpawnCategory.getConfigNameSpawnLimit(category);
+                String nameConfigTicksPerSpawn = CraftSpawnCategory.getConfigNameTicksPerSpawn(category);
             }
-        }
-
-        for (SpawnCategory spawnCategory : SpawnCategory.values()) {
-            // If it is missing a convert to NMS then throw a UnsupportedOperationException
-            MobCategory category = CraftSpawnCategory.toNMS(spawnCategory);
         }
     }
 }
