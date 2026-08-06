3b4dce83ee9ab72c3be61ba0033765d819dd3021
Kyle Smith <kyle@cabotmc.dev>
2026-08-06 11:20:23 -0300

    Migrate jetbrains -> jspecify Nullable annotation

--- paper-api/src/main/java/io/papermc/paper/event/block/BlockDropResourcesEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/block/BlockDropResourcesEvent.java
@@ -8,7 +8,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.block.BlockEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 import org.jspecify.annotations.NullMarked;
 import java.util.List;
 
