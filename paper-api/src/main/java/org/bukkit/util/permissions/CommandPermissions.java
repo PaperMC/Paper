f57e6ef718cb0fc34525f0bd3ac57e10a363049c
Mickey42302 <46900084+Mickey42302@users.noreply.github.com>
2026-08-08 20:27:04 -0400

    Change permission level for plugins and version commands

--- paper-api/src/main/java/org/bukkit/util/permissions/CommandPermissions.java
+++ paper-api/src/main/java/org/bukkit/util/permissions/CommandPermissions.java
@@ -15,9 +15,9 @@ public final class CommandPermissions {
         Permission commands = DefaultPermissions.registerPermission(ROOT, "Gives the user the ability to use all CraftBukkit commands", parent);
 
         DefaultPermissions.registerPermission(PREFIX + "help", "Allows the user to view the vanilla help menu", PermissionDefault.TRUE, commands);
-        DefaultPermissions.registerPermission(PREFIX + "plugins", "Allows the user to view the list of plugins running on this server", PermissionDefault.TRUE, commands);
+        DefaultPermissions.registerPermission(PREFIX + "plugins", "Allows the user to view the list of plugins running on this server", PermissionDefault.OP, commands);
         DefaultPermissions.registerPermission(PREFIX + "reload", "Allows the user to reload the server settings", PermissionDefault.OP, commands);
-        DefaultPermissions.registerPermission(PREFIX + "version", "Allows the user to view the version of the server", PermissionDefault.TRUE, commands);
+        DefaultPermissions.registerPermission(PREFIX + "version", "Allows the user to view the version of the server", PermissionDefault.OP, commands);
 
         commands.recalculatePermissibles();
         return commands;
