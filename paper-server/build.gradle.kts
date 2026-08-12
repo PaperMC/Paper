5797c6158f30b0a6db723456896c166e51a88b6f
Bjarne Koll <git@lynxplay.dev>
2026-08-12 19:28:13 +0200

    Update to 26.3-snapshot-8 base line

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:26.3-snapshot-7+build.2")
+    mache("io.papermc:mache:26.3-snapshot-8+build.1")
     paperclip("io.papermc:paperclip:3.0.4")
 }
 
