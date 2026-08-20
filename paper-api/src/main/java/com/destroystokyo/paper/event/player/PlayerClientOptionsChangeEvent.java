298d203e92c51fe9d9a585ce09f347b4a05e1442
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:03 +0200

    PlayerClientOptionsChangeEvent

--- paper-api/src/main/java/com/destroystokyo/paper/event/player/PlayerClientOptionsChangeEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/event/player/PlayerClientOptionsChangeEvent.java
@@ -1,128 +1,59 @@
 package com.destroystokyo.paper.event.player;
 
-import com.destroystokyo.paper.ClientOption;
 import com.destroystokyo.paper.ClientOption.ChatVisibility;
 import com.destroystokyo.paper.ClientOption.ParticleVisibility;
 import com.destroystokyo.paper.SkinParts;
-import java.util.Map;
-import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerEventNew;
 import org.bukkit.inventory.MainHand;
-import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when the player changes their client settings
  */
 @NullMarked
-public class PlayerClientOptionsChangeEvent extends PlayerEvent {
+public interface PlayerClientOptionsChangeEvent extends PlayerEventNew { // todo javadocs?
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
+    String getLocale();
 
-    private final String locale;
-    private final int viewDistance;
-    private final ChatVisibility chatVisibility;
-    private final boolean chatColors;
-    private final SkinParts skinparts;
-    private final MainHand mainHand;
-    private final boolean allowsServerListings;
-    private final boolean textFilteringEnabled;
-    private final ParticleVisibility particleVisibility;
+    boolean hasLocaleChanged();
 
-    @ApiStatus.Internal
-    public PlayerClientOptionsChangeEvent(final Player player, final Map<ClientOption<?>, ?> options) {
-        super(player);
+    int getViewDistance();
 
-        this.locale = (String) options.get(ClientOption.LOCALE);
-        this.viewDistance = (int) options.get(ClientOption.VIEW_DISTANCE);
-        this.chatVisibility = (ChatVisibility) options.get(ClientOption.CHAT_VISIBILITY);
-        this.chatColors = (boolean) options.get(ClientOption.CHAT_COLORS_ENABLED);
-        this.skinparts = (SkinParts) options.get(ClientOption.SKIN_PARTS);
-        this.mainHand = (MainHand) options.get(ClientOption.MAIN_HAND);
-        this.allowsServerListings = (boolean) options.get(ClientOption.ALLOW_SERVER_LISTINGS);
-        this.textFilteringEnabled = (boolean) options.get(ClientOption.TEXT_FILTERING_ENABLED);
-        this.particleVisibility = (ParticleVisibility) options.get(ClientOption.PARTICLE_VISIBILITY);
-    }
+    boolean hasViewDistanceChanged();
 
-    public String getLocale() {
-        return this.locale;
-    }
+    ChatVisibility getChatVisibility();
 
-    public boolean hasLocaleChanged() {
-        return !this.locale.equals(this.player.getClientOption(ClientOption.LOCALE));
-    }
+    boolean hasChatVisibilityChanged();
 
-    public int getViewDistance() {
-        return this.viewDistance;
-    }
+    boolean hasChatColorsEnabled();
 
-    public boolean hasViewDistanceChanged() {
-        return this.viewDistance != this.player.getClientOption(ClientOption.VIEW_DISTANCE);
-    }
+    boolean hasChatColorsEnabledChanged();
 
-    public ChatVisibility getChatVisibility() {
-        return this.chatVisibility;
-    }
+    SkinParts getSkinParts();
 
-    public boolean hasChatVisibilityChanged() {
-        return this.chatVisibility != this.player.getClientOption(ClientOption.CHAT_VISIBILITY);
-    }
+    boolean hasSkinPartsChanged();
 
-    public boolean hasChatColorsEnabled() {
-        return this.chatColors;
-    }
+    MainHand getMainHand();
 
-    public boolean hasChatColorsEnabledChanged() {
-        return this.chatColors != this.player.getClientOption(ClientOption.CHAT_COLORS_ENABLED);
-    }
+    boolean hasMainHandChanged();
 
-    public SkinParts getSkinParts() {
-        return this.skinparts;
-    }
+    boolean hasTextFilteringEnabled();
 
-    public boolean hasSkinPartsChanged() {
-        return this.skinparts.getRaw() != this.player.getClientOption(ClientOption.SKIN_PARTS).getRaw();
-    }
+    boolean hasTextFilteringChanged();
 
-    public MainHand getMainHand() {
-        return this.mainHand;
-    }
+    boolean allowsServerListings();
 
-    public boolean hasMainHandChanged() {
-        return this.mainHand != this.player.getClientOption(ClientOption.MAIN_HAND);
-    }
+    boolean hasAllowServerListingsChanged();
 
-    public boolean hasTextFilteringEnabled() {
-        return this.textFilteringEnabled;
-    }
+    ParticleVisibility getParticleVisibility();
 
-    public boolean hasTextFilteringChanged() {
-        return this.textFilteringEnabled != this.player.getClientOption(ClientOption.TEXT_FILTERING_ENABLED);
-    }
+    boolean hasParticleVisibilityChanged();
 
-    public boolean allowsServerListings() {
-        return this.allowsServerListings;
-    }
-
-    public boolean hasAllowServerListingsChanged() {
-        return this.allowsServerListings != this.player.getClientOption(ClientOption.ALLOW_SERVER_LISTINGS);
-    }
-
-    public ParticleVisibility getParticleVisibility() {
-        return this.particleVisibility;
-    }
-
-    public boolean hasParticleVisibilityChanged() {
-        return this.particleVisibility != this.player.getClientOption(ClientOption.PARTICLE_VISIBILITY);
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
