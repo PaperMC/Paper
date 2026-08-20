05201926f032b236715fcaf7ccaf8431ed5bb50d
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:04 +0200

    PlayerSetSpawnEvent

--- paper-api/src/main/java/com/destroystokyo/paper/event/player/PlayerSetSpawnEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/event/player/PlayerSetSpawnEvent.java
@@ -5,8 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
-import org.jetbrains.annotations.ApiStatus;
+import org.bukkit.event.player.PlayerEventNew;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
@@ -16,36 +15,14 @@ import org.jspecify.annotations.Nullable;
  * Cancelling this event will prevent the spawn from being set.
  */
 @NullMarked
-public class PlayerSetSpawnEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Cause cause;
-    private @Nullable Location location;
-    private boolean forced;
-    private boolean notifyPlayer;
-    private @Nullable Component notification;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerSetSpawnEvent(final Player player, final Cause cause, final @Nullable Location location, final boolean forced, final boolean notifyPlayer, final @Nullable Component notification) {
-        super(player);
-        this.cause = cause;
-        this.location = location;
-        this.forced = forced;
-        this.notifyPlayer = notifyPlayer;
-        this.notification = notification;
-    }
+public interface PlayerSetSpawnEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the cause of this event.
      *
      * @return the cause
      */
-    public Cause getCause() {
-        return this.cause;
-    }
+    Cause getCause();
 
     /**
      * Gets the location that the spawn is set to. The yaw
@@ -55,9 +32,7 @@ public class PlayerSetSpawnEvent extends PlayerEvent implements Cancellable {
      *
      * @return the spawn location, or {@code null} if removing the location
      */
-    public @Nullable Location getLocation() {
-        return this.location;
-    }
+    @Nullable Location getLocation();
 
     /**
      * Sets the location to be set as the spawn location. The yaw
@@ -65,27 +40,21 @@ public class PlayerSetSpawnEvent extends PlayerEvent implements Cancellable {
      *
      * @param location the spawn location, or {@code null} to remove the spawn location
      */
-    public void setLocation(final @Nullable Location location) {
-        this.location = location != null ? location.clone() : null;
-    }
+    void setLocation(@Nullable Location location);
 
     /**
      * Gets if this is a force spawn location
      *
      * @return {@code true} if forced
      */
-    public boolean isForced() {
-        return this.forced;
-    }
+    boolean isForced();
 
     /**
      * Sets if this is a forced spawn location
      *
      * @param forced {@code true} to force
      */
-    public void setForced(final boolean forced) {
-        this.forced = forced;
-    }
+    void setForced(boolean forced);
 
     /**
      * Gets if this action will notify the player their spawn
@@ -93,9 +62,7 @@ public class PlayerSetSpawnEvent extends PlayerEvent implements Cancellable {
      *
      * @return {@code true} to notify
      */
-    public boolean willNotifyPlayer() {
-        return this.notifyPlayer;
-    }
+    boolean willNotifyPlayer();
 
     /**
      * Sets if this action will notify the player that their spawn
@@ -103,9 +70,7 @@ public class PlayerSetSpawnEvent extends PlayerEvent implements Cancellable {
      *
      * @param notifyPlayer {@code true} to notify
      */
-    public void setNotifyPlayer(final boolean notifyPlayer) {
-        this.notifyPlayer = notifyPlayer;
-    }
+    void setNotifyPlayer(boolean notifyPlayer);
 
     /**
      * Gets the notification message that will be sent to the player
@@ -113,39 +78,23 @@ public class PlayerSetSpawnEvent extends PlayerEvent implements Cancellable {
      *
      * @return {@code null} if no notification
      */
-    public @Nullable Component getNotification() {
-        return this.notification;
-    }
+    @Nullable Component getNotification();
 
     /**
      * Sets the notification message that will be sent to the player.
      *
      * @param notification {@code null} to send no message
      */
-    public void setNotification(final @Nullable Component notification) {
-        this.notification = notification;
+    void setNotification(@Nullable Component notification);
+
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
-    }
-
-    public enum Cause {
+    enum Cause {
         /**
          * When a player interacts successfully with a bed.
          */
