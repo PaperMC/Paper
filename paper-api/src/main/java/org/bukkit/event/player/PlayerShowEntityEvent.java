6dbd412e2c66e2a196fafd2b46ccde185363a951
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:00 +0200

    PlayerShowEntityEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerShowEntityEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerShowEntityEvent.java
@@ -3,8 +3,6 @@ package org.bukkit.event.player;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a hidden entity is shown to a player.
@@ -15,38 +13,21 @@ import org.jetbrains.annotations.NotNull;
  * This event is called regardless of whether the entity was within tracking
  * range.
  *
- * @see Player#showEntity(org.bukkit.plugin.Plugin, org.bukkit.entity.Entity)
+ * @see Player#showEntity(org.bukkit.plugin.Plugin, Entity)
  */
-public class PlayerShowEntityEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Entity entity;
-
-    @ApiStatus.Internal
-    public PlayerShowEntityEvent(@NotNull Player player, @NotNull Entity entity) {
-        super(player);
-        this.entity = entity;
-    }
+public interface PlayerShowEntityEvent extends PlayerEventNew {
 
     /**
      * Gets the entity which has been shown to the player.
      *
      * @return the shown entity
      */
-    @NotNull
-    public Entity getEntity() {
-        return this.entity;
-    }
+    Entity getEntity();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
