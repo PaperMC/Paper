85f4d46701dffb83e0952aaeb31f9c619d86f464
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:54 +0200

    PlayerAdvancementDoneEvent

--- paper-api/src/main/java/org/bukkit/event/player/package-info.java
+++ paper-api/src/main/java/org/bukkit/event/player/package-info.java
@@ -1,6 +1,10 @@
 /**
- * {@link org.bukkit.event.Event Events} relating to {@link
- * org.bukkit.entity.Player players}.
+ * {@linkplain org.bukkit.event.Event Events} relating to {@linkplain org.bukkit.entity.Player players}.
+ *
+ * @see io.papermc.paper.event.player
+ * @see com.destroystokyo.paper.event.player
  */
+@NullMarked
 package org.bukkit.event.player;
 
+import org.jspecify.annotations.NullMarked;
