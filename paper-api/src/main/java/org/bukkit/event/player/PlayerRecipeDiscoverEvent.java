7ae1e998659932b3e8a80cec82e4a92effa66f36
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:00 +0200

    PlayerRecipeDiscoverEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerRecipeDiscoverEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerRecipeDiscoverEvent.java
@@ -1,74 +1,39 @@
 package org.bukkit.event.player;
 
 import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when a player discovers a new recipe in the recipe book.
  */
-@NullMarked
-public class PlayerRecipeDiscoverEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final NamespacedKey recipe;
-    private boolean cancelled;
-    private boolean showNotification;
-
-    @ApiStatus.Internal
-    public PlayerRecipeDiscoverEvent(Player player, NamespacedKey recipe, boolean showNotification) {
-        super(player);
-        this.recipe = recipe;
-        this.showNotification = showNotification;
-    }
+public interface PlayerRecipeDiscoverEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Get the namespaced key of the discovered recipe.
      *
      * @return the discovered recipe
      */
-    public NamespacedKey getRecipe() {
-        return this.recipe;
-    }
+    NamespacedKey getRecipe();
 
     /**
      * Get if the player should be notified (toast) of the discovery.
      *
-     * @return true if the player should be notified
+     * @return {@code true} if the player should be notified
      */
-    public boolean shouldShowNotification() {
-        return this.showNotification;
-    }
+    boolean shouldShowNotification();
 
     /**
      * Set if the player should be notified (toast) of the discovery.
      *
-     * @param showNotification true if the player should be notified
+     * @param showNotification {@code true} if the player should be notified
      */
-    public void shouldShowNotification(boolean showNotification) {
-        this.showNotification = showNotification;
-    }
+    void shouldShowNotification(boolean showNotification);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
