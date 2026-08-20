16914e9c4adec6ab87b90d83346e68f89b851469
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:01 +0200

    PlayerTakeLecternBookEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerTakeLecternBookEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerTakeLecternBookEvent.java
@@ -1,69 +1,35 @@
 package org.bukkit.event.player;
 
 import org.bukkit.block.Lectern;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * This event is called when a player clicks the button to take a book of a
  * Lectern. If this event is cancelled the book remains on the lectern.
  */
-public class PlayerTakeLecternBookEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Lectern lectern;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerTakeLecternBookEvent(@NotNull Player player, @NotNull Lectern lectern) {
-        super(player);
-        this.lectern = lectern;
-    }
+public interface PlayerTakeLecternBookEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the lectern involved.
      *
      * @return the Lectern
      */
-    @NotNull
-    public Lectern getLectern() {
-        return this.lectern;
-    }
+    Lectern getLectern();
 
     /**
      * Gets the current ItemStack on the lectern.
      *
      * @return the ItemStack on the Lectern
      */
-    @Nullable
-    public ItemStack getBook() {
-        return this.lectern.getInventory().getItem(0);
-    }
+    @Nullable ItemStack getBook();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
