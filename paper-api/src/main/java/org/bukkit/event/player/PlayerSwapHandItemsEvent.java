590797a8b8cc78090e8be7c5dc12367f7b30361d
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:01 +0200

    PlayerSwapHandItemsEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerSwapHandItemsEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerSwapHandItemsEvent.java
@@ -1,90 +1,48 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Called when a player swap items between main hand and off-hand using the
  * hotkey.
  */
-public class PlayerSwapHandItemsEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private ItemStack mainHandItem;
-    private ItemStack offHandItem;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerSwapHandItemsEvent(@NotNull Player player, @NotNull ItemStack mainHandItem, @NotNull ItemStack offHandItem) {
-        super(player);
-
-        this.mainHandItem = mainHandItem;
-        this.offHandItem = offHandItem;
-    }
+public interface PlayerSwapHandItemsEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the item switched to the main hand.
      *
      * @return item in the main hand
      */
-    @NotNull
-    public ItemStack getMainHandItem() {
-        return this.mainHandItem;
-    }
+    ItemStack getMainHandItem();
 
     /**
      * Sets the item in the main hand.
      *
      * @param mainHandItem new item in the main hand
      */
-    public void setMainHandItem(@Nullable ItemStack mainHandItem) {
-        this.mainHandItem = mainHandItem == null ? ItemStack.empty() : mainHandItem;
-    }
+    void setMainHandItem(@Nullable ItemStack mainHandItem);
 
     /**
      * Gets the item switched to the off-hand.
      *
      * @return item in the off-hand
      */
-    @NotNull // Paper
-    public ItemStack getOffHandItem() {
-        return this.offHandItem;
-    }
+    ItemStack getOffHandItem();
 
     /**
      * Sets the item in the off-hand.
      *
      * @param offHandItem new item in the off-hand
      */
-    public void setOffHandItem(@Nullable ItemStack offHandItem) {
-        this.offHandItem = offHandItem == null ? ItemStack.empty() : offHandItem;
-    }
+    void setOffHandItem(@Nullable ItemStack offHandItem);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
