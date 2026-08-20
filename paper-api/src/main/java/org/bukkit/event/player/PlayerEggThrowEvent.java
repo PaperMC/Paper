6d1471c2fbbf1594e64d94add57f091f1f9d3dd5
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:56 +0200

    PlayerEggThrowEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerEggThrowEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerEggThrowEvent.java
@@ -2,41 +2,19 @@ package org.bukkit.event.player;
 
 import org.bukkit.entity.Egg;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player throws an egg and it might hatch
  */
-public class PlayerEggThrowEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Egg egg;
-    private boolean hatching;
-    private EntityType hatchType;
-    private byte numHatches;
-
-    @ApiStatus.Internal
-    public PlayerEggThrowEvent(@NotNull final Player player, @NotNull final Egg egg, final boolean hatching, final byte numHatches, @NotNull final EntityType hatchingType) {
-        super(player);
-        this.egg = egg;
-        this.hatching = hatching;
-        this.numHatches = numHatches;
-        this.hatchType = hatchingType;
-    }
+public interface PlayerEggThrowEvent extends PlayerEventNew {
 
     /**
      * Gets the egg involved in this event.
      *
      * @return the egg involved in this event
      */
-    @NotNull
-    public Egg getEgg() {
-        return this.egg;
-    }
+    Egg getEgg();
 
     /**
      * Gets whether the egg is hatching or not. Will be what the server
@@ -44,9 +22,7 @@ public class PlayerEggThrowEvent extends PlayerEvent {
      *
      * @return boolean Whether the egg is going to hatch or not
      */
-    public boolean isHatching() {
-        return this.hatching;
-    }
+    boolean isHatching();
 
     /**
      * Sets whether the egg will hatch or not.
@@ -54,29 +30,21 @@ public class PlayerEggThrowEvent extends PlayerEvent {
      * @param hatching {@code true} if you want the egg to hatch, {@code false} if you want it
      *     not to
      */
-    public void setHatching(boolean hatching) {
-        this.hatching = hatching;
-    }
+    void setHatching(boolean hatching);
 
     /**
      * Get the type of the mob being hatched (EntityType.CHICKEN by default)
      *
      * @return The type of the mob being hatched by the egg
      */
-    @NotNull
-    public EntityType getHatchingType() {
-        return this.hatchType;
-    }
+    EntityType getHatchingType();
 
     /**
      * Change the type of mob being hatched by the egg
      *
      * @param hatchType The type of the mob being hatched by the egg
      */
-    public void setHatchingType(@NotNull EntityType hatchType) {
-        if (!hatchType.isSpawnable()) throw new IllegalArgumentException("Can't spawn that entity type from an egg!");
-        this.hatchType = hatchType;
-    }
+    void setHatchingType(EntityType hatchType);
 
     /**
      * Get the number of mob hatches from the egg. By default the number will
@@ -89,9 +57,7 @@ public class PlayerEggThrowEvent extends PlayerEvent {
      *
      * @return The number of mobs going to be hatched by the egg
      */
-    public byte getNumHatches() {
-        return this.numHatches;
-    }
+    byte getNumHatches();
 
     /**
      * Change the number of mobs coming out of the hatched egg
@@ -101,18 +67,12 @@ public class PlayerEggThrowEvent extends PlayerEvent {
      *
      * @param numHatches The number of mobs coming out of the egg
      */
-    public void setNumHatches(byte numHatches) {
-        this.numHatches = numHatches;
-    }
+    void setNumHatches(byte numHatches);
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
