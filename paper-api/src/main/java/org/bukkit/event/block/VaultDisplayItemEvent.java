59dbce117649d8cca11de526a3965313fe406efb
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:13 +0200

    VaultDisplayItemEvent

--- paper-api/src/main/java/org/bukkit/event/block/VaultDisplayItemEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/VaultDisplayItemEvent.java
@@ -1,66 +1,33 @@
 package org.bukkit.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Called when a vault in a trial chamber is about to display an item.
  */
-public class VaultDisplayItemEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private ItemStack displayItem;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public VaultDisplayItemEvent(@NotNull Block vault, @Nullable ItemStack displayItem) {
-        super(vault);
-        this.displayItem = displayItem;
-    }
+public interface VaultDisplayItemEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the item that will be displayed inside the vault.
      *
      * @return the item to be displayed
      */
-    @Nullable
-    public ItemStack getDisplayItem() {
-        return this.displayItem;
-    }
+    @Nullable ItemStack getDisplayItem();
 
     /**
      * Sets the item that will be displayed inside the vault.
      *
      * @param displayItem the item to be displayed
      */
-    public void setDisplayItem(@Nullable ItemStack displayItem) {
-        this.displayItem = displayItem;
-    }
+    void setDisplayItem(@Nullable ItemStack displayItem);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
