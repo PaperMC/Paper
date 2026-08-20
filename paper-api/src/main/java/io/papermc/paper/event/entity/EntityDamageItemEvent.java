a741d5a06076ccaff37ef02249b9cf0fb64aaba1
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:16 +0200

    EntityDamageItemEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/EntityDamageItemEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/EntityDamageItemEvent.java
@@ -1,12 +1,9 @@
 package io.papermc.paper.event.entity;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.entity.EntityEventNew;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when an item on or used by an entity takes durability damage as a result of being hit/used.
@@ -14,66 +11,33 @@ import org.jspecify.annotations.NullMarked;
  * NOTE: default vanilla behaviour dictates that armor/tools picked up by
  * mobs do not take damage (except via Thorns).
  */
-@NullMarked
-public class EntityDamageItemEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ItemStack item;
-    private int damage;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public EntityDamageItemEvent(final Entity entity, final ItemStack item, final int damage) {
-        super(entity);
-        this.item = item;
-        this.damage = damage;
-    }
+public interface EntityDamageItemEvent extends EntityEventNew, Cancellable {
 
     /**
      * Gets the item being damaged.
      *
      * @return the item
      */
-    public ItemStack getItem() {
-        return this.item;
-    }
+    ItemStack getItem();
 
     /**
      * Gets the amount of durability damage this item will be taking.
      *
      * @return durability change
      */
-    public int getDamage() {
-        return this.damage;
-    }
+    int getDamage();
 
     /**
      * Sets the amount of durability damage this item will be taking.
      *
      * @param damage the damage amount to cause
      */
-    public void setDamage(final int damage) {
-        this.damage = damage;
-    }
+    void setDamage(int damage);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
