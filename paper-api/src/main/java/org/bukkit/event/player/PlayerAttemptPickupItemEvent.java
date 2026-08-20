eb35680671b941fb010e1ee23f6c51f2a3141582
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:54 +0200

    PlayerAttemptPickupItemEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerAttemptPickupItemEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerAttemptPickupItemEvent.java
@@ -1,56 +1,27 @@
 package org.bukkit.event.player;
 
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
 
 /**
  * Thrown when a player attempts to pick an item up from the ground
  */
-@NullMarked
-public class PlayerAttemptPickupItemEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Item item;
-    private final int remaining;
-    private boolean flyAtPlayer = true;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    @Deprecated(forRemoval = true)
-    public PlayerAttemptPickupItemEvent(final Player player, final Item item) {
-        this(player, item, 0);
-    }
-
-    @ApiStatus.Internal
-    public PlayerAttemptPickupItemEvent(final Player player, final Item item, final int remaining) {
-        super(player);
-        this.item = item;
-        this.remaining = remaining;
-    }
+public interface PlayerAttemptPickupItemEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the Item attempted by the player.
      *
      * @return Item
      */
-    public Item getItem() {
-        return this.item;
-    }
+    Item getItem();
 
     /**
      * Gets the amount that will remain on the ground, if any
      *
      * @return amount that will remain on the ground
      */
-    public int getRemaining() {
-        return this.remaining;
-    }
+    int getRemaining();
 
     /**
      * Set if the item will fly at the player
@@ -58,37 +29,19 @@ public class PlayerAttemptPickupItemEvent extends PlayerEvent implements Cancell
      *
      * @param flyAtPlayer {@code true} for item to fly at player
      */
-    public void setFlyAtPlayer(boolean flyAtPlayer) {
-        this.flyAtPlayer = flyAtPlayer;
-    }
+    void setFlyAtPlayer(boolean flyAtPlayer);
 
     /**
      * Gets if the item will fly at the player
      *
      * @return {@code true} if the item will fly at the player
      */
-    public boolean getFlyAtPlayer() {
-        return this.flyAtPlayer;
-    }
+    boolean getFlyAtPlayer();
 
-
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-        this.flyAtPlayer = !cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
