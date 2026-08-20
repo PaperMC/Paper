ffa098d1f75bb7289a741d809e495313cee9df0b
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:02 +0200

    PlayerToggleSneakEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerToggleSneakEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerToggleSneakEvent.java
@@ -1,54 +1,24 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player toggles their sneaking state
  */
-public class PlayerToggleSneakEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final boolean isSneaking;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerToggleSneakEvent(@NotNull final Player player, final boolean isSneaking) {
-        super(player);
-        this.isSneaking = isSneaking;
-    }
+public interface PlayerToggleSneakEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Returns whether the player is now sneaking or not.
      *
      * @return sneaking state
      */
-    public boolean isSneaking() {
-        return this.isSneaking;
-    }
+    boolean isSneaking();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
