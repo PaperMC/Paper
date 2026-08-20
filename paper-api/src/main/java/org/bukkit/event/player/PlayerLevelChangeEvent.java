21f30ebf5c0c285a88b477b8f3d2406bea3e045a
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:58 +0200

    PlayerLevelChangeEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerLevelChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerLevelChangeEvent.java
@@ -1,53 +1,30 @@
 package org.bukkit.event.player;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a players level changes
  */
-public class PlayerLevelChangeEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final int oldLevel;
-    private final int newLevel;
-
-    @ApiStatus.Internal
-    public PlayerLevelChangeEvent(@NotNull final Player player, final int oldLevel, final int newLevel) {
-        super(player);
-        this.oldLevel = oldLevel;
-        this.newLevel = newLevel;
-    }
+public interface PlayerLevelChangeEvent extends PlayerEventNew {
 
     /**
      * Gets the old level of the player
      *
      * @return The old level of the player
      */
-    public int getOldLevel() {
-        return this.oldLevel;
-    }
+    int getOldLevel();
 
     /**
      * Gets the new level of the player
      *
      * @return The new (current) level of the player
      */
-    public int getNewLevel() {
-        return this.newLevel;
-    }
+    int getNewLevel();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
