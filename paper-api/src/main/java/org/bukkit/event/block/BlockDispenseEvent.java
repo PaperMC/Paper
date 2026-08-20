70d3e011fdd40d75e2913412f942cc26993319af
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:09 +0200

    BlockDispenseEvent

--- paper-api/src/main/java/org/bukkit/event/block/BlockDispenseEvent.java
+++ paper-api/src/main/java/org/bukkit/event/block/BlockDispenseEvent.java
@@ -1,12 +1,9 @@
 package org.bukkit.event.block;
 
-import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when an item is dispensed from a block.
@@ -14,21 +11,7 @@ import org.jetbrains.annotations.NotNull;
  * If this event is cancelled, the block will not dispense the
  * item.
  */
-public class BlockDispenseEvent extends BlockEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private ItemStack item;
-    private Vector velocity;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public BlockDispenseEvent(@NotNull final Block block, @NotNull final ItemStack item, @NotNull final Vector velocity) {
-        super(block);
-        this.item = item;
-        this.velocity = velocity;
-    }
+public interface BlockDispenseEvent extends BlockEventNew, Cancellable {
 
     /**
      * Gets the item that is being dispensed. Modifying the returned item will
@@ -37,19 +20,14 @@ public class BlockDispenseEvent extends BlockEvent implements Cancellable {
      *
      * @return An ItemStack for the item being dispensed
      */
-    @NotNull
-    public ItemStack getItem() {
-        return this.item.clone();
-    }
+    ItemStack getItem();
 
     /**
      * Sets the item being dispensed.
      *
      * @param item the item being dispensed
      */
-    public void setItem(@NotNull ItemStack item) {
-        this.item = item;
-    }
+    void setItem(ItemStack item);
 
     /**
      * Gets the velocity in meters per tick.
@@ -59,38 +37,19 @@ public class BlockDispenseEvent extends BlockEvent implements Cancellable {
      *
      * @return A Vector for the dispensed item's velocity
      */
-    @NotNull
-    public Vector getVelocity() {
-        return this.velocity.clone();
-    }
+    Vector getVelocity();
 
     /**
      * Sets the velocity of the item being dispensed in meters per tick.
      *
      * @param velocity the velocity of the item being dispensed
      */
-    public void setVelocity(@NotNull Vector velocity) {
-        this.velocity = velocity.clone();
-    }
+    void setVelocity(Vector velocity);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
