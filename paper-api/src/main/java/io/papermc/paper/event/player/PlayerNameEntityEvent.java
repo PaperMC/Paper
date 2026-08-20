e6540faed8704c02705e2b1f06cdffac9b88b93a
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:51 +0200

    PlayerNameEntityEvent

--- paper-api/src/main/java/io/papermc/paper/event/player/PlayerNameEntityEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/player/PlayerNameEntityEvent.java
@@ -2,11 +2,9 @@ package io.papermc.paper.event.player;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
-import org.jetbrains.annotations.ApiStatus;
+import org.bukkit.event.player.PlayerEventNew;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
@@ -14,94 +12,54 @@ import org.jspecify.annotations.Nullable;
  * Called when the player is attempting to rename a mob
  */
 @NullMarked
-public class PlayerNameEntityEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private LivingEntity entity;
-    private @Nullable Component name;
-    private boolean persistent;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerNameEntityEvent(final Player player, final LivingEntity entity, final Component name, final boolean persistent) {
-        super(player);
-        this.entity = entity;
-        this.name = name;
-        this.persistent = persistent;
-    }
+public interface PlayerNameEntityEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the name to be given to the entity.
      *
      * @return the name
      */
-    public @Nullable Component getName() {
-        return this.name;
-    }
+    @Nullable Component getName();
 
     /**
      * Sets the name to be given to the entity.
      *
      * @param name the name
      */
-    public void setName(final @Nullable Component name) {
-        this.name = name;
-    }
+    void setName(@Nullable Component name);
 
     /**
      * Gets the entity involved in this event.
      *
      * @return the entity
      */
-    public LivingEntity getEntity() {
-        return this.entity;
-    }
+    LivingEntity getEntity();
 
     /**
      * Sets the entity involved in this event.
      *
      * @param entity the entity
      */
-    public void setEntity(final LivingEntity entity) {
-        this.entity = entity;
-    }
+    void setEntity(LivingEntity entity);
 
     /**
      * Gets whether this will set the mob to be persistent.
      *
      * @return persistent
      */
-    public boolean isPersistent() {
-        return this.persistent;
-    }
+    boolean isPersistent();
 
     /**
      * Sets whether this will set the mob to be persistent.
      *
      * @param persistent persistent
      */
-    public void setPersistent(final boolean persistent) {
-        this.persistent = persistent;
-    }
+    void setPersistent(boolean persistent);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
