fa601068d314fbe82256898d9a33335190faf02f
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:59 +0200

    PlayerPickupItemEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerPickupItemEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerPickupItemEvent.java
@@ -2,54 +2,31 @@ package org.bukkit.event.player;
 
 import org.bukkit.Warning;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Thrown when a player picks an item up from the ground
  * @deprecated {@link EntityPickupItemEvent}
  */
-@Deprecated(since = "1.12")
 @Warning
-public class PlayerPickupItemEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Item item;
-    private final int remaining;
-    private boolean flyAtPlayer = true;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerPickupItemEvent(@NotNull final Player player, @NotNull final Item item, final int remaining) {
-        super(player);
-        this.item = item;
-        this.remaining = remaining;
-    }
+@Deprecated(since = "1.12")
+public interface PlayerPickupItemEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the Item picked up by the player.
      *
      * @return Item
      */
-    @NotNull
-    public Item getItem() {
-        return this.item;
-    }
+    Item getItem();
 
     /**
      * Gets the amount remaining on the ground, if any
      *
      * @return amount remaining on the ground
      */
-    public int getRemaining() {
-        return this.remaining;
-    }
+    int getRemaining();
 
     /**
      * Set if the item will fly at the player
@@ -58,38 +35,19 @@ public class PlayerPickupItemEvent extends PlayerEvent implements Cancellable {
      *
      * @param flyAtPlayer {@code true} for item to fly at player
      */
-    public void setFlyAtPlayer(boolean flyAtPlayer) {
-        this.flyAtPlayer = flyAtPlayer;
-    }
+    void setFlyAtPlayer(boolean flyAtPlayer);
 
     /**
      * Gets if the item will fly at the player
      *
      * @return {@code true} if the item will fly at the player
      */
-    public boolean getFlyAtPlayer() {
-        return this.flyAtPlayer;
-    }
+    boolean getFlyAtPlayer();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-        this.flyAtPlayer = !cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
