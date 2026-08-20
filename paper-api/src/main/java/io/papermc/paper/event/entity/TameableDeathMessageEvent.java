a7dc3f8d1ecf353b2c20bb34f6da1d11bed13d0b
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:19 +0200

    TameableDeathMessageEvent

--- paper-api/src/main/java/io/papermc/paper/event/entity/TameableDeathMessageEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/TameableDeathMessageEvent.java
@@ -4,66 +4,34 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Tameable;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
+import org.bukkit.event.entity.EntityEventNew;
 
 /**
  * Called when a {@link Tameable} dies and sends a death message.
  */
-@NullMarked
-public class TameableDeathMessageEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private Component deathMessage;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public TameableDeathMessageEvent(final Tameable tameable, final Component deathMessage) {
-        super(tameable);
-        this.deathMessage = deathMessage;
-    }
-
-    /**
-     * Set the death message that appears to the owner of the tameable.
-     *
-     * @param deathMessage Death message to appear
-     */
-    public void deathMessage(final Component deathMessage) {
-        this.deathMessage = deathMessage;
-    }
+public interface TameableDeathMessageEvent extends EntityEventNew, Cancellable {
 
     /**
      * Get the death message that appears to the owner of the tameable.
      *
      * @return Death message to appear
      */
-    public Component deathMessage() {
-        return this.deathMessage;
-    }
+    Component deathMessage();
+
+    /**
+     * Set the death message that appears to the owner of the tameable.
+     *
+     * @param deathMessage Death message to appear
+     */
+    void deathMessage(Component deathMessage);
 
     @Override
-    public Tameable getEntity() {
-        return (Tameable) super.getEntity();
-    }
+    Tameable getEntity();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
