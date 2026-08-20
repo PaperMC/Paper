5145588a306d89c40e482f8829b321351fc1c751
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:06 +0200

    LootableInventoryReplenishEvent

--- paper-api/src/main/java/com/destroystokyo/paper/loottable/LootableInventoryReplenishEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/loottable/LootableInventoryReplenishEvent.java
@@ -1,46 +1,19 @@
 package com.destroystokyo.paper.loottable;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
-import org.jetbrains.annotations.ApiStatus;
+import org.bukkit.event.player.PlayerEventNew;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public class LootableInventoryReplenishEvent extends PlayerEvent implements Cancellable {
+public interface LootableInventoryReplenishEvent extends PlayerEventNew, Cancellable { // todo javadocs?
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
+    LootableInventory getInventory();
 
-    private final LootableInventory inventory;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public LootableInventoryReplenishEvent(final Player player, final LootableInventory inventory) {
-        super(player);
-        this.inventory = inventory;
-    }
-
-    public LootableInventory getInventory() {
-        return this.inventory;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
