0e01dbbd49f02cac59a3e5fc2a7940be53deb098
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:11 +0200

    InventoryBlockStartEvent

--- paper-api/src/main/java/org/bukkit/event/inventory/FurnaceStartSmeltEvent.java
+++ paper-api/src/main/java/org/bukkit/event/inventory/FurnaceStartSmeltEvent.java
@@ -1,12 +1,7 @@
 package org.bukkit.event.inventory;
 
-import org.bukkit.block.Block;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.block.InventoryBlockStartEvent;
 import org.bukkit.inventory.CookingRecipe;
-import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when any of the furnace-like blocks start smelting.
@@ -14,49 +9,26 @@ import org.jetbrains.annotations.NotNull;
  * Furnace-like blocks are {@link org.bukkit.block.Furnace},
  * {@link org.bukkit.block.Smoker}, and {@link org.bukkit.block.BlastFurnace}.
  */
-public class FurnaceStartSmeltEvent extends InventoryBlockStartEvent {
-
-    private final CookingRecipe<?> recipe;
-    private int totalCookTime;
-
-    @ApiStatus.Internal
-    @Deprecated(forRemoval = true)
-    public FurnaceStartSmeltEvent(@NotNull final Block furnace, @NotNull ItemStack source, @NotNull final CookingRecipe<?> recipe) {
-        this(furnace, source, recipe, recipe.getCookingTime());
-    }
-
-    @ApiStatus.Internal
-    public FurnaceStartSmeltEvent(final @NotNull Block furnace, final @NotNull ItemStack source, final @NotNull CookingRecipe<?> recipe, final int cookingTime) {
-        super(furnace, source);
-        this.recipe = recipe;
-        this.totalCookTime = cookingTime;
-    }
+public interface FurnaceStartSmeltEvent extends InventoryBlockStartEvent {
 
     /**
      * Gets the FurnaceRecipe associated with this event
      *
      * @return the FurnaceRecipe being cooked
      */
-    @NotNull
-    public CookingRecipe<?> getRecipe() {
-        return this.recipe;
-    }
+    CookingRecipe<?> getRecipe();
 
     /**
      * Gets the total cook time associated with this event
      *
      * @return the total cook time
      */
-    public int getTotalCookTime() {
-        return this.totalCookTime;
-    }
+    int getTotalCookTime();
 
     /**
      * Sets the total cook time for this event
      *
      * @param cookTime the new total cook time
      */
-    public void setTotalCookTime(int cookTime) {
-        this.totalCookTime = cookTime;
-    }
+    void setTotalCookTime(int cookTime);
 }
