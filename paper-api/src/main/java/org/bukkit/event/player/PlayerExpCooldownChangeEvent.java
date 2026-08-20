e59879c3288f4eb726a660fa1365aea0158ac310
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:43:56 +0200

    PlayerExpCooldownChangeEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerExpCooldownChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerExpCooldownChangeEvent.java
@@ -2,35 +2,18 @@ package org.bukkit.event.player;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a player's experience cooldown changes.
  */
-public class PlayerExpCooldownChangeEvent extends PlayerEvent {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ChangeReason reason;
-    private int newCooldown;
-
-    @ApiStatus.Internal
-    public PlayerExpCooldownChangeEvent(@NotNull final Player player, int newCooldown, @NotNull ChangeReason reason) {
-        super(player);
-        this.newCooldown = newCooldown;
-        this.reason = reason;
-    }
+public interface PlayerExpCooldownChangeEvent extends PlayerEventNew {
 
     /**
      * Gets the reason for the change.
      *
      * @return The reason for the change
      */
-    @NotNull
-    public ChangeReason getReason() {
-        return this.reason;
-    }
+    ChangeReason getReason();
 
     /**
      * Gets the new cooldown for the player.
@@ -38,9 +21,7 @@ public class PlayerExpCooldownChangeEvent extends PlayerEvent {
      * @return The new cooldown
      * @see Player#getExpCooldown()
      */
-    public int getNewCooldown() {
-        return this.newCooldown;
-    }
+    int getNewCooldown();
 
     /**
      * Sets the new cooldown for the player.
@@ -48,22 +29,16 @@ public class PlayerExpCooldownChangeEvent extends PlayerEvent {
      * @param newCooldown The new cooldown to set
      * @see Player#setExpCooldown(int)
      */
-    public void setNewCooldown(int newCooldown) {
-        this.newCooldown = newCooldown;
+    void setNewCooldown(int newCooldown);
+
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
-    }
-
-    public enum ChangeReason {
+    enum ChangeReason {
 
         /**
          * The cooldown was set by picking up an experience orb.
@@ -74,6 +49,6 @@ public class PlayerExpCooldownChangeEvent extends PlayerEvent {
          *
          * @see Player#setExpCooldown(int)
          */
-        PLUGIN;
+        PLUGIN
     }
 }
