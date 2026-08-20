f968ceb56f7b019e8a9e89b8427647c70f497b43
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-20 16:44:01 +0200

    PlayerSpawnChangeEvent

--- paper-api/src/main/java/org/bukkit/event/player/PlayerSpawnChangeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/player/PlayerSpawnChangeEvent.java
@@ -1,13 +1,9 @@
 package org.bukkit.event.player;
 
-import com.google.common.base.Preconditions;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * This event is fired when the spawn point of the player is changed.
@@ -15,57 +11,28 @@ import org.jetbrains.annotations.Nullable;
  * @deprecated use {@link com.destroystokyo.paper.event.player.PlayerSetSpawnEvent}
  */
 @Deprecated(forRemoval = true)
-public class PlayerSpawnChangeEvent extends PlayerEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Cause cause;
-    private Location newSpawn;
-    private boolean forced;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PlayerSpawnChangeEvent(@NotNull final Player player, @Nullable Location newSpawn, boolean forced, @NotNull final Cause cause) {
-        super(player);
-        this.newSpawn = newSpawn;
-        this.cause = cause;
-        this.forced = forced;
-    }
+public interface PlayerSpawnChangeEvent extends PlayerEventNew, Cancellable {
 
     /**
      * Gets the new spawn to be set.
      *
      * @return new spawn location
      */
-    @Nullable
-    public Location getNewSpawn() {
-        return this.newSpawn;
-    }
+    @Nullable Location getNewSpawn();
 
     /**
      * Sets the new spawn location.
      *
      * @param newSpawn new spawn location, with non-null world
      */
-    public void setNewSpawn(@Nullable Location newSpawn) {
-        if (newSpawn != null) {
-            Preconditions.checkArgument(newSpawn.getWorld() != null, "Spawn location must have a world set");
-            this.newSpawn = newSpawn.clone();
-        } else {
-            this.newSpawn = null;
-        }
-    }
+    void setNewSpawn(@Nullable Location newSpawn);
 
     /**
      * Gets the cause of spawn change.
      *
      * @return change cause
      */
-    @NotNull
-    public Cause getCause() {
-        return this.cause;
-    }
+    Cause getCause();
 
     /**
      * Gets if the spawn position will be used regardless of bed obstruction
@@ -73,9 +40,7 @@ public class PlayerSpawnChangeEvent extends PlayerEvent implements Cancellable {
      *
      * @return {@code true} if is forced
      */
-    public boolean isForced() {
-        return this.forced;
-    }
+    boolean isForced();
 
     /**
      * Sets if the spawn position will be used regardless of bed obstruction
@@ -83,32 +48,16 @@ public class PlayerSpawnChangeEvent extends PlayerEvent implements Cancellable {
      *
      * @param forced {@code true} if forced
      */
-    public void setForced(boolean forced) {
-        this.forced = forced;
+    void setForced(boolean forced);
+
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
-    }
-
-    public enum Cause {
+    enum Cause {
 
         /**
          * Indicate the spawn was set by a command.
