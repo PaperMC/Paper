99ec15b51c28d9396ab516c50bc53389d9b1cc6c
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-21 15:49:01 +0200

    EntityCreatePortalEvent

--- paper-api/src/main/java/org/bukkit/event/entity/EntityCreatePortalEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityCreatePortalEvent.java
@@ -7,8 +7,6 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.world.PortalCreateEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Thrown when a Living Entity creates a portal in a world.
@@ -16,67 +14,19 @@ import org.jetbrains.annotations.NotNull;
  * @deprecated Use {@link PortalCreateEvent}
  */
 @Deprecated(since = "1.14.1")
-public class EntityCreatePortalEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final List<BlockState> blocks;
-    private final PortalType type;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public EntityCreatePortalEvent(@NotNull final LivingEntity livingEntity, @NotNull final List<BlockState> blocks, @NotNull final PortalType type) {
-        super(livingEntity);
-
-        this.blocks = blocks;
-        this.type = type;
-    }
-
-    @NotNull
-    @Override
-    public LivingEntity getEntity() {
-        return (LivingEntity) this.entity;
-    }
-
-    /**
-     * Gets a list of all blocks associated with the portal.
-     *
-     * @return List of blocks that will be changed.
-     */
-    @NotNull
-    public List<BlockState> getBlocks() {
-        return this.blocks;
-    }
-
-    /**
-     * Gets the type of portal that is trying to be created.
-     *
-     * @return Type of portal.
-     */
-    @NotNull
-    public PortalType getPortalType() {
-        return this.type;
-    }
+public interface EntityCreatePortalEvent extends EntityEventNew, Cancellable {
 
     @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    LivingEntity getEntity();
 
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
+    List<BlockState> getBlocks();
 
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
+    PortalType getPortalType();
 
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
