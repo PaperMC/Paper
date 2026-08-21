1a312f0ef3d8e5995dda4df1d0868522808f4978
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-21 15:48:57 +0200

    EnderDragonChangePhaseEvent

--- paper-api/src/main/java/org/bukkit/event/entity/EnderDragonChangePhaseEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EnderDragonChangePhaseEvent.java
@@ -1,37 +1,17 @@
 package org.bukkit.event.entity;
 
-import com.google.common.base.Preconditions;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Called when an EnderDragon switches controller phase.
  */
-public class EnderDragonChangePhaseEvent extends EntityEvent implements Cancellable {
+public interface EnderDragonChangePhaseEvent extends EntityEventNew, Cancellable {
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final EnderDragon.Phase currentPhase;
-    private EnderDragon.Phase newPhase;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public EnderDragonChangePhaseEvent(@NotNull EnderDragon enderDragon, @Nullable EnderDragon.Phase currentPhase, @NotNull EnderDragon.Phase newPhase) {
-        super(enderDragon);
-        this.currentPhase = currentPhase;
-        this.newPhase = newPhase;
-    }
-
-    @NotNull
     @Override
-    public EnderDragon getEntity() {
-        return (EnderDragon) this.entity;
-    }
+    EnderDragon getEntity();
 
     /**
      * Gets the current phase that the dragon is in. This method will return null
@@ -39,49 +19,26 @@ public class EnderDragonChangePhaseEvent extends EntityEvent implements Cancella
      *
      * @return the current dragon phase
      */
-    @Nullable
-    public EnderDragon.Phase getCurrentPhase() {
-        return this.currentPhase;
-    }
+    EnderDragon.@Nullable Phase getCurrentPhase();
 
     /**
      * Gets the new phase that the dragon will switch to.
      *
      * @return the new dragon phase
      */
-    @NotNull
-    public EnderDragon.Phase getNewPhase() {
-        return this.newPhase;
-    }
+    EnderDragon.Phase getNewPhase();
 
     /**
      * Sets the new phase for the ender dragon.
      *
      * @param newPhase the new dragon phase
      */
-    public void setNewPhase(@NotNull EnderDragon.Phase newPhase) {
-        Preconditions.checkArgument(newPhase != null, "New dragon phase cannot be null");
-        this.newPhase = newPhase;
-    }
+    void setNewPhase(EnderDragon.Phase newPhase);
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
