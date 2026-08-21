5b53d2c6e0275f62bf2b0f0c0f777d27046600eb
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-21 15:49:03 +0200

    EntityInteractEvent

--- paper-api/src/main/java/org/bukkit/event/entity/EntityInteractEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityInteractEvent.java
@@ -1,57 +1,25 @@
 package org.bukkit.event.entity;
 
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when an entity interacts with an object
  */
-public class EntityInteractEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    protected Block block;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public EntityInteractEvent(@NotNull final Entity entity, @NotNull final Block block) {
-        super(entity);
-        this.block = block;
-    }
+public interface EntityInteractEvent extends EntityEventNew, Cancellable {
 
     /**
      * Returns the involved block
      *
      * @return the block clicked with this item.
      */
-    @NotNull
-    public Block getBlock() {
-        return this.block;
-    }
+    Block getBlock();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
