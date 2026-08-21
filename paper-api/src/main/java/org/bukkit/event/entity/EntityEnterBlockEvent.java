245724da820c79dfcac14cc591bea27fd8bf8701
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-21 15:49:02 +0200

    EntityEnterBlockEvent

--- paper-api/src/main/java/org/bukkit/event/entity/EntityEnterBlockEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityEnterBlockEvent.java
@@ -4,8 +4,6 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when an {@link Entity} enters a block and is stored in that block.
@@ -15,49 +13,19 @@ import org.jetbrains.annotations.NotNull;
  * It is not called when a silverfish "enters" a stone block. For that listen to
  * the {@link EntityChangeBlockEvent}.
  */
-public class EntityEnterBlockEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Block block;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public EntityEnterBlockEvent(@NotNull final Entity entity, @NotNull final Block block) {
-        super(entity);
-
-        this.block = block;
-    }
+public interface EntityEnterBlockEvent extends EntityEventNew, Cancellable {
 
     /**
      * Get the block the entity will enter.
      *
      * @return the block
      */
-    @NotNull
-    public Block getBlock() {
-        return this.block;
-    }
+    Block getBlock();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
