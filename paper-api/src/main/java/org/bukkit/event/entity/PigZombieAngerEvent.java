bcf9065d15dfe04eceed46de12ebee627759c7c9
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-22 17:35:34 +0200

    PigZombieAngerEvent

--- paper-api/src/main/java/org/bukkit/event/entity/PigZombieAngerEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/PigZombieAngerEvent.java
@@ -4,40 +4,21 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Called when a Zombified piglin is angered by another entity.
  * <p>
  * If the event is cancelled, the zombified piglin will not be angered.
  */
-public class PigZombieAngerEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Entity target;
-    private int newAnger;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public PigZombieAngerEvent(@NotNull final PigZombie zombifiedPiglin, @Nullable final Entity target, final int newAnger) {
-        super(zombifiedPiglin);
-        this.target = target;
-        this.newAnger = newAnger;
-    }
+public interface PigZombieAngerEvent extends EntityEventNew, Cancellable {
 
     /**
      * Gets the entity (if any) which triggered this anger update.
      *
      * @return triggering entity, or {@code null}
      */
-    @Nullable
-    public Entity getTarget() {
-        return this.target;
-    }
+    @Nullable Entity getTarget();
 
     /**
      * Gets the new anger resulting from this event.
@@ -45,9 +26,7 @@ public class PigZombieAngerEvent extends EntityEvent implements Cancellable {
      * @return new anger
      * @see PigZombie#getAnger()
      */
-    public int getNewAnger() {
-        return this.newAnger;
-    }
+    int getNewAnger();
 
     /**
      * Sets the new anger resulting from this event.
@@ -55,34 +34,15 @@ public class PigZombieAngerEvent extends EntityEvent implements Cancellable {
      * @param newAnger the new anger
      * @see PigZombie#setAnger(int)
      */
-    public void setNewAnger(int newAnger) {
-        this.newAnger = newAnger;
-    }
-
-    @NotNull
-    @Override
-    public PigZombie getEntity() {
-        return (PigZombie) this.entity;
-    }
+    void setNewAnger(int newAnger);
 
     @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    PigZombie getEntity();
 
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
