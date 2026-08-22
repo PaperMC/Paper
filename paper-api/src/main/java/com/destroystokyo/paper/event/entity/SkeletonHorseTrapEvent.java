f5bbddc1833c710b104839b3d772978d62d64355
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-22 17:35:37 +0200

    SkeletonHorseTrapEvent

--- paper-api/src/main/java/com/destroystokyo/paper/event/entity/SkeletonHorseTrapEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/event/entity/SkeletonHorseTrapEvent.java
@@ -5,53 +5,23 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.SkeletonHorse;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
+import org.bukkit.event.entity.EntityEventNew;
 
 /**
  * Event called when a player gets close to a skeleton horse and triggers the lightning trap
  */
-@NullMarked
-public class SkeletonHorseTrapEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final List<HumanEntity> eligibleHumans;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public SkeletonHorseTrapEvent(final SkeletonHorse horse, final List<HumanEntity> eligibleHumans) {
-        super(horse);
-        this.eligibleHumans = eligibleHumans;
-    }
+public interface SkeletonHorseTrapEvent extends EntityEventNew, Cancellable { // todo javadocs?
 
     @Override
-    public SkeletonHorse getEntity() {
-        return (SkeletonHorse) super.getEntity();
-    }
+    SkeletonHorse getEntity();
 
-    public List<HumanEntity> getEligibleHumans() {
-        return this.eligibleHumans;
-    }
+    List<HumanEntity> getEligibleHumans();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
 
