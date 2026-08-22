a63218912cf0e0e7d80c39020c3c222910399d7c
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-22 17:35:34 +0200

    ItemMergeEvent

--- paper-api/src/main/java/org/bukkit/event/entity/ItemMergeEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/ItemMergeEvent.java
@@ -3,57 +3,23 @@ package org.bukkit.event.entity;
 import org.bukkit.entity.Item;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
-public class ItemMergeEvent extends EntityEvent implements Cancellable {
+public interface ItemMergeEvent extends EntityEventNew, Cancellable {
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final Item target;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public ItemMergeEvent(@NotNull Item item, @NotNull Item target) {
-        super(item);
-        this.target = target;
-    }
-
-    @NotNull
     @Override
-    public Item getEntity() {
-        return (Item) this.entity;
-    }
+    Item getEntity();
 
     /**
      * Gets the Item entity the main Item is being merged into.
      *
      * @return The Item being merged with
      */
-    @NotNull
-    public Item getTarget() {
-        return this.target;
-    }
+    Item getTarget();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
