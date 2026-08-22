bf5aba72608aa017943f66ca9bcfe95be04369b2
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-22 17:35:32 +0200

    EntityTameEvent

--- paper-api/src/main/java/org/bukkit/event/entity/EntityTameEvent.java
+++ paper-api/src/main/java/org/bukkit/event/entity/EntityTameEvent.java
@@ -4,59 +4,26 @@ import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
- * Thrown when a LivingEntity is tamed
+ * Thrown when a living entity is tamed.
  */
-public class EntityTameEvent extends EntityEvent implements Cancellable {
+public interface EntityTameEvent extends EntityEventNew, Cancellable {
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final AnimalTamer owner;
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public EntityTameEvent(@NotNull final LivingEntity entity, @NotNull final AnimalTamer owner) {
-        super(entity);
-        this.owner = owner;
-    }
-
-    @NotNull
     @Override
-    public LivingEntity getEntity() {
-        return (LivingEntity) this.entity;
-    }
+    LivingEntity getEntity();
 
     /**
      * Gets the owning AnimalTamer
      *
      * @return the owning AnimalTamer
      */
-    @NotNull
-    public AnimalTamer getOwner() {
-        return this.owner;
-    }
+    AnimalTamer getOwner();
 
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
