8c8395a8774fc3a4eb2de92e04bab4c50aaebeb5
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-22 17:35:37 +0200

    ExperienceOrbMergeEvent

--- paper-api/src/main/java/com/destroystokyo/paper/event/entity/ExperienceOrbMergeEvent.java
+++ paper-api/src/main/java/com/destroystokyo/paper/event/entity/ExperienceOrbMergeEvent.java
@@ -26,63 +26,33 @@ package com.destroystokyo.paper.event.entity;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.entity.EntityEvent;
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
+import org.bukkit.event.entity.EntityEventNew;
 
 /**
  * Fired anytime the server is about to merge 2 experience orbs into one
  */
-@NullMarked
-public class ExperienceOrbMergeEvent extends EntityEvent implements Cancellable {
-
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    private final ExperienceOrb mergeTarget;
-    private final ExperienceOrb mergeSource;
-
-    private boolean cancelled;
-
-    @ApiStatus.Internal
-    public ExperienceOrbMergeEvent(final ExperienceOrb mergeTarget, final ExperienceOrb mergeSource) {
-        super(mergeTarget);
-        this.mergeTarget = mergeTarget;
-        this.mergeSource = mergeSource;
-    }
+public interface ExperienceOrbMergeEvent extends EntityEventNew, Cancellable {
 
     /**
-     * @return The orb that will absorb the other experience orb
+     * {@return the orb that will absorb the other experience orb}
      */
-    public ExperienceOrb getMergeTarget() {
-        return this.mergeTarget;
-    }
+    ExperienceOrb getMergeTarget();
 
     /**
-     * @return The orb that is subject to being removed and merged into the target orb
+     * {@return the orb that is subject to being removed and merged into the target orb}
      */
-    public ExperienceOrb getMergeSource() {
-        return this.mergeSource;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
+    ExperienceOrb getMergeSource();
 
     /**
      * @param cancel {@code true} if you wish to cancel this event, and prevent the orbs from merging
      */
     @Override
-    public void setCancelled(final boolean cancel) {
-        this.cancelled = cancel;
-    }
+    void setCancelled(boolean cancel);
 
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
