f839cf8d8432982487c523a5274ad1fd9560176b
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-25 16:40:38 +0200

    WorldGameRuleChangeEvent

--- paper-server/src/main/java/io/papermc/paper/event/world/PaperWorldGameRuleChangeEvent.java
+++ paper-server/src/main/java/io/papermc/paper/event/world/PaperWorldGameRuleChangeEvent.java
@@ -4,12 +4,37 @@ import org.bukkit.GameRule;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.CraftGameRule;
+import org.bukkit.craftbukkit.event.world.CraftWorldEvent;
+import org.bukkit.event.HandlerList;
 import org.jspecify.annotations.Nullable;
 
-public class PaperWorldGameRuleChangeEvent extends WorldGameRuleChangeEvent {
+public class PaperWorldGameRuleChangeEvent extends CraftWorldEvent implements WorldGameRuleChangeEvent {
+
+    private final @Nullable CommandSender commandSender;
+    protected final GameRule<?> gameRule;
+    private String value;
+    private boolean cancelled;
 
     public PaperWorldGameRuleChangeEvent(final World world, final @Nullable CommandSender commandSender, final GameRule<?> gameRule, final String value) {
-        super(world, commandSender, gameRule, value);
+        super(world);
+        this.commandSender = commandSender;
+        this.gameRule = gameRule;
+        this.value = value;
+    }
+
+    @Override
+    public @Nullable CommandSender getCommandSender() {
+        return this.commandSender;
+    }
+
+    @Override
+    public GameRule<?> getGameRule() {
+        return this.gameRule;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
     }
 
     @Override
@@ -17,6 +42,21 @@ public class PaperWorldGameRuleChangeEvent extends WorldGameRuleChangeEvent {
         ((CraftGameRule<?>) this.gameRule).getHandle().deserialize(value).ifError(error -> {
             throw CraftGameRule.INVALID_VALUE.apply(value, error);
         });
-        super.setValue(value);
+        this.value = value;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(final boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return WorldGameRuleChangeEvent.getHandlerList();
     }
 }
