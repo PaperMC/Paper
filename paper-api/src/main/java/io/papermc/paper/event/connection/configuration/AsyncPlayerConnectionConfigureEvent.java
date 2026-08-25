bd09d4f70bb6b0ba6f9380c17a5735a3772c0623
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-25 16:40:42 +0200

    AsyncPlayerConnectionConfigureEvent

--- paper-api/src/main/java/io/papermc/paper/event/connection/configuration/AsyncPlayerConnectionConfigureEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/connection/configuration/AsyncPlayerConnectionConfigureEvent.java
@@ -2,9 +2,7 @@ package io.papermc.paper.event.connection.configuration;
 
 import io.papermc.paper.connection.PlayerConfigurationConnection;
 import org.bukkit.event.Event;
-import org.bukkit.event.EventTmp;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * An event that allows you to configure the player.
@@ -13,28 +11,14 @@ import org.jetbrains.annotations.ApiStatus;
  * <p>
  * This occurs after configuration, but before the player has entered the world.
  */
-public class AsyncPlayerConnectionConfigureEvent extends EventTmp {
+public interface AsyncPlayerConnectionConfigureEvent extends Event {
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
+    PlayerConfigurationConnection getConnection();
 
-    private final PlayerConfigurationConnection connection;
-
-    @ApiStatus.Internal
-    public AsyncPlayerConnectionConfigureEvent(final PlayerConfigurationConnection connection) {
-        super(true);
-        this.connection = connection;
-    }
-
-    public PlayerConfigurationConnection getConnection() {
-        return this.connection;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
