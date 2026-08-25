8ac694005a60a6ed3dcffc95fec74cf1c14dfc03
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-08-25 16:40:42 +0200

    VehicleUpdateEvent

--- paper-api/src/main/java/org/bukkit/event/vehicle/VehicleUpdateEvent.java
+++ paper-api/src/main/java/org/bukkit/event/vehicle/VehicleUpdateEvent.java
@@ -1,30 +1,16 @@
 package org.bukkit.event.vehicle;
 
-import org.bukkit.entity.Vehicle;
 import org.bukkit.event.HandlerList;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a vehicle updates
  */
-public class VehicleUpdateEvent extends VehicleEvent {
+public interface VehicleUpdateEvent extends VehicleEventNew {
 
-    private static final HandlerList HANDLER_LIST = new HandlerList();
-
-    @ApiStatus.Internal
-    public VehicleUpdateEvent(@NotNull final Vehicle vehicle) {
-        super(vehicle);
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return HANDLER_LIST;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
+    static HandlerList getHandlerList() {
+        final class Holder {
+            private static final HandlerList HANDLER_LIST = new HandlerList();
+        }
+        return Holder.HANDLER_LIST;
     }
 }
