3b7f7991a41f8e435bead527d6a6bb889d9fe580
Doc <git@mrdoc.dev>
2026-08-28 22:34:47 -0400

    Fix bad PaperSwingAnimation duration annotation

--- paper-server/src/main/java/io/papermc/paper/datacomponent/item/PaperSwingAnimation.java
+++ paper-server/src/main/java/io/papermc/paper/datacomponent/item/PaperSwingAnimation.java
@@ -3,7 +3,6 @@ package io.papermc.paper.datacomponent.item;
 import net.minecraft.world.item.SwingAnimationType;
 import org.bukkit.craftbukkit.util.Handleable;
 import org.checkerframework.checker.index.qual.NonNegative;
-import org.checkerframework.checker.index.qual.Positive;
 
 import static io.papermc.paper.util.BoundChecker.requireNonNegative;
 
@@ -38,7 +37,7 @@ public record PaperSwingAnimation(
         }
 
         @Override
-        public SwingAnimation.Builder duration(final @Positive int duration) {
+        public SwingAnimation.Builder duration(final @NonNegative int duration) {
             this.duration = requireNonNegative(duration, "duration");
             return this;
         }
