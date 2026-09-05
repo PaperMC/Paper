a74f4de06239d24fe119e43da981e9531097cf7b
Doc <git@mrdoc.dev>
2026-09-05 09:03:15 -0400

    Fix compile issue for CollectingContext

--- paper-generator/src/main/java/io/papermc/generator/utils/experimental/CollectingContext.java
+++ paper-generator/src/main/java/io/papermc/generator/utils/experimental/CollectingContext.java
@@ -1,8 +1,8 @@
 package io.papermc.generator.utils.experimental;
 
-import com.mojang.serialization.Lifecycle;
 import io.papermc.generator.Main;
 import java.util.Set;
+import java.util.stream.Stream;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.Registry;
@@ -11,11 +11,13 @@ import net.minecraft.resources.ResourceKey;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public record CollectingContext<T>(Set<ResourceKey<T>> registered,
-                                   Registry<T> registry) implements BootstrapContext<T> {
+public record CollectingContext<T>(
+    Set<ResourceKey<T>> registered,
+    Registry<T> registry
+) implements BootstrapContext<T> {
 
     @Override
-    public Holder.Reference<T> register(ResourceKey<T> key, T value, Lifecycle lifecycle) {
+    public Holder.Reference<T> register(ResourceKey<T> key, T value) {
         this.registered.add(key);
         return Holder.Reference.createStandAlone(this.registry, key);
     }
@@ -23,5 +25,10 @@ public record CollectingContext<T>(Set<ResourceKey<T>> registered,
     @Override
     public <S> HolderGetter<S> lookup(ResourceKey<? extends Registry<? extends S>> key) {
         return Main.REGISTRY_ACCESS.lookupOrThrow(key);
+    }
+
+    @Override
+    public <S> Stream<Holder.Reference<S>> listContextElements(ResourceKey<? extends Registry<? extends S>> key) {
+        return Main.REGISTRY_ACCESS.lookupOrThrow(key).listElements();
     }
 }
