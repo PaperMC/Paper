837b4fff4ec59f721f078c0bb6704b3e4f2e005a
Doc <git@mrdoc.dev>
2026-09-07 19:45:55 -0300

    Revert "Update JSpecify dependency to 1.0.1"
    This reverts commit cfbf9f14fdc4ebc072471871ac02906f74a41bd4.

--- paper-checkstyle/build.gradle.kts
+++ paper-checkstyle/build.gradle.kts
@@ -7,14 +7,14 @@ val testData = sourceSets.create("testData")
 
 dependencies {
     implementation("com.puppycrawl.tools:checkstyle:13.8.0")
-    implementation("org.jspecify:jspecify:1.0.1")
+    implementation("org.jspecify:jspecify:1.0.0")
 
     testCompileOnly("org.jetbrains:annotations:26.1.0")
     testImplementation(testData.output)
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testData.implementationConfigurationName("org.jspecify:jspecify:1.0.1")
+    testData.implementationConfigurationName("org.jspecify:jspecify:1.0.0")
 
     checkstyle(project(":paper-checkstyle"))
 }
