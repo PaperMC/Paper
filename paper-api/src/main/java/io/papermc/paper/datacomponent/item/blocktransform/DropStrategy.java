d446f20cc6fecfff44f4e3507c1db2ba27515e0e
Doc <git@mrdoc.dev>
2026-09-08 22:00:42 -0300

    Start generator for BlockTransform Component

---
+++ paper-api/src/main/java/io/papermc/paper/datacomponent/item/blocktransform/DropStrategy.java
@@ -0,0 +1,10 @@
+package io.papermc.paper.datacomponent.item.blocktransform;
+
+public enum DropStrategy {
+
+    // Start generate - DropStrategy
+    CLICKED_FACE,
+    FROM_MIDDLE;
+    // End generate - DropStrategy
+
+}
