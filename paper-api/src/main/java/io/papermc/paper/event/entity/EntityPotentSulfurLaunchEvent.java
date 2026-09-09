35c21891eeaee2c7071f3f9f410e2cf1bf4d03c2
personalized-advertising <265311956+personalized-advertising@users.noreply.github.com>
2026-09-09 19:01:07 +0100

    switch to jspecify

--- paper-api/src/main/java/io/papermc/paper/event/entity/EntityPotentSulfurLaunchEvent.java
+++ paper-api/src/main/java/io/papermc/paper/event/entity/EntityPotentSulfurLaunchEvent.java
@@ -7,11 +7,12 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NullMarked;
 
 /**
  * Called when an entity is launched upward by a Potent Sulfur geyser.
  */
+@NullMarked
 public class EntityPotentSulfurLaunchEvent extends EntityEvent implements Cancellable {
     private static final HandlerList HANDLER_LIST = new HandlerList();
 
@@ -22,7 +23,12 @@ public class EntityPotentSulfurLaunchEvent extends EntityEvent implements Cancel
     private boolean cancelled;
 
     @ApiStatus.Internal
-    public EntityPotentSulfurLaunchEvent(@NotNull Entity entity, @NotNull Location sulfurLocation, @NotNull Location sourceLocation, @NotNull Vector launchVelocity) {
+    public EntityPotentSulfurLaunchEvent(
+        Entity entity,
+        Location sulfurLocation,
+        Location sourceLocation,
+        Vector launchVelocity
+    ) {
         super(entity);
         this.sulfurLocation = sulfurLocation;
         this.sourceLocation = sourceLocation;
@@ -32,28 +38,28 @@ public class EntityPotentSulfurLaunchEvent extends EntityEvent implements Cancel
     /**
      * Gets the location of the Potent Sulfur block.
      */
-    public @NotNull Location getSulfurLocation() {
+    public Location getSulfurLocation() {
         return this.sulfurLocation.clone();
     }
 
     /**
      * Gets the location where the geyser exits the water column.
      */
-    public @NotNull Location getSourceLocation() {
+    public Location getSourceLocation() {
         return this.sourceLocation.clone();
     }
 
     /**
      * Gets the velocity that will be added to the entity.
      */
-    public @NotNull Vector getLaunchVelocity() {
+    public Vector getLaunchVelocity() {
         return this.launchVelocity.clone();
     }
 
     /**
      * Changes the velocity that will be added to the entity.
      */
-    public void setLaunchVelocity(@NotNull Vector launchVelocity) {
+    public void setLaunchVelocity(Vector launchVelocity) {
         this.launchVelocity = launchVelocity.clone();
     }
 
@@ -68,11 +74,11 @@ public class EntityPotentSulfurLaunchEvent extends EntityEvent implements Cancel
     }
 
     @Override
-    public @NotNull HandlerList getHandlers() {
+    public HandlerList getHandlers() {
         return HANDLER_LIST;
     }
 
-    public static @NotNull HandlerList getHandlerList() {
+    public static HandlerList getHandlerList() {
         return HANDLER_LIST;
     }
 }
