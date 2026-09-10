dcf362de9b288d35c3d7c755ad1f356243078fb6
Bjarne Koll <git@lynxplay.dev>
2026-09-10 19:15:39 +0100

    Update to 26.3-rc-1

--- paper-server/build.gradle.kts
+++ paper-server/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 dependencies {
-    mache("io.papermc:mache:26.3-pre-3+build.1")
+    mache("io.papermc:mache:26.3-rc-1+build.1")
     paperclip("io.papermc:paperclip:3.0.4")
 }
 
