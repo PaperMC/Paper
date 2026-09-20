f53c48fb873af147dbf1f3788da52190e8436753
SirYwell <hannesgreule@outlook.de>
2026-09-20 09:43:52 +0200

    make checkstyle partially happy

--- paper-api/src/main/java/io/papermc/paper/event/EventGuard.java
+++ paper-api/src/main/java/io/papermc/paper/event/EventGuard.java
@@ -5,14 +5,16 @@ import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Internal
 public final class EventGuard {
-    private EventGuard() {}
+    private EventGuard() {
+    }
 
     /**
      * {@return false if there are definitely no listeners associated with this event}
      * The implementation of this method is conservative: it might return true even when no listeners are registered.
+     *
      * @param eventClass the type of the event.
      */
     public static boolean hasListeners(Class<? extends Event> eventClass) {
-        throw new UnsupportedOperationException("should be replaced");
+        return true;
     }
 }
